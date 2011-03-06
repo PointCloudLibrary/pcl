3fabd7a4cea3af81e5ddd612312eb866e2e70ce5
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2011-03-06 10:55:23 +0000

    Moved include files again, added uninstall target
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@15 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_targets.cmake
+++ cmake/pcl_targets.cmake
@@ -1,6 +1,23 @@
 include(${PROJECT_SOURCE_DIR}/cmake/pcl_utils.cmake)
 
 ###############################################################################
+# Add an option to build a subsystem or not.
+# _var The name of the variable to store the option in.
+# _name The name of the option's target subsystem.
+# _desc The description of the subsystem.
+# _default The default value (ON or OFF)
+# ARGN Any dependencies that this subsystem has.
+macro(PCL_SUBSYS_OPTION _var _name _desc _default)
+    # TODO: placeholder
+    if(${_default})
+        set(${_var} TRUE)
+    else(${_default})
+        set(${_var} FALSE)
+    endif(${_default})
+endmacro(PCL_SUBSYS_OPTION)
+
+
+###############################################################################
 # Add a library target.
 # _name The library name.
 # _component The part of PCL that this library belongs to.
