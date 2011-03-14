53f65a041521a3067dce4c3a8688c9d2091e495e
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2011-03-14 12:23:41 +0000

    Moved doc to its own dir; fix openmp searching; fix bug in target options; made tests optional
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@143 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_openmp.cmake
+++ cmake/pcl_openmp.cmake
@@ -3,28 +3,14 @@
 
 include(${PROJECT_SOURCE_DIR}/cmake/pcl_targets.cmake)
 
-###############################################################################
-# Search for the correct flags for OpenMP.
-# The variable OPENMP_FLAGS_FOUND will be set to true if the correct flags were
-# found.
-# The flags are stored in OPENMP_FLAGS.
-macro(PCL_FIND_OPENMP_FLAGS)
-	include(FindOpenMP)
-	if(OPENMP_FOUND)
-		set(OPENMP_FLAGS_FOUND FALSE)
-	else(OPENMP_FOUND)
-		set(OPENMP_FLAGS_FOUND true)
-	endif(OPENMP_FOUND)
-endmacro(PCL_FIND_OPENMP_FLAGS)
-
+find_package(OpenMP)
 
 ###############################################################################
 # Add the OpenMP flags to a target.
 # _name The name of the target to add the flags to.
 macro(PCL_ADD_OPENMP_FLAGS _name)
-    if(OPENMP_FLAGS_FOUND)
-        PCL_ADD_CFLAGS(${_name} ${OPENMP_FLAGS})
-        PCL_ADD_LINKFLAGS(${_name} ${OPENMP_FLAGS})
-    endif(OPENMP_FLAGS_FOUND)
+    if(OPENMP_FOUND)
+        PCL_ADD_CFLAGS(${_name} ${OpenMP_CXX_FLAGS})
+    endif(OPENMP_FOUND)
 endmacro(PCL_ADD_OPENMP_FLAGS)
 
--- cmake/pcl_options.cmake
+++ cmake/pcl_options.cmake
@@ -1,5 +1,5 @@
 # Options for building PCL.
 
 # Build shared libraries by default.
-set(PCL_SHARED_LIBS TRUE CACHE BOOL "Build shared libraries.")
+option(PCL_SHARED_LIBS "Build shared libraries." ON)
 
