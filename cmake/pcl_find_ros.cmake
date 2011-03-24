8229d0b3260b5e4a344bdcdae54f24e81a44ecbb
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2011-03-24 09:00:22 +0000

    Search for ROS packages
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@205 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ cmake/pcl_find_ros.cmake
@@ -0,0 +1,31 @@
+# Look for ROS.
+# This is a temporary and ugly hack.
+
+macro(get_ros_inc_path _dest _pkg)
+    execute_process(COMMAND rospack find ${_pkg}
+        RESULT_VARIABLE _res
+        OUTPUT_VARIABLE ${_dest}
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(RESULT_VARIABLE)
+        message(STATUS "Found ROS package ${_pkg} path: ${${_dest}}")
+    endif(RESULT_VARIABLE)
+endmacro(get_ros_inc_path)
+
+if(ENV{ROS_ROOT})
+    option(USE_ROS "Integrate with ROS rather than using native files" ON)
+    message(STATUS "Found ROS; USE_ROS is ${USE_ROS}")
+    if(USE_ROS)
+        set(_ros_pkgs std_msgs sensor_msgs roscpp_serialization cpp_common rostime
+            roscpp_traits roscpp rosconsole std_msgs)
+        set(_ros_paths)
+        foreach(_pkg ${_ros_pkgs)
+            get_ros_path(_path ${_pkg})
+            list(APPEND _ros_paths ${_path})
+        endforeach(_pkg)
+        foreach(_path ${_ros_paths})
+            include_directories(${_path}/include)
+            link_directories(${_path}/lib)
+        endforeach(_path)
+    endif(USE_ROS)
+endif(ENV{ROS_ROOT})
+
