215dcc9bf5e4d5b1f7e1a2a67949039ed9045929
Nizar Sallem <nksallem@laas.fr>
2011-03-24 09:29:25 +0000

    Fix: Requires field of a .pc file should require library names not files
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@209 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_find_boost.cmake
+++ cmake/pcl_find_boost.cmake
@@ -5,7 +5,14 @@ if(NOT PCL_SHARED_LIBS)
 endif(NOT PCL_SHARED_LIBS)
 
 find_package(Boost 1.44.0 COMPONENTS system filesystem thread)
-# TODO: What should the minimum version number be?
+
 include_directories(${Boost_INCLUDE_DIRS})
 link_directories(${Boost_LIBRARY_DIRS})
 
+foreach(boost_component ${Boost_LIBRARIES})
+  string(REGEX MATCH "boost_.*" component "${boost_component}")
+  if(NOT "${component}" EQUAL "")
+    string(REGEX REPLACE "(boost_.*)\\..*" "\\1" component "${component}")
+    list(APPEND boost_components ${component})
+  endif(NOT "${component}" EQUAL "")
+endforeach(boost_component)
