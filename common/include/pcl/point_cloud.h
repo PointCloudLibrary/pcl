084eba5f2077c373fddcbd7d451036b7d93b2495
Ethan Rublee <erublee@willowgarage.com>
2011-03-24 06:03:53 +0000

    Small fix to error, preventing build on my machine
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@195 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/include/pcl/point_cloud.h
+++ common/include/pcl/point_cloud.h
@@ -130,7 +130,7 @@ namespace pcl
 
       /** \brief The point cloud header. It contains information about the acquisition time, as well as a transform 
         * frame (see \a tf). */
-      std_msgs::Header header;
+      roslib::Header header;
 
       /** \brief The point data. */
       std::vector<PointT, Eigen::aligned_allocator<PointT> > points;
