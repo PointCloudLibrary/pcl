64b5efa396c193af06cb035ff04eca5d7892e6c3
Nizar Sallem <nksallem@laas.fr>
2011-03-28 23:02:48 +0000

    Update: remove non pkg-config[able] requirements
    As pkg-config is stubborn, it keeps complains of a requirement which 
    doesn't have a .pc file. Boost don't offer them neither wxWidgets.
    While there revert boost version to 1.42 since I am the only one having 
    mysterious system3 unfound symbols.
    
    
    
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@280 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_find_boost.cmake
+++ cmake/pcl_find_boost.cmake
@@ -4,15 +4,7 @@ if(NOT PCL_SHARED_LIBS)
     set(Boost_USE_STATIC_LIBS ON)
 endif(NOT PCL_SHARED_LIBS)
 
-find_package(Boost 1.44.0 COMPONENTS system filesystem thread)
+find_package(Boost 1.42.0 COMPONENTS system filesystem thread)
 
 include_directories(${Boost_INCLUDE_DIRS})
 link_directories(${Boost_LIBRARY_DIRS})
-
-foreach(boost_component ${Boost_LIBRARIES})
-  string(REGEX MATCH "boost_.*" component "${boost_component}")
-  if(NOT "${component}" EQUAL "")
-    string(REGEX REPLACE "(boost_.*)\\..*" "\\1" component "${component}")
-    list(APPEND boost_components ${component})
-  endif(NOT "${component}" EQUAL "")
-endforeach(boost_component)
