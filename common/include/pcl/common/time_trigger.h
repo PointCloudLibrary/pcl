cbf14dbbfbd2b2bb40e928ff3ea98a9243584a86
Suat Gedikli <suat.gedikli@gmail.com>
2011-03-31 20:22:13 +0000

    added time trigger class
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@305 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ common/include/pcl/common/time_trigger.h
@@ -0,0 +1,108 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2009, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Suat Gedikli (gedikli@willowgarage.com)
+ *
+ */
+
+#include <boost/timer.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/signals2.hpp>
+#include <boost/signals2/slot.hpp>
+
+#ifndef __PCL_IO_TIME_TRIGGER__
+#define __PCL_IO_TIME_TRIGGER__
+
+namespace pcl
+{
+
+/**
+ * @brief timer class that calls a callback method periodically.
+ * @param interval_seconds interval in seconds
+ * @param callback callback to be invoked periodically
+ */
+class TimeTrigger
+{
+public:
+  typedef boost::function<void() > callback_type;
+  /**
+   * @brief timer class that calls a callback method periodically. Due to possible blocking calls, only one callback method can be registered per instance.
+   * @param interval_seconds interval in seconds
+   * @param callback callback to be invoked periodically
+   */
+  TimeTrigger (double interval_seconds, const callback_type& callback);
+  /**
+   * @brief timer class that calls a callback method periodically. Due to possible blocking calls, only one callback method can be registered per instance.
+   * @param interval_seconds interval in seconds
+   */
+  TimeTrigger (double interval_seconds);
+  /**
+   * @brief desctructor
+   */
+  ~TimeTrigger ();
+  /**
+   * @brief registeres a callback
+   * @param callback callback function to the list of callbacks. signature has to be boost::function<void()>
+   * @return connection the connection, which can be used to disable/enable and remove callback from list
+   */
+  boost::signals2::connection registerCallback (const callback_type& callback);
+  /**
+   * @brief resets the timer interval
+   * @param interval_seconds interval in seconds
+   */
+  void setInterval (double interval_seconds);
+  /**
+   * @brief start the Trigger
+   */
+  void start ();
+  /**
+   * @brief stop the Trigger
+   */
+  void stop ();
+private:
+  void thread_function ();
+  boost::signals2::signal <void() > callbacks_;
+
+  double time_;
+  double interval_;
+
+  bool quit_;
+  bool running_;
+  boost::thread timer_thread_;
+
+  boost::condition_variable condition_;
+  boost::mutex condition_mutex_;
+};
+}
+#endif
---
+++ common/src/time_trigger.cpp
@@ -0,0 +1,123 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2009, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Suat Gedikli (gedikli@willowgarage.com)
+ *
+ */
+
+#include "pcl/common/time_trigger.h"
+#include "pcl/common/time.h"
+#include <iostream>
+
+using namespace boost;
+
+pcl::TimeTrigger::TimeTrigger (double interval, const callback_type& callback)
+: interval_ (interval)
+, quit_ (false)
+, running_ (false)
+, timer_thread_ (boost::bind (&TimeTrigger::thread_function, this))
+{
+  registerCallback (callback);
+}
+
+pcl::TimeTrigger::TimeTrigger (double interval)
+: interval_ (interval)
+, quit_ (false)
+, running_ (false)
+, timer_thread_ (boost::bind (&TimeTrigger::thread_function, this))
+{
+}
+
+pcl::TimeTrigger::~TimeTrigger ()
+{
+  unique_lock<mutex> lock (condition_mutex_);
+  quit_ = true;
+  condition_.notify_all ();
+  lock.unlock ();
+  
+  timer_thread_.join ();
+}
+
+boost::signals2::connection pcl::TimeTrigger::registerCallback (const callback_type& callback)
+{
+  unique_lock<mutex> lock (condition_mutex_);
+  return callbacks_.connect (callback);
+}
+
+void pcl::TimeTrigger::setInterval (double interval_seconds)
+{
+  unique_lock<mutex> lock (condition_mutex_);
+  interval_ = interval_seconds;
+  // notify, since we could switch from a large interval to a shorter one -> interrupt waiting for timeout!
+  condition_.notify_all ();
+}
+
+void pcl::TimeTrigger::start ()
+{
+  unique_lock<mutex> lock (condition_mutex_);
+  if (!running_)
+  {
+    running_ = true;
+    time_ = getTime ();
+    condition_.notify_all ();
+  }
+}
+
+void pcl::TimeTrigger::stop ()
+{
+  unique_lock<mutex> lock (condition_mutex_);
+  if (running_)
+  {
+    running_ = false;
+    condition_.notify_all ();
+  }
+}
+
+void pcl::TimeTrigger::thread_function ()
+{
+  while (!quit_)
+  {
+    time_ = getTime ();
+    unique_lock<mutex> lock (condition_mutex_);
+    if (!running_)
+    {
+      condition_.wait (lock); // wait util start is called or destructor is called
+    }
+    else
+    {
+      callbacks_();
+      double rest = interval_ + time_ - getTime ();
+      condition_.timed_wait (lock, posix_time::microseconds(rest * 1000000.0));
+    }
+  }
+}
