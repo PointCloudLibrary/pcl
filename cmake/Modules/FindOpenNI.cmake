bd2672429ae0586f88a065bccd3ab6be27815508
Ethan Rublee <erublee@willowgarage.com>
2011-04-02 05:36:05 +0000

    FindOpenNI now has one extra hint, as the deb in diamond back does not currently have a pc file.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@328 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/Modules/FindOpenNI.cmake
+++ cmake/Modules/FindOpenNI.cmake
@@ -8,20 +8,21 @@
 # OPENNI_DEFINITIONS - Compiler flags for OPENNI.
 
 find_package(PkgConfig)
-if(${CMAKE_VERSION} VERSION_LESS 2.8.3)
-	pkg_check_modules(PC_OPENNI openni-dev)
-else(${CMAKE_VERSION} VERSION_LESS 2.8.3)
-	pkg_check_modules(PC_OPENNI QUIET openni-dev)
-endif(${CMAKE_VERSION} VERSION_LESS 2.8.3)
+if(${CMAKE_VERSION} VERSION_LESS 2.8.2)
+  pkg_check_modules(PC_OPENNI openni-dev)
+else()
+  pkg_check_modules(PC_OPENNI QUIET openni-dev)
+endif()
 
 set(OPENNI_DEFINITIONS ${PC_OPENNI_CFLAGS_OTHER})
 
+#add a hint so that it can find it without the pkg-config
 find_path(OPENNI_INCLUDE_DIR XnStatus.h
-    HINTS ${PC_OPENNI_INCLUDEDIR} ${PC_OPENNI_INCLUDE_DIRS} 
+    HINTS ${PC_OPENNI_INCLUDEDIR} ${PC_OPENNI_INCLUDE_DIRS} /usr/include/openni
     PATH_SUFFIXES openni)
-
+#add a hint so that it can find it without the pkg-config
 find_library(OPENNI_LIBRARY OpenNI
-    HINTS ${PC_OPENNI_LIBDIR} ${PC_OPENNI_LIBRARY_DIRS})
+    HINTS ${PC_OPENNI_LIBDIR} ${PC_OPENNI_LIBRARY_DIRS} /usr/lib)
 
 set(OPENNI_INCLUDE_DIRS ${OPENNI_INCLUDE_DIR})
 set(OPENNI_LIBRARIES ${OPENNI_LIBRARY})
@@ -29,6 +30,6 @@ set(OPENNI_LIBRARIES ${OPENNI_LIBRARY})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(OpenNI DEFAULT_MSG
     OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
-
+    
 mark_as_advanced(OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
 
