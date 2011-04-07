448311f360b4a74df622a1bf417f7bfdb0a489bb
Nico Blodow <blodow@cs.tum.edu>
2011-04-07 00:04:44 +0000

    added kinect_change_viewer, need julius's help :D
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@380 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ visualization/tools/kinect_change_viewer.cpp
@@ -0,0 +1,113 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *	
+ * Author: Nico Blodow (blodow@cs.tum.edu)
+ */
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/kinect_grabber.h>
+#include <pcl/io/kinect_grabber.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/octree/octree.h>
+#include <pcl/filters/extract_indices.h>
+
+class KinectChangeViewer
+{
+  public:
+    KinectChangeViewer (double resolution) 
+      : viewer ("PCL Kinect Viewer")
+    {
+      octree = new pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ>(resolution);
+    }
+
+    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
+    {
+      pcl::PointCloud<pcl::PointXYZ> filtered_cloud;
+      pcl::ExtractIndices<pcl::PointXYZ> ei;
+      ei.setInputCloud (cloud);
+      ei.filter (filtered_cloud);
+//      // assign point cloud to octree
+//      octree->setInputCloud (cloud);
+//
+//      // add points from cloud to octree
+//      octree->addPointsFromInputCloud ();
+//
+      std::vector<int> newPointIdxVector;
+//
+//      // get a vector of new points, which did not exist in previous buffer
+//      octree->getPointIndicesFromNewVoxels (newPointIdxVector);
+//
+      std::cerr << newPointIdxVector.size() << std::endl;
+      if (!viewer.wasStopped())
+        viewer.showCloud (filtered_cloud);
+//      // switch buffers - reset tree
+//      octree->switchBuffers ();
+
+    }
+    
+    void run ()
+    {
+      pcl::Grabber* interface = new pcl::OpenNIGrabber();
+
+      boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f = 
+        boost::bind (&KinectChangeViewer::cloud_cb_, this, _1);
+
+      boost::signals2::connection c = interface->registerCallback (f);
+      
+      interface->start ();
+      
+      while (!viewer.wasStopped())
+      {
+        sleep (1);
+      }
+
+      interface->stop ();
+    }
+
+    pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> *octree;
+    pcl_visualization::CloudViewer viewer;
+};
+
+int main (int argc, char* argv[])
+{
+  double resolution = 0.05;
+  if (argc > 1)
+    resolution = atof (argv[1]);
+
+  KinectChangeViewer v (resolution);
+  v.run ();
+  return 0;
+}
+
+
