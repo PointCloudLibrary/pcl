ccac4c249a424a3f0618526ea0dd0d60db1b9b1b
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-04-14 19:01:49 +0000

    attempting to fix the qhull cmake module
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@460 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/Modules/FindQhull.cmake
+++ cmake/Modules/FindQhull.cmake
@@ -7,9 +7,15 @@
 # QHULL_LIBRARIES - Libraries needed to use QHULL.
 # QHULL_DEFINITIONS - Compiler flags for QHULL.
 
-find_path(QHULL_INCLUDE_DIR qhull/qhull.h)
+#find_path(QHULL_INCLUDE_DIR qhull/qhull.h)
+find_path(QHULL_INCLUDE_DIR qhull.h
+          HINTS ${QHULL_ROOT}
+          PATH_SUFFIXES qhull src)
 
-find_library(QHULL_LIBRARY qhull)
+#find_library(QHULL_LIBRARY qhull)
+find_library(QHULL_LIBRARY qhull
+             HINTS ${QHULL_ROOT}
+             PATH_SUFFIXES project)
 
 set(QHULL_INCLUDE_DIRS ${QHULL_INCLUDE_DIR})
 set(QHULL_LIBRARIES ${QHULL_LIBRARY})
@@ -19,4 +25,8 @@ find_package_handle_standard_args(Qhull DEFAULT_MSG QHULL_LIBRARY
     QHULL_INCLUDE_DIR)
 
 mark_as_advanced(QHULL_LIBRARY QHULL_INCLUDE_DIR)
+
+if(QHULL_FOUND)
+  message(STATUS "QHULL found (include: ${QHULL_INCLUDE_DIRS}, lib: ${QHULL_LIBRARIES})")
+endif(QHULL_FOUND)
 
