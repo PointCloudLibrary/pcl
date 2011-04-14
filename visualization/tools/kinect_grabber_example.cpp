8457af2362b229fa0e7298acf09781e7343ad91f
Suat Gedikli <suat.gedikli@gmail.com>
2011-04-14 21:57:59 +0000

    new minimalistic example for the grabber interface for OpenNI devices
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@465 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ visualization/tools/kinect_grabber_example.cpp
@@ -0,0 +1,87 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *	
+ * Author: Suat Gedikli (gedikli@willowgarage.com)
+ */
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/kinect_grabber.h>
+#include <pcl/common/time.h>
+
+class SimpleKinectProcessor
+{
+  public:
+    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
+    {
+      static unsigned count = 0;
+      static double last = pcl::getTime ();
+      if (++count == 30)
+      {
+        double now = pcl::getTime ();
+        std::cout << "distance of center pixel :" << cloud->points [(cloud->width >> 1) * (cloud->height + 1)].z << " mm. Average framerate: " << double(count)/double(now - last) << " Hz" <<  std::endl;
+        count = 0;
+        last = now;
+      }
+    }
+    
+    void run ()
+    {
+      // create a new grabber for OpenNI devices
+      pcl::Grabber* interface = new pcl::OpenNIGrabber();
+
+      // make callback function from member function
+      boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f =
+        boost::bind (&SimpleKinectProcessor::cloud_cb_, this, _1);
+
+      // connect callback function for desired signal. In this case its a point cloud with color values
+      boost::signals2::connection c = interface->registerCallback (f);
+
+      // start receiving point clouds
+      interface->start ();
+
+      // wait until user quits program with Ctrl-C, but no busy-waiting -> sleep (1);
+      while (true)
+        sleep(1);
+
+      // stop the grabber
+      interface->stop ();
+    }
+};
+
+int main ()
+{
+  SimpleKinectProcessor v;
+  v.run ();
+  return 0;
+}
