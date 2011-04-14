21c8738c1edbbe09919d6c66e4f643ef7d3b39be
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-04-14 18:45:23 +0000

    cmake cleanup + verbose OpenNI messages
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@456 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/Modules/FindOpenNI.cmake
+++ cmake/Modules/FindOpenNI.cmake
@@ -32,4 +32,8 @@ find_package_handle_standard_args(OpenNI DEFAULT_MSG
     OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
     
 mark_as_advanced(OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
+if(OPENNI_FOUND)
+    include_directories(${OPENNI_INCLUDE_DIRS})
+    message(STATUS "OpenNI found (include: ${OPENNI_INCLUDE_DIR}, lib: ${OPENNI_LIBRARY})")
+endif(OPENNI_FOUND)
 
