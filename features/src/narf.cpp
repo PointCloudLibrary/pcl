f9d3f470775b59e776edb3770ed76a48410b8a39
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-04-14 18:57:59 +0000

    got rid of a warning on linux/posix systems (special case WIN32 handling)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@459 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- features/src/narf.cpp
+++ features/src/narf.cpp
@@ -358,7 +358,11 @@ Narf::extractForInterestPoints (const RangeImage& range_image, const PointCloud<
 {
   # pragma omp parallel for num_threads(max_no_of_threads) default(shared) schedule(dynamic, 10)
   //!!! nizar 20110408 : for OpenMP sake on MSVC this must be kept signed
+#ifdef _WIN32
   for (int interest_point_idx = 0; interest_point_idx < interest_points.points.size (); ++interest_point_idx)
+#else
+  for (size_t interest_point_idx = 0; interest_point_idx < interest_points.points.size (); ++interest_point_idx)
+#endif
   {
     Vector3fMapConst point = interest_points.points[interest_point_idx].getVector3fMap ();
     
