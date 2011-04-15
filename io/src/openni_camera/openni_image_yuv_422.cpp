d4973c78f5b235569cedd93b8b097c2983d33fd6
Suat Gedikli <suat.gedikli@gmail.com>
2011-04-15 04:26:13 +0000

    switched from slow floating point YUV2RGB conversion to the implementation in OpenCV. approx. 2-3x faster in debug mode.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@476 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- io/src/openni_camera/openni_image_yuv_422.cpp
+++ io/src/openni_camera/openni_image_yuv_422.cpp
@@ -38,6 +38,7 @@
 #include <sstream>
 #include <iostream>
 
+#define CLIP_CHAR(c) ((c)>255?255:(c)<0?0:(c))
 using namespace std;
 namespace openni_wrapper
 {
@@ -58,13 +59,18 @@ bool ImageYUV422::isResizingSupported (unsigned input_width, unsigned input_heig
 
 void ImageYUV422::fillRGB (unsigned width, unsigned height, unsigned char* rgb_buffer, unsigned rgb_line_step) const throw (OpenNIException)
 {
-  // u y1 v y2
-  if (width > image_md_->XRes () || height > image_md_->YRes ())
-    THROW_OPENNI_EXCEPTION ("Upsampling not supported. Request was: %d x %d -> %d x %d", image_md_->XRes (), image_md_->YRes (), width, height);
+  // 0  1   2  3
+  // u  y1  v  y2
 
-  if (image_md_->XRes () % width != 0 || image_md_->YRes () % height != 0)
-      THROW_OPENNI_EXCEPTION ("Downsampling only possible for integer scales in both dimensions. Request was %d x %d -> %d x %d.", image_md_->XRes (), image_md_->YRes (), width, height);
+  if (image_md_->XRes() != width && image_md_->YRes() != height)
+  {
+    if (width > image_md_->XRes () || height > image_md_->YRes ())
+      THROW_OPENNI_EXCEPTION ("Upsampling not supported. Request was: %d x %d -> %d x %d", image_md_->XRes (), image_md_->YRes (), width, height);
 
+    if ( image_md_->XRes () % width != 0 || image_md_->YRes () % height != 0
+        || (image_md_->XRes () / width) & 0x01 || (image_md_->YRes () / height & 0x01) )
+        THROW_OPENNI_EXCEPTION ("Downsampling only possible for power of two scale in both dimensions. Request was %d x %d -> %d x %d.", image_md_->XRes (), image_md_->YRes (), width, height);
+  }
   register const XnUInt8* yuv_buffer = image_md_->Data();
 
   unsigned rgb_line_skip = 0;
@@ -77,13 +83,16 @@ void ImageYUV422::fillRGB (unsigned width, unsigned height, unsigned char* rgb_b
     {
       for( register unsigned xIdx = 0; xIdx < width; xIdx += 2, rgb_buffer += 6, yuv_buffer += 4 )
       {
-        rgb_buffer[0] = max (0, min (255, (int)yuv_buffer[1] + (int)(1.13983 * (float)(yuv_buffer[2]-128) ) ) );
-        rgb_buffer[1] = max (0, min (255, (int)yuv_buffer[1] + (int)(124.832 -0.39465 * (float)yuv_buffer[0] - 0.58060 * (float)yuv_buffer[2] )));
-        rgb_buffer[2] = max (0, min (255, (int)yuv_buffer[1] + (int)(2.03211 * (float)(yuv_buffer[0]-128)) ) );
+        int v = yuv_buffer[2] - 128;
+        int u = yuv_buffer[0] - 128;
 
-        rgb_buffer[3] = max (0, min (255, (int)yuv_buffer[3] + (int)(1.13983 * (float)(yuv_buffer[2]-128) ) ) );
-        rgb_buffer[4] = max (0, min (255, (int)yuv_buffer[3] + (int)(124.832 -0.39465 * (float)yuv_buffer[0] - 0.58060 * (float)yuv_buffer[2] )));
-        rgb_buffer[5] = max (0, min (255, (int)yuv_buffer[3] + (int)(2.03211 * (float)(yuv_buffer[0]-128)) ) );
+        rgb_buffer[0] =  CLIP_CHAR (yuv_buffer[1] + ((v * 18678 + 8192 ) >> 14));
+        rgb_buffer[1] =  CLIP_CHAR (yuv_buffer[1] + ((v * -9519 - u * 6472 + 8192 ) >> 14));
+        rgb_buffer[2] =  CLIP_CHAR (yuv_buffer[1] + ((u * 33292 + 8192 ) >> 14));
+
+        rgb_buffer[3] =  CLIP_CHAR (yuv_buffer[3] + ((v * 18678 + 8192 ) >> 14));
+        rgb_buffer[4] =  CLIP_CHAR (yuv_buffer[3] + ((v * -9519 - u * 6472 + 8192 ) >> 14));
+        rgb_buffer[5] =  CLIP_CHAR (yuv_buffer[3] + ((u * 33292 + 8192 ) >> 14));
       }
     }
   }
@@ -92,13 +101,17 @@ void ImageYUV422::fillRGB (unsigned width, unsigned height, unsigned char* rgb_b
     register unsigned yuv_step = image_md_->XRes() / width;
     register unsigned yuv_x_step = yuv_step << 1;
     register unsigned yuv_skip = (image_md_->YRes() / height - 1) * ( image_md_->XRes() << 1 );
+
     for( register unsigned yIdx = 0; yIdx < image_md_->YRes(); yIdx += yuv_step, yuv_buffer += yuv_skip, rgb_buffer += rgb_line_skip )
     {
       for( register unsigned xIdx = 0; xIdx < image_md_->XRes(); xIdx += yuv_step, rgb_buffer += 3, yuv_buffer += yuv_x_step )
       {
-        rgb_buffer[0] = max (0, min (255, (int)yuv_buffer[1] + (int)(1.13983 * (float)(yuv_buffer[2]-128) ) ) );
-        rgb_buffer[1] = max (0, min (255, (int)yuv_buffer[1] + (int)(124.832 -0.39465 * (float)yuv_buffer[0] - 0.58060 * (float)yuv_buffer[2] )));
-        rgb_buffer[2] = max (0, min (255, (int)yuv_buffer[1] + (int)(2.03211 * (float)(yuv_buffer[0]-128)) ) );
+        int v = yuv_buffer[2] - 128;
+        int u = yuv_buffer[0] - 128;
+
+        rgb_buffer[0] =  CLIP_CHAR (yuv_buffer[1] + ((v * 18678 + 8192 ) >> 14));
+        rgb_buffer[1] =  CLIP_CHAR (yuv_buffer[1] + ((v * -9519 - u * 6472 + 8192 ) >> 14));
+        rgb_buffer[2] =  CLIP_CHAR (yuv_buffer[1] + ((u * 33292 + 8192 ) >> 14));
       }
     }
   }
