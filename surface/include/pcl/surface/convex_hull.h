79f331a571f73df363a60b5958d6bcbeb28d854e
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-04-19 18:34:40 +0000

    moved the qhull includes in the implementation, thus preventing users from having to find qhull themselves
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@598 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- surface/include/pcl/surface/convex_hull.h
+++ surface/include/pcl/surface/convex_hull.h
@@ -47,37 +47,6 @@
 #include "pcl/PolygonMesh.h"
 #include <math.h>
 
-#ifndef _WIN32
-  #if defined(__cplusplus)
-  extern "C"
-  {
-  #endif
-#  include <stdio.h>
-#  include <stdlib.h>
-#  include "qhull/qhull.h"
-#  include "qhull/mem.h"
-#  include "qhull/qset.h"
-#  include "qhull/geom.h"
-#  include "qhull/merge.h"
-#  include "qhull/poly.h"
-#  include "qhull/io.h"
-#  include "qhull/stat.h"
-  #if defined(__cplusplus)
-  }
-  #endif
-#else
-#  include <stdio.h>
-#  include <stdlib.h>
-#  include "libqhull/qhull.h"
-#  include "libqhull/mem.h"
-#  include "libqhull/qset.h"
-#  include "libqhull/geom.h"
-#  include "libqhull/merge.h"
-#  include "libqhull/poly.h"
-#  include "libqhull/io.h"
-#  include "libqhull/stat.h"
-#endif
-
 namespace pcl
 {
   /** \brief Sort 2D points in a vector structure
--- surface/include/pcl/surface/impl/convex_hull.hpp
+++ surface/include/pcl/surface/impl/convex_hull.hpp
@@ -42,6 +42,37 @@
 #include "pcl/common/common.h"
 #include "pcl/registration/transforms.h"
 
+#ifndef _WIN32
+  #if defined(__cplusplus)
+  extern "C"
+  {
+  #endif
+#  include <stdio.h>
+#  include <stdlib.h>
+#  include "qhull/qhull.h"
+#  include "qhull/mem.h"
+#  include "qhull/qset.h"
+#  include "qhull/geom.h"
+#  include "qhull/merge.h"
+#  include "qhull/poly.h"
+#  include "qhull/io.h"
+#  include "qhull/stat.h"
+  #if defined(__cplusplus)
+  }
+  #endif
+#else
+#  include <stdio.h>
+#  include <stdlib.h>
+#  include "libqhull/qhull.h"
+#  include "libqhull/mem.h"
+#  include "libqhull/qset.h"
+#  include "libqhull/geom.h"
+#  include "libqhull/merge.h"
+#  include "libqhull/poly.h"
+#  include "libqhull/io.h"
+#  include "libqhull/stat.h"
+#endif
+
 
 //////////////////////////////////////////////////////////////////////////
 template<typename PointInT> void
