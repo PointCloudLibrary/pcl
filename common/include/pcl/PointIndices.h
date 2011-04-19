3fe421634f32c35f3c24384c22e27fa31fb4869d
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-04-19 00:24:40 +0000

    simplifying PointIndices
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@581 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/include/pcl/PointIndices.h
+++ common/include/pcl/PointIndices.h
@@ -9,60 +9,35 @@
 
 namespace pcl
 {
-  template <class ContainerAllocator>
-  struct PointIndices_
+  struct PointIndices
   {
-    typedef PointIndices_<ContainerAllocator> Type;
+    PointIndices () : header (), indices ()
+    {}
 
-    PointIndices_()
-    : header()
-    , indices()
-    {
-    }
+    ::std_msgs::Header_<std::allocator<void> >  header;
 
-    PointIndices_(const ContainerAllocator& _alloc)
-    : header(_alloc)
-    , indices(_alloc)
-    {
-    }
-
-    typedef ::std_msgs::Header_<ContainerAllocator>  _header_type;
-    ::std_msgs::Header_<ContainerAllocator>  header;
-
-    typedef std::vector<int32_t, typename ContainerAllocator::template rebind<int32_t>::other >  _indices_type;
-    std::vector<int32_t, typename ContainerAllocator::template rebind<int32_t>::other >  indices;
+    std::vector<int> indices;
 
     public:
-      typedef boost::shared_ptr< ::pcl::PointIndices_<ContainerAllocator> > Ptr;
-      typedef boost::shared_ptr< ::pcl::PointIndices_<ContainerAllocator>  const> ConstPtr;
+      typedef boost::shared_ptr< ::pcl::PointIndices> Ptr;
+      typedef boost::shared_ptr< ::pcl::PointIndices const> ConstPtr;
   }; // struct PointIndices
-  typedef  ::pcl::PointIndices_<std::allocator<void> > PointIndices;
 
   typedef boost::shared_ptr< ::pcl::PointIndices> PointIndicesPtr;
   typedef boost::shared_ptr< ::pcl::PointIndices const> PointIndicesConstPtr;
 
-  template<typename ContainerAllocator>
-  std::ostream& stream_with_indentation (std::ostream& s, const std::string& indent, 
-                                         const ::pcl::PointIndices_<ContainerAllocator> & v)
+  inline std::ostream& operator << (std::ostream& s, const ::pcl::PointIndices &v)
   {
-    s << indent << "header: " << std::endl;
-    stream_with_indentation(s, indent + "  ", v.header);
-    s << indent << "indices[]" << std::endl;
+    s << "header: " << std::endl;
+    s << "  " << v.header;
+    s << "indices[]" << std::endl;
     for (size_t i = 0; i < v.indices.size (); ++i)
     {
-      s << indent << "  indices[" << i << "]: ";
-      s << indent << "  " << v.indices[i] << std::endl;
+      s << "  indices[" << i << "]: ";
+      s << "  " << v.indices[i] << std::endl;
     }
     return (s);
   }
-
-  template<typename ContainerAllocator>
-  std::ostream& operator<<(std::ostream& s, const  ::pcl::PointIndices_<ContainerAllocator> & v)
-  {
-    stream_with_indentation (s, "", v);
-    return (s);
-  }
-
 } // namespace pcl
 
 #endif // PCL_MESSAGE_POINTINDICES_H
