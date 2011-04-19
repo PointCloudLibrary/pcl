ab702064074a2bf97dabef86252b01e1cfae6d3e
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-04-19 18:32:48 +0000

    fixed qhull cmake files (thanks Nizar!)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@597 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/Modules/FindQhull.cmake
+++ cmake/Modules/FindQhull.cmake
@@ -7,17 +7,15 @@
 # QHULL_LIBRARIES - Libraries needed to use QHULL.
 # QHULL_DEFINITIONS - Compiler flags for QHULL.
 
-#find_path(QHULL_INCLUDE_DIR qhull/qhull.h)
 find_path(QHULL_INCLUDE_DIR qhull.h
           HINTS ${QHULL_ROOT}
-          PATH_SUFFIXES qhull src)
+          PATH_SUFFIXES qhull src/libqhull libqhull)
 
-#find_library(QHULL_LIBRARY qhull)
 find_library(QHULL_LIBRARY qhull
              HINTS ${QHULL_ROOT}
              PATH_SUFFIXES project)
 
-set(QHULL_INCLUDE_DIRS ${QHULL_INCLUDE_DIR}/qhull ${QHULL_INCLUDE_DIR})
+set(QHULL_INCLUDE_DIRS ${QHULL_INCLUDE_DIR})
 set(QHULL_LIBRARIES ${QHULL_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
