e4a01a41e5598b5783236d6c49b50a55d2d46c55
Michael Dixon <mdixon@willowgarage.com>
2011-04-25 16:49:42 +0000

    Changed boost version from 1.42 back to 1.40
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@711 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_find_boost.cmake
+++ cmake/pcl_find_boost.cmake
@@ -4,7 +4,7 @@ if(NOT PCL_SHARED_LIBS)
     set(Boost_USE_STATIC_LIBS ON)
 endif(NOT PCL_SHARED_LIBS)
 
-find_package(Boost 1.42.0 COMPONENTS system filesystem thread date_time)
+find_package(Boost 1.40.0 COMPONENTS system filesystem thread date_time)
 
 include_directories(${Boost_INCLUDE_DIRS})
 link_directories(${Boost_LIBRARY_DIRS})
