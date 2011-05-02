b06b884d34fa1f8b5d387d615a8d9e0cc37f91d3
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-05-02 16:08:25 +0000

    fixing windows colored output (thanks to Mourad)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@788 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/src/print.cpp
+++ common/src/print.cpp
@@ -35,35 +35,90 @@
  *
  */
 #include <pcl/console/print.h>
+#ifdef WIN32
+# include <windows.h>
+
+DWORD convertAttributesColor(int attribute, int fg, int bg=-1)
+{
+    static DWORD wAttributes[7]  = { 0, //TT_RESET
+                                     FOREGROUND_INTENSITY , // TT_BRIGHT
+                                     0, // TT_DIM
+                                     COMMON_LVB_UNDERSCORE, // TT_UNDERLINE
+                                     0, // TT_BLINK     = 4,
+                                     COMMON_LVB_REVERSE_VIDEO, // TT_REVERSE
+                                     0 //TT_HIDDEN    = 8
+                                    };
+    static DWORD wFgColors[8]  = { 0, // TT_BLACK
+                                   FOREGROUND_RED, // TT_RED
+                                   FOREGROUND_GREEN , // TT_GREEN
+                                   FOREGROUND_GREEN | FOREGROUND_RED , // TT_YELLOW
+                                   FOREGROUND_BLUE , // TT_BLUE
+                                   FOREGROUND_RED | FOREGROUND_BLUE , // TT_MAGENTA
+                                   FOREGROUND_GREEN | FOREGROUND_BLUE, // TT_CYAN
+                                   FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED // TT_WHITE
+                                 };
+    static DWORD wBgColors[8]  = { 0, // TT_BLACK
+                                   BACKGROUND_RED, // TT_RED
+                                   BACKGROUND_GREEN , // TT_GREEN
+                                   BACKGROUND_GREEN | BACKGROUND_BLUE , // TT_YELLOW
+                                   BACKGROUND_BLUE , // TT_BLUE
+                                   BACKGROUND_RED | BACKGROUND_BLUE , // TT_MAGENTA
+                                   BACKGROUND_GREEN | BACKGROUND_BLUE, // TT_CYAN
+                                   BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_RED // TT_WHITE
+                                 };
+
+    if(bg < 0)
+        return wAttributes[attribute] | wFgColors[fg];
+    else
+        return wAttributes[attribute] | wFgColors[fg] | wBgColors[bg];
+
+}
+
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 void
 pcl::console::change_text_color (FILE *stream, int attribute, int fg, int bg)
 {
+#ifdef WIN32
+  HANDLE h = GetStdHandle((stream == stdout) ? STD_OUTPUT_HANDLE : STD_ERROR_HANDLE);
+  SetConsoleTextAttribute(h, convertAttributesColor(attribute, fg, bg));
+#else
   char command[13];
   // Command is the control command to the terminal
   sprintf (command, "%c[%d;%d;%dm", 0x1B, attribute, fg + 30, bg + 40);
   fprintf (stream, "%s", command);
+#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void
 pcl::console::change_text_color (FILE *stream, int attribute, int fg)
 {
+#ifdef WIN32
+  HANDLE h = GetStdHandle((stream == stdout) ? STD_OUTPUT_HANDLE : STD_ERROR_HANDLE);
+  SetConsoleTextAttribute(h, convertAttributesColor(attribute, fg));
+#else
   char command[13];
   // Command is the control command to the terminal
   sprintf (command, "%c[%d;%dm", 0x1B, attribute, fg + 30);
   fprintf (stream, "%s", command);
+#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void
 pcl::console::reset_text_color (FILE *stream)
 {
+#ifdef WIN32
+  HANDLE h = GetStdHandle((stream == stdout) ? STD_OUTPUT_HANDLE : STD_ERROR_HANDLE);
+  SetConsoleTextAttribute(h, convertAttributesColor(0, TT_WHITE, TT_BLACK));
+#else
   char command[13];
   // Command is the control command to the terminal
   sprintf (command, "%c[0;m", 0x1B);
   fprintf (stream, "%s", command);
+#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -140,7 +195,7 @@ pcl::console::print_highlight (FILE *stream, const char *format, ...)
 void
 pcl::console::print_error (const char *format, ...)
 {
-  change_text_color (stderr, TT_RESET, TT_RED);
+  change_text_color (stderr, TT_BRIGHT, TT_RED);
   va_list ap;
 
   va_start (ap, format);
@@ -154,7 +209,7 @@ pcl::console::print_error (const char *format, ...)
 void
 pcl::console::print_error (FILE *stream, const char *format, ...)
 {
-  change_text_color (stream, TT_RESET, TT_RED);
+  change_text_color (stream, TT_BRIGHT, TT_RED);
   va_list ap;
 
   va_start (ap, format);
@@ -168,7 +223,7 @@ pcl::console::print_error (FILE *stream, const char *format, ...)
 void
 pcl::console::print_warn (const char *format, ...)
 {
-  change_text_color (stderr, TT_RESET, TT_YELLOW);
+  change_text_color (stderr, TT_BRIGHT, TT_YELLOW);
   va_list ap;
 
   va_start (ap, format);
@@ -182,7 +237,7 @@ pcl::console::print_warn (const char *format, ...)
 void
 pcl::console::print_warn (FILE *stream, const char *format, ...)
 {
-  change_text_color (stream, TT_RESET, TT_YELLOW);
+  change_text_color (stream, TT_BRIGHT, TT_YELLOW);
   va_list ap;
 
   va_start (ap, format);
