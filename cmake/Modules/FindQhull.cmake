dd58f79196ef50f8eb3bae3c974024a79b5c1aaf
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-05-05 00:25:48 +0000

    too tired to specify everything manually in Windows. PATHS is standard CMake practice (http://www.cmake.org/cmake/help/cmake-2-8-docs.html#command:find_library) and we should use it accordingly. We will revise this in 2.x, but for now, this should help a lot in automatically detecting the Win32/Win64 binaries that we provide for 3rd party dependencies.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@841 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/Modules/FindQhull.cmake
+++ cmake/Modules/FindQhull.cmake
@@ -12,15 +12,18 @@ set(QHULL_MAJOR_VERSION 6)
 find_path(QHULL_INCLUDE_DIR 
           NAMES libqhull/libqhull.h qhull.h
           HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}"
+          PATHS "$ENV{PROGRAMFILES}/qhull 6.2.0.1373" "$ENV{PROGRAMW6432}/qhull 6.2.0.1373" 
           PATH_SUFFIXES qhull src/libqhull libqhull include)
 
 find_library(QHULL_LIBRARY 
              NAMES qhullstatic qhull qhull${QHULL_MAJOR_VERSION}
              HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}"
+             PATHS "$ENV{PROGRAMFILES}/qhull 6.2.0.1373" "$ENV{PROGRAMW6432}/qhull 6.2.0.1373" 
              PATH_SUFFIXES project build bin lib)
 
 find_library(QHULL_LIBRARY_DEBUG qhullstatic_d qhull_d qhull_d${QHULL_MAJOR_VERSION} qhull qhull${QHULL_MAJOR_VERSION}
              HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}"
+             PATHS "$ENV{PROGRAMFILES}/qhull 6.2.0.1373" "$ENV{PROGRAMW6432}/qhull 6.2.0.1373" 
              PATH_SUFFIXES project build bin lib)
 
 if(NOT QHULL_LIBRARY_DEBUG)
