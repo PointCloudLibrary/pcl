f89d834d1f93f300e263fe2e7f14befe5b9f436e
Nizar Sallem <nksallem@laas.fr>
2011-05-07 21:01:22 +0000

    Fix: oups missed with the last commit
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.0-rc@903 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_options.cmake
+++ cmake/pcl_options.cmake
@@ -1,5 +1,9 @@
 # Options for building PCL.
 
 # Build shared libraries by default.
-option(PCL_SHARED_LIBS "Build shared libraries." ON)
+if(WIN32)
+	option(PCL_SHARED_LIBS "Build shared libraries." OFF)
+else(WIN32)
+	option(PCL_SHARED_LIBS "Build shared libraries." ON)
+endif(WIN32)
 mark_as_advanced(PCL_SHARED_LIBS)
