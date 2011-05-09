5ea318be2634a4360d7f54f27336b01a41e5ed72
Mourad Boufarguine <mourad@boufarguine.name>
2011-05-09 22:32:58 +0000

    making shared PCL libs as default also for windows
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.0-rc@943 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_options.cmake
+++ cmake/pcl_options.cmake
@@ -1,9 +1,5 @@
 # Options for building PCL.
 
 # Build shared libraries by default.
-if(WIN32)
-	option(PCL_SHARED_LIBS "Build shared libraries." OFF)
-else(WIN32)
-	option(PCL_SHARED_LIBS "Build shared libraries." ON)
-endif(WIN32)
+option(PCL_SHARED_LIBS "Build shared libraries." ON)
 mark_as_advanced(PCL_SHARED_LIBS)
