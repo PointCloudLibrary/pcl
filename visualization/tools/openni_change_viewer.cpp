712a9f294c82a6f0ff7d2017c4fbe8c8c4936dda
Julius Kammerl <julius@kammerl.de>
2011-05-18 04:09:40 +0000

    extended openni_change_viewer with improved command line interface
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1023 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- visualization/tools/openni_change_viewer.cpp
+++ visualization/tools/openni_change_viewer.cpp
@@ -31,7 +31,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *	
- * Author: Nico Blodow (blodow@cs.tum.edu)
+ * Author: Nico Blodow (blodow@cs.tum.edu), Julius Kammerl (julius@kammerl.de)
  */
 
 #include <pcl/point_cloud.h>
@@ -42,13 +42,24 @@
 #include <pcl/octree/octree.h>
 #include <pcl/filters/extract_indices.h>
 
+#include <pcl/console/parse.h>
+
+enum {
+  REDDIFF_MODE,
+  ONLYDIFF_MODE,
+
+  MODE_COUNT
+};
+
 class OpenNIChangeViewer
 {
   public:
-    OpenNIChangeViewer (double resolution) 
+    OpenNIChangeViewer (double resolution, int mode, int noise_filter)
       : viewer ("PCL OpenNI Viewer")
     {
       octree = new pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZRGB>(resolution);
+      mode_ = mode;
+      noise_filter_ = noise_filter;
     }
 
     void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
@@ -66,22 +77,39 @@ class OpenNIChangeViewer
       boost::shared_ptr<std::vector<int> > newPointIdxVector (new std::vector<int>);
 
       // get a vector of new points, which did not exist in previous buffer
-      octree->getPointIndicesFromNewVoxels (*newPointIdxVector, 7);
+      octree->getPointIndicesFromNewVoxels (*newPointIdxVector, noise_filter_);
 
       std::cerr << newPointIdxVector->size() << std::endl;
 
-      //// extract points from pointcloud
-      pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud (new pcl::PointCloud<pcl::PointXYZRGB> (*cloud));
-      //pcl::ExtractIndices<pcl::PointXYZRGB> ei;
-      //ei.setInputCloud (cloud);
-      //ei.setIndices (newPointIdxVector);
-      //ei.filter (filtered_cloud);
+      pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud;
 
-      for (std::vector<int>::iterator it = newPointIdxVector->begin (); it != newPointIdxVector->end (); it++)
-        filtered_cloud->points[*it].rgb = 255<<16; 
-      
-      if (!viewer.wasStopped())
-        viewer.showCloud (filtered_cloud);
+      switch (mode_) {
+        case REDDIFF_MODE:
+          filtered_cloud = (pcl::PointCloud<pcl::PointXYZRGB>::Ptr) new pcl::PointCloud<pcl::PointXYZRGB> (*cloud);
+
+          filtered_cloud->points.reserve(newPointIdxVector->size());
+
+          for (std::vector<int>::iterator it = newPointIdxVector->begin (); it != newPointIdxVector->end (); it++)
+            filtered_cloud->points[*it].rgb = 255<<16;
+
+          if (!viewer.wasStopped())
+            viewer.showCloud (filtered_cloud);
+
+          break;
+        case ONLYDIFF_MODE:
+          filtered_cloud = (pcl::PointCloud<pcl::PointXYZRGB>::Ptr) new pcl::PointCloud<pcl::PointXYZRGB> ();
+
+          filtered_cloud->points.reserve(newPointIdxVector->size());
+
+          for (std::vector<int>::iterator it = newPointIdxVector->begin (); it != newPointIdxVector->end (); it++)
+            filtered_cloud->points.push_back(cloud->points[*it]);
+
+
+          if (!viewer.wasStopped())
+            viewer.showCloud (filtered_cloud);
+          break;
+      }
+
       
       // switch buffers - reset tree
       octree->switchBuffers ();
@@ -108,16 +136,31 @@ class OpenNIChangeViewer
 
     pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZRGB> *octree;
     pcl::visualization::CloudViewer viewer;
+
+    int mode_;
+    int noise_filter_;
 };
 
 int 
 main (int argc, char* argv[])
 {
-  double resolution = 0.01;
-  if (argc > 1)
-    resolution = atof (argv[1]);
 
-  OpenNIChangeViewer v (resolution);
+  std::cout << "Syntax is " << argv[0] << " [-r octree resolution] [-d] [-n noise_filter intensity] \n";
+
+  int mode = REDDIFF_MODE;
+  int noise_filter = 7;
+  double resolution = 0.01;
+
+  pcl::console::parse_argument (argc, argv, "-r", resolution);
+
+  pcl::console::parse_argument (argc, argv, "-n", noise_filter);
+
+  if (pcl::console::find_argument (argc, argv, "-d")>0) {
+    mode = ONLYDIFF_MODE;
+  }
+
+
+  OpenNIChangeViewer v (resolution, mode, noise_filter);
   v.run ();
   return 0;
 }
