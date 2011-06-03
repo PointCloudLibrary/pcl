4737ea396678166283d65d155ed1bbe195859ca4
Mourad Boufarguine <mourad@boufarguine.name>
2011-06-03 20:28:31 +0000

    adding a FindSphinx cmake module
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.0.x@1162 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ cmake/Modules/FindSphinx.cmake
@@ -0,0 +1,26 @@
+###############################################################################
+# Find Sphinx
+#
+# This sets the following variables:
+# SPHINX_FOUND - True if Sphinx was found.
+# SPHINX_EXECUTABLE - Sphinx-build executable
+
+find_package(PkgConfig)
+pkg_check_modules(PC_SPHINX sphinx-build)
+
+find_package(PythonInterp)
+
+if(PYTHONINTERP_FOUND)
+  get_filename_component(PYTHON_DIR "${PYTHON_EXECUTABLE}" PATH)
+endif(PYTHONINTERP_FOUND)
+
+find_program(SPHINX_EXECUTABLE NAMES sphinx-build
+             HINTS ${PC_SPHINX_EXECUTABLE} $ENV{SPHINX_DIR} ${PYTHON_DIR}/Scripts
+             PATH_SUFFIXES bin
+             DOC "Sphinx documentation generator"
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
+
+mark_as_advanced(SPHINX_EXECUTABLE)
