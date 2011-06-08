59d3ec7c581e477b5dbd77a7791b094df5b3d64b
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-06-08 18:59:45 +0000

    documented vertices better
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1198 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- surface/include/pcl/surface/concave_hull.h
+++ surface/include/pcl/surface/concave_hull.h
@@ -1,39 +1,39 @@
-/*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2010, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * $Id$
- *
- */
+/**
+  * Software License Agreement (BSD License)
+  *
+  *  Copyright (c) 2010, Willow Garage, Inc.
+  *  All rights reserved.
+  *
+  *  Redistribution and use in source and binary forms, with or without
+  *  modification, are permitted provided that the following conditions
+  *  are met:
+  *
+  *   * Redistributions of source code must retain the above copyright
+  *     notice, this list of conditions and the following disclaimer.
+  *   * Redistributions in binary form must reproduce the above
+  *     copyright notice, this list of conditions and the following
+  *     disclaimer in the documentation and/or other materials provided
+  *     with the distribution.
+  *   * Neither the name of Willow Garage, Inc. nor the names of its
+  *     contributors may be used to endorse or promote products derived
+  *     from this software without specific prior written permission.
+  *
+  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+  *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+  *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  *  POSSIBILITY OF SUCH DAMAGE.
+  *
+  * $Id$
+  *
+  */
 
 #include <pcl/pcl_config.h>
 #ifdef HAVE_QHULL
@@ -70,11 +70,14 @@ namespace pcl
       };
 
       /** \brief Compute a concave hull for all points given 
-       * \param points the resultant concave hull vertices
-       * \param polygons the resultant concave hull polygons (vertex indices)
-       */
+        *
+        * \param points the resultant points lying on the concave hull 
+        * \param polygons the resultant concave hull polygons, as a set of
+        * vertices. The Vertices structure contains an array of point indices.
+        */
       void
-      reconstruct (PointCloud &points, std::vector<pcl::Vertices> &polygons);
+      reconstruct (PointCloud &points, 
+                   std::vector<pcl::Vertices> &polygons);
 
       /** \brief Compute a concave hull for all points given 
        * \param output the resultant concave hull vertices
@@ -82,9 +85,12 @@ namespace pcl
       void
       reconstruct (PointCloud &output);
 
-      /** \brief Set the alpha value, which limits the size of the resultant hull segments (the smaller the more detailed the hull).
-       * \param alpha positive, non-zero value, defining the maximum length from a vertex to the facet center (center of the voronoi cell).
-       */
+      /** \brief Set the alpha value, which limits the size of the resultant
+        * hull segments (the smaller the more detailed the hull).  
+        *
+        * \param alpha  positive, non-zero value, defining the maximum length
+        * from a vertex to the facet center (center of the voronoi cell).
+        */
       inline void
       setAlpha (double alpha)
       {
@@ -135,12 +141,15 @@ namespace pcl
       /** \brief describe voronoi_centers here.. */
       PointCloudPtr voronoi_centers_;
 
-      /** \brief The actual reconstruction method. 
-       * \param points the resultant concave hull vertices
-       * \param polygons the resultant concave hull polygons (vertex indices)
-       */
+      /** \brief The actual reconstruction method.
+        * 
+        * \param points the resultant points lying on the concave hull 
+        * \param polygons the resultant concave hull polygons, as a set of
+        * vertices. The Vertices structure contains an array of point indices.
+        */
       void
-      performReconstruction (PointCloud &points, std::vector<pcl::Vertices> &polygons);
+      performReconstruction (PointCloud &points, 
+                             std::vector<pcl::Vertices> &polygons);
     };
 
 }
--- surface/include/pcl/surface/convex_hull.h
+++ surface/include/pcl/surface/convex_hull.h
@@ -1,39 +1,39 @@
-/*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2010, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * $Id$
- *
- */
+/**
+  * Software License Agreement (BSD License)
+  *
+  *  Copyright (c) 2010, Willow Garage, Inc.
+  *  All rights reserved.
+  *
+  *  Redistribution and use in source and binary forms, with or without
+  *  modification, are permitted provided that the following conditions
+  *  are met:
+  *
+  *   * Redistributions of source code must retain the above copyright
+  *     notice, this list of conditions and the following disclaimer.
+  *   * Redistributions in binary form must reproduce the above
+  *     copyright notice, this list of conditions and the following
+  *     disclaimer in the documentation and/or other materials provided
+  *     with the distribution.
+  *   * Neither the name of Willow Garage, Inc. nor the names of its
+  *     contributors may be used to endorse or promote products derived
+  *     from this software without specific prior written permission.
+  *
+  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+  *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+  *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  *  POSSIBILITY OF SUCH DAMAGE.
+  *
+  * $Id$
+  *
+  */
 
 #include <pcl/pcl_config.h>
 #ifdef HAVE_QHULL
@@ -90,11 +90,14 @@ namespace pcl
       };
 
       /** \brief Compute a convex hull for all points given 
-       * \param points the resultant convex hull vertices
-       * \param polygons the resultant convex hull polygons (vertex indices)
-       */
+        *
+        * \param points the resultant points lying on the convex hull 
+        * \param polygons the resultant convex hull polygons, as a set of
+        * vertices. The Vertices structure contains an array of point indices.
+        */
       void
-      reconstruct (PointCloud &points, std::vector<pcl::Vertices> &polygons);
+      reconstruct (PointCloud &points, 
+                   std::vector<pcl::Vertices> &polygons);
 
       /** \brief Compute a convex hull for all points given 
        * \param output the resultant convex hull vertices
@@ -114,12 +117,16 @@ namespace pcl
 
     private:
       /** \brief The actual reconstruction method. 
-       * \param points the resultant convex hull vertices
-       * \param polygons the resultant convex hull polygons (vertex indices)
-       * \param fill_polygon_data true if polygons should be filled, false otherwise
-       */
+        * 
+        * \param points the resultant points lying on the convex hull 
+        * \param polygons the resultant convex hull polygons, as a set of
+        * vertices. The Vertices structure contains an array of point indices.
+        * \param fill_polygon_data true if polygons should be filled, false otherwise
+        */
       void
-      performReconstruction (PointCloud &points, std::vector<pcl::Vertices> &polygons, bool fill_polygon_data = false);
+      performReconstruction (PointCloud &points, 
+                             std::vector<pcl::Vertices> &polygons, 
+                             bool fill_polygon_data = false);
 
       bool keep_information_;
 
