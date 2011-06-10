4d80adf255def47ab7671d0c822f9a259f90c75a
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-06-10 18:52:12 +0000

    minor doxygen edits
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1210 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- surface/include/pcl/surface/convex_hull.h
+++ surface/include/pcl/surface/convex_hull.h
@@ -1,39 +1,39 @@
-/**
-  * Software License Agreement (BSD License)
-  *
-  *  Copyright (c) 2010, Willow Garage, Inc.
-  *  All rights reserved.
-  *
-  *  Redistribution and use in source and binary forms, with or without
-  *  modification, are permitted provided that the following conditions
-  *  are met:
-  *
-  *   * Redistributions of source code must retain the above copyright
-  *     notice, this list of conditions and the following disclaimer.
-  *   * Redistributions in binary form must reproduce the above
-  *     copyright notice, this list of conditions and the following
-  *     disclaimer in the documentation and/or other materials provided
-  *     with the distribution.
-  *   * Neither the name of Willow Garage, Inc. nor the names of its
-  *     contributors may be used to endorse or promote products derived
-  *     from this software without specific prior written permission.
-  *
-  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-  *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-  *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-  *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  *  POSSIBILITY OF SUCH DAMAGE.
-  *
-  * $Id$
-  *
-  */
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
 
 #include <pcl/pcl_config.h>
 #ifdef HAVE_QHULL
@@ -53,10 +53,10 @@
 namespace pcl
 {
   /** \brief Sort 2D points in a vector structure
-   * \param p1 the first point
-   * \param p2 the second point
-   * \ingroup surface
-   */
+    * \param p1 the first point
+    * \param p2 the second point
+    * \ingroup surface
+    */
   inline bool
   comparePoints2D (const std::pair<int, Eigen::Vector4f> & p1, const std::pair<int, Eigen::Vector4f> & p2)
   {
@@ -67,16 +67,16 @@ namespace pcl
 
   ////////////////////////////////////////////////////////////////////////////////////////////
   /** \brief @b ConvexHull using libqhull library.
-   * \author Aitor Aldoma
-   * \ingroup surface
-   */
+    * \author Aitor Aldoma
+    * \ingroup surface
+    */
   template<typename PointInT>
-    class ConvexHull : public PCLBase<PointInT>
-    {
-      using PCLBase<PointInT>::input_;
-      using PCLBase<PointInT>::indices_;
-      using PCLBase<PointInT>::initCompute;
-      using PCLBase<PointInT>::deinitCompute;
+  class ConvexHull : public PCLBase<PointInT>
+  {
+    using PCLBase<PointInT>::input_;
+    using PCLBase<PointInT>::indices_;
+    using PCLBase<PointInT>::initCompute;
+    using PCLBase<PointInT>::deinitCompute;
 
     public:
       typedef pcl::PointCloud<PointInT> PointCloud;
@@ -100,15 +100,15 @@ namespace pcl
                    std::vector<pcl::Vertices> &polygons);
 
       /** \brief Compute a convex hull for all points given 
-       * \param output the resultant convex hull vertices
-       */
+        * \param output the resultant convex hull vertices
+        */
       void
       reconstruct (PointCloud &output);
 
       /** \brief If keep_information_is set to true the convex hull
-       * points keep other information like rgb, normals, ...
-       * \param value where to keep the information or not, default is false
-       */
+        * points keep other information like rgb, normals, ...
+        * \param value where to keep the information or not, default is false
+        */
       void
       setKeepInformation (bool value)
       {
