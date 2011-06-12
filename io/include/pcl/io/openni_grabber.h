4e1057d95285076b149fe51806ae4dfa79bbc520
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-06-12 20:11:21 +0000

    indentation
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1229 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- io/include/pcl/io/openni_grabber.h
+++ io/include/pcl/io/openni_grabber.h
@@ -63,129 +63,210 @@ namespace pcl
    */
   class PCL_EXPORTS OpenNIGrabber : public Grabber
   {
-  public:
+    public:
 
-    typedef enum
-    {
-      OpenNI_Default_Mode = 0, /*This can depend on the device. For now all devices (PSDK, Xtion, Kinect) its VGA@30Hz*/
-      OpenNI_SXGA_15Hz = 1,  /*Only supported by the Kinect*/
-      OpenNI_VGA_30Hz = 2,   /*Supported by PSDK, Xtion and Kinect*/
-      OpenNI_VGA_25Hz = 3,   /*Supportged by PSDK and Xtion*/
-      OpenNI_QVGA_25Hz = 4,  /*Supported by PSDK and Xtion*/
-      OpenNI_QVGA_30Hz = 5,  /*Supported by PSDK, Xtion and Kinect*/
-      OpenNI_QVGA_60Hz = 6,  /*Supported by PSDK and Xtion*/
-      OpenNI_QQVGA_25Hz = 7, /*Not supported -> using software downsampling (only for integer scale factor and only NN)*/
-      OpenNI_QQVGA_30Hz = 8, /*Not supported -> using software downsampling (only for integer scale factor and only NN)*/
-      OpenNI_QQVGA_60Hz = 9  /*Not supported -> using software downsampling (only for integer scale factor and only NN)*/
-    } Mode;
+      typedef enum
+      {
+        OpenNI_Default_Mode = 0, /*This can depend on the device. For now all devices (PSDK, Xtion, Kinect) its VGA@30Hz*/
+        OpenNI_SXGA_15Hz = 1,  /*Only supported by the Kinect*/
+        OpenNI_VGA_30Hz = 2,   /*Supported by PSDK, Xtion and Kinect*/
+        OpenNI_VGA_25Hz = 3,   /*Supportged by PSDK and Xtion*/
+        OpenNI_QVGA_25Hz = 4,  /*Supported by PSDK and Xtion*/
+        OpenNI_QVGA_30Hz = 5,  /*Supported by PSDK, Xtion and Kinect*/
+        OpenNI_QVGA_60Hz = 6,  /*Supported by PSDK and Xtion*/
+        OpenNI_QQVGA_25Hz = 7, /*Not supported -> using software downsampling (only for integer scale factor and only NN)*/
+        OpenNI_QQVGA_30Hz = 8, /*Not supported -> using software downsampling (only for integer scale factor and only NN)*/
+        OpenNI_QQVGA_60Hz = 9  /*Not supported -> using software downsampling (only for integer scale factor and only NN)*/
+      } Mode;
 
-    //define callback signature typedefs
-    typedef void (sig_cb_openni_image) (const boost::shared_ptr<openni_wrapper::Image>&);
-    typedef void (sig_cb_openni_depth_image) (const boost::shared_ptr<openni_wrapper::DepthImage>&);
-    typedef void (sig_cb_openni_ir_image) (const boost::shared_ptr<openni_wrapper::IRImage>&);
-    typedef void (sig_cb_openni_image_depth_image) (const boost::shared_ptr<openni_wrapper::Image>&, const boost::shared_ptr<openni_wrapper::DepthImage>&, float constant) ;
-    typedef void (sig_cb_openni_ir_depth_image) (const boost::shared_ptr<openni_wrapper::IRImage>&, const boost::shared_ptr<openni_wrapper::DepthImage>&, float constant) ;
-    typedef void (sig_cb_openni_point_cloud) (const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZ> >&);
-    typedef void (sig_cb_openni_point_cloud_rgb) (const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZRGB> >&);
-    typedef void (sig_cb_openni_point_cloud_i) (const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZI> >&);
-    
-  public:
-    //enable using some openni parameters in the constructor.
-    OpenNIGrabber (const std::string& device_id = "", const Mode& depth_mode = OpenNI_Default_Mode, const Mode& image_mode = OpenNI_Default_Mode) throw (openni_wrapper::OpenNIException);
+      //define callback signature typedefs
+      typedef void (sig_cb_openni_image) (const boost::shared_ptr<openni_wrapper::Image>&);
+      typedef void (sig_cb_openni_depth_image) (const boost::shared_ptr<openni_wrapper::DepthImage>&);
+      typedef void (sig_cb_openni_ir_image) (const boost::shared_ptr<openni_wrapper::IRImage>&);
+      typedef void (sig_cb_openni_image_depth_image) (const boost::shared_ptr<openni_wrapper::Image>&, const boost::shared_ptr<openni_wrapper::DepthImage>&, float constant) ;
+      typedef void (sig_cb_openni_ir_depth_image) (const boost::shared_ptr<openni_wrapper::IRImage>&, const boost::shared_ptr<openni_wrapper::DepthImage>&, float constant) ;
+      typedef void (sig_cb_openni_point_cloud) (const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZ> >&);
+      typedef void (sig_cb_openni_point_cloud_rgb) (const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZRGB> >&);
+      typedef void (sig_cb_openni_point_cloud_i) (const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZI> >&);
+      
+    public:
+      //enable using some openni parameters in the constructor.
+      /** \brief Constructor
+        * \param device_id
+        * \param depth_mode
+        * \param image_mode
+        */
+      OpenNIGrabber (const std::string& device_id = "", 
+                     const Mode& depth_mode = OpenNI_Default_Mode, 
+                     const Mode& image_mode = OpenNI_Default_Mode) throw (openni_wrapper::OpenNIException);
 
-    virtual ~OpenNIGrabber () throw ();
-    virtual void start () throw (pcl::PCLIOException);
-    virtual void stop () throw (pcl::PCLIOException);
-    virtual bool isRunning () const throw (pcl::PCLIOException);
-    virtual std::string getName () const;
+      /** \brief Destructor */
+      virtual ~OpenNIGrabber () throw ();
+      
+      /** \brief ... */
+      virtual void 
+      start () throw (pcl::PCLIOException);
 
-    inline const openni_wrapper::OpenNIDevice& getDevice () const;
-    std::vector<std::pair<int, XnMapOutputMode> > getAvailableDepthModes () const;
-    std::vector<std::pair<int, XnMapOutputMode> > getAvailableImageModes () const;
-  private:
-    void onInit (const std::string& device_id, const Mode& depth_mode, const Mode& image_mode);
-    void setupDevice (const std::string& device_id, const Mode& depth_mode, const Mode& image_mode);
-    void updateModeMaps ();
-    void startSynchronization ();
-    void stopSynchronization ();
+      /** \brief ... */
+      virtual void 
+      stop () throw (pcl::PCLIOException);
 
-    bool mapConfigMode2XnMode (int mode, XnMapOutputMode &xnmode) const;
+      /** \brief ... */
+      virtual bool 
+      isRunning () const throw (pcl::PCLIOException);
 
-    // callback methods
-    void imageCallback (boost::shared_ptr<openni_wrapper::Image> image, void* cookie);
-    void depthCallback (boost::shared_ptr<openni_wrapper::DepthImage> depth_image, void* cookie);
-    void irCallback (boost::shared_ptr<openni_wrapper::IRImage> ir_image, void* cookie);
-    void imageDepthImageCallback (const boost::shared_ptr<openni_wrapper::Image> &image,
-                                  const boost::shared_ptr<openni_wrapper::DepthImage> &depth_image);
-    void irDepthImageCallback (const boost::shared_ptr<openni_wrapper::IRImage> &image,
+      /** \brief ... */
+      virtual std::string 
+      getName () const;
+
+      /** \brief ... */
+      inline const openni_wrapper::OpenNIDevice& 
+      getDevice () const;
+
+      /** \brief ... */
+      std::vector<std::pair<int, XnMapOutputMode> > 
+      getAvailableDepthModes () const;
+
+      /** \brief ... */
+      std::vector<std::pair<int, XnMapOutputMode> > 
+      getAvailableImageModes () const;
+
+    private:
+      /** \brief ... */
+      void 
+      onInit (const std::string& device_id, const Mode& depth_mode, const Mode& image_mode);
+
+      /** \brief ... */
+      void 
+      setupDevice (const std::string& device_id, const Mode& depth_mode, const Mode& image_mode);
+
+      /** \brief ... */
+      void 
+      updateModeMaps ();
+
+      /** \brief ... */
+      void 
+      startSynchronization ();
+
+      /** \brief ... */
+      void 
+      stopSynchronization ();
+
+
+      /** \brief ... */
+      bool 
+      mapConfigMode2XnMode (int mode, XnMapOutputMode &xnmode) const;
+
+      // callback methods
+      /** \brief ... */
+      void 
+      imageCallback (boost::shared_ptr<openni_wrapper::Image> image, void* cookie);
+
+      /** \brief ... */
+      void 
+      depthCallback (boost::shared_ptr<openni_wrapper::DepthImage> depth_image, void* cookie);
+
+      /** \brief ... */
+      void 
+      irCallback (boost::shared_ptr<openni_wrapper::IRImage> ir_image, void* cookie);
+
+      /** \brief ... */
+      void 
+      imageDepthImageCallback (const boost::shared_ptr<openni_wrapper::Image> &image,
                                const boost::shared_ptr<openni_wrapper::DepthImage> &depth_image);
 
-    virtual void signalsChanged ();
+      /** \brief ... */
+      void 
+      irDepthImageCallback (const boost::shared_ptr<openni_wrapper::IRImage> &image,
+                            const boost::shared_ptr<openni_wrapper::DepthImage> &depth_image);
 
-    // helper methods
+      /** \brief ... */
+      virtual void 
+      signalsChanged ();
 
-    virtual inline void checkImageAndDepthSynchronizationRequired ();
-    virtual inline void checkImageStreamRequired ();
-    virtual inline void checkDepthStreamRequired ();
-    virtual inline void checkIRStreamRequired();
-    boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ> > convertToXYZPointCloud (const boost::shared_ptr<openni_wrapper::DepthImage> &depth) const;
-    boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB> > convertToXYZRGBPointCloud (const boost::shared_ptr<openni_wrapper::Image> &image,
-                                                                                     const boost::shared_ptr<openni_wrapper::DepthImage> &depth_image) const;
-    boost::shared_ptr<pcl::PointCloud<pcl::PointXYZI> > convertToXYZIPointCloud (const boost::shared_ptr<openni_wrapper::IRImage> &image,
-                                                                                 const boost::shared_ptr<openni_wrapper::DepthImage> &depth_image) const;
+      // helper methods
 
-    Synchronizer<boost::shared_ptr<openni_wrapper::Image>, boost::shared_ptr<openni_wrapper::DepthImage> > rgb_sync_;
-    Synchronizer<boost::shared_ptr<openni_wrapper::IRImage>, boost::shared_ptr<openni_wrapper::DepthImage> > ir_sync_;
+      /** \brief ... */
+      virtual inline void 
+      checkImageAndDepthSynchronizationRequired ();
 
-    /** \brief the actual openni device*/
-    boost::shared_ptr<openni_wrapper::OpenNIDevice> device_;
+      /** \brief ... */
+      virtual inline void 
+      checkImageStreamRequired ();
+      
+      /** \brief ... */
+      virtual inline void 
+      checkDepthStreamRequired ();
+      
+      /** \brief ... */
+      virtual inline void 
+      checkIRStreamRequired();
+      
+      /** \brief ... */
+      boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ> > 
+      convertToXYZPointCloud (const boost::shared_ptr<openni_wrapper::DepthImage> &depth) const;
 
-    std::string rgb_frame_id_;
-    std::string depth_frame_id_;
-    unsigned image_width_;
-    unsigned image_height_;
-    unsigned depth_width_;
-    unsigned depth_height_;
+      /** \brief ... */
+      boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB> > 
+      convertToXYZRGBPointCloud (const boost::shared_ptr<openni_wrapper::Image> &image,
+                                 const boost::shared_ptr<openni_wrapper::DepthImage> &depth_image) const;
+      /** \brief ... */
+      boost::shared_ptr<pcl::PointCloud<pcl::PointXYZI> > 
+      convertToXYZIPointCloud (const boost::shared_ptr<openni_wrapper::IRImage> &image,
+                               const boost::shared_ptr<openni_wrapper::DepthImage> &depth_image) const;
 
-    bool image_required_;
-    bool depth_required_;
-    bool ir_required_;
-    bool sync_required_;
+      Synchronizer<boost::shared_ptr<openni_wrapper::Image>, boost::shared_ptr<openni_wrapper::DepthImage> > rgb_sync_;
+      Synchronizer<boost::shared_ptr<openni_wrapper::IRImage>, boost::shared_ptr<openni_wrapper::DepthImage> > ir_sync_;
 
-    boost::signals2::signal<sig_cb_openni_image >* image_signal_;
-    boost::signals2::signal<sig_cb_openni_depth_image >* depth_image_signal_;
-    boost::signals2::signal<sig_cb_openni_ir_image >* ir_image_signal_;
-    boost::signals2::signal<sig_cb_openni_image_depth_image>* image_depth_image_signal_;
-    boost::signals2::signal<sig_cb_openni_ir_depth_image>* ir_depth_image_signal_;
-    boost::signals2::signal<sig_cb_openni_point_cloud >* point_cloud_signal_;
-    boost::signals2::signal<sig_cb_openni_point_cloud_i >* point_cloud_i_signal_;
-    boost::signals2::signal<sig_cb_openni_point_cloud_rgb >* point_cloud_rgb_signal_;
+      /** \brief the actual openni device*/
+      boost::shared_ptr<openni_wrapper::OpenNIDevice> device_;
 
-    struct modeComp
-    {
+      std::string rgb_frame_id_;
+      std::string depth_frame_id_;
+      unsigned image_width_;
+      unsigned image_height_;
+      unsigned depth_width_;
+      unsigned depth_height_;
 
-      bool operator () (const XnMapOutputMode& mode1, const XnMapOutputMode & mode2) const
+      bool image_required_;
+      bool depth_required_;
+      bool ir_required_;
+      bool sync_required_;
+
+      boost::signals2::signal<sig_cb_openni_image >* image_signal_;
+      boost::signals2::signal<sig_cb_openni_depth_image >* depth_image_signal_;
+      boost::signals2::signal<sig_cb_openni_ir_image >* ir_image_signal_;
+      boost::signals2::signal<sig_cb_openni_image_depth_image>* image_depth_image_signal_;
+      boost::signals2::signal<sig_cb_openni_ir_depth_image>* ir_depth_image_signal_;
+      boost::signals2::signal<sig_cb_openni_point_cloud >* point_cloud_signal_;
+      boost::signals2::signal<sig_cb_openni_point_cloud_i >* point_cloud_i_signal_;
+      boost::signals2::signal<sig_cb_openni_point_cloud_rgb >* point_cloud_rgb_signal_;
+
+      struct modeComp
       {
-        if (mode1.nXRes < mode2.nXRes)
-          return true;
-        else if (mode1.nXRes > mode2.nXRes)
-          return false;
-        else if (mode1.nYRes < mode2.nYRes)
-          return true;
-        else if (mode1.nYRes > mode2.nYRes)
-          return false;
-        else if (mode1.nFPS < mode2.nFPS)
-          return true;
-        else
-          return false;
-      }
-    } ;
-    std::map<int, XnMapOutputMode> config2xn_map_;
 
-    openni_wrapper::OpenNIDevice::CallbackHandle depth_callback_handle;
-    openni_wrapper::OpenNIDevice::CallbackHandle image_callback_handle;
-    openni_wrapper::OpenNIDevice::CallbackHandle ir_callback_handle;
-    bool running_;
+        bool operator () (const XnMapOutputMode& mode1, const XnMapOutputMode & mode2) const
+        {
+          if (mode1.nXRes < mode2.nXRes)
+            return true;
+          else if (mode1.nXRes > mode2.nXRes)
+            return false;
+          else if (mode1.nYRes < mode2.nYRes)
+            return true;
+          else if (mode1.nYRes > mode2.nYRes)
+            return false;
+          else if (mode1.nFPS < mode2.nFPS)
+            return true;
+          else
+            return false;
+        }
+      } ;
+      std::map<int, XnMapOutputMode> config2xn_map_;
+
+      openni_wrapper::OpenNIDevice::CallbackHandle depth_callback_handle;
+      openni_wrapper::OpenNIDevice::CallbackHandle image_callback_handle;
+      openni_wrapper::OpenNIDevice::CallbackHandle ir_callback_handle;
+      bool running_;
   } ;
 
   const openni_wrapper::OpenNIDevice&
