6d80212d6034071155695eb9bdcb8894ee42af79
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-06-15 02:31:55 +0000

    adding an II normal estimation prototype
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1286 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ apps/src/openni_ii_normal_estimation.cpp
@@ -0,0 +1,199 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *	
+ */
+
+#include <boost/thread/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/openni_grabber.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/io/openni_camera/openni_driver.h>
+#include <pcl/features/integral_image_normal.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/time.h>
+
+#define FPS_CALC(_WHAT_) \
+do \
+{ \
+    static unsigned count = 0;\
+    static double last = pcl::getTime ();\
+    if (++count == 100) \
+    { \
+      double now = pcl::getTime (); \
+      std::cout << "Average framerate("<< _WHAT_ << "): " << double(count)/double(now - last) << " Hz" <<  std::endl; \
+      count = 0; \
+      last = now; \
+    } \
+}while(false)
+
+template <typename PointType>
+class OpenNIIntegralImageNormalEstimation
+{
+  public:
+    typedef pcl::PointCloud<PointType> Cloud;
+    typedef typename Cloud::Ptr CloudPtr;
+    typedef typename Cloud::ConstPtr CloudConstPtr;
+
+    OpenNIIntegralImageNormalEstimation (const std::string& device_id = "", 
+                     float leaf_size_x = 0.01, float leaf_size_y = 0.01, float leaf_size_z = 0.01)
+    : viewer ("PCL OpenNI NormalEstimation Viewer")
+    , device_id_(device_id)
+    {
+      ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::AVERAGE_3D_GRADIENT);
+      new_cloud_ = false;
+    }
+
+    
+    void 
+    cloud_cb (const CloudConstPtr& cloud)
+    {
+      boost::mutex::scoped_lock lock (mtx_);
+      new_cloud_ = false;
+      //lock while we set our cloud;
+
+      FPS_CALC ("computation");
+
+      normals_.reset (new pcl::PointCloud<pcl::Normal>);
+
+      cloud_ = cloud;
+      // Estimate surface normals
+      ne_.setInputCloud (cloud);
+      ne_.compute (*normals_);
+
+      new_cloud_ = true;
+      std::cerr <<"done computing" << std::endl;
+    }
+
+    void
+    viz_cb (pcl::visualization::PCLVisualizer& viz)
+    {
+      boost::mutex::scoped_lock lock (mtx_);
+      if (new_cloud_)
+      {
+        FPS_CALC ("visualization");
+
+        if (cloud_)
+        {
+          //viz.addPointCloudNormals<PointType, pcl::Normal> (cloud_, normals_, 200, 0.1, "normalcloud");
+          viewer.showCloud (cloud_);
+        }
+      }
+
+    }
+
+    void
+    run ()
+    {
+      pcl::Grabber* interface = new pcl::OpenNIGrabber (device_id_);
+
+      boost::function<void (const CloudConstPtr&)> f = boost::bind (&OpenNIIntegralImageNormalEstimation::cloud_cb, this, _1);
+      boost::signals2::connection c = interface->registerCallback (f);
+     
+      viewer.runOnVisualizationThread (boost::bind(&OpenNIIntegralImageNormalEstimation::viz_cb, this, _1), "viz_cb");
+
+      interface->start ();
+      
+      while (!viewer.wasStopped ())
+      {
+        sleep (1);
+      }
+
+      interface->stop ();
+    }
+
+    pcl::IntegralImageNormalEstimation<PointType, pcl::Normal> ne_;
+    pcl::visualization::CloudViewer viewer;
+    std::string device_id_;
+    boost::mutex mtx_;
+    // Data
+    pcl::PointCloud<pcl::Normal>::Ptr normals_;
+    CloudConstPtr cloud_;
+    bool new_cloud_;
+};
+
+void
+usage (char ** argv)
+{
+  std::cout << "usage: " << argv[0] << " <device_id> <options>\n\n";
+
+  openni_wrapper::OpenNIDriver& driver = openni_wrapper::OpenNIDriver::getInstance ();
+  if (driver.getNumberDevices () > 0)
+  {
+    for (unsigned deviceIdx = 0; deviceIdx < driver.getNumberDevices (); ++deviceIdx)
+    {
+      cout << "Device: " << deviceIdx + 1 << ", vendor: " << driver.getVendorName (deviceIdx) << ", product: " << driver.getProductName (deviceIdx)
+              << ", connected: " << (int)driver.getBus (deviceIdx) << " @ " << (int)driver.getAddress (deviceIdx) << ", serial number: \'" << driver.getSerialNumber (deviceIdx) << "\'" << endl;
+      cout << "device_id may be #1, #2, ... for the first second etc device in the list or" << endl
+           << "                 bus@address for the device connected to a specific usb-bus / address combination (wotks only in Linux) or" << endl
+           << "                 <serial-number> (only in Linux and for devices which provide serial numbers)"  << endl;
+    }
+  }
+  else
+    cout << "No devices connected." << endl;
+}
+
+int 
+main (int argc, char ** argv)
+{
+  if (argc < 2)
+  {
+    usage (argv);
+    return 1;
+  }
+
+  std::string arg (argv[1]);
+  
+  if (arg == "--help" || arg == "-h")
+  {
+    usage (argv);
+    return 1;
+  }
+
+  double leaf_x = 0.01, leaf_y = 0.01, leaf_z = 0.01;
+
+  pcl::OpenNIGrabber grabber (arg);
+  if (grabber.providesCallback<pcl::OpenNIGrabber::sig_cb_openni_point_cloud_rgb> ())
+  {
+    OpenNIIntegralImageNormalEstimation<pcl::PointXYZRGB> v (arg, leaf_x, leaf_y, leaf_z);
+    v.run ();
+  }
+  else
+  {
+    OpenNIIntegralImageNormalEstimation<pcl::PointXYZ> v (arg, leaf_x, leaf_y, leaf_z);
+    v.run ();
+  }
+
+  return (0);
+}
