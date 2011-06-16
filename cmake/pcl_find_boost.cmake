ad37410cd25b514cbe8a3017f463966298e86d64
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-06-16 19:30:30 +0000

    reverting iostreams for now until we find a solution (installer)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1331 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_find_boost.cmake
+++ cmake/pcl_find_boost.cmake
@@ -4,7 +4,7 @@ if(NOT PCL_SHARED_LIBS OR WIN32)
   set(Boost_USE_STATIC_LIBS ON)
 endif(NOT PCL_SHARED_LIBS OR WIN32)
 
-find_package(Boost 1.40.0 COMPONENTS system filesystem thread date_time iostreams)
+find_package(Boost 1.40.0 COMPONENTS system filesystem thread date_time)
 
 if(Boost_FOUND)
   # Obtain diagnostic information about Boost's automatic linking outputted 
