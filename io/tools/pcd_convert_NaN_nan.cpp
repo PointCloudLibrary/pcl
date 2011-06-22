a2f2ca492cfae18f7f4985ed509a27f9102a3560
Jochen Sprickerhof <jochen@sprickerhof.de>
2011-06-22 18:18:52 +0000

    Added tool to fix NaN string in pcd files.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1448 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ io/tools/pcd_convert_NaN_nan.cpp
@@ -0,0 +1,81 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <sstream>
+#include <string>
+
+int
+main (int argc, char **argv)
+{
+  if (argc != 3)
+  {
+    std::cout << "call with " << argv[0] << " input.pcd output.pcd" << std::endl;
+    return 0;
+  }
+
+  if (!strcmp (argv[1], argv[2]))
+  {
+    std::cout << "called with same name for input and output! (done nothing)" << std::endl;
+    return 1;
+  }
+
+  std::ostringstream ss;
+  ss << std::numeric_limits<float>::quiet_NaN ();
+  std::string nanStr (ss.str ());
+
+  std::cout << "converting \"" << nanStr << "\" to \"nan\"" << std::endl;
+
+  std::ifstream input (argv[1]);
+  std::ofstream output (argv[2]);
+  std::string str;
+  char next;
+
+  while (input >> str)
+  {
+    if (str == nanStr)
+      output << "nan";
+    else
+      output << str;
+    next = input.peek ();
+    if (next == '\n' || next == '\r')
+      output << "\n";
+    else
+      output << " ";
+  }
+  return 0;
+}
