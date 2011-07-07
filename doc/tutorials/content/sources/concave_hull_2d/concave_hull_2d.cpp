5d8458a95100e5c7b27ab0fa859bbb252659b4d6
Gabe O'Leary <oleary.gabe@gmail.com>
2011-07-07 19:53:52 +0000

    added ConcaveHull tutorial
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1601 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ doc/tutorials/content/sources/concave_hull_2d/concave_hull_2d.cpp
@@ -0,0 +1,59 @@
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/surface/concave_hull.h>
+
+int
+ main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PCDReader reader;
+
+  reader.read ("table_scene_mug_stereo_textured.pcd", *cloud);
+  // Build a filter to remove spurious NaNs
+  pcl::PassThrough<pcl::PointXYZ> pass;
+  pass.setInputCloud (cloud);
+  pass.setFilterFieldName ("z");
+  pass.setFilterLimits (0, 1.1);
+  pass.filter (*cloud_filtered);
+  std::cerr << "PointCloud after filtering has: " << cloud_filtered->points.size () << " data points." << std::endl;
+
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+
+  seg.setInputCloud (cloud_filtered);
+  seg.segment (*inliers, *coefficients);
+
+  // Project the model inliers
+  pcl::ProjectInliers<pcl::PointXYZ> proj;
+  proj.setModelType (pcl::SACMODEL_PLANE);
+  proj.setInputCloud (cloud_filtered);
+  proj.setModelCoefficients (coefficients);
+  proj.filter (*cloud_projected);
+
+  // Create a Concave Hull representation of the projected inliers
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::ConcaveHull<pcl::PointXYZ> chull;
+  chull.setInputCloud (cloud_projected);
+  chull.reconstruct (*cloud_hull);
+
+  std::cerr << "Concave hull has: " << cloud_hull->points.size () << " data points." << std::endl;
+
+  pcl::PCDWriter writer;
+  writer.write ("table_scene_mug_stereo_textured_hull.pcd", *cloud_hull, false);
+
+  return (0);
+}
