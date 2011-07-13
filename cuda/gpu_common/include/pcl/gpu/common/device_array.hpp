f97670f44f7adc286ccf33f6d092de98fbf56ff5
Anatoly Baksheev <no@mail>
2011-07-13 09:34:43 +0000

    adding gpu_common
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1722 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ cuda/gpu_common/include/pcl/gpu/common/device_array.hpp
@@ -0,0 +1,232 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef PCL_GPU_DEVICE_ARRAY_
+#define PCL_GPU_DEVICE_ARRAY_
+
+#include <vector>
+#include "pcl/pcl_macros.h"
+
+namespace pcl
+{
+    namespace gpu
+    {
+        class PCL_EXPORTS DeviceArray
+        {
+        public:
+            DeviceArray();
+            ~DeviceArray();            
+
+            DeviceArray(size_t sizeBytes);
+            DeviceArray(void *ptr, size_t sizeBytes);
+
+            DeviceArray(const DeviceArray& other);
+            DeviceArray& operator=(const DeviceArray& other);
+
+            void create(size_t size);
+            void release();
+
+            void upload(const void *host_ptr, size_t sizeBytes);
+            void download(void *host_ptr) const;
+            
+            template<class T> T* ptr() { return (T*)data_; }
+            template<class T> const T* ptr() const { return (const T*)data_; }
+
+            size_t sizeBytes() const;
+
+        private:
+            void *data_;
+            size_t size_;
+
+            int* refcount_;
+        };
+
+        class PCL_EXPORTS DeviceArray2D
+        {
+        public:
+            DeviceArray2D();
+            ~DeviceArray2D();            
+
+            DeviceArray2D(int rows, int colsBytes);
+            DeviceArray2D(int rows, int colsBytes, void *data, size_t stepBytes);
+
+            DeviceArray2D(const DeviceArray2D& other);
+            DeviceArray2D& operator=(const DeviceArray2D& other);
+
+            void create(int rows, int colsBytes);
+            void release();
+
+            void upload(const void *host_ptr, size_t host_step, int rows, int colsBytes);
+            void download(void *host_ptr, size_t host_step) const;
+            
+            template<class T> T* ptr(int y = 0) { return (T*)(data_ + y * step); }
+            template<class T> const T* ptr(int y = 0) const { return (const T*)(data_ + y * step); }            
+
+            void size(int& rows, int& colsBytes) const;
+
+        private:
+            int colsBytes_;
+            int rows_;
+
+            char *data_;
+            size_t step_;
+
+            int* refcount_;
+        };
+
+
+        ////////////////////// Template versions /////////////////////////////
+
+        template<class T> 
+        class PCL_EXPORTS DeviceArray_ : private DeviceArray
+        {
+        public:
+            typedef T type;
+            enum { elem_size = sizeof(T) };
+
+            DeviceArray_();
+            DeviceArray_(size_t sizeBytes);
+            DeviceArray_(T *ptr, size_t sizeBytes);
+
+            DeviceArray_(const DeviceArray_& other);
+            DeviceArray_& operator = (const DeviceArray_& other);
+
+            void create(size_t sizeBytes);
+            void release();
+
+            void upload(const T *host_ptr, size_t sizeBytes);
+            void download(T *host_ptr) const;
+
+            void upload(const std::vector<T>& data);
+            void download(std::vector<T>& data) const;
+
+            T* ptr(); 
+            const T* ptr() const;            
+
+            operator T*();
+            operator const T*() const;
+
+            size_t size() const;
+        };
+
+        template<class T> 
+        class PCL_EXPORTS DeviceArray2D_ : private DeviceArray2D
+        {
+        public:
+            typedef T type;
+            enum { elem_size = sizeof(T) };
+
+            DeviceArray2D_();
+            DeviceArray2D_(int rows, int cols);
+            DeviceArray2D_(int rows, int cols, void *data, size_t stepBytes);
+
+            DeviceArray2D_(const DeviceArray2D_& other);
+            DeviceArray2D_& operator = (const DeviceArray2D_& other);
+
+            void create(int rows, int cols);
+            void release();
+
+            void upload(const void *host_ptr, size_t host_step, int rows, int cols);
+            void download(void *host_ptr, size_t host_step) const;
+
+            void upload(const std::vector<T>& data, int cols);
+            void download(std::vector<T>& data, int& cols) const;
+                                   
+            T* ptr(int y = 0); 
+            const T* ptr(int y = 0) const;            
+
+            operator T*();
+            operator const T*() const;
+
+            void size(int& rows, int& cols) const;
+        };        
+        
+    }
+}
+
+/////////////////////  Inline implementations of DeviceArray_ ////////////////////////////////////////////
+
+template<class T> inline pcl::gpu::DeviceArray_<T>::DeviceArray_() {}
+template<class T> inline pcl::gpu::DeviceArray_<T>::DeviceArray_(size_t size) : DeviceArray(size * elem_size) {}
+template<class T> inline pcl::gpu::DeviceArray_<T>::DeviceArray_(T *ptr, size_t size) : DeviceArray(ptr, size * elem_size) {}
+template<class T> inline pcl::gpu::DeviceArray_<T>::DeviceArray_(const DeviceArray_& other) : DeviceArray(other) {}
+template<class T> inline pcl::gpu::DeviceArray_<T>& pcl::gpu::DeviceArray_<T>::operator=(const DeviceArray_& other)
+{ DeviceArray::operator=(other); return *this; }
+
+template<class T> inline void pcl::gpu::DeviceArray_<T>::create(size_t size) { DeviceArray::create(size * elem_size); }
+template<class T> inline void pcl::gpu::DeviceArray_<T>::release()  { DeviceArray::release(); }
+template<class T> inline void pcl::gpu::DeviceArray_<T>::upload(const T *host_ptr, size_t size) { DeviceArray::upload(host_ptr, size * elem_size); }
+template<class T> inline void pcl::gpu::DeviceArray_<T>::download(T *host_ptr) const { DeviceArray::download( host_ptr ); }
+template<class T> inline T* pcl::gpu::DeviceArray_<T>::ptr() { return DeviceArray::ptr<T>(); };
+template<class T> inline const T* pcl::gpu::DeviceArray_<T>::ptr() const { return DeviceArray::ptr<T>(); };
+template<class T> inline pcl::gpu::DeviceArray_<T>::operator T*() { return ptr(); }
+template<class T> inline pcl::gpu::DeviceArray_<T>::operator const T*() const { return ptr(); }
+template<class T> inline size_t pcl::gpu::DeviceArray_<T>::size() const { return DeviceArray::sizeBytes() / elem_size; }
+
+template<class T> inline void pcl::gpu::DeviceArray_<T>::upload(const std::vector<T>& data) { upload(&data[0], data.size()); }
+template<class T> inline void pcl::gpu::DeviceArray_<T>::download(std::vector<T>& data) const { data.resize(size()); download(&data[0]); }
+
+
+/////////////////////  Inline implementations of DeviceArray2D_ ////////////////////////////////////////////
+
+template<class T> inline pcl::gpu::DeviceArray2D_<T>::DeviceArray2D_() {}
+template<class T> inline pcl::gpu::DeviceArray2D_<T>::DeviceArray2D_(int rows, int cols) : DeviceArray2D(rows, cols * elem_size) {}
+template<class T> inline pcl::gpu::DeviceArray2D_<T>::DeviceArray2D_(int rows, int cols, void *data, size_t stepBytes) : DeviceArray2D(rows, cols * elem_size, data, stepBytes) {}
+template<class T> inline pcl::gpu::DeviceArray2D_<T>::DeviceArray2D_(const DeviceArray2D_& other) : DeviceArray2D(other) {}
+template<class T> inline pcl::gpu::DeviceArray2D_<T>& pcl::gpu::DeviceArray2D_<T>::operator=(const DeviceArray2D_& other)
+{ DeviceArray2D::operator=(other); return *this; }
+
+template<class T> inline void pcl::gpu::DeviceArray2D_<T>::create(int rows, int cols) { DeviceArray2D::create(rows, cols * elem_size); }
+template<class T> inline void pcl::gpu::DeviceArray2D_<T>::release()  { DeviceArray2D::release(); }
+
+template<class T> inline void pcl::gpu::DeviceArray2D_<T>::upload(const void *host_ptr, size_t host_step, int rows, int cols) 
+{ DeviceArray2D::upload(host_ptr, host_step, rows, cols * elem_size); }
+template<class T> inline void pcl::gpu::DeviceArray2D_<T>::download(void *host_ptr, size_t host_step) const 
+{ DeviceArray2D::download( host_ptr, host_step ); }
+
+template<class T> inline T* pcl::gpu::DeviceArray2D_<T>::ptr(int y) { return DeviceArray2D::ptr<T>(y); };
+template<class T> inline const T* pcl::gpu::DeviceArray2D_<T>::ptr(int y) const { return DeviceArray2D::ptr<T>(y); };
+template<class T> inline pcl::gpu::DeviceArray2D_<T>::operator T*() { return ptr(); }
+template<class T> inline pcl::gpu::DeviceArray2D_<T>::operator const T*() const { return ptr(); }
+template<class T> inline void pcl::gpu::DeviceArray2D_<T>::size(int& rows, int& cols) const 
+{ DeviceArray2D::size(rows, cols); cols /= elem_size; }
+
+template<class T> inline void pcl::gpu::DeviceArray2D_<T>::upload(const std::vector<T>& data, int cols) 
+{ upload(&data[0], cols * elem_size, data.size()/cols, cols); }
+template<class T> inline void pcl::gpu::DeviceArray2D_<T>::download(std::vector<T>& data, int& elem_step) const 
+{ int rows, cols; size(rows, cols); data.resize(cols * rows); download(&data[0], cols * elem_size);  }
+
+
+#endif /* PCL_GPU_DEVICE_ARRAY_ */
---
+++ cuda/gpu_common/src/device_array.cpp
@@ -0,0 +1,239 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+#include "pcl/gpu/common/device_array.hpp"
+#include "pcl/gpu/common/safe_call.hpp"
+
+#include "cuda_runtime_api.h"
+
+//////////////////////////    XADD    ///////////////////////////////
+
+#ifdef __GNUC__
+    
+    #if __GNUC__*10 + __GNUC_MINOR__ >= 42
+
+        #if !defined WIN32 && (defined __i486__ || defined __i586__ || defined __i686__ || defined __MMX__ || defined __SSE__  || defined __ppc__)
+            #define CV_XADD __sync_fetch_and_add
+        #else
+            #include <ext/atomicity.h>
+            #define CV_XADD __gnu_cxx::__exchange_and_add
+        #endif
+    #else
+        #include <bits/atomicity.h>
+        #if __GNUC__*10 + __GNUC_MINOR__ >= 34
+            #define CV_XADD __gnu_cxx::__exchange_and_add
+        #else
+            #define CV_XADD __exchange_and_add
+        #endif
+  #endif
+    
+#elif defined WIN32 || defined _WIN32
+    #include <intrin.h>
+    #define CV_XADD(addr,delta) _InterlockedExchangeAdd((long volatile*)(addr), (delta))
+#else
+
+    template<typename _Tp> static inline _Tp CV_XADD(_Tp* addr, _Tp delta)
+    { int tmp = *addr; *addr += delta; return tmp; }
+    
+#endif
+
+////////////////////////    DeviceArray    /////////////////////////////
+    
+pcl::gpu::DeviceArray::DeviceArray() : data_(0), size_(0), refcount_(0) {}
+pcl::gpu::DeviceArray::DeviceArray(void *ptr, size_t size) : data_(ptr), size_(size), refcount_(0) {}
+pcl::gpu::DeviceArray::DeviceArray(size_t size)  : data_(0), size_(0), refcount_(0)  { create(size_); }
+pcl::gpu::DeviceArray::~DeviceArray() { release(); }
+
+
+pcl::gpu::DeviceArray::DeviceArray(const DeviceArray& other) : data_(other.data_), size_(other.size_), refcount_(other.refcount_)
+{
+    if( refcount_ )
+        CV_XADD(refcount_, 1);
+}
+
+pcl::gpu::DeviceArray& pcl::gpu::DeviceArray::operator = (const pcl::gpu::DeviceArray& other)
+{
+    if( this != &other )
+    {
+        if( other.refcount_ )
+            CV_XADD(other.refcount_, 1);
+        release();
+        
+        data_ = other.data_;
+        size_ = other.size_;        
+        refcount_ = other.refcount_;
+    }
+    return *this;
+}
+
+void pcl::gpu::DeviceArray::create(size_t size)
+{
+    if (size_ == size)
+        return;
+            
+    if( size > 0)
+    {        
+        if( data_ )
+            release();
+
+        size_ = size;
+                        
+        cudaSafeCall( cudaMalloc(&data_, size_) );        
+
+        //refcount_ = (int*)cv::fastMalloc(sizeof(*refcount_));
+        refcount_ = new int;
+        *refcount_ = 1;
+    }
+}
+
+void pcl::gpu::DeviceArray::release()
+{
+    if( refcount_ && CV_XADD(refcount_, -1) == 1 )
+    {
+        //cv::fastFree(refcount_);
+        delete refcount_;
+        cudaSafeCall( cudaFree(data_) );
+    }
+    data_ = 0;
+    size_ = 0;
+    refcount_ = 0;
+}
+
+void pcl::gpu::DeviceArray::upload(const void *host_ptr, size_t size)
+{
+    create(size);
+    cudaSafeCall( cudaMemcpy(data_, host_ptr, size_, cudaMemcpyHostToDevice) );        
+}
+
+void pcl::gpu::DeviceArray::download(void *host_ptr) const
+{    
+    cudaSafeCall( cudaMemcpy(host_ptr, data_, size_, cudaMemcpyDeviceToHost) );
+}
+        
+      
+size_t pcl::gpu::DeviceArray::sizeBytes() const { return size_; }
+
+
+
+////////////////////////    DeviceArray2D    /////////////////////////////
+
+pcl::gpu::DeviceArray2D::DeviceArray2D() : colsBytes_(0), rows_(0), data_(0), step_(0), refcount_(0) {}
+
+pcl::gpu::DeviceArray2D::DeviceArray2D(int rows, int colsBytes) 
+    : colsBytes_(0), rows_(0), data_(0), step_(0), refcount_(0) 
+{ 
+    create(rows, colsBytes); 
+}
+
+pcl::gpu::DeviceArray2D::DeviceArray2D(int rows, int colsBytes, void *data, size_t stepBytes) 
+    : colsBytes_(colsBytes), rows_(rows), data_((char*)data), step_(stepBytes), refcount_(0) {}
+
+pcl::gpu::DeviceArray2D::~DeviceArray2D() { release(); }
+
+
+pcl::gpu::DeviceArray2D::DeviceArray2D(const DeviceArray2D& other) : 
+    colsBytes_(other.colsBytes_), rows_(other.rows_), data_(other.data_), step_(other.step_), refcount_(other.refcount_)
+{
+    if( refcount_ )
+        CV_XADD(refcount_, 1);
+}
+
+pcl::gpu::DeviceArray2D& pcl::gpu::DeviceArray2D::operator = (const pcl::gpu::DeviceArray2D& other)
+{
+    if( this != &other )
+    {
+        if( other.refcount_ )
+            CV_XADD(other.refcount_, 1);
+        release();
+        
+        colsBytes_ = other.colsBytes_;
+        rows_ = other.rows_;
+        data_ = other.data_;
+        step_ = other.step_;
+                
+        refcount_ = other.refcount_;
+    }
+    return *this;
+}
+
+void pcl::gpu::DeviceArray2D::create(int rows, int colsBytes)
+{
+    if (colsBytes_ == colsBytes && rows_ == rows)
+        return;
+            
+    if( rows > 0 && colsBytes > 0)
+    {        
+        if( data_ )
+            release();
+              
+        colsBytes = colsBytes;
+        rows_ = rows;
+                        
+        cudaSafeCall( cudaMallocPitch( (void**)&data_, &step_, colsBytes, rows) );        
+
+        //refcount_ = (int*)cv::fastMalloc(sizeof(*refcount_));
+        refcount_ = new int;
+        *refcount_ = 1;
+    }
+}
+
+void pcl::gpu::DeviceArray2D::release()
+{
+    if( refcount_ && CV_XADD(refcount_, -1) == 1 )
+    {
+        //cv::fastFree(refcount_);
+        delete refcount_;
+        cudaSafeCall( cudaFree(data_) );
+    }
+
+    colsBytes_ = 0;
+    rows_ = 0;    
+    data_ = 0;    
+    step_ = 0;
+    refcount_ = 0;
+}
+
+void pcl::gpu::DeviceArray2D::upload(const void *host_ptr, size_t host_step, int rows, int colsBytes)
+{
+    create(rows, colsBytes);
+    cudaSafeCall( cudaMemcpy2D(data_, step_, host_ptr, host_step, colsBytes_, rows_, cudaMemcpyHostToDevice) );        
+}
+
+void pcl::gpu::DeviceArray2D::download(void *host_ptr, size_t host_step) const
+{    
+    cudaSafeCall( cudaMemcpy2D(host_ptr, host_step, data_, step_, colsBytes_, rows_, cudaMemcpyDeviceToHost) );
+}
+        
+      
+void pcl::gpu::DeviceArray2D::size(int& rows, int& colsBytes) const { rows = rows_; colsBytes = colsBytes_; }
