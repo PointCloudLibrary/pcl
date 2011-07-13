1310c06589c9ea73b604fb40fd81052319d43af5
Gabe O'Leary <oleary.gabe@gmail.com>
2011-07-13 19:21:18 +0000

    changed name of file back to original
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1738 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- doc/tutorials/content/sources/pcl_visualizer/pcl_visualizer_demo.cpp
+++ doc/tutorials/content/sources/pcl_visualizer/pcl_visualizer_demo.cpp
@@ -14,7 +14,7 @@
 // --------------
 // -----Help-----
 // --------------
-void 
+void
 printUsage (const char* progName)
 {
   std::cout << "\n\nUsage: "<<progName<<" [options]\n\n"
@@ -38,7 +38,7 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis (pcl::PointCloud<
   // --------------------------------------------
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
   viewer->setBackgroundColor (0, 0, 0);
-  viewer->addPointCloud (cloud, "sample cloud");
+  viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
   viewer->addCoordinateSystem (1.0);
   viewer->initCameraParameters ();
@@ -176,7 +176,7 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> viewportsVis (
 // --------------
 // -----Main-----
 // --------------
-int 
+int
 main (int argc, char** argv)
 {
   // --------------------------------------
