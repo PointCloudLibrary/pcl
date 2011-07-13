fd4f19aaaade1d93d6ba4f8c1bf89f087357559d
Alexandru Eugen Ichim <alex.e.ichim@gmail.com>
2011-07-13 22:04:00 +0000

    too many undos in the editor - build now works :-)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1749 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- features/src/pfh.cpp
+++ features/src/pfh.cpp
@@ -50,10 +50,10 @@ pcl::computePairFeatures (const Eigen::Vector4f &p1, const Eigen::Vector4f &n1,
   dp2p1[3] = 0.0f;
   f4 = dp2p1.norm ();
 
-  if (f4 == 0)
+  if (f4 == 0.0f)
   {
     PCL_ERROR ("Euclidean distance between points is 0!\n");
-    f1 = f2 = f3 = f4 = 0;
+    f1 = f2 = f3 = f4 = 0.0f;
     return (false);
   }
 
@@ -81,10 +81,10 @@ pcl::computePairFeatures (const Eigen::Vector4f &p1, const Eigen::Vector4f &n1,
   Eigen::Vector4f v = dp2p1.cross3 (n1_copy);
   v[3] = 0.0f;
   float v_norm = v.norm ();
-  if (v_norm == 0)
+  if (v_norm == 0.0f)
   {
     PCL_ERROR ("Norm of Delta x U is 0!\n");
-    f1 = f2 = f3 = f4 = 0;
+    f1 = f2 = f3 = f4 = 0.0f;
     return (false);
   }
   // Normalize v
@@ -97,7 +97,7 @@ pcl::computePairFeatures (const Eigen::Vector4f &p1, const Eigen::Vector4f &n1,
   f2 = v.dot (n2_copy);
   w[3] = 0.0f;
   // Compute f1 = arctan (w * n2, u * n2) i.e. angle of n2 in the x=u, y=w coordinate system
-  f1 = atan2f (w.dot (n2_copy), u.dot (n2_copy)); // @todo optimize this
+  f1 = atan2f (w.dot (n2_copy), n1_copy.dot (n2_copy)); // @todo optimize this
 
   return (true);
 }
