2e3a6dd0e2842d9be8c97601f6c73c04a606cfc8
Zoltan-Csaba Marton <marton@cs.tum.edu>
2011-07-15 13:16:23 +0000

    added utility function that maps an Eigen::MatrixXf onto all or some of the PointCloud dimensions + unit tests
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.x@1803 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/test/test_common.cpp
+++ common/test/test_common.cpp
@@ -163,6 +163,28 @@ TEST (PCL, PointCloud)
   
   cloud.height = 1;
   EXPECT_EQ (cloud.isOrganized (), false);
+  
+  cloud.width = 10;
+  for (uint32_t i = 0; i < cloud.width*cloud.height; ++i)
+    cloud.points.push_back (PointXYZ (3*i+0,3*i+1,3*i+2));
+    
+  Eigen::MatrixXf mat_xyz1 = cloud.getMatrixXfMap();
+  EXPECT_EQ (mat_xyz1.rows (), 4);
+  EXPECT_EQ (mat_xyz1.cols (), cloud.width);
+  EXPECT_EQ (mat_xyz1 (0,0), 0);
+  EXPECT_EQ (mat_xyz1 (2,cloud.width-1), 3*cloud.width-1);
+
+  Eigen::MatrixXf mat_xyz = cloud.getMatrixXfMap(3,4,0);
+  EXPECT_EQ (mat_xyz.rows (), 3);
+  EXPECT_EQ (mat_xyz.cols (), cloud.width);
+  EXPECT_EQ (mat_xyz (0,0), 0);
+  EXPECT_EQ (mat_xyz (2,cloud.width-1), 3*cloud.width-1);
+
+  Eigen::MatrixXf mat_yz = cloud.getMatrixXfMap(2,4,1);
+  EXPECT_EQ (mat_yz.rows (), 2);
+  EXPECT_EQ (mat_yz.cols (), cloud.width);
+  EXPECT_EQ (mat_yz (0,0), 1);
+  EXPECT_EQ (mat_yz (1,cloud.width-1), 3*cloud.width-1);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
