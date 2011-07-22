ec29783ad48f666df9c3f499c84d1d04772963bb
Bastian Steder <steder@informatik.uni-freiburg.de>
2011-07-22 15:37:43 +0000

    removed using namespace
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1861 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- doc/tutorials/content/sources/openni_narf_keypoint_extraction/openni_narf_keypoint_extraction.cpp
+++ doc/tutorials/content/sources/openni_narf_keypoint_extraction/openni_narf_keypoint_extraction.cpp
@@ -12,9 +12,6 @@ using namespace std;
 #include "pcl/keypoints/narf_keypoint.h"
 #include "pcl/console/parse.h"
 
-using namespace pcl;
-using namespace pcl::visualization;
-
 std::string device_id = "#1";
 
 float angular_resolution = 0.5f;
@@ -54,8 +51,9 @@ printUsage (const char* progName)
        << "Options:\n"
        << "-------------------------------------------\n"
        << "-d <device_id>  set the device id (default \""<<device_id<<"\")\n"
-       << "-r <float>      angular resolution in degrees (default "<<rad2deg (angular_resolution)<<")\n"
-       << "-s <float>      support size for the interest points (diameter of the used sphere in meters) (default "<<support_size<<")\n"
+       << "-r <float>      angular resolution in degrees (default "<<pcl::rad2deg (angular_resolution)<<")\n"
+       << "-s <float>      support size for the interest points (diameter of the used sphere in meters)"
+       <<                 " (default "<<support_size<<")\n"
        << "-h              this help\n"
        << "\n\n";
 }
@@ -76,11 +74,11 @@ int main (int argc, char** argv)
     cout << "Setting angular resolution to "<<angular_resolution<<"deg.\n";
   if (pcl::console::parse (argc, argv, "-s", support_size) >= 0)
     cout << "Setting support size to "<<angular_resolution<<"m.\n";
-  angular_resolution = deg2rad (angular_resolution);
+  angular_resolution = pcl::deg2rad (angular_resolution);
   
-  RangeImageVisualizer range_image_widget ("Range Image");
+  pcl::visualization::RangeImageVisualizer range_image_widget ("Range Image");
   
-  PCLVisualizer viewer ("3D Viewer");
+  pcl::visualization::PCLVisualizer viewer ("3D Viewer");
   viewer.addCoordinateSystem (1.0f);
   viewer.setBackgroundColor (1, 1, 1);
   
@@ -94,7 +92,7 @@ int main (int argc, char** argv)
   viewer.camera_.view[0] = 0.0;
   viewer.camera_.view[1] = -1.0;
   viewer.camera_.view[2] = 0.0;
-  viewer.updateCamera();
+  viewer.updateCamera ();
   
   openni_wrapper::OpenNIDriver& driver = openni_wrapper::OpenNIDriver::getInstance ();
   if (driver.getNumberDevices () > 0)
@@ -124,23 +122,23 @@ int main (int argc, char** argv)
   interface->start ();
   cout << "Done\n";
   
-  boost::shared_ptr<RangeImagePlanar> range_image_planar_ptr (new RangeImagePlanar);
-  RangeImagePlanar& range_image_planar = *range_image_planar_ptr;
+  boost::shared_ptr<pcl::RangeImagePlanar> range_image_planar_ptr (new pcl::RangeImagePlanar);
+  pcl::RangeImagePlanar& range_image_planar = *range_image_planar_ptr;
   
-  RangeImageBorderExtractor range_image_border_extractor;
-  NarfKeypoint narf_keypoint_detector;
-  narf_keypoint_detector.setRangeImageBorderExtractor(&range_image_border_extractor);
-  narf_keypoint_detector.getParameters().support_size = support_size;
-  //narf_keypoint_detector.getParameters().add_points_on_straight_edges = true;
-  //narf_keypoint_detector.getParameters().distance_for_additional_points = 0.5;
+  pcl::RangeImageBorderExtractor range_image_border_extractor;
+  pcl::NarfKeypoint narf_keypoint_detector;
+  narf_keypoint_detector.setRangeImageBorderExtractor (&range_image_border_extractor);
+  narf_keypoint_detector.getParameters ().support_size = support_size;
+  //narf_keypoint_detector.getParameloadters ().add_points_on_straight_edges = true;
+  //narf_keypoint_detector.getParameters ().distance_for_additional_points = 0.5;
   
-  PointCloud<PointXYZ>::Ptr keypoints_cloud_ptr(new PointCloud<PointXYZ>);
-  PointCloud<PointXYZ>& keypoints_cloud = *keypoints_cloud_ptr;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr keypoints_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>& keypoints_cloud = *keypoints_cloud_ptr;
   
   while (true)//!viewer.wasStopped () && range_image_widget.isShown ())
   {
     viewer.spinOnce (10);
-    ImageWidgetWX::spinOnce ();  // process GUI events
+    pcl::visualization::ImageWidgetWX::spinOnce ();  // process GUI events
     usleep (10000);
     
     bool got_new_range_image = false;
@@ -150,16 +148,16 @@ int main (int argc, char** argv)
       
       //unsigned long time_stamp = depth_image_ptr->getTimeStamp ();
       //int frame_id = depth_image_ptr->getFrameID ();
-      const unsigned short* depth_map = depth_image_ptr->getDepthMetaData().Data();
+      const unsigned short* depth_map = depth_image_ptr->getDepthMetaData ().Data ();
       int width = depth_image_ptr->getWidth (), height = depth_image_ptr->getHeight ();
       float center_x = width/2, center_y = height/2;
-      float focal_length_x = depth_image_ptr->getFocalLength(), focal_length_y = focal_length_x;
-      float original_angular_resolution = asinf (0.5f*float(width)/float(focal_length_x)) / (0.5f*float(width));
+      float focal_length_x = depth_image_ptr->getFocalLength (), focal_length_y = focal_length_x;
+      float original_angular_resolution = asinf (0.5f*float (width)/float (focal_length_x)) / (0.5f*float (width));
       float desired_angular_resolution = angular_resolution;
       range_image_planar.setDepthImage (depth_map, width, height, center_x, center_y,
                                         focal_length_x, focal_length_y, desired_angular_resolution);
       depth_image_mutex.unlock ();
-      got_new_range_image = !range_image_planar.points.empty();
+      got_new_range_image = !range_image_planar.points.empty ();
     }
     
     if (!got_new_range_image)
@@ -169,34 +167,37 @@ int main (int argc, char** argv)
     // -----Extract NARF keypoints-----
     // --------------------------------
     if (set_unseen_to_max_range)
-      range_image_planar.setUnseenToMaxRange();
-    narf_keypoint_detector.setRangeImage(&range_image_planar);
-    PointCloud<int> keypoint_indices;
-    narf_keypoint_detector.compute(keypoint_indices);
-    std::cout << "Found "<<keypoint_indices.points.size()<<" key points.\n";
+      range_image_planar.setUnseenToMaxRange ();
+    narf_keypoint_detector.setRangeImage (&range_image_planar);
+    pcl::PointCloud<int> keypoint_indices;
+    narf_keypoint_detector.compute (keypoint_indices);
+    std::cout << "Found "<<keypoint_indices.points.size ()<<" key points.\n";
     
     // ----------------------------------------------
     // -----Show keypoints in range image widget-----
     // ----------------------------------------------
     range_image_widget.setRangeImage (range_image_planar, 0.0f, 10.0f);
-    for (size_t i=0; i<keypoint_indices.points.size(); ++i)
-      range_image_widget.markPoint(keypoint_indices.points[i]%range_image_planar.width,
+    for (size_t i=0; i<keypoint_indices.points.size (); ++i)
+      range_image_widget.markPoint (keypoint_indices.points[i]%range_image_planar.width,
                                    keypoint_indices.points[i]/range_image_planar.width);
     
     // -------------------------------------
     // -----Show keypoints in 3D viewer-----
     // -------------------------------------
-    PointCloudColorHandlerCustom<pcl::PointWithRange> color_handler_cloud (range_image_planar_ptr, 0, 0, 0);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointWithRange> color_handler_cloud
+      (range_image_planar_ptr, 0, 0, 0);
     if (!viewer.updatePointCloud<pcl::PointWithRange> (range_image_planar_ptr, color_handler_cloud, "range image"))
       viewer.addPointCloud<pcl::PointWithRange> (range_image_planar_ptr, color_handler_cloud, "range image");
     
-    keypoints_cloud.points.resize(keypoint_indices.points.size());
-    for (size_t i=0; i<keypoint_indices.points.size(); ++i)
-      keypoints_cloud.points[i].getVector3fMap() = range_image_planar.points[keypoint_indices.points[i]].getVector3fMap();
-    PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler_keypoints (keypoints_cloud_ptr, 0, 255, 0);
-    if (!viewer.updatePointCloud(keypoints_cloud_ptr, color_handler_keypoints, "keypoints"))
-      viewer.addPointCloud(keypoints_cloud_ptr, color_handler_keypoints, "keypoints");
-    viewer.setPointCloudRenderingProperties(PCL_VISUALIZER_POINT_SIZE, 7, "keypoints");
+    keypoints_cloud.points.resize (keypoint_indices.points.size ());
+    for (size_t i=0; i<keypoint_indices.points.size (); ++i)
+      keypoints_cloud.points[i].getVector3fMap () =
+        range_image_planar.points[keypoint_indices.points[i]].getVector3fMap ();
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler_keypoints
+      (keypoints_cloud_ptr, 0, 255, 0);
+    if (!viewer.updatePointCloud (keypoints_cloud_ptr, color_handler_keypoints, "keypoints"))
+      viewer.addPointCloud (keypoints_cloud_ptr, color_handler_keypoints, "keypoints");
+    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 7, "keypoints");
   }
 
   interface->stop ();
