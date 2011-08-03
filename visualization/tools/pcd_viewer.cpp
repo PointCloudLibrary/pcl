33901686c4a393b6a690ae0db847898c3db422de
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-08-03 18:32:25 +0000

    make pcd_viewer reset the camera viewpoint when no camera given (fixing #303)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.x@1915 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- visualization/tools/pcd_viewer.cpp
+++ visualization/tools/pcd_viewer.cpp
@@ -412,6 +412,10 @@ main (int argc, char** argv)
     // Change the cloud rendered opacity
     if (opaque.size () > 0)
       p->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_OPACITY, opaque.at (i), cloud_name.str ());
+
+    // Reset camera viewpoint to center of cloud if camera parameters were not passed manually and this is the first loaded cloud
+    if (i == 0 && !p->cameraParamsSet ())
+      p->resetCameraViewpoint (cloud_name.str ());
   }
 
   if (p)
