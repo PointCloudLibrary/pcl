4ccd7b18b694b56f20df06a83e7063d22d661a72
Nizar Sallem <nksallem@laas.fr>
2011-08-10 12:29:43 +0000

    Add iostreams as required component
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@1994 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_find_boost.cmake
+++ cmake/pcl_find_boost.cmake
@@ -10,7 +10,7 @@ else(${CMAKE_VERSION} VERSION_LESS 2.8.5)
   SET(Boost_ADDITIONAL_VERSIONS "1.47" "1.47.0")
 endif(${CMAKE_VERSION} VERSION_LESS 2.8.5)
 
-find_package(Boost 1.40.0 REQUIRED COMPONENTS system filesystem thread date_time)
+find_package(Boost 1.40.0 REQUIRED COMPONENTS system filesystem thread date_time iostreams)
 
 if(Boost_FOUND)
   # Obtain diagnostic information about Boost's automatic linking outputted 
