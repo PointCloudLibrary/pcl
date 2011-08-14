979c2966610747be88a94030391c687d4877ff8d
Jochen Sprickerhof <jochen@sprickerhof.de>
2011-08-14 16:02:24 +0000

    adding template to projectPoint to make Visual Studio happy
    thanks Teserakt for finding
    
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2065 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- tools/plane_projection.cpp
+++ tools/plane_projection.cpp
@@ -102,7 +102,7 @@ project (const sensor_msgs::PointCloud2::ConstPtr &input, sensor_msgs::PointClou
   for(size_t i = 0; i < xyz->points.size(); ++i)
   {
     pcl::PointXYZ projection;
-    pcl::projectPoint(xyz->points[i], coeffs, projection);
+    pcl::projectPoint<PointXYZ> (xyz->points[i], coeffs, projection);
     projected_cloud_pcl->points.push_back(projection);
   }
 
