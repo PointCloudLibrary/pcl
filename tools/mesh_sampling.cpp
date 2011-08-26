6fae15da93f15ee8fc9005e0e6a3ac97184c7cea
Aitor Aldoma <aldoma.aitor@gmail.com>
2011-08-26 09:52:16 +0000

    Added mesh_sampling tool. Performs uniform_sampling on a mesh.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2230 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- tools/mesh_sampling.cpp
+++ tools/mesh_sampling.cpp
@@ -40,17 +40,100 @@
 #include <pcl/registration/transforms.h>
 #include <vtkPLYReader.h>
 #include <vtkOBJReader.h>
+#include <vtkTriangle.h>
+#include <vtkTriangleFilter.h>
 #include <vtkPolyDataMapper.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/console/print.h>
 #include <pcl/console/parse.h>
 
+inline double
+uniform_deviate (int seed)
+{
+  double ran = seed * (1.0 / (RAND_MAX + 1.0));
+  return ran;
+}
+
+inline void
+randomPointTriangle (float a1, float a2, float a3, float b1, float b2, float b3, float c1, float c2, float c3,
+                     Eigen::Vector4f& p)
+{
+  float r1 = uniform_deviate (rand ());
+  float r2 = uniform_deviate (rand ());
+  float r1sqr = sqrt (r1);
+  float OneMinR1Sqr = (1 - r1sqr);
+  float OneMinR2 = (1 - r2);
+  a1 *= OneMinR1Sqr;
+  a2 *= OneMinR1Sqr;
+  a3 *= OneMinR1Sqr;
+  b1 *= OneMinR2;
+  b2 *= OneMinR2;
+  b3 *= OneMinR2;
+  c1 = r1sqr * (r2 * c1 + b1) + a1;
+  c2 = r1sqr * (r2 * c2 + b2) + a2;
+  c3 = r1sqr * (r2 * c3 + b3) + a3;
+  p[0] = c1;
+  p[1] = c2;
+  p[2] = c3;
+  p[3] = 0;
+}
+
+inline void
+randPSurface (vtkPolyData * polydata, std::vector<double> * cumulativeAreas, double totalArea, Eigen::Vector4f& p)
+{
+  float r = uniform_deviate (rand ()) * totalArea;
+
+  std::vector<double>::iterator low = lower_bound (cumulativeAreas->begin (), cumulativeAreas->end (), r);
+  vtkIdType el = (vtkIdType)(low - cumulativeAreas->begin ());
+
+  double A[3], B[3], C[3];
+  vtkIdType npts = 0;
+  vtkIdType *ptIds = NULL;
+  polydata->GetCellPoints (el, npts, ptIds);
+  polydata->GetPoint (ptIds[0], A);
+  polydata->GetPoint (ptIds[1], B);
+  polydata->GetPoint (ptIds[2], C);
+  randomPointTriangle (A[0], A[1], A[2], B[0], B[1], B[2], C[0], C[1], C[2], p);
+}
+
+void
+uniform_sampling (vtkSmartPointer<vtkPolyData> polydata, size_t n_samples, pcl::PointCloud<pcl::PointXYZ> & cloud_out)
+{
+  polydata->BuildCells ();
+  vtkSmartPointer<vtkCellArray> cells = polydata->GetPolys ();
+
+  double p1[3], p2[3], p3[3], totalArea = 0;
+  std::vector<double> cumulativeAreas (cells->GetNumberOfCells (), 0);
+  size_t i = 0;
+  vtkIdType npts = 0, *ptIds = NULL;
+  for (cells->InitTraversal (); cells->GetNextCell (npts, ptIds); i++)
+  {
+    polydata->GetPoint (ptIds[0], p1);
+    polydata->GetPoint (ptIds[1], p2);
+    polydata->GetPoint (ptIds[2], p3);
+    totalArea += vtkTriangle::TriangleArea (p1, p2, p3);
+    cumulativeAreas[i] = totalArea;
+  }
+
+  cloud_out.points.resize (n_samples);
+  cloud_out.width = n_samples;
+  cloud_out.height = 1;
+
+  for (i = 0; i < n_samples; i++)
+  {
+    Eigen::Vector4f p;
+    randPSurface (polydata, &cumulativeAreas, totalArea, p);
+    cloud_out.points[i].x = p[0];
+    cloud_out.points[i].y = p[1];
+    cloud_out.points[i].z = p[2];
+  }
+}
+
 using namespace pcl;
 using namespace pcl::io;
 using namespace pcl::console;
 
-int default_tesselated_sphere_level = 2;
-int default_resolution = 100;
+int default_number_samples = 100000;
 float default_leaf_size = 0.01;
 
 void
@@ -58,12 +141,9 @@ printHelp (int argc, char **argv)
 {
   print_error ("Syntax is: %s input.{ply,obj} output.pcd <options>\n", argv[0]);
   print_info ("  where options are:\n");
-  print_info ("                     -level X      = tesselated sphere level (default: ");
-  print_value ("%d", default_tesselated_sphere_level);
+  print_info ("                     -n_samples X      = number of samples (default: ");
+  print_value ("%d", default_number_samples);
   print_info (")\n");
-  print_info ("                     -resolution X = the sphere resolution in angle increments (default: ");
-  print_value ("%d", default_resolution);
-  print_info (" deg)\n");
   print_info (
               "                     -leaf_size X  = the XYZ leaf size for the VoxelGrid -- for data reduction (default: ");
   print_value ("%f", default_leaf_size);
@@ -84,10 +164,8 @@ main (int argc, char **argv)
   }
 
   // Parse command line arguments
-  int tesselated_sphere_level = default_tesselated_sphere_level;
-  parse_argument (argc, argv, "-level", tesselated_sphere_level);
-  int resolution = default_resolution;
-  parse_argument (argc, argv, "-resolution", resolution);
+  int SAMPLE_POINTS_ = default_number_samples;
+  parse_argument (argc, argv, "-n_samples", SAMPLE_POINTS_);
   double leaf_size = default_leaf_size;
   parse_argument (argc, argv, "-leaf_size", leaf_size);
 
@@ -111,8 +189,6 @@ main (int argc, char **argv)
   {
     vtkSmartPointer<vtkPLYReader> readerQuery = vtkSmartPointer<vtkPLYReader>::New ();
     readerQuery->SetFileName (argv[ply_file_indices[0]]);
-    polydata1 = readerQuery->GetOutput ();
-    polydata1->Update ();
   }
   else if (obj_file_indices.size () == 1)
   {
@@ -122,67 +198,50 @@ main (int argc, char **argv)
     polydata1->Update ();
   }
 
+  //make sure that the polygons are triangles!
+  vtkSmartPointer<vtkTriangleFilter> triangleFilter = vtkSmartPointer<vtkTriangleFilter>::New ();
+  triangleFilter->SetInput (polydata1);
+  triangleFilter->Update ();
+
+  vtkSmartPointer<vtkPolyDataMapper> triangleMapper = vtkSmartPointer<vtkPolyDataMapper>::New ();
+  triangleMapper->SetInputConnection (triangleFilter->GetOutputPort ());
+  triangleMapper->Update();
+  polydata1 = triangleMapper->GetInput();
+  polydata1->Update ();
+
   bool INTER_VIS = false;
   bool VIS = true;
 
-  visualization::PCLVisualizer vis;
-  vis.addModelFromPolyData (polydata1, "mesh1", 0);
-  vis.setRepresentationToSurfaceForAllActors ();
-
-  std::vector<PointCloud<PointXYZ> , Eigen::aligned_allocator<PointCloud<PointXYZ> > > views_xyz;
-  std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > poses;
-  std::vector<float> enthropies;
-  vis.renderViewTesselatedSphere (resolution, resolution, views_xyz, poses, enthropies, tesselated_sphere_level);
-
-  //take views and fuse them together
-  std::vector<PointCloud<PointXYZ>::Ptr> aligned_clouds;
-
-  for (size_t i = 0; i < views_xyz.size (); i++)
+  if (INTER_VIS)
   {
-    PointCloud<PointXYZ>::Ptr cloud (new PointCloud<PointXYZ> ());
-    Eigen::Matrix4f pose_inverse;
-    pose_inverse = poses[i].inverse ();
-    transformPointCloud (views_xyz[i], *cloud, pose_inverse);
-    aligned_clouds.push_back (cloud);
+    visualization::PCLVisualizer vis;
+    vis.addModelFromPolyData (polydata1, "mesh1", 0);
+    vis.setRepresentationToSurfaceForAllActors ();
+    vis.spin();
   }
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_1 (new pcl::PointCloud<pcl::PointXYZ>);
+  uniform_sampling (polydata1, SAMPLE_POINTS_, *cloud_1);
 
   if (INTER_VIS)
   {
-    visualization::PCLVisualizer vis2 ("visualize");
-
-    for (size_t i = 0; i < aligned_clouds.size (); i++)
-    {
-      std::stringstream name;
-      name << "cloud_" << i;
-      vis2.addPointCloud (aligned_clouds[i], name.str ());
-      vis2.spin ();
-    }
-  }
-
-  // Fuse clouds
-  PointCloud<PointXYZ>::Ptr big_boy (new PointCloud<PointXYZ> ());
-  for (size_t i = 0; i < aligned_clouds.size (); i++)
-    *big_boy += *aligned_clouds[i];
-
-  if (VIS)
-  {
-    visualization::PCLVisualizer vis2 ("visualize");
-    vis2.addPointCloud (big_boy);
-    vis2.spin ();
+    visualization::PCLVisualizer vis_sampled;
+    vis_sampled.addPointCloud (cloud_1);
+    vis_sampled.spin ();
   }
 
   // Voxelgrid
   VoxelGrid<PointXYZ> grid_;
-  grid_.setInputCloud (big_boy);
+  grid_.setInputCloud (cloud_1);
   grid_.setLeafSize (leaf_size, leaf_size, leaf_size);
-  grid_.filter (*big_boy);
+  grid_.filter (*cloud_1);
 
   if (VIS)
   {
-    visualization::PCLVisualizer vis3 ("visualize");
-    vis3.addPointCloud (big_boy);
+    visualization::PCLVisualizer vis3 ("VOXELIZED SAMPLES CLOUD");
+    vis3.addPointCloud (cloud_1);
     vis3.spin ();
   }
 
-  savePCDFileASCII (argv[pcd_file_indices[0]], *big_boy);
+  savePCDFileASCII (argv[pcd_file_indices[0]], *cloud_1);
 }
