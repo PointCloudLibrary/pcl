f919ed203c6f77796c255da292ff2c7553b85108
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-08-30 05:05:23 +0000

    added a filter written by Luca, per #260
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2294 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ filters/include/pcl/filters/bilateral.h
@@ -0,0 +1,148 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef PCL_FILTERS_BILATERAL_H_
+#define PCL_FILTERS_BILATERAL_H_
+
+#include <pcl/filters/filter.h>
+#include <pcl/kdtree/kdtree.h>
+
+namespace pcl
+{
+  /** \brief A bilateral filter implementation for point cloud data. Uses the intensity data channel.
+    * \note For more information please see 
+    * <b>C. Tomasi and R. Manduchi. Bilateral Filtering for Gray and Color Images.
+    * In Proceedings of the IEEE International Conference on Computer Vision,
+    * 1998.</b>
+    * \author Luca Penasa
+    */
+  template<typename PointT>
+  class BilateralFilter : public Filter<PointT>
+  {
+    using Filter<PointT>::input_;
+    typedef typename Filter<PointT>::PointCloud PointCloud;
+    typedef typename pcl::KdTree<PointT>::Ptr KdTreePtr;
+
+    public:
+      /** \brief Constructor. 
+        * Sets \ref sigma_s_ to 0 and \ref sigma_r_ to MAXDBL
+        */
+      BilateralFilter () : sigma_s_ (0), 
+                           sigma_r_ (std::numeric_limits<double>::max ())
+      {
+      }
+
+
+      /** \brief Filter the input data and store the results into output
+        * \param[out] output the resultant point cloud message
+        */
+      void
+      applyFilter (PointCloud &output);
+
+      /** \brief Compute the intensity average for a single point
+        * \param[in] pid the point index to compute the weight for
+        * \param[in] indices the set of nearest neighor indices 
+        * \param[in] distances the set of nearest neighbor distances
+        * \return the intensity average at a given point index
+        */
+      double 
+      computePointWeight (const int pid, const std::vector<int> &indices, const std::vector<float> &distances);
+
+      /** \brief Set the half size of the Gaussian bilateral filter window.
+        * \param[in] sigma_s the half size of the Gaussian bilateral filter window to use
+        */
+      inline void 
+      setHalfSize (const double sigma_s)
+      {
+        sigma_s_ = sigma_s;
+      }
+
+      /** \brief Get the half size of the Gaussian bilateral filter window as set by the user. */
+      double 
+      getHalfSize ()
+      {
+        return (sigma_s_);
+      }
+
+      /** \brief Set the standard deviation parameter
+        * \param[in] sigma_r the new standard deviation parameter
+        */
+      void
+      setStdDev (const double sigma_r)
+      {
+        sigma_r_ = sigma_r;
+      }
+
+      /** \brief Get the value of the current standard deviation parameter of the bilateral filter. */
+      double 
+      getStdDev ()
+      {
+        return (sigma_r_);
+      }
+
+      /** \brief Provide a pointer to the search object.
+        * \param[in] tree a pointer to the spatial search object.
+        */
+      void
+      setSearchMethod (const KdTreePtr &tree)
+      {
+        tree_ = tree;
+      }
+
+    private:
+
+      /** \brief The bilateral filter Gaussian distance kernel.
+        * \param[in] x the spatial distance (distance or intensity)
+        * \param[in] sigma standard deviation
+        */
+      inline double
+      kernel (double x, double sigma)
+      {
+        return (exp (- (x*x)/(2*sigma*sigma)));
+      }
+
+      /** \brief The half size of the Gaussian bilateral filter window (e.g., spatial extents in Euclidean). */
+      double sigma_s_;
+      /** \brief The standard deviation of the bilateral filter (e.g., standard deviation in intensity). */
+      double sigma_r_;
+
+      /** \brief A pointer to the spatial search object. */
+      KdTreePtr tree_;
+  };
+}
+
+#endif // PCL_FILTERS_BILATERAL_H_
---
+++ filters/include/pcl/filters/impl/bilateral.hpp
@@ -0,0 +1,113 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef PCL_FILTERS_BILATERAL_IMPL_H_
+#define PCL_FILTERS_BILATERAL_IMPL_H_
+
+#include <pcl/filters/bilateral.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/kdtree/organized_data.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointT> double
+pcl::BilateralFilter<PointT>::computePointWeight (const int pid, 
+                                                  const std::vector<int> &indices,
+                                                  const std::vector<float> &distances)
+{
+  double BF = 0, W = 0;
+
+  // For each neighbor
+  for (size_t n_id = 0; n_id < indices.size (); ++n_id)
+  {
+    double id = indices[n_id];
+    // Compute the difference in intensity
+    double intensity_dist = abs (input_->points[pid].intensity - input_->points[id].intensity);
+
+    // Compute the Gaussian intensity weights both in Euclidean and in intensity space
+    double dist = std::sqrt (distances[n_id]);
+    double weight = kernel (dist, sigma_s_) * kernel (intensity_dist, sigma_r_);
+
+    // Calculate the bilateral filter response
+    BF += weight * input_->points[id].intensity;
+    W += weight;
+  }
+  return (BF / W);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointT> void
+pcl::BilateralFilter<PointT>::applyFilter (PointCloud &output)
+{
+  // Check if sigma_s has been given by the user
+  if (sigma_s_ == 0)
+  {
+    PCL_ERROR ("[pcl::BilateralFilter::applyFilter] Need a sigma_s value given before continuing.\n");
+    return;
+  }
+  // In case a search method has not been given, initialize it using some defaults
+  if (!tree_)
+  {
+    // For organized datasets, use an OrganizedDataIndex
+    if (input_->isOrganized ())
+      tree_.reset (new pcl::OrganizedDataIndex<PointT> ());
+    // For unorganized data, use a FLANN kdtree
+    else
+      tree_.reset (new pcl::KdTreeFLANN<PointT> (false));
+  }
+  tree_->setInputCloud (input_);
+
+  std::vector<int> k_indices;
+  std::vector<float> k_distances;
+
+  // Copy the input data into the output
+  output = *input_;
+
+  // For all the indices given (equal to the entire cloud if none given)
+  for (size_t i = 0; i < indices->size (); ++i)
+  {
+    // Perform a radius search to find the nearest neighbors
+    tree_->radiusSearch ((*indices)[i], sigma_s_ * 2, k_indices, k_distances);
+
+    // Overwrite the intensity value with the computed average
+    output.points[(*indices)[i]].intensity = computePointWeight ((*indices)[i], k_indices, k_distances);
+  }
+}
+ 
+#define PCL_INSTANTIATE_BilateralFilter(T) template class PCL_EXPORTS pcl::BilateralFilter<T>;
+
+#endif // PCL_FILTERS_BILATERAL_H_
+
