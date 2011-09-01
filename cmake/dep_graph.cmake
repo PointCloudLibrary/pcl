a4b9e662797787798cb1d5888cc5dd0443363cb2
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-09-01 21:16:30 +0000

    disabled test box
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2370 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/dep_graph.cmake
+++ cmake/dep_graph.cmake
@@ -22,4 +22,9 @@ function(MAKE_DEP_GRAPH)
         endforeach(_dep)
       endif()
     endforeach(_ss)
+
+    #file(APPEND ${_dot_file}
+    #    "  \"test\" [style=\"filled\" fillcolor=\"#A3A27C\" shape=\"box\"];\n ")
+    file(APPEND ${_dot_file} "}\n")
 endfunction(MAKE_DEP_GRAPH)
+
