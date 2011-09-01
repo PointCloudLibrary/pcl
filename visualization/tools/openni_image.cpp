a04cc52b896c85940ea482b0e304458f9ed610fd
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-09-01 18:30:53 +0000

    * moved octree_viewer from visualization/tools/ to tools/ * removed visualization/tools/pcl_stream_compression as it was a duplicate of apps/openni_stream_compression * moved openni_change_viewer from visualization/tools to apps * removed libpcl_octree dependency on libpcl_visualization
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2346 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- visualization/tools/openni_image.cpp
+++ visualization/tools/openni_image.cpp
@@ -129,19 +129,15 @@ class SimpleOpenNIViewer
       temp_image.swap (image_);
       return (temp_image);
     }
-    /**
-     * @brief starts the main loop
-     */
+
     void
     run ()
     {
-      string mouseMsg3D("Mouse coordinates in PCL Visualizer");
       string mouseMsg2D("Mouse coordinates in image viewer");
-      string keyMsg3D("Key event for PCL Visualizer");
       string keyMsg2D("Key event for image viewer");
 
-  //    image_viewer_.registerMouseCallback (&SimpleOpenNIViewer::mouse_callback, *this, (void*)(&mouseMsg2D));
-  //    image_viewer_.registerKeyboardCallback(&SimpleOpenNIViewer::keyboard_callback, *this, (void*)(&keyMsg2D));
+      //image_viewer_.registerMouseCallback (&SimpleOpenNIViewer::mouse_callback, *this, (void*)(&mouseMsg2D));
+      //image_viewer_.registerKeyboardCallback(&SimpleOpenNIViewer::keyboard_callback, *this, (void*)(&keyMsg2D));
         
       boost::function<void (const boost::shared_ptr<openni_wrapper::Image>&) > image_cb = boost::bind (&SimpleOpenNIViewer::image_callback, this, _1);
       boost::signals2::connection image_connection = grabber_.registerCallback (image_cb);
