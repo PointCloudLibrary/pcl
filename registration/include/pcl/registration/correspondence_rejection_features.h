d0e313c1dd530d6779d2ce3f4f5bb1b1231e0964
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-09-19 03:20:10 +0000

    two more files
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.x@2506 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ registration/include/pcl/registration/correspondence_rejection_features.h
@@ -0,0 +1,279 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ */
+#ifndef PCL_REGISTRATION_CORRESPONDENCE_REJECTION_FEATURES_H_
+#define PCL_REGISTRATION_CORRESPONDENCE_REJECTION_FEATURES_H_
+
+#include <boost/function.hpp>
+#include <boost/unordered_map.hpp>
+#include <pcl/registration/correspondence_rejection.h>
+
+namespace pcl
+{
+  namespace registration
+  {
+    /**
+      * @b CorrespondenceRejectorFeatures implements a correspondence rejection method based on a set of feature
+      * descriptors. Given an input feature space, the method checks if each feature in the source cloud has a
+      * correspondence in the target cloud, either by checking the first K (given) point correspondences, or 
+      * by defining a tolerance threshold via a radius in feature space.
+      * \todo explain this better.
+      * \author Radu B. Rusu
+      * \ingroup registration
+      */
+    class CorrespondenceRejectorFeatures: public CorrespondenceRejector
+    {
+      using CorrespondenceRejector::input_correspondences_;
+      using CorrespondenceRejector::rejection_name_;
+      using CorrespondenceRejector::getClassName;
+
+      public:
+        /** \brief Empty constructor. */
+        CorrespondenceRejectorFeatures () : max_distance_ (std::numeric_limits<float>::max ())
+        {
+          rejection_name_ = "CorrespondenceRejectorFeatures";
+        }
+
+        /** \brief Get a list of valid correspondences after rejection from the original set of correspondences
+          * \param original_correspondences the set of initial correspondences given
+          * \param remaining_correspondences the resultant filtered set of remaining correspondences
+          */
+        void 
+        getRemainingCorrespondences (const pcl::Correspondences& original_correspondences, 
+                                     pcl::Correspondences& remaining_correspondences);
+
+        /** \brief Provide a pointer to a cloud of feature descriptors associated with the source point cloud
+          * \param source_feature a cloud of feature descriptors associated with the source point cloud
+          * \param key a string that uniquely identifies the feature
+          */
+        template <typename FeatureT> inline void 
+        setSourceFeature (const typename pcl::PointCloud<FeatureT>::ConstPtr &source_feature, 
+                          const std::string &key);
+
+        /** \brief Get a pointer to the source cloud's feature descriptors, specified by the given \a key
+          * \param key a string that uniquely identifies the feature (must match the key provided by setSourceFeature)
+          */
+        template <typename FeatureT> inline typename pcl::PointCloud<FeatureT>::ConstPtr 
+        getSourceFeature (const std::string &key);
+
+        /** \brief Provide a pointer to a cloud of feature descriptors associated with the target point cloud
+          * \param target_feature a cloud of feature descriptors associated with the target point cloud
+          * \param key a string that uniquely identifies the feature
+          */
+        template <typename FeatureT> inline void 
+        setTargetFeature (const typename pcl::PointCloud<FeatureT>::ConstPtr &target_feature, 
+                          const std::string &key);
+
+        /** \brief Get a pointer to the source cloud's feature descriptors, specified by the given \a key
+          * \param key a string that uniquely identifies the feature (must match the key provided by setTargetFeature)
+          */
+        template <typename FeatureT> inline typename pcl::PointCloud<FeatureT>::ConstPtr 
+        getTargetFeature (const std::string &key);
+
+        /** \brief Set a hard distance threshold in the feature \a FeatureT space, between source and target
+          * features. Any feature correspondence that is above this threshold will be considered bad and will be
+          * filtered out.
+          * \param[in] thresh the distance threshold
+          * \param[in] key a string that uniquely identifies the feature
+          */
+        template <typename FeatureT> inline void 
+        setDistanceThreshold (double thresh, const std::string &key);
+
+        /** \brief Test that all features are valid (i.e., does each key have a valid source cloud, target cloud, 
+          * and search method)
+          */
+        inline bool 
+        hasValidFeatures ();
+
+        /** \brief Provide a boost shared pointer to a PointRepresentation to be used when comparing features
+          * \param key a string that uniquely identifies the feature
+          * \param fr the point feature representation to be used 
+          */
+        template <typename FeatureT> inline void
+        setFeatureRepresentation (const typename pcl::PointRepresentation<FeatureT>::ConstPtr &fr,
+                                  const std::string &key);
+
+     protected:
+
+        void 
+        applyRejection (pcl::Correspondences &correspondences);
+
+        float max_distance_;
+
+        class FeatureContainerInterface
+        {
+          public:
+            virtual bool isValid () = 0;
+            virtual double getCorrespondenceScore (int index) = 0;
+            virtual bool isCorrespondenceValid (int index) = 0;
+        };
+
+        typedef boost::unordered_map<std::string, boost::shared_ptr<FeatureContainerInterface> > FeaturesMap;
+
+        /** \brief An STL map containing features to use when performing the correspondence search.*/
+        FeaturesMap features_map_;
+
+        /** \brief An inner class containing pointers to the source and target feature clouds 
+          * and the parameters needed to perform the correspondence search.  This class extends 
+          * FeatureContainerInterface, which contains abstract methods for any methods that do not depend on the 
+          * FeatureT --- these methods can thus be called from a pointer to FeatureContainerInterface without 
+          * casting to the derived class.
+          */
+        template <typename FeatureT>
+        class FeatureContainer : public pcl::registration::CorrespondenceRejectorFeatures::FeatureContainerInterface
+        {
+          public:
+            typedef typename pcl::PointCloud<FeatureT>::ConstPtr FeatureCloudConstPtr;
+            typedef boost::function<int (const pcl::PointCloud<FeatureT> &, int, std::vector<int> &, 
+                                          std::vector<float> &)> SearchMethod;
+            
+            typedef typename pcl::PointRepresentation<FeatureT>::ConstPtr PointRepresentationConstPtr;
+
+            FeatureContainer () : thresh_(std::numeric_limits<double>::max ()), feature_representation_()
+            {
+            }
+
+            inline void 
+            setSourceFeature (const FeatureCloudConstPtr &source_features)
+            {
+              source_features_ = source_features;
+            }
+            
+            inline FeatureCloudConstPtr 
+            getSourceFeature ()
+            {
+              return (source_features_);
+            }
+            
+            inline void 
+            setTargetFeature (const FeatureCloudConstPtr &target_features)
+            {
+              target_features_ = target_features;
+            }
+            
+            inline FeatureCloudConstPtr 
+            getTargetFeature ()
+            {
+              return (target_features_);
+            }
+            
+            inline void 
+            setDistanceThreshold (double thresh)
+            {
+              thresh_ = thresh;
+            }
+
+            virtual inline bool 
+            isValid ()
+            {
+              if (!source_features_ || !target_features_)
+                return (false);
+              else
+                return (source_features_->points.size () > 0 && 
+                        target_features_->points.size () > 0);
+            }
+
+            /** \brief Provide a boost shared pointer to a PointRepresentation to be used when comparing features
+              * \param[in] fr the point feature representation to be used
+              */
+            inline void
+            setFeatureRepresentation (const PointRepresentationConstPtr &fr)
+            {
+              feature_representation_ = fr;
+            }
+
+            /** \brief Obtain a score between a pair of correspondences.
+              * \param[in] the index to check in the list of correspondences
+              * \return score the resultant computed score
+              */
+            virtual inline double
+            getCorrespondenceScore (int index)
+            {
+              // If no feature representation was given, reset to the default implementation for FeatureT
+              if (!feature_representation_)
+                feature_representation_.reset (new DefaultFeatureRepresentation<FeatureT>);
+
+              // Get the source and the target feature from the list
+              const FeatureT &feat_src = source_features_->points[index];
+              const FeatureT &feat_tgt = target_features_->points[index];
+
+              // Check if the representations are valid
+              if (!feature_representation_->isValid (feat_src) || !feature_representation_->isValid (feat_tgt))
+              {
+                PCL_ERROR ("[pcl::registration::CorrespondenceRejectorFeatures::getCorrespondenceScore] Invalid feature representation given!\n");
+                return (std::numeric_limits<double>::max ());
+              }
+
+              // Set the internal feature point representation of choice
+              Eigen::VectorXf feat_src_ptr = Eigen::VectorXf::Zero (feature_representation_->getNumberOfDimensions ());
+              feature_representation_->vectorize ((FeatureT)feat_src, feat_src_ptr);
+              Eigen::VectorXf feat_tgt_ptr = Eigen::VectorXf::Zero (feature_representation_->getNumberOfDimensions ());
+              feature_representation_->vectorize ((FeatureT)feat_tgt, feat_tgt_ptr);
+
+              // Compute the L2 norm
+              return ((feat_src_ptr - feat_tgt_ptr).squaredNorm ());
+            }
+
+            /** \brief Check whether the correspondence pair at the given index is valid
+              * by computing the score and testing it against the user given threshold 
+              * \param[in] the index to check in the list of correspondences
+              * \return true if the correspondence is good, false otherwise
+              */
+            virtual inline bool
+            isCorrespondenceValid (int index)
+            {
+              if (getCorrespondenceScore (index) < thresh_ * thresh_)
+                return (true);
+              else
+                return (false);
+            }
+             
+          private:
+            FeatureCloudConstPtr source_features_, target_features_;
+            SearchMethod search_method_;
+
+            /** \brief The L2 squared Euclidean threshold. */
+            double thresh_;
+
+            /** \brief The internal point feature representation used. */
+            PointRepresentationConstPtr feature_representation_;
+        };
+    };
+  }
+}
+
+#include "pcl/registration/impl/correspondence_rejection_features.hpp"
+
+#endif /* PCL_REGISTRATION_CORRESPONDENCE_REJECTION_FEATURES_H_ */
---
+++ registration/include/pcl/registration/transformation_estimation_lm.h
@@ -0,0 +1,253 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: icp.h 1928 2011-08-05 03:43:20Z rusu $
+ *
+ */
+#ifndef PCL_REGISTRATION_TRANSFORMATION_ESTIMATION_LM_H_
+#define PCL_REGISTRATION_TRANSFORMATION_ESTIMATION_LM_H_
+
+#include <pcl/registration/transformation_estimation.h>
+#include <pcl/registration/warp_point_rigid.h>
+
+namespace pcl
+{
+  namespace registration
+  {
+    /** @b TransformationEstimationLM implements Levenberg Marquardt-based
+      * estimation of the transformation aligning the given correspondences
+      *
+      * \author Radu B. Rusu
+      * \ingroup registration
+      */
+    template <typename PointSource, typename PointTarget>
+    class TransformationEstimationLM : public TransformationEstimation<PointSource, PointTarget>
+    {
+      typedef pcl::PointCloud<PointSource> PointCloudSource;
+      typedef typename PointCloudSource::Ptr PointCloudSourcePtr;
+      typedef typename PointCloudSource::ConstPtr PointCloudSourceConstPtr;
+
+      typedef pcl::PointCloud<PointTarget> PointCloudTarget;
+
+      typedef PointIndices::Ptr PointIndicesPtr;
+      typedef PointIndices::ConstPtr PointIndicesConstPtr;
+
+      public:
+        TransformationEstimationLM () {};
+        virtual ~TransformationEstimationLM () {};
+
+        /** \brief Estimate a rigid rotation transformation between a source and a target point cloud using LM.
+          * \param[in] cloud_src the source point cloud dataset
+          * \param[in] cloud_tgt the target point cloud dataset
+          * \param[out] transformation_matrix the resultant transformation matrix
+          */
+        inline void
+        estimateRigidTransformation (
+            const pcl::PointCloud<PointSource> &cloud_src,
+            const pcl::PointCloud<PointTarget> &cloud_tgt,
+            Eigen::Matrix4f &transformation_matrix);
+
+        /** \brief Estimate a rigid rotation transformation between a source and a target point cloud using LM.
+         * \param[in] cloud_src the source point cloud dataset
+         * \param[in] indices_src the vector of indices describing the points of interest in \a cloud_src
+         * \param[in] cloud_tgt the target point cloud dataset
+         * \param[out] transformation_matrix the resultant transformation matrix
+         */
+        inline void
+        estimateRigidTransformation (
+            const pcl::PointCloud<PointSource> &cloud_src,
+            const std::vector<int> &indices_src,
+            const pcl::PointCloud<PointTarget> &cloud_tgt,
+            Eigen::Matrix4f &transformation_matrix);
+
+        /** \brief Estimate a rigid rotation transformation between a source and a target point cloud using LM.
+         * \param[in] cloud_src the source point cloud dataset
+         * \param[in] indices_src the vector of indices describing the points of interest in \a cloud_src
+         * \param[in] cloud_tgt the target point cloud dataset
+         * \param[in] indices_tgt the vector of indices describing the correspondences of the interst points from \a indices_src
+         * \param[out] transformation_matrix the resultant transformation matrix
+         */
+        inline void
+        estimateRigidTransformation (
+            const pcl::PointCloud<PointSource> &cloud_src,
+            const std::vector<int> &indices_src,
+            const pcl::PointCloud<PointTarget> &cloud_tgt,
+            const std::vector<int> &indices_tgt,
+            Eigen::Matrix4f &transformation_matrix);
+
+        /** \brief Estimate a rigid rotation transformation between a source and a target point cloud using LM.
+         * \param[in] cloud_src the source point cloud dataset
+         * \param[in] cloud_tgt the target point cloud dataset
+         * \param[in] correspondences the vector of correspondences between source and target point cloud
+         * \param[out] transformation_matrix the resultant transformation matrix
+         */
+        inline void
+        estimateRigidTransformation (
+            const pcl::PointCloud<PointSource> &cloud_src,
+            const pcl::PointCloud<PointTarget> &cloud_tgt,
+            const std::vector<pcl::Correspondence> &correspondences,
+            Eigen::Matrix4f &transformation_matrix);
+
+        /** \brief Set the function we use to warp points. Defaults to rigid 6D warp.
+          * \param[in] shared pointer to object that warps points
+          */
+        void
+        setWarpFunction (const boost::shared_ptr<WarpPointRigid<PointSource, PointTarget> > &warp_fcn)
+        {
+          warp_point_ = warp_fcn;
+        }
+
+      private:
+        /** \brief Cost function to be minimized
+          * \param[in] p a pointer to our data structure array
+          * \param[in] m the number of functions
+          * \param[in] n the number of variables
+          * \param[out] x a pointer to the variables array
+          * \param[out] fvec a pointer to the resultant functions evaluations
+          * \param[in] iflag set to -1 inside the function to terminate execution
+          */
+        static int 
+        functionToOptimize (void *p, int m, int n, const double *x, double *fvec, int iflag);
+
+        /** \brief Cost function to be minimized
+          * \param[in] p a pointer to our data structure array
+          * \param[in] m the number of functions
+          * \param[in] n the number of variables
+          * \param[out] x a pointer to the variables array
+          * \param[out] fvec a pointer to the resultant functions evaluations
+          * \param[in] iflag set to -1 inside the function to terminate execution
+          */
+        static int 
+        functionToOptimizeIndices (void *p, int m, int n, const double *x, double *fvec, int iflag);
+
+        /** \brief Compute the median value from a set of doubles
+          * \param[in] fvec the set of doubles
+          * \param[in] m the number of doubles in the set
+          */
+        inline double 
+        computeMedian (double *fvec, int m);
+
+        /** \brief Use a Huber kernel to estimate the distance between two vectors
+          * \param[in] p_src the first eigen vector
+          * \param[in] p_tgt the second eigen vector
+          * \param[in] sigma the sigma value
+          */
+        inline double
+        distHuber (const Eigen::Vector4f &p_src, const Eigen::Vector4f &p_tgt, double sigma)
+        {
+          Eigen::Array4f diff = (p_tgt.array () - p_src.array ()).abs ();
+          double norm = 0.0;
+          for (int i = 0; i < 3; ++i)
+          {
+            if (diff[i] < sigma)
+              norm += diff[i] * diff[i];
+            else
+              norm += 2.0 * sigma * diff[i] - sigma * sigma;
+          }
+          return (norm);
+        }
+
+        /** \brief Use a Huber kernel to estimate the distance between two vectors
+          * \param[in] diff the norm difference between two vectors
+          * \param[in] sigma the sigma value
+          */
+        inline double
+        distHuber (double diff, double sigma)
+        {
+          double norm = 0.0;
+          if (diff < sigma)
+            norm += diff * diff;
+          else
+            norm += 2.0 * sigma * diff - sigma * sigma;
+          return (norm);
+        }
+
+        /** \brief Use a Gedikli kernel to estimate the distance between two vectors
+          * (for more information, see 
+          * \param[in] val the norm difference between two vectors
+          * \param[in] clipping the clipping value
+          */
+        inline double
+        distGedikli (double val, double clipping)
+        {
+          clipping *= 1.5;
+          return (1.0 / (1.0 + pow (val / clipping, 4)));
+        }
+
+        /** \brief Compute the Manhattan distance between two eigen vectors.
+          * \param[in] p_src the first eigen vector
+          * \param[in] p_tgt the second eigen vector
+          */
+        inline double
+        distL1 (const Eigen::Vector4f &p_src, const Eigen::Vector4f &p_tgt)
+        {
+          return ((p_src.array () - p_tgt.array ()).abs ().sum ());
+        }
+
+        /** \brief Compute the squared Euclidean distance between two eigen vectors.
+          * \param[in] p_src the first eigen vector
+          * \param[in] p_tgt the second eigen vector
+          */
+        inline double
+        distL2Sqr (const Eigen::Vector4f &p_src, const Eigen::Vector4f &p_tgt)
+        {
+          return ((p_src - p_tgt).squaredNorm ());
+        }
+
+        /** \brief The vector of residual weights. Used internall in the LM loop. */
+        std::vector<double> weights_;
+
+        /** \brief Temporary pointer to the source dataset. */
+        const PointCloudSource *tmp_src_;
+
+        /** \brief Temporary pointer to the target dataset. */
+        const PointCloudTarget  *tmp_tgt_;
+
+        /** \brief Temporary pointer to the source dataset indices. */
+        const std::vector<int> *tmp_idx_src_;
+
+        /** \brief Temporary pointer to the target dataset indices. */
+        const std::vector<int> *tmp_idx_tgt_;
+
+        /** \brief Temporary pointer to the target dataset indices. */
+        boost::shared_ptr<WarpPointRigid<PointSource, PointTarget> > warp_point_;
+    };
+  }
+}
+
+#include <pcl/registration/impl/transformation_estimation_lm.hpp>
+
+#endif /* PCL_REGISTRATION_TRANSFORMATION_ESTIMATION_LM_H_ */
+
