7a33cecd92d1069343806ba4775b016f81b6e12e
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-09-22 03:32:14 +0000

    fix for API change
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.x@2557 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- doc/tutorials/content/sources/greedy_projection/greedy_projection.cpp
+++ doc/tutorials/content/sources/greedy_projection/greedy_projection.cpp
@@ -44,7 +44,7 @@ main (int argc, char** argv)
   // Set typical values for the parameters
   gp3.setMu (2.5);
   gp3.setMaximumNearestNeighbors (100);
-  gp3.setMaximumSurfaceAgle(M_PI/4); // 45 degrees
+  gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
   gp3.setMinimumAngle(M_PI/18); // 10 degrees
   gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
   gp3.setNormalConsistency(false);
