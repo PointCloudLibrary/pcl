cfb57b64277aa00dce183301286463bc5eba8c14
Ryohei Ueda <ueda@jsk.t.u-tokyo.ac.jp>
2011-09-22 22:23:40 +0000

    OpenMP implementation
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2574 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ tracking/include/pcl/tracking/impl/particle_filter_omp.hpp
@@ -0,0 +1,18 @@
+#ifndef PCL_TRACKING_IMPL_PARTICLE_OMP_FILTER_H_
+#define PCL_TRACKING_IMPL_PARTICLE_OMP_FILTER_H_
+
+template <typename PointInT, typename StateT> void
+pcl::tracking::ParticleFilterOMPTracker<PointInT, StateT>::weight ()
+{
+  coherence_->initCompute ();
+#pragma omp parallel for schedule (dynamic, threads_)
+  for ( int i = 0; i < particle_num_; i++ )
+  {
+    double likelihood = calcLikelihood (particles_->points[i]);
+    particles_->points[i].weight = likelihood;
+  }
+  normalizeWeight ();
+}
+
+
+#endif
