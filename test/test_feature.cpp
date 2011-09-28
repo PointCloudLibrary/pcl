8254b63dfa62ed7b38da8eb7ffe63e73f417365c
Michael Dixon <mdixon@willowgarage.com>
2011-09-28 17:30:11 +0000

    Improved the FPFH unit test; confirmed that #349 is fixed
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2608 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- test/test_feature.cpp
+++ test/test_feature.cpp
@@ -1080,36 +1080,49 @@ TEST (PCL, FPFHEstimation)
 
 
   // Test results when both setIndices and setSearchSurface are used
-
   PointCloud<PointXYZ>::Ptr search_surface_points = cloud.makeShared ();
   PointCloud<Normal>::Ptr search_surface_normals = normals;
 
   // Subsample the search surface to get a different input_ cloud
-  std::vector<int> subsample_inds (0);
+  std::vector<int> full_input_indices (0);
   for (size_t i = 0; i < search_surface_points->size (); i+=3)
-    subsample_inds.push_back (i);
+  {
+    full_input_indices.push_back (i);
+  }
   PointCloud<PointXYZ>::Ptr full_input_cloud (new PointCloud<PointXYZ>);
-  copyPointCloud (*search_surface_points, subsample_inds, *full_input_cloud);
+  copyPointCloud (*search_surface_points, full_input_indices, *full_input_cloud);
 
   // Create some indices
-  boost::shared_ptr<std::vector<int> > inds (new std::vector<int>);
+  boost::shared_ptr<std::vector<int> > partial_input_indices (new std::vector<int>);
+  std::vector<int> partial_to_surface_indices;
   for (size_t i = 0; i < (full_input_cloud->size ()/2); i+=2)
   {
-    inds->push_back (i);
+    partial_input_indices->push_back (i);
+    partial_to_surface_indices.push_back (full_input_indices[i]);
   }
 
-  // Create a subcloud of "full_input_cloud" based on the indices above
+  // Create "partial_input_cloud" as a subcloud of "full_input_cloud" based on the "partial_input_indices"
   PointCloud<PointXYZ>::Ptr partial_input_cloud (new PointCloud<PointXYZ>);
-  copyPointCloud (*full_input_cloud, *inds, *partial_input_cloud);
+  copyPointCloud (*full_input_cloud, *partial_input_indices, *partial_input_cloud);
 
-  // Estimate FPFH with a search surface and with input/indices pair: full_input_cloud + inds 
+  // Estimate FPFH with all points and then downsample the results to get the features at the "partial_input_points"
+  FPFHEstimation<PointXYZ, Normal, FPFHSignature33> fpfh0;
+  fpfh0.setSearchMethod (KdTreeFLANN<PointXYZ>::Ptr (new KdTreeFLANN<PointXYZ>));
+  fpfh0.setRadiusSearch (0.05);
+  fpfh0.setInputCloud (search_surface_points);
+  fpfh0.setInputNormals (search_surface_normals);
+  PointCloud<FPFHSignature33> output_for_entire_search_surface, output0;
+  fpfh0.compute (output_for_entire_search_surface);
+  copyPointCloud (output_for_entire_search_surface, partial_to_surface_indices, output0);
+
+  // Estimate FPFH with a search surface and with input/indices pair: full_input_cloud + partial_input_indices
   FPFHEstimation<PointXYZ, Normal, FPFHSignature33> fpfh1;
   fpfh1.setSearchMethod (KdTreeFLANN<PointXYZ>::Ptr (new KdTreeFLANN<PointXYZ>));
   fpfh1.setRadiusSearch (0.05);
   fpfh1.setSearchSurface (search_surface_points);
   fpfh1.setInputNormals (search_surface_normals);
   fpfh1.setInputCloud (full_input_cloud);
-  fpfh1.setIndices (inds);
+  fpfh1.setIndices (partial_input_indices);
   PointCloud<FPFHSignature33> output1;
   fpfh1.compute (output1);
 
@@ -1123,11 +1136,17 @@ TEST (PCL, FPFHEstimation)
   PointCloud<FPFHSignature33> output2;
   fpfh2.compute (output2);
 
-  // The two results should be equal
+  // The three results should be equal
+  ASSERT_EQ (output0.points.size (), output1.points.size ());
   ASSERT_EQ (output1.points.size (), output2.points.size ());
   for (size_t i = 0; i < output1.points.size (); ++i)
+  {
     for (int j = 0; j < 33; ++j)
+    {
+      ASSERT_EQ (output0.points[i].histogram[j], output1.points[i].histogram[j]);
       ASSERT_EQ (output1.points[i].histogram[j], output2.points[i].histogram[j]);
+    }
+  }
 
 }
 
