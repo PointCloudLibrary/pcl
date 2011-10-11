d7b51a4c6da3973330f261c0ef610fd42f09e550
Mourad Boufarguine <mourad@boufarguine.name>
2011-10-11 18:41:53 +0000

    deiniteCompute is overloaded in Feature class
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2705 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- features/include/pcl/features/feature.h
+++ features/include/pcl/features/feature.h
@@ -96,9 +96,6 @@ namespace pcl
   template <typename PointInT, typename PointOutT>
   class Feature : public PCLBase<PointInT>
   {
-    protected:
-      using PCLBase<PointInT>::deinitCompute;
-
     public:
       using PCLBase<PointInT>::indices_;
       using PCLBase<PointInT>::input_;
