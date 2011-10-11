830f8998065c12c3a9750b936be16e2999b3867d
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-10-11 02:36:24 +0000

    fix for the cluster_extraction tutorial, per #357 (thanks Anthony!)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.x@2692 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- doc/tutorials/content/sources/cluster_extraction/cluster_extraction.cpp
+++ doc/tutorials/content/sources/cluster_extraction/cluster_extraction.cpp
@@ -86,6 +86,9 @@ main (int argc, char** argv)
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
     for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
       cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
+    cloud_cluster->width = cloud_cluster->points.size ();
+    cloud_cluster->height = 1;
+    cloud_cluster->is_dense = true;
 
     std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
     std::stringstream ss;
