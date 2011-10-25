4edde67dade51a9e5041f30fa27d00d78ce086a8
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2011-10-25 06:20:47 +0000

    Fix check for python stuff, fix CUDA default
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2856 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_utils.cmake
+++ cmake/pcl_utils.cmake
@@ -396,3 +396,29 @@ macro(sort_relative _list _sorted_list _to_sort_relative)
   # swap the temporary list and list to sort
   set(${_to_sort_relative} ${tmp_list})
 endmacro(sort_relative)
+
+
+###############################################################################
+# Find a Python module
+# From http://www.cmake.org/pipermail/cmake/2011-January/041666.html
+function(find_python_module module)
+  string(TOUPPER ${module} module_upper)
+  if(NOT PY_${module_upper})
+    if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
+      set(${module}_FIND_REQUIRED TRUE)
+    endif()
+    # A module's location is usually a directory, but for binary modules
+    # it's a .so file.
+    execute_process(COMMAND "${PYTHON_EXEC}" "-c"
+      "import re, ${module}; print re.compile('/__init__.py.*').sub('',${module}.__file__)"
+      RESULT_VARIABLE _${module}_status
+      OUTPUT_VARIABLE _${module}_location
+      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT _${module}_status)
+      set(PY_${module_upper} ${_${module}_location} CACHE STRING
+        "Location of Python module ${module}")
+    endif(NOT _${module}_status)
+  endif(NOT PY_${module_upper})
+  find_package_handle_standard_args(PY_${module} DEFAULT_MSG PY_${module_upper})
+endfunction(find_python_module)
+
