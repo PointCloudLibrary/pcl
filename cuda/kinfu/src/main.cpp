0651273413306f7e84d27fd826e87c4e40407876
Anatoly Baksheev <no@mail>
2011-10-31 14:40:58 +0000

    license/author headers
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@2978 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cuda/kinfu/src/main.cpp
+++ cuda/kinfu/src/main.cpp
@@ -1,8 +1,43 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Author: Anatoly Baskeheev, Itseez Ltd, (myname.mysurname@mycompany.com)
+ */
+
 #define _CRT_SECURE_NO_DEPRECATE
 
 #include <iostream>
 //#include <opencv2/opencv.hpp>
-#include "pcl/point_cloud.h"
 #include "pcl/gpu/kinfu/openni_capture.hpp"
 #include "pcl/gpu/kinfu/kinfu.hpp"
 //#include "pcl/gpu/kinfu/video_recorder.hpp"
@@ -31,13 +66,12 @@ int main()
         KinfuTracker kinfu;
         kinfu.fx = kinfu.fy = cap.depth_focal_length_VGA;
 
-        KinfuTracker::DepthMap depth_device;
-        KinfuTracker::View view_device;
-
         pcl::visualization::ImageViewer viewer;
-        pcl::visualization::ImageViewer viewer3d;
+        pcl::visualization::ImageViewer viewer3d;        
+
+        KinfuTracker::DepthMap depth_device;
+        KinfuTracker::View view_device;       
         vector<uchar3> view3d_host;
-                                
         for(int i = 0;;++i)
         {                        
             cout << i << endl;
