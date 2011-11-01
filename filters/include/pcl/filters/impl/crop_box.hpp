14fac6d53aa01e2063c5d4a0755c22ddf00a3f53
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-11-01 14:59:53 +0000

    s
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.x@3056 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- filters/include/pcl/filters/impl/crop_box.hpp
+++ filters/include/pcl/filters/impl/crop_box.hpp
@@ -46,14 +46,54 @@ template<typename PointT>
 void
 pcl::CropBox<PointT>::applyFilter (PointCloud &output)
 {
+  output.resize (input_->points.size ());
+  int indice_count = 0;
 
+  Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+  Eigen::Affine3f inverse_transform = Eigen::Affine3f::Identity();
+
+  if (rotation_ != Eigen::Vector3f::Zero ())
+  {
+    pcl::getTransformation (0, 0, 0,
+                            rotation_ (0), rotation_ (1), rotation_ (2),
+                            transform);
+    inverse_transform = transform.inverse();
+  }
+
+  for (size_t index = 0; index < indices_->size (); ++index)
+  {
+    // Get local point
+    PointT local_pt = input_->points[(*indices_)[index]];
+
+    // Transform point to world space
+    if (!(transform_.matrix().isIdentity()))
+      local_pt = pcl::transformPoint<PointT> (local_pt, transform_);
+
+    if (translation_ != Eigen::Vector3f::Zero ())
+    {
+      local_pt.x -= translation_ (0);
+      local_pt.y -= translation_ (1);
+      local_pt.z -= translation_ (2);
+    }
+
+    // Transform point to local space of crop box
+    if (!(inverse_transform.matrix().isIdentity()))
+      local_pt = pcl::transformPoint<PointT> (local_pt, inverse_transform);
+
+    if (local_pt.x < min_pt_[0] || local_pt.y < min_pt_[1] || local_pt.z < min_pt_[2])
+      continue;
+    if (local_pt.x > max_pt_[0] || local_pt.y > max_pt_[1] || local_pt.z > max_pt_[2])
+      continue;
+
+    output.points[indice_count++] = input_->points[(*indices_)[index]];
+  }
+  output.resize (indice_count);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 template<typename PointT> void
 pcl::CropBox<PointT>::applyFilter (std::vector<int> &indices)
 {
-
   indices.resize (input_->points.size ());
   int indice_count = 0;
 
@@ -63,7 +103,8 @@ pcl::CropBox<PointT>::applyFilter (std::vector<int> &indices)
   if (rotation_ != Eigen::Vector3f::Zero ())
   {
     pcl::getTransformation (0, 0, 0,
-                            rotation_ (0), rotation_ (1), rotation_ (2), transform);
+                            rotation_ (0), rotation_ (1), rotation_ (2),
+                            transform);
     inverse_transform = transform.inverse();
   }
 
