84d0fb73ac2d2ff0a4212468d653d18048a2b98c
Justin Rosen <jmylesrosen@gmail.com>
2011-11-02 06:34:26 +0000

    crop_box filter supports PointCloud2 and created unit test
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3079 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- filters/src/crop_box.cpp
+++ filters/src/crop_box.cpp
@@ -40,18 +40,125 @@
 #include "pcl/filters/crop_box.h"
 #include "pcl/filters/impl/crop_box.hpp"
 
+
 ///////////////////////////////////////////////////////////////////////////////
 void
 pcl::CropBox<sensor_msgs::PointCloud2>::applyFilter (PointCloud2 &output)
 {
+  // Resize output cloud to sample size
+  output.data.resize (input_->data.size ());
 
+  // Copy the common fields
+  output.fields = input_->fields;
+  output.is_bigendian = input_->is_bigendian;
+  output.row_step = input_->row_step;
+  output.point_step = input_->point_step;
+  output.height = 1;
+
+  int indice_count = 0;
+
+  Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+  Eigen::Affine3f inverse_transform = Eigen::Affine3f::Identity();
+
+  if (rotation_ != Eigen::Vector3f::Zero ())
+  {
+    pcl::getTransformation (0, 0, 0,
+                            rotation_ (0), rotation_ (1), rotation_ (2),
+                            transform);
+    inverse_transform = transform.inverse();
+  }
+
+  //PointXYZ local_pt;
+  Eigen::Vector3f local_pt (Eigen::Vector3f::Zero ());
+
+  for (size_t index = 0; index < indices_->size (); ++index)
+  {
+    // Get local point
+    int point_offset = ((*indices_)[index] * input_->point_step);
+    int offset = point_offset + input_->fields[x_idx_].offset;
+    memcpy (&local_pt, &input_->data[offset], sizeof (float)*3);
+
+    // Transform point to world space
+    if (!(transform_.matrix().isIdentity()))
+      local_pt = transform_ * local_pt;
+
+    if (translation_ != Eigen::Vector3f::Zero ())
+    {
+      local_pt.x () = local_pt.x () - translation_ (0);
+      local_pt.y () = local_pt.y () - translation_ (1);
+      local_pt.z () = local_pt.z () - translation_ (2);
+    }
+
+    // Transform point to local space of crop box
+    if (!(inverse_transform.matrix ().isIdentity ()))
+      local_pt = inverse_transform * local_pt;
+
+    if (local_pt.x () < min_pt_[0] || local_pt.y () < min_pt_[1] || local_pt.z () < min_pt_[2])
+      continue;
+    if (local_pt.x () > max_pt_[0] || local_pt.y () > max_pt_[1] || local_pt.z () > max_pt_[2])
+      continue;
+
+    memcpy (&output.data[indice_count++ * output.point_step],
+            &input_->data[index * output.point_step], output.point_step);
+  }
+
+  output.width = indice_count;
+  output.row_step = output.point_step * output.width;
+  output.data.resize (output.width * output.height * output.point_step);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void
 pcl::CropBox<sensor_msgs::PointCloud2>::applyFilter (std::vector<int> &indices)
 {
+  indices.resize (input_->width * input_->height);
+  int indice_count = 0;
 
+  Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+  Eigen::Affine3f inverse_transform = Eigen::Affine3f::Identity();
+
+  if (rotation_ != Eigen::Vector3f::Zero ())
+  {
+    pcl::getTransformation (0, 0, 0,
+                            rotation_ (0), rotation_ (1), rotation_ (2),
+                            transform);
+    inverse_transform = transform.inverse();
+  }
+
+  //PointXYZ local_pt;
+  Eigen::Vector3f local_pt (Eigen::Vector3f::Zero ());
+
+  for (size_t index = 0; index < indices_->size (); index++)
+  {
+    // Get local point
+    int point_offset = ((*indices_)[index] * input_->point_step);
+    int offset = point_offset + input_->fields[x_idx_].offset;
+    memcpy (&local_pt, &input_->data[offset], sizeof (float)*3);
+
+    // Transform point to world space
+    if (!(transform_.matrix().isIdentity()))
+      local_pt = transform_ * local_pt;
+
+    if (translation_ != Eigen::Vector3f::Zero ())
+    {
+      local_pt.x () -= translation_ (0);
+      local_pt.y () -= translation_ (1);
+      local_pt.z () -= translation_ (2);
+    }
+
+    // Transform point to local space of crop box
+    if (!(inverse_transform.matrix().isIdentity()))
+      local_pt = inverse_transform * local_pt;
+
+    if (local_pt.x () < min_pt_[0] || local_pt.y () < min_pt_[1] || local_pt.z () < min_pt_[2])
+      continue;
+    if (local_pt.x () > max_pt_[0] || local_pt.y () > max_pt_[1] || local_pt.z () > max_pt_[2])
+      continue;
+
+    indices[indice_count++] = (*indices_)[index];
+  }
+
+  indices.resize (indice_count);
 }
 
 PCL_INSTANTIATE(CropBox, PCL_XYZ_POINT_TYPES);
