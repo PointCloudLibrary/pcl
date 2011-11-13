d1bab017d19228af12d4cbaf852842fd71183f70
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-11-13 09:18:08 +0000

    there's no development foreseen for the Python module in 1.x, so removing this for now to de-clutter the modules list
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3158 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- python/gencode.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-from Cheetah.Template import Template
-
-srcdir=sys.argv[1]
-s=str(Template(open(os.path.join(srcdir, "cloud.tmpl")).read(), searchList=[{'type': 'PointXYZ', 'members' : ['x', 'y', 'z']}]))
-open(os.path.join(sys.argv[2], "pygen.i"), "w").write(s)
