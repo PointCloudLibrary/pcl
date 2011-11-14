cc2d67a3aa88f6c98bad4ef19249ab81e6a99a08
Alexandru Eugen Ichim <alex.e.ichim@gmail.com>
2011-11-14 12:03:49 +0000

    Simulation fixed MacOS compilation error
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3164 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- simulation/include/pcl/simulation/range_likelihood.hpp
+++ simulation/include/pcl/simulation/range_likelihood.hpp
@@ -9,6 +9,11 @@
 #include <math.h>
 #include <Eigen/StdVector>
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 namespace pcl
 {
 class RangeLikelihoodTime
@@ -16,7 +21,17 @@ class RangeLikelihoodTime
 public:
   static double get_time() {
     struct timespec ts;
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts.tv_sec = mts.tv_sec;
+    ts.tv_nsec = mts.tv_nsec;
+#else
     clock_gettime(CLOCK_REALTIME, &ts);
+#endif
     return (double)ts.tv_sec + (double)ts.tv_nsec*1E-9;
   }
 };
