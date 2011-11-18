c864176d1b63eff19c394c75d0f05a7994db66a5
Aravindhan K. Krishnan <aravindhan.krishnan@gmail.com>
2011-11-18 11:45:37 +0000

    Adding header file for Normal space sampling
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3203 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ filters/include/pcl/filters/normal_space.h
@@ -0,0 +1,190 @@
+/*
+ * Software License Agreement (BSD License)
+ * 
+ * Point Cloud Library (PCL) - www.pointclouds.org
+ * Copyright (c) 2009-2011, Willow Garage, Inc.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *  * Neither the name of Willow Garage, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef PCL_FILTERS_NORMAL_SUBSAMPLE_H_
+#define PCL_FILTERS_NORMAL_SUBSAMPLE_H_
+
+#include "pcl/filters/filter_indices.h"
+#include <time.h>
+#include <limits.h>
+
+#include <boost/dynamic_bitset.hpp>
+
+namespace pcl
+{
+  /** \brief @b NormalSpaceSampling samples the input point cloud in the space of normal directions computed at every point.
+    * \ingroup filters
+    */
+  template<typename PointT, typename NormalT>
+  class NormalSpaceSampling : public FilterIndices<PointT>
+  {
+    using FilterIndices<PointT>::filter_name_;
+    using FilterIndices<PointT>::getClassName;
+    using FilterIndices<PointT>::indices_;
+    using FilterIndices<PointT>::input_;
+
+    typedef typename FilterIndices<PointT>::PointCloud PointCloud;
+    typedef typename PointCloud::Ptr PointCloudPtr;
+    typedef typename PointCloud::ConstPtr PointCloudConstPtr;
+    typedef typename pcl::PointCloud<NormalT>::Ptr NormalsPtr;
+
+    public:
+      /** \brief Empty constructor. */
+      NormalSpaceSampling () : sample_ (UINT_MAX), seed_(time(NULL))
+      {
+        filter_name_ = "NormalSpaceSampling";
+      }
+
+      /** \brief Set number of indices to be sampled.
+        * \param sample
+        */
+      inline void
+      setSample (unsigned int sample)
+      {
+        sample_ = sample;
+      }
+
+      /** \brief Get the value of the internal \a sample parameter.
+        */
+      inline unsigned int
+      getSample ()
+      {
+        return (sample_);
+      }
+
+      /** \brief Set seed of random function.
+        * \param seed
+        */
+      inline void
+      setSeed (unsigned int seed)
+      {
+        seed_ = seed;
+      }
+
+      /** \brief Get the value of the internal \a seed parameter.
+        */
+      inline unsigned int
+      getSeed ()
+      {
+        return (seed_);
+      }
+
+      /** \brief Set the number of bins in x, y and z direction
+        * \param binsx number of bins in x direction
+        * \param binsy number of bins in y direction
+        * \param binsz number of bins in z direction
+        */
+      inline void 
+      setBins (unsigned int binsx, unsigned int binsy, unsigned int binsz)
+      {
+        binsx_ = binsx;
+	binsy_ = binsy;
+	binsz_ = binsz;
+      }
+
+      /** \brief Get the number of bins in x, y and z direction
+        * \param binsx number of bins in x direction
+        * \param binsy number of bins in y direction
+        * \param binsz number of bins in z direction
+        */
+      inline void 
+      getBins (unsigned int& binsx, unsigned int& binsy, unsigned int& binsz)
+      {
+        binsx = binsx_;
+	binsy = binsy_;
+	binsz = binsz_;
+      }
+
+      /** \brief Set the normals computed on the input point cloud
+       * \param normals the normals computed for the input cloud
+       */
+      inline void 
+      setNormals (NormalsPtr &normals) { input_normals_ = normals; }
+
+      /** \brief Get the normals computed on the input point cloud
+       */
+      inline NormalsPtr
+      getNormals () { return input_normals_; }
+
+    protected:
+      
+      /** \brief Number of indices that will be returned. */
+      unsigned int sample_;
+      /** \brief Random number seed. */
+      unsigned int seed_;
+
+      /** \brief Number of bins in x direction. */
+      unsigned int binsx_;
+      /** \brief Number of bins in y direction. */
+      unsigned int binsy_;
+      /** \brief Number of bins in z direction. */
+      unsigned int binsz_;
+     
+      /** \brief The normals computed at each point in the input cloud */
+      NormalsPtr input_normals_; 
+
+      /** \brief Sample of point indices into a separate PointCloud
+        * \param output the resultant point cloud
+        */
+      void
+      applyFilter (PointCloud &output);
+
+      /** \brief Sample of point indices
+        * \param indices the resultant point cloud indices
+        */
+      void
+      applyFilter (std::vector<int> &indices);
+
+    private:
+      /** \brief Finds the bin number of the input normal, returns the bin number
+        * \param normal the input normal 
+        * \param nbins total number of bins
+        */
+      unsigned int 
+      findBin (float *normal, unsigned int nbins);
+
+      /** \brief Checks of the entire bin is sampled, returns true or false
+        * \param array flag which says whether a point is sampled or not
+        * \param start_index the index to the first point of the bin in array.
+        * \param length number of points in the bin
+        */
+      bool
+      isEntireBinSampled (boost::dynamic_bitset<> &array, unsigned int start_index, unsigned int length);
+
+  };
+}
+#endif  //#ifndef PCL_FILTERS_NORMAL_SPACE_SUBSAMPLE_H_
