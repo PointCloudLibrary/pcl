9895e7c0596f3ed8e1442a45af930d15b60cbd31
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-12-05 06:00:51 +0000

    * change the implementation of the VoxelGrid app to use ApproximateVoxelGrid
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3392 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- apps/src/openni_voxel_grid.cpp
+++ apps/src/openni_voxel_grid.cpp
@@ -40,6 +40,7 @@
 #include <pcl/io/openni_grabber.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/openni_camera/openni_driver.h>
+#include <pcl/filters/approximate_voxel_grid.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/console/parse.h>
 #include <pcl/common/time.h>
@@ -129,7 +130,7 @@ class OpenNIVoxelGrid
       interface->stop ();
     }
 
-    pcl::VoxelGrid<PointType> grid_;
+    pcl::ApproximateVoxelGrid<PointType> grid_;
     pcl::visualization::CloudViewer viewer;
     std::string device_id_;
     boost::mutex mtx_;
@@ -140,10 +141,10 @@ void
 usage (char ** argv)
 {
   std::cout << "usage: " << argv[0] << " <device_id> <options>\n\n"
-            << "where options are:\n         -minmax min-max  :: set the PassThrough min-max cutting values (default: 0-5.0)\n"
+            << "where options are:\n         -minmax min-max  :: set the ApproximateVoxelGrid min-max cutting values (default: 0-5.0)\n"
             <<                     "         -field  X        :: use field/dimension 'X' to filter data on (default: 'z')\n"
 
-            << "                             -leaf x, y, z  :: set the VoxelGrid leaf size (default: 0.01)\n";
+            << "                             -leaf x, y, z  :: set the ApproximateVoxelGrid leaf size (default: 0.01)\n";
 
   openni_wrapper::OpenNIDriver& driver = openni_wrapper::OpenNIDriver::getInstance ();
   if (driver.getNumberDevices () > 0)
@@ -164,18 +165,15 @@ usage (char ** argv)
 int 
 main (int argc, char ** argv)
 {
-  if (pcl::console::find_argument (argc, argv, "-h"))
-  {
+  if (pcl::console::find_argument (argc, argv, "-h") != -1)
     usage (argv);
-    return (1);
-  }
 
   double min_v = 0, max_v = 5.0;
   pcl::console::parse_2x_arguments (argc, argv, "-minmax", min_v, max_v, false);
   std::string field_name ("z");
   pcl::console::parse_argument (argc, argv, "-field", field_name);
   PCL_INFO ("Filtering data on %s between %f -> %f.\n", field_name.c_str (), min_v, max_v);
-  double leaf_x = 0.025, leaf_y = 0.025, leaf_z = 0.025;
+  double leaf_x = 0.01, leaf_y = 0.01, leaf_z = 0.01;
   pcl::console::parse_3x_arguments (argc, argv, "-leaf", leaf_x, leaf_y, leaf_z, false);
   PCL_INFO ("Using %f, %f, %f as a leaf size for VoxelGrid.\n", leaf_x, leaf_y, leaf_z);
 
