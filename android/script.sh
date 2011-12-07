48ab1c7c76817828b0faaa6955d9c6fc8204afed
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-12-07 18:35:56 +0000

    modified android script
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3443 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- android/script.sh
+++ android/script.sh
@@ -2,4 +2,6 @@
 
 mkdir -p build
 cd build
-android-cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN -DANDROID_LEVEL=9 -DBUILD_search=0 -DBUILD_io=0 -DBUILD_kdtree=0 -DBUILD_octree=0 -DBUILD_range_image=0 -DBUILD_sample_consensus=0 -DBUILD_segmentation=0 -DBUILD_surface=0 -DBUILD_tools=0 -DBUILD_tracking=0 -DBUILD_tutorials=0 -DBUILD_visualization=0 ..
+android-cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN -DANDROID_LEVEL=9 -DBUILD_search=OFF -DBUILD_io=OFF -DBUILD_kdtree=OFF -DBUILD_octree=OFF -DBUILD_range_image=OFF -DBUILD_sample_consensus=OFF -DBUILD_segmentation=OFF -DBUILD_surface=OFF -DBUILD_tools=OFF -DBUILD_tracking=OFF -DBUILD_tutorials=OFF -DBUILD_visualization=OFF ..
+
+
