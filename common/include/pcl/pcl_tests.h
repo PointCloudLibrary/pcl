2bf500b51577c06d89ff2b105e5feb4a1ff0cd00
Nizar Sallem <nksallem@laas.fr>
2011-12-10 18:09:20 +0000

    New: helper functions to test vectors
    I have had enough of iterating on vectors so wrote 2 functions
    EXPECT_EQ_VECTORS and EXPECT_NEAR_VECTORS to run googletest on 2 
    vectors to test for equality.
    Refer to test/test_macros.cpp as example.
    
    
    
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3483 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ common/include/pcl/pcl_tests.h
@@ -0,0 +1,71 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+
+#ifndef PCL_TEST_MACROS
+#define PCL_TEST_MACROS
+
+namespace pcl
+{
+  /** \file test_macros.h provide helper macros for testing vectors, matrices etc.
+    * We took some liberty with upcasing names to make them look like googletest
+    * macros names so that reader is not confused.
+    *
+    * \author Nizar Sallem 
+    */
+  namespace test
+  {
+    template <typename V1, typename V2>
+    void EXPECT_EQUAL_VECTORS (const V1& v1, const V2& v2)
+    {
+      EXPECT_EQ (v1.size (), v2.size ());
+      int length = v1.size ();
+      for (int i = 0; i < length; ++i)
+        EXPECT_EQ (v1[i], v2[i]);
+    }
+
+    template <typename V1, typename V2, typename Scalar>
+    void EXPECT_NEAR_VECTORS (const V1& v1, const V2& v2, const Scalar& epsilon)
+    {
+      EXPECT_EQ (v1.size (), v2.size ());
+      int length = v1.size ();
+      for (int i = 0; i < length; ++i)
+        EXPECT_NEAR (v1[i], v2[i], epsilon);
+    };
+  };
+}
+
+#endif
---
+++ common/test/test_macros.cpp
@@ -0,0 +1,89 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+/** \author Nizar Sallem */
+#include <gtest/gtest.h>
+#include "pcl/pcl_tests.h"
+#include <vector>
+#include <Eigen/Core>
+
+using namespace pcl::test;
+
+std::vector<float> v1, v2;
+Eigen::Vector3f ev1, ev2;
+
+TEST(MACROS, expect_eq_vectors_macro)
+{
+  for (size_t i = 0; i < 3; i++)
+  {
+    float val = i * 1.5;
+    v1.push_back (val);
+    v2.push_back (val);
+    ev1[i] = val;
+    ev2[i] = val;
+  }
+
+  EXPECT_EQUAL_VECTORS (v1, v2);
+  EXPECT_EQUAL_VECTORS (ev1, ev2);
+  EXPECT_EQUAL_VECTORS (v1, ev2);
+  EXPECT_EQUAL_VECTORS (ev1, v2);
+//  equal_vectors<std::vector<float>, std::vector<float> >(v1, v2);
+}
+
+TEST(MACROS, expect_near_vectors_macro)
+{
+  v1.clear ();
+  v2.clear ();
+  const static float epsilon = 1e-5;
+  for (size_t i = 0; i < 3; i++)
+  {
+    float val = i * 1.5;
+    v1.push_back (val);
+    v2.push_back (val + epsilon);
+    ev1[i] = val;
+    ev2[i] = val + epsilon;
+  }
+  EXPECT_NEAR_VECTORS (v1, v2, 2*epsilon);
+  EXPECT_NEAR_VECTORS (ev1, ev2, 2*epsilon);
+  EXPECT_NEAR_VECTORS (v1, ev2, 2*epsilon);
+  EXPECT_NEAR_VECTORS (ev1, v2, 2*epsilon);
+}
+
+int main (int argc, char** argv)
+{
+  testing::InitGoogleTest (&argc, argv);
+  return (RUN_ALL_TESTS ());
+}
