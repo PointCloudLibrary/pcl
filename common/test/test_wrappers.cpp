3720ad5b8f1ee22e15332aea7e1dd22513b0c741
Nizar Sallem <nksallem@laas.fr>
2011-12-12 06:00:35 +0000

    Add: provide unit tests for wrapping methods
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3500 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ common/test/test_wrappers.cpp
@@ -0,0 +1,98 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/pcl_tests.h>
+
+using namespace pcl;
+using namespace pcl::test;
+
+PointCloud<PointXYZ> cloud;
+const size_t size = 10 * 480;
+
+TEST (PointCloud, size)
+{
+  EXPECT_EQ(cloud.points.size (), cloud.size ());
+}
+
+TEST (PointCloud, sq_brackets_wrapper)
+{
+  for (uint32_t i = 0; i < size; ++i)
+  {
+    Eigen::Vector3f direct = cloud.points[i].getVector3fMap ();
+    Eigen::Vector3f wrapped = cloud[i].getVector3fMap ();
+    EXPECT_EQ_VECTORS (direct,wrapped);
+  }
+}
+
+TEST (PointCloud, at)
+{
+  for (uint32_t i = 0; i < size; ++i)
+  {
+    Eigen::Vector3f direct = cloud.points.at (i).getVector3fMap ();
+    Eigen::Vector3f wrapped = cloud.at (i).getVector3fMap ();
+    EXPECT_EQ_VECTORS (direct,wrapped);
+  }
+}
+
+TEST (PointCloud, front)
+{
+  Eigen::Vector3f direct = cloud.points.front ().getVector3fMap ();
+  Eigen::Vector3f wrapped = cloud.front ().getVector3fMap ();
+  EXPECT_EQ_VECTORS (direct,wrapped);
+}
+
+TEST (PointCloud, back)
+{
+  Eigen::Vector3f direct = cloud.points.back ().getVector3fMap ();
+  Eigen::Vector3f wrapped = cloud.back ().getVector3fMap ();
+  EXPECT_EQ_VECTORS (direct,wrapped);
+}
+
+int
+main (int argc, char** argv)
+{
+  cloud.width = 10;
+  cloud.height = 480;
+  for (uint32_t i = 0; i < size; ++i)
+    cloud.points.push_back (PointXYZ (3*i+0,3*i+1,3*i+2));
+
+  testing::InitGoogleTest (&argc, argv);
+  return (RUN_ALL_TESTS ());
+}
