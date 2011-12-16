c40fbe4e1195e4cbd690680763b0ee40f65ddb53
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-12-16 22:59:29 +0000

    * fixed unit test that resulted as a consequence of the r3550 change
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3573 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- features/include/pcl/features/impl/intensity_spin.hpp
+++ features/include/pcl/features/impl/intensity_spin.hpp
@@ -149,8 +149,10 @@ pcl::IntensitySpinEstimation<PointInT, PointOutT>::computeFeature (PointCloudOut
     computeIntensitySpinImage (*surface_, search_radius_, sigma_, k, nn_indices, nn_dist_sqr, intensity_spin_image);
 
     // Copy into the resultant cloud
-    for (int bin = 0; bin < intensity_spin_image.size (); ++bin)
-      output.points[idx].histogram[bin] = intensity_spin_image (bin);
+    int bin = 0;
+    for (int bin_j = 0; bin_j < intensity_spin_image.cols (); ++bin_j)
+      for (int bin_i = 0; bin_i < intensity_spin_image.rows (); ++bin_i)
+        output.points[idx].histogram[bin++] = intensity_spin_image (bin_i, bin_j);
   }
 }
 
