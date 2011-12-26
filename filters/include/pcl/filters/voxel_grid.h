3e0d818959fed4d72c9209883ebabceef66a12d4
Radu B. Rusu <radu.b.rusu@gmail.com>
2011-12-26 18:04:36 +0000

    nm
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3652 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- filters/include/pcl/filters/voxel_grid.h
+++ filters/include/pcl/filters/voxel_grid.h
@@ -383,7 +383,7 @@ namespace pcl
       /** \brief Destructor. */
       virtual ~VoxelGrid ()
       {
-        leaves_.clear();
+        leaves_.clear ();
       }
 
       /** \brief Set the voxel grid leaf size.
