562aeff454477b890e15a435fdc9e9d8a7255c91
Suat Gedikli <suat.gedikli@gmail.com>
2012-01-08 09:10:15 +0000

    added method selection
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@3840 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- apps/src/openni_ii_normal_estimation.cpp
+++ apps/src/openni_ii_normal_estimation.cpp
@@ -30,7 +30,7 @@
  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
- *	
+ *
  */
 
 #include <boost/thread/thread.hpp>
@@ -69,27 +69,32 @@ class OpenNIIntegralImageNormalEstimation
     typedef typename Cloud::ConstPtr CloudConstPtr;
 
     OpenNIIntegralImageNormalEstimation (const std::string& device_id = "")
-      : viewer ("PCL OpenNI NormalEstimation Viewer") 
+      : viewer ("PCL OpenNI NormalEstimation Viewer")
     , device_id_(device_id)
     {
-//      ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::AVERAGE_3D_GRADIENT);
+      //ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::AVERAGE_3D_GRADIENT);
       ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::COVARIANCE_MATRIX);
-      ne_.setRectSize (50, 50);
+      ne_.setNormalSmoothingSize (11.0);
       new_cloud_ = false;
+      viewer.registerKeyboardCallback(&OpenNIIntegralImageNormalEstimation::keyboard_callback, *this);
     }
 
-    
-    void 
+
+    void
     cloud_cb (const CloudConstPtr& cloud)
     {
       boost::mutex::scoped_lock lock (mtx_);
       //lock while we set our cloud;
-      FPS_CALC ("computation");
+      //FPS_CALC ("computation");
       // Estimate surface normals
-      ne_.setInputCloud (cloud);
 
       normals_.reset (new pcl::PointCloud<pcl::Normal>);
+
+      //double start = pcl::getTime ();
+      ne_.setInputCloud (cloud);
       ne_.compute (*normals_);
+      //double stop = pcl::getTime ();
+      //std::cout << "Time for normal estimation: " << (stop - start) * 1000.0 << " ms" << std::endl;
       cloud_ = cloud;
 
       new_cloud_ = true;
@@ -98,27 +103,56 @@ class OpenNIIntegralImageNormalEstimation
     void
     viz_cb (pcl::visualization::PCLVisualizer& viz)
     {
-      boost::mutex::scoped_lock lock (mtx_);
-      if (!cloud_)
+      mtx_.lock ();
+      if (!cloud_ || !normals_)
       {
-        boost::this_thread::sleep(boost::posix_time::seconds(1));
+        //boost::this_thread::sleep(boost::posix_time::seconds(1));
+        mtx_.unlock ();
         return;
       }
 
       CloudConstPtr temp_cloud;
+      pcl::PointCloud<pcl::Normal>::Ptr temp_normals;
       temp_cloud.swap (cloud_); //here we set cloud_ to null, so that
+      temp_normals.swap (normals_);
+      mtx_.unlock ();
 
       if (!viz.updatePointCloud (temp_cloud, "OpenNICloud"))
       {
         viz.addPointCloud (temp_cloud, "OpenNICloud");
         viz.resetCameraViewpoint ("OpenNICloud");
       }
-      // Render the data 
-      if (new_cloud_ && normals_)
+      // Render the data
+      if (new_cloud_)
       {
         viz.removePointCloud ("normalcloud");
-        viz.addPointCloudNormals<PointType, pcl::Normal> (temp_cloud, normals_, 200, 0.05, "normalcloud");
+        viz.addPointCloudNormals<PointType, pcl::Normal> (temp_cloud, temp_normals, 100, 0.05, "normalcloud");
         new_cloud_ = false;
+      }
+    }
+
+    void
+    keyboard_callback (const pcl::visualization::KeyboardEvent& event, void* cookie)
+    {
+      boost::mutex::scoped_lock lock (mtx_);
+      switch (event.getKeyCode ())
+      {
+        case '1':
+          ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::COVARIANCE_MATRIX);
+          std::cout << "switched to COVARIANCE_MATRIX method\n";
+          break;
+        case '2':
+          ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::AVERAGE_3D_GRADIENT);
+          std::cout << "switched to AVERAGE_3D_GRADIENT method\n";
+          break;
+        case '3':
+          ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::AVERAGE_DEPTH_CHANGE);
+          std::cout << "switched to AVERAGE_DEPTH_CHANGE method\n";
+          break;
+        case '4':
+          ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::SIMPLE_3D_GRADIENT);
+          std::cout << "switched to SIMPLE_3D_GRADIENT method\n";
+          break;
       }
     }
 
@@ -129,11 +163,11 @@ class OpenNIIntegralImageNormalEstimation
 
       boost::function<void (const CloudConstPtr&)> f = boost::bind (&OpenNIIntegralImageNormalEstimation::cloud_cb, this, _1);
       boost::signals2::connection c = interface->registerCallback (f);
-     
+
       viewer.runOnVisualizationThread (boost::bind(&OpenNIIntegralImageNormalEstimation::viz_cb, this, _1), "viz_cb");
 
       interface->start ();
-      
+
       while (!viewer.wasStopped ())
       {
         boost::this_thread::sleep(boost::posix_time::seconds(1));
@@ -155,7 +189,7 @@ class OpenNIIntegralImageNormalEstimation
 void
 usage (char ** argv)
 {
-  std::cout << "usage: " << argv[0] << " <device_id> <options>\n\n";
+  std::cout << "usage: " << argv[0] << " [<device_id>]\n\n";
 
   openni_wrapper::OpenNIDriver& driver = openni_wrapper::OpenNIDriver::getInstance ();
   if (driver.getNumberDevices () > 0)
@@ -173,18 +207,26 @@ usage (char ** argv)
     cout << "No devices connected." << endl;
 }
 
-int 
+int
 main (int argc, char ** argv)
 {
   std::string arg;
   if (argc > 1)
     arg = std::string (argv[1]);
-  
-  if (arg == "--help" || arg == "-h")
+
+  openni_wrapper::OpenNIDriver& driver = openni_wrapper::OpenNIDriver::getInstance ();
+  if (arg == "--help" || arg == "-h" || driver.getNumberDevices () == 0)
   {
     usage (argv);
     return 1;
   }
+
+  std::cout << "Press following keys to switch to the different integral image normal estimation methods:\n";
+  std::cout << "<1> COVARIANCE_MATRIX method\n";
+  std::cout << "<2> AVERAGE_3D_GRADIENT method\n";
+  std::cout << "<3> AVERAGE_DEPTH_CHANGE method\n";
+  std::cout << "<4> SIMPLE_3D_GRADIENT method\n";
+  std::cout << "<Q,q> quit\n\n";
 
   pcl::OpenNIGrabber grabber ("");
   if (grabber.providesCallback<pcl::OpenNIGrabber::sig_cb_openni_point_cloud_rgb> ())
