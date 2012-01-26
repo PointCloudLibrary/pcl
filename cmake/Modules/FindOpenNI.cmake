6028267d01ad419b810224eaf80bad566dffb376
Nizar Sallem <nksallem@laas.fr>
2012-01-26 20:53:28 +0000

    Fix for issue #506
    Quit earlier if libusb-1.0 is not found to prevent OPENNI_FOUND from being set
    
    
    
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4036 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/Modules/FindOpenNI.cmake
+++ cmake/Modules/FindOpenNI.cmake
@@ -23,15 +23,22 @@ if(NOT WIN32)
                PATH_SUFFIXES lib)
    include(FindPackageHandleStandardArgs)
    find_package_handle_standard_args(USB_10 DEFAULT_MSG USB_10_LIBRARY USB_10_INCLUDE_DIR)
+   if(NOT USB_10_FOUND)
+     message(STATUS "OpeNI disabled because libusb-1.0 not found.")
+     set(HAVE_OPENNI OFF)
+     return()
+   else(NOT USB_10_FOUND)
+     include_directories(${USB_10_INCLUDE_DIR})
+   endif(NOT USB_10_FOUND)
 endif(NOT WIN32)
-
+ 
 if(${CMAKE_VERSION} VERSION_LESS 2.8.2)
   pkg_check_modules(PC_OPENNI openni-dev)
 else()
   pkg_check_modules(PC_OPENNI QUIET openni-dev)
 endif()
 
-set(OPENNI_DEFINITIONS ${PC_OPENNI_CFLAGS_OTHER} ${PC_USB_10_CFLAGS_OTHER})
+set(OPENNI_DEFINITIONS ${PC_OPENNI_CFLAGS_OTHER})
 
 #add a hint so that it can find it without the pkg-config
 find_path(OPENNI_INCLUDE_DIR XnStatus.h
@@ -43,7 +50,7 @@ find_library(OPENNI_LIBRARY
              NAMES OpenNI64 OpenNI
              HINTS ${PC_OPENNI_LIBDIR} ${PC_OPENNI_LIBRARY_DIRS} /usr/lib "${OPENNI_ROOT}" "$ENV{OPENNI_ROOT}"
              PATHS "$ENV{PROGRAMFILES}/OpenNI/Lib" "$ENV{PROGRAMW6432}/OpenNI/Lib64"
-			 PATH_SUFFIXES lib Lib Lib64)
+             PATH_SUFFIXES lib Lib Lib64)
 
 set(OPENNI_INCLUDE_DIRS ${OPENNI_INCLUDE_DIR})
 if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
@@ -57,14 +64,6 @@ find_package_handle_standard_args(OpenNI DEFAULT_MSG
     OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
     
 mark_as_advanced(OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
-if(NOT WIN32 AND NOT USB_10_FOUND)
-  message(STATUS "OpenNI disabled because libusb-1.0 not found.")
-  set(HAVE_OPENNI OFF)
-  set(OPENNI_FOUND OFF)
-  return()
-else(NOT WIN32 AND NOT USB_10_FOUND)
-	include_directories(${USB_10_INCLUDE_DIR})
-endif(NOT WIN32 AND NOT USB_10_FOUND)
 
 if(OPENNI_FOUND)
   set(HAVE_OPENNI ON)
