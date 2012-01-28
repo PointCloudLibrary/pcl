b83d3bb1162b6f2c62d24cfaacabe872a14195cd
Mourad Boufarguine <mourad@boufarguine.name>
2012-01-28 11:37:41 +0000

    fixed an internal error in msvc compiler 32bit
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4052 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- doc/tutorials/content/sources/cluster_extraction/cluster_extraction.cpp
+++ doc/tutorials/content/sources/cluster_extraction/cluster_extraction.cpp
@@ -1,9 +1,9 @@
 #include <pcl/ModelCoefficients.h>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
-#include <pcl/features/normal_3d.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
 #include <pcl/kdtree/kdtree.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
