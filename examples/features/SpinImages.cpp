942dad109d77d9b562cf84238e5a32c374fd4c06
David Doria <daviddoria@gmail.com>
2012-01-29 17:58:29 +0000

    Added SpinImages and ShapeContexts examples.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4061 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ examples/features/SpinImages.cpp
@@ -0,0 +1,91 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/spin_image.h>
+
+int
+main (int argc, char** argv) {
+
+  std::string filename = argv[1];
+  std::cout << "Reading " << filename << std::endl;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  if (pcl::io::loadPCDFile <pcl::PointXYZ> (filename.c_str(), *cloud) == -1)
+  // load the file
+  {
+    PCL_ERROR ("Couldn't read file");
+    return (-1);
+  }
+  std::cout << "Loaded " << cloud->points.size() << " points." << std::endl;
+
+  // Compute the normals
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimation;
+  normal_estimation.setInputCloud (cloud);
+
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZ>);
+  normal_estimation.setSearchMethod (kdtree);
+
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud< pcl::Normal>);
+  normal_estimation.setRadiusSearch (0.03);
+  normal_estimation.compute (*normals);
+
+  // Setup spin image computation
+  pcl::SpinImageEstimation<pcl::PointXYZ, pcl::Normal, pcl::Histogram<153>> spin_image_descriptor(8, 0.5, 16);
+  spin_image_descriptor.setInputWithNormals(cloud, normals);
+
+  // Use the same KdTree from the normal estimation
+  spin_image_descriptor.setSearchMethod (kdtree);
+  pcl::PointCloud<pcl::Histogram<153>>::Ptr spin_images(new pcl::PointCloud<pcl::Histogram<153>>);
+  spin_image_descriptor.setRadiusSearch (0.2);
+
+  // Actually compute the spin images
+  spin_image_descriptor.compute (*spin_images);
+  std::cout << "SI output points.size (): " << spin_images->points.size () << std::endl;
+
+  // Display and retrieve the spin image descriptor vector for the first point.
+  pcl::Histogram<153> first_descriptor = spin_images->points[0];
+  std::cout << first_descriptor << std::endl;
+
+  return 0;
+}
