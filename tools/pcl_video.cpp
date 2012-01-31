0b7a68e356ad7b2ddb8dcf5ef21cc10fbb5639ff
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2012-01-31 09:06:38 +0000

    Don't make corrupt files. Added playback support.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4108 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- tools/pcl_video.cpp
+++ tools/pcl_video.cpp
@@ -35,6 +35,7 @@
  */
 
 #include <boost/date_time/gregorian/gregorian_types.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
@@ -52,6 +53,7 @@
 #include <pcl/io/openni_grabber.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/ros/conversions.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 namespace bpt = boost::posix_time;
 
@@ -61,7 +63,7 @@ std::fstream* stream(0);
 bpt::ptime start;
 bpt::ptime c_start;
 bpt::time_duration c_td;
-tide::MemoryCluster* cluster;
+tide::MemoryCluster* cluster(0);
 bool new_cluster(true);
 
 
@@ -76,7 +78,8 @@ void record_cb(pcl::PointCloud<pcl::PointXYZ>::ConstPtr const& cloud)
         cluster->timecode(c_td.total_microseconds() / 10000);
         cluster->write(*stream);
         new_cluster = false;
-        std::cerr << "Starting new cluster at " << cluster->timecode() << '\n';
+        std::cerr << "Starting new cluster at " << c_start << " (" <<
+            cluster->timecode() << ")\n";
     }
     // When creating a block, the track number must be specified. Currently,
     // all blocks belong to track 1 (because this program only records one
@@ -106,18 +109,167 @@ void record_cb(pcl::PointCloud<pcl::PointXYZ>::ConstPtr const& cloud)
         // Finalise the cluster
         cluster->finalise(*stream);
         delete cluster;
+        cluster = 0;
         // Record that a new cluster is needed
         new_cluster = true;
     }
 }
 
 
+int playback(std::string const& filename)
+{
+    // Open the file and check for the EBML header. This confirms that the file
+    // is an EBML file, and is a Tide document.
+    std::ifstream stream(filename, std::ios::in);
+    tide::ids::ReadResult id = tide::ids::read(stream);
+    if (id.first != tide::ids::EBML)
+    {
+        std::cerr << "File does not begin with an EBML header.\n";
+        return 1;
+    }
+    tide::EBMLElement ebml_el;
+    ebml_el.read(stream);
+    if (ebml_el.doc_type() != tide::TideDocType)
+    {
+        std::cerr << "Specified EBML file is not a Tide document.\n";
+        return 1;
+    }
+    if (ebml_el.read_version() > tide::TideEBMLVersion)
+    {
+        std::cerr << "This Tide document requires read version " <<
+            ebml_el.read_version() << ".\n";
+        return 1;
+    }
+    if (ebml_el.doc_read_version() > tide::TideVersionMajor)
+    {
+        std::cerr << "This Tide document requires doc read version " <<
+            ebml_el.read_version() << ".\n";
+        return 1;
+    }
+    std::cerr << "Found EBML header\n";
+
+    // Open the file's segment. This will read some meta-data about the segment
+    // and read (or build, if necessary) an index of the level 1 elements. With
+    // this index, we will be able to quickly jump to important elements such
+    // as the Tracks and the first Cluster.
+    id = tide::ids::read(stream);
+    if (id.first != tide::ids::Segment)
+    {
+        std::cerr << "Segment element not found\n";
+        return 1;
+    }
+    tide::Segment segment;
+    segment.read(stream);
+    // The segment's date is stored as the number of nanoseconds since the
+    // start of the millenium. Boost::Date_Time is invaluable here.
+    bpt::ptime basis(boost::gregorian::date(2001, 1, 1));
+    bpt::time_duration sd(bpt::microseconds(segment.info.date() / 1000));
+    bpt::ptime start(basis + sd);
+
+    // The segment is now open and we can start reading its child elements. To
+    // begin with, we get the tracks element (their may be more than one, if
+    // the document was created by merging other documents) but generally only
+    // one will exist).
+    // We can guarantee that there is at least one in the index because
+    // otherwise the call to segment.read() would have thrown an error.
+    std::streampos tracks_pos(segment.index.find(tide::ids::Tracks)->second);
+    stream.seekg(segment.to_stream_offset(tracks_pos));
+    // To be sure, we can check it really is a Tracks element, but this is
+    // usually not necessary.
+    id = tide::ids::read(stream);
+    if (id.first != tide::ids::Tracks)
+    {
+        std::cerr << "Tracks element not at indicated position.\n";
+        return 1;
+    }
+    // Read the tracks
+    tide::Tracks tracks;
+    tracks.read(stream);
+    // Now we can introspect the tracks available in the file.
+    if (tracks.empty())
+    {
+        std::cerr << "No tracks found.\n";
+        return 1;
+    }
+    // Let's check that the file contains the codec we expect for the first
+    // track.
+    if (tracks[1]->codec_id() != "pointcloud2")
+    {
+        std::cerr << "Track #1 has wrong codec type " <<
+            tracks[1]->codec_id() << '\n';
+        return 1;
+    }
+
+    // Before we start reading frames, we need to set up a viewer to display
+    // them.
+    pcl::visualization::CloudViewer viewer("3D Video Player");
+
+    // Now we can start reading the clusters. Get an iterator to the clusters
+    // in the segment.
+    // In this case, we are using a memory-based cluster implementation, which
+    // reads all blocks in the cluster in one go and stores them in memory. For
+    // larger quantities of data (such as point clouds), using the in-file
+    // cluster implementation will use significantly less memory.
+    for (tide::Segment::MemClusterIterator cluster(segment.clusters_begin(stream));
+            cluster != segment.clusters_end(stream); ++cluster)
+    {
+        for (tide::MemoryCluster::Iterator block(cluster->begin());
+                block != cluster->end(); ++block)
+        {
+            // Some blocks may actually contain multiple frames in a lace. In
+            // this case, we are reading blocks that do not use lacing, so
+            // there is only one frame per block. This is the general case;
+            // lacing is typically only used when the frame size is very small
+            // to reduce overhead.
+            tide::BlockElement::Ptr first_block(*block);
+            tide::BlockElement::FramePtr frame_data(*(first_block->begin()));
+            // Copy the frame data into a serialised cloud structure
+            sensor_msgs::PointCloud2 blob;
+            blob.height = 480;
+            blob.width = 640;
+            sensor_msgs::PointField ptype;
+            ptype.name = "x";
+            ptype.offset = 0;
+            ptype.datatype = 7;
+            ptype.count = 1;
+            blob.fields.push_back(ptype);
+            ptype.name = "y";
+            ptype.offset = 4;
+            ptype.datatype = 7;
+            ptype.count = 1;
+            blob.fields.push_back(ptype);
+            ptype.name = "z";
+            ptype.offset = 8;
+            ptype.datatype = 7;
+            ptype.count = 1;
+            blob.fields.push_back(ptype);
+            blob.is_bigendian = false;
+            blob.point_step = 16;
+            blob.row_step = 10240;
+            blob.is_dense = false;
+            blob.data.assign(frame_data->begin(), frame_data->end());
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::fromROSMsg(blob, *cloud);
+            viewer.showCloud(cloud);
+        }
+    }
+
+    return 0;
+}
+
+
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    // TODO: proper args parsing using the existing stuff
+    if (argc < 2)
     {
         std::cerr << "Usage: " << argv[0] << " <file name>\n";
         return 1;
+    }
+    if (argc == 3)
+    {
+        return playback(argv[1]);
     }
 
     // Open a new file and write the EBML Header. This specifies that the file
@@ -147,12 +299,12 @@ int main(int argc, char** argv)
     // but here we set it to ten milliseconds for demonstrative purposes.
     segment.info.timecode_scale(10000000);
     // The segment's date should be set. It is the somewhat-awkward value of
-    // the number of seconds since the start of the millenium. Boost::Date_Time
+    // the number of seconds since the start of the millennium. Boost::Date_Time
     // to the rescue!
     bpt::ptime basis(boost::gregorian::date(2001, 1, 1));
     start = boost::posix_time::microsec_clock::local_time();
     bpt::time_duration td = start - basis;
-    segment.info.date(td.total_seconds());
+    segment.info.date(td.total_microseconds() * 1000);
     // Let's give the segment an inspirational title.
     segment.info.title("World's first true 3D video");
     // It sometimes helps to know what created a Tide file.
@@ -199,18 +351,28 @@ int main(int argc, char** argv)
     boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f(
             boost::bind(&record_cb, _1));
     interface->registerCallback(f);
+    c_start = bpt::ptime(bpt::microsec_clock::local_time());
     interface->start();
 
+    std::cerr << "Segment starts at " << start << " (" <<
+        td.total_microseconds() * 1000 << ")\n";
     std::cout << "Recording frames. Press any key to stop.\n";
     getchar();
 
     interface->stop();
+    // Close the last open cluster
+    if (cluster)
+    {
+        cluster->finalise(*stream);
+        delete cluster;
+    }
 
     // Now that the data has been written, the last thing to do is to finalise
     // the segment.
     segment.finalise(*stream);
     // And finally, close the file.
     stream->close();
+    delete stream;
 
     return 0;
 }
