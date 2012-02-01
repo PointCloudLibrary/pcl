7658e37f8ecc4d404abb2fa99b2275a0588bb2b0
Alexandru Eugen Ichim <alex.e.ichim@gmail.com>
2012-02-01 13:34:27 +0000

    examples/ added another license
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4174 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- examples/features/example_PointFeatureHistograms.cpp
+++ examples/features/example_PointFeatureHistograms.cpp
@@ -1,3 +1,42 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Point Cloud Library (PCL) - www.pointclouds.org
+ * Copyright (c) 2009-2011, Willow Garage, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ * * Neither the name of Willow Garage, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
 #include <iostream>
 #include <vector>
 
@@ -5,6 +44,7 @@
 #include <pcl/point_types.h>
 #include <pcl/features/pfh.h>
 #include <pcl/features/normal_3d.h>
+
 
 int
 main (int argc, char** argv)
@@ -20,7 +60,7 @@ main (int argc, char** argv)
     return (-1);
   }
 
-  std::cout << "Loaded " << cloud->points.size() << " points." << std::endl;
+  std::cout << "Loaded " << cloud->points.size () << " points." << std::endl;
 
   // Compute the normals
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimation;
@@ -41,13 +81,13 @@ main (int argc, char** argv)
   // Provide the original point cloud (without normals)
   pfhEstimation.setInputCloud (cloud);
   // Provide the point cloud with normals
-  pfhEstimation.setInputNormals(cloudWithNormals);
+  pfhEstimation.setInputNormals (cloudWithNormals);
 
-  // pfhEstimation.setInputWithNormals(cloud, cloudWithNormals); PFHEstimation does not have this function
+  // pfhEstimation.setInputWithNormals (cloud, cloudWithNormals); PFHEstimation does not have this function
   // Use the same KdTree from the normal estimation
   pfhEstimation.setSearchMethod (tree);
 
-  pcl::PointCloud<pcl::PFHSignature125>::Ptr pfhFeatures(new pcl::PointCloud<pcl::PFHSignature125>);
+  pcl::PointCloud<pcl::PFHSignature125>::Ptr pfhFeatures (new pcl::PointCloud<pcl::PFHSignature125>);
 
   pfhEstimation.setRadiusSearch (0.2);
 
