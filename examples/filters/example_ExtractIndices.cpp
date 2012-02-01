f46e7eadf680420899acce216dde11937c932b73
David Doria <daviddoria@gmail.com>
2012-02-01 21:05:23 +0000

    Add an example of extracting specified points.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4188 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ examples/filters/example_ExtractIndices.cpp
@@ -0,0 +1,72 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Point Cloud Library (PCL) - www.pointclouds.org
+ * Copyright (c) 2009-2011, Willow Garage, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ * * Neither the name of Willow Garage, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: example_ExtractIndices.cpp 4117 2012-01-31 17:56:02Z aichim $
+ *
+ */
+
+#include <iostream>
+
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+
+int main (int argc, char** argv)
+{
+  typedef pcl::PointXYZ PointType;
+  typedef pcl::PointCloud<PointType> CloudType;
+  CloudType::Ptr cloud (new CloudType);
+  cloud->is_dense = false;
+  PointType p;
+  for(unsigned int i = 0; i < 5; ++i)
+  {
+    p.x = i; p.y = i; p.z = i;
+    cloud->push_back(p);
+  }
+
+  std::cout << "Cloud has " << cloud->points.size() << " points." << std::endl;
+
+  pcl::PointIndices indices;
+  indices.indices.push_back(0);
+  indices.indices.push_back(2);
+
+  pcl::ExtractIndices<PointType> extractIndices;
+  extractIndices.setIndices(boost::make_shared<const pcl::PointIndices> (indices));
+  extractIndices.setInputCloud(cloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr output(new pcl::PointCloud<pcl::PointXYZ>);
+  extractIndices.filter(*output);
+
+  std::cout << "Output has " << output->points.size() << " points." << std::endl;
+  return (0);
+}
