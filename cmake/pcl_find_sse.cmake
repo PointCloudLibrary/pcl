a583e22dadea7c26d17311d9be157d0f97256655
Mourad Boufarguine <mourad@boufarguine.name>
2012-02-09 08:36:02 +0000

    refactor pcl_find_sse.cmake : * test all SSE extensions on Windows * on Windows, SSE specific flags are only needed for x86 * move /fp:fast flag to the main CMakeLists.txt
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4334 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/pcl_find_sse.cmake
+++ cmake/pcl_find_sse.cmake
@@ -2,137 +2,132 @@
 # Check for the presence of SSE and figure out the flags to use for it.
 macro(PCL_CHECK_FOR_SSE)
     include(CheckCXXSourceRuns)
+    set(SSE_FLAGS)
+    set(CMAKE_REQUIRED_FLAGS)
+
+    check_cxx_source_runs("
+        #include <mm_malloc.h>
+        int main()
+        {
+          void* mem = _mm_malloc (100, 16);
+          return 0;
+        }"
+        HAVE_MM_MALLOC)
+
+    check_cxx_source_runs("
+        #include <stdlib.h>
+        int main()
+        {
+          void* mem;
+          return posix_memalign (&mem, 16, 100);
+        }"
+        HAVE_POSIX_MEMALIGN)
+
     if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
-        set(SSE_FLAGS)
-
-        check_cxx_source_runs("
-            #include <mm_malloc.h>
-            int main()
-            {
-              void* mem = _mm_malloc (100, 16);
-              return 0;
-            }"
-            HAVE_MM_MALLOC)
-
-        check_cxx_source_runs("
-            #include <stdlib.h>
-            int main()
-            {
-              void* mem;
-              return posix_memalign (&mem, 16, 100);
-            }"
-            HAVE_POSIX_MEMALIGN)
-
         set(CMAKE_REQUIRED_FLAGS "-msse4.1")
-        check_cxx_source_runs("
-            #include <smmintrin.h>
-            int main()
-            {
-                __m128 a, b;
-                float vals[4] = {1, 2, 3, 4};
-                const int mask = 123;
-                a = _mm_loadu_ps(vals);
-                b = a;
-                b = _mm_dp_ps (a, a, mask);
-                _mm_storeu_ps(vals,b);
-                return 0;
-            }"
-            HAVE_SSE4_1_EXTENSIONS)
-
-        set(CMAKE_REQUIRED_FLAGS "-msse3")
-        check_cxx_source_runs("
-            #include <pmmintrin.h>
-
-            int main()
-            {
-                __m128d a, b;
-                double vals[2] = {0};
-                a = _mm_loadu_pd(vals);
-                b = _mm_hadd_pd(a,a);
-                _mm_storeu_pd(vals, b);
-                return 0;
-            }"
-            HAVE_SSE3_EXTENSIONS)
-
-        set(CMAKE_REQUIRED_FLAGS "-msse2")
-        check_cxx_source_runs("
-            #include <emmintrin.h>
-
-            int main()
-            {
-                __m128d a, b;
-                double vals[2] = {0};
-                a = _mm_loadu_pd(vals);
-                b = _mm_add_pd(a,a);
-                _mm_storeu_pd(vals,b);
-                return 0;
-            }"
-            HAVE_SSE2_EXTENSIONS)
-
-        set(CMAKE_REQUIRED_FLAGS "-msse")
-        check_cxx_source_runs("
-            #include <xmmintrin.h>
-            int main()
-            {
-                __m128 a, b;
-                float vals[4] = {0};
-                a = _mm_loadu_ps(vals);
-                b = a;
-                b = _mm_add_ps(a,b);
-                _mm_storeu_ps(vals,b);
-                return 0;
-            }"
-            HAVE_SSE_EXTENSIONS)
-
-       set(CMAKE_REQUIRED_FLAGS)
-
-       if (NOT APPLE)
-         SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++0x -march=native")
-       endif (NOT APPLE)
-
-       if (HAVE_SSE4_1_EXTENSIONS)
-           if (APPLE)
-             SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse4.1 -mfpmath=sse")
-           endif (APPLE)
-           message(STATUS "Found SSE4.1 extensions, using flags: ${SSE_FLAGS}")
-       elseif(HAVE_SSE3_EXTENSIONS)
-           if (APPLE)
-             SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse3 -mfpmath=sse")
-           endif (APPLE)
-           message(STATUS "Found SSE3 extensions, using flags: ${SSE_FLAGS}")
-       elseif(HAVE_SSE2_EXTENSIONS)
-           if (APPLE)
-             SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse2 -mfpmath=sse")
-           endif (APPLE)
-           message(STATUS "Found SSE2 extensions, using flags: ${SSE_FLAGS}")
-       elseif(HAVE_SSE_EXTENSIONS)
-           if (APPLE)
-             SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse -mfpmath=sse")
-           endif (APPLE)
-           message(STATUS "Found SSE extensions, using flags: ${SSE_FLAGS}")
-       else (HAVE_SSE4_1_EXTENSIONS)
-           message(STATUS "No SSE extensions found")
-       endif(HAVE_SSE4_1_EXTENSIONS)
-
-    elseif(MSVC)
-
-        check_cxx_source_runs("
-            #include <emmintrin.h>
-
-            int main()
-            {
-                __m128d a, b;
-                double vals[2] = {0};
-                a = _mm_loadu_pd(vals);
-                b = _mm_add_pd(a,a);
-                _mm_storeu_pd(vals,b);
-                return 0;
-            }"
-            HAVE_SSE2_EXTENSIONS)
-
-        if(HAVE_SSE2_EXTENSIONS)
-            SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /arch:SSE2 /fp:fast -D__SSE__ -D__SSE2__")
-            message(STATUS "Found SSE2 extensions, using flags: ${SSE_FLAGS}")
-        endif(HAVE_SSE2_EXTENSIONS)
     endif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+
+    check_cxx_source_runs("
+        #include <smmintrin.h>
+        int main()
+        {
+          __m128 a, b;
+          float vals[4] = {1, 2, 3, 4};
+          const int mask = 123;
+          a = _mm_loadu_ps(vals);
+          b = a;
+          b = _mm_dp_ps (a, a, mask);
+          _mm_storeu_ps(vals,b);
+          return 0;
+        }"
+        HAVE_SSE4_1_EXTENSIONS)
+
+    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+        set(CMAKE_REQUIRED_FLAGS "-msse3")
+    endif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+
+    check_cxx_source_runs("
+        #include <pmmintrin.h>
+        int main()
+        {
+            __m128d a, b;
+            double vals[2] = {0};
+            a = _mm_loadu_pd(vals);
+            b = _mm_hadd_pd(a,a);
+            _mm_storeu_pd(vals, b);
+            return 0;
+        }"
+        HAVE_SSE3_EXTENSIONS)
+
+    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+        set(CMAKE_REQUIRED_FLAGS "-msse2")
+    elseif(MSVC AND CMAKE_SIZE_VOID_P EQUAL 4)
+        set(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
+    endif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+    
+    check_cxx_source_runs("
+        #include <emmintrin.h>
+        int main()
+        {
+            __m128d a, b;
+            double vals[2] = {0};
+            a = _mm_loadu_pd(vals);
+            b = _mm_add_pd(a,a);
+            _mm_storeu_pd(vals,b);
+            return 0;
+        }"
+        HAVE_SSE2_EXTENSIONS)
+
+    if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+        set(CMAKE_REQUIRED_FLAGS "-msse")
+    elseif(MSVC AND CMAKE_SIZE_VOID_P EQUAL 4)
+        set(CMAKE_REQUIRED_FLAGS "/arch:SSE")
+    endif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+
+    check_cxx_source_runs("
+        #include <xmmintrin.h>
+        int main()
+        {
+            __m128 a, b;
+            float vals[4] = {0};
+            a = _mm_loadu_ps(vals);
+            b = a;
+            b = _mm_add_ps(a,b);
+            _mm_storeu_ps(vals,b);
+            return 0;
+        }"
+        HAVE_SSE_EXTENSIONS)
+
+    set(CMAKE_REQUIRED_FLAGS)
+
+    if (NOT APPLE AND NOT MSVC)
+        SET(SSE_FLAGS "${SSE_FLAGS} -std=gnu++0x -march=native")
+    elseif (APPLE)
+        if (HAVE_SSE4_1_EXTENSIONS)
+            SET(SSE_FLAGS "${SSE_FLAGS} -msse4.1 -mfpmath=sse")
+            message(STATUS "Found SSE4.1 extensions, using flags: ${SSE_FLAGS}")
+        elseif(HAVE_SSE3_EXTENSIONS)
+            SET(SSE_FLAGS "${SSE_FLAGS} -msse3 -mfpmath=sse")
+            message(STATUS "Found SSE3 extensions, using flags: ${SSE_FLAGS}")
+        elseif(HAVE_SSE2_EXTENSIONS)
+            SET(SSE_FLAGS "${SSE_FLAGS} -msse2 -mfpmath=sse")
+            message(STATUS "Found SSE2 extensions, using flags: ${SSE_FLAGS}")
+        elseif(HAVE_SSE_EXTENSIONS)
+            SET(SSE_FLAGS "${SSE_FLAGS} -msse -mfpmath=sse")
+            message(STATUS "Found SSE extensions, using flags: ${SSE_FLAGS}")
+        else (HAVE_SSE4_1_EXTENSIONS)
+            message(STATUS "No SSE extensions found")
+        endif(HAVE_SSE4_1_EXTENSIONS)
+    elseif (MSVC AND CMAKE_SIZE_VOID_P EQUAL 4)
+        if(HAVE_SSE2_EXTENSIONS)
+            SET(SSE_FLAGS "${SSE_FLAGS} /arch:SSE2")
+            message(STATUS "Found SSE2 extensions, using flags: ${SSE_FLAGS}")
+        elseif(HAVE_SSE_EXTENSIONS)
+            SET(SSE_FLAGS "${SSE_FLAGS} /arch:SSE")
+            message(STATUS "Found SSE extensions, using flags: ${SSE_FLAGS}")
+        endif(HAVE_SSE2_EXTENSIONS)
+    endif (NOT APPLE AND NOT MSVC)
+   
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SSE_FLAGS}")
+
 endmacro(PCL_CHECK_FOR_SSE)
