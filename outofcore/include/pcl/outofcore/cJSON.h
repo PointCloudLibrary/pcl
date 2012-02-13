dc0508f73f702086e601adaf3af88e9c96c65fe0
Mourad Boufarguine <mourad@boufarguine.name>
2012-02-13 23:03:43 +0000

    export symbols from outofcore for Windows
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4446 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- outofcore/include/pcl/outofcore/cJSON.h
+++ outofcore/include/pcl/outofcore/cJSON.h
@@ -23,6 +23,8 @@
 #ifndef cJSON__h
 #define cJSON__h
 
+#include <pcl/pcl_macros.h>
+
 //make interop with c++ string easier
 #include <string>
 
@@ -62,64 +64,64 @@ typedef struct cJSON_Hooks {
 } cJSON_Hooks;
 
 /* Supply malloc, realloc and free functions to cJSON */
-extern void cJSON_InitHooks(cJSON_Hooks* hooks);
+PCLAPI(void) cJSON_InitHooks(cJSON_Hooks* hooks);
 
 
 /* Supply a block of JSON, and this returns a cJSON object you can interrogate. Call cJSON_Delete when finished. */
-extern cJSON *cJSON_Parse(const char *value);
+PCLAPI(cJSON *) cJSON_Parse(const char *value);
 /* Render a cJSON entity to text for transfer/storage. Free the char* when finished. */
-extern char  *cJSON_Print(cJSON *item);
+PCLAPI(char  *) cJSON_Print(cJSON *item);
 /* Render a cJSON entity to text for transfer/storage without any formatting. Free the char* when finished. */
-extern char  *cJSON_PrintUnformatted(cJSON *item);
+PCLAPI(char  *) cJSON_PrintUnformatted(cJSON *item);
 /* Delete a cJSON entity and all subentities. */
-extern void   cJSON_Delete(cJSON *c);
+PCLAPI(void)   cJSON_Delete(cJSON *c);
 /* Render a cJSON entity to text for transfer/storage. */
-extern void cJSON_PrintStr(cJSON *item, std::string& s);
+PCLAPI(void) cJSON_PrintStr(cJSON *item, std::string& s);
 /* Render a cJSON entity to text for transfer/storage without any formatting. */
-extern void cJSON_PrintUnformattedStr(cJSON *item, std::string& s);
+PCLAPI(void) cJSON_PrintUnformattedStr(cJSON *item, std::string& s);
 
 /* Returns the number of items in an array (or object). */
-extern int	  cJSON_GetArraySize(cJSON *array);
+PCLAPI(int)	  cJSON_GetArraySize(cJSON *array);
 /* Retrieve item number "item" from array "array". Returns NULL if unsuccessful. */
-extern cJSON *cJSON_GetArrayItem(cJSON *array,int item);
+PCLAPI(cJSON *) cJSON_GetArrayItem(cJSON *array,int item);
 /* Get item "string" from object. Case insensitive. */
-extern cJSON *cJSON_GetObjectItem(cJSON *object,const char *string);
+PCLAPI(cJSON *) cJSON_GetObjectItem(cJSON *object,const char *string);
 
 /* For analysing failed parses. This returns a pointer to the parse error. You'll probably need to look a few chars back to make sense of it. Defined when cJSON_Parse() returns 0. 0 when cJSON_Parse() succeeds. */
-extern const char *cJSON_GetErrorPtr();
+PCLAPI(const char *) cJSON_GetErrorPtr();
 	
 /* These calls create a cJSON item of the appropriate type. */
-extern cJSON *cJSON_CreateNull();
-extern cJSON *cJSON_CreateTrue();
-extern cJSON *cJSON_CreateFalse();
-extern cJSON *cJSON_CreateBool(int b);
-extern cJSON *cJSON_CreateNumber(double num);
-extern cJSON *cJSON_CreateString(const char *string);
-extern cJSON *cJSON_CreateArray();
-extern cJSON *cJSON_CreateObject();
+PCLAPI(cJSON *) cJSON_CreateNull();
+PCLAPI(cJSON *) cJSON_CreateTrue();
+PCLAPI(cJSON *) cJSON_CreateFalse();
+PCLAPI(cJSON *) cJSON_CreateBool(int b);
+PCLAPI(cJSON *) cJSON_CreateNumber(double num);
+PCLAPI(cJSON *) cJSON_CreateString(const char *string);
+PCLAPI(cJSON *) cJSON_CreateArray();
+PCLAPI(cJSON *) cJSON_CreateObject();
 
 /* These utilities create an Array of count items. */
-extern cJSON *cJSON_CreateIntArray(int *numbers,int count);
-extern cJSON *cJSON_CreateFloatArray(float *numbers,int count);
-extern cJSON *cJSON_CreateDoubleArray(double *numbers,int count);
-extern cJSON *cJSON_CreateStringArray(const char **strings,int count);
+PCLAPI(cJSON *) cJSON_CreateIntArray(int *numbers,int count);
+PCLAPI(cJSON *) cJSON_CreateFloatArray(float *numbers,int count);
+PCLAPI(cJSON *) cJSON_CreateDoubleArray(double *numbers,int count);
+PCLAPI(cJSON *) cJSON_CreateStringArray(const char **strings,int count);
 
 /* Append item to the specified array/object. */
-extern void cJSON_AddItemToArray(cJSON *array, cJSON *item);
-extern void	cJSON_AddItemToObject(cJSON *object,const char *string,cJSON *item);
+PCLAPI(void) cJSON_AddItemToArray(cJSON *array, cJSON *item);
+PCLAPI(void) cJSON_AddItemToObject(cJSON *object,const char *string,cJSON *item);
 /* Append reference to item to the specified array/object. Use this when you want to add an existing cJSON to a new cJSON, but don't want to corrupt your existing cJSON. */
-extern void cJSON_AddItemReferenceToArray(cJSON *array, cJSON *item);
-extern void	cJSON_AddItemReferenceToObject(cJSON *object,const char *string,cJSON *item);
+PCLAPI(void) cJSON_AddItemReferenceToArray(cJSON *array, cJSON *item);
+PCLAPI(void) cJSON_AddItemReferenceToObject(cJSON *object,const char *string,cJSON *item);
 
 /* Remove/Detatch items from Arrays/Objects. */
-extern cJSON *cJSON_DetachItemFromArray(cJSON *array,int which);
-extern void   cJSON_DeleteItemFromArray(cJSON *array,int which);
-extern cJSON *cJSON_DetachItemFromObject(cJSON *object,const char *string);
-extern void   cJSON_DeleteItemFromObject(cJSON *object,const char *string);
+PCLAPI(cJSON *) cJSON_DetachItemFromArray(cJSON *array,int which);
+PCLAPI(void)    cJSON_DeleteItemFromArray(cJSON *array,int which);
+PCLAPI(cJSON *) cJSON_DetachItemFromObject(cJSON *object,const char *string);
+PCLAPI(void)    cJSON_DeleteItemFromObject(cJSON *object,const char *string);
 	
 /* Update array items. */
-extern void cJSON_ReplaceItemInArray(cJSON *array,int which,cJSON *newitem);
-extern void cJSON_ReplaceItemInObject(cJSON *object,const char *string,cJSON *newitem);
+PCLAPI(void) cJSON_ReplaceItemInArray(cJSON *array,int which,cJSON *newitem);
+PCLAPI(void) cJSON_ReplaceItemInObject(cJSON *object,const char *string,cJSON *newitem);
 
 #define cJSON_AddNullToObject(object,name)	cJSON_AddItemToObject(object, name, cJSON_CreateNull())
 #define cJSON_AddTrueToObject(object,name)	cJSON_AddItemToObject(object, name, cJSON_CreateTrue())
