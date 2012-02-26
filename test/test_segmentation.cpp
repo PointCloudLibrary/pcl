662a5d7fb9c516e9fcfa657b49439b48607e7d21
Sergey Ushakov <mine_all_mine@bk.ru>
2012-02-26 22:46:41 +0000

    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4756 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- test/test_segmentation.cpp
+++ test/test_segmentation.cpp
@@ -42,9 +42,12 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/search/search.h>
+#include <pcl/features/normal_3d.h>
 
 #include <pcl/segmentation/extract_polygonal_prism_data.h>
 #include <pcl/segmentation/segment_differences.h>
+#include <pcl/segmentation/region_growing.h>
 
 using namespace pcl;
 using namespace pcl::io;
@@ -52,6 +55,139 @@ using namespace pcl::io;
 PointCloud<PointXYZ>::Ptr cloud_;
 PointCloud<PointXYZ>::Ptr cloud_t_;
 KdTree<PointXYZ>::Ptr tree_;
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr another_cloud_;
+pcl::PointCloud<pcl::Normal>::Ptr normals_;
+pcl::PointCloud<pcl::Normal>::Ptr another_normals_;
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+TEST (RegionGrowingTest, Segment)
+{
+  pcl::RegionGrowing<pcl::PointXYZ> rg;
+  rg.setCloud(cloud_);
+  rg.setNormals(normals_);
+
+  int num_of_segments = rg.segmentPoints();
+  EXPECT_NE(0, num_of_segments);
+
+  std::vector<std::list<int>> segments;
+  segments = rg.getSegments();
+  EXPECT_NE(0, segments.size());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+TEST (RegionGrowingTest, SegmentWithoutCloud)
+{
+  pcl::RegionGrowing<pcl::PointXYZ> rg;
+  rg.setNormals(normals_);
+
+  int num_of_segments = rg.segmentPoints();
+  EXPECT_EQ(0, num_of_segments);
+
+  std::vector<std::list<int>> segments;
+  segments = rg.getSegments();
+  EXPECT_EQ(0, segments.size());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+TEST (RegionGrowingTest, SegmentWithoutNormals)
+{
+  pcl::RegionGrowing<pcl::PointXYZ> rg;
+  rg.setCloud(cloud_);
+
+  int num_of_segments = rg.segmentPoints();
+  EXPECT_EQ(0, num_of_segments);
+
+  std::vector<std::list<int>> segments;
+  segments = rg.getSegments();
+  EXPECT_EQ(0, segments.size());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+TEST (RegionGrowingTest, SegmentEmptyCloud)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr empty_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::Normal>::Ptr empty_normals(new pcl::PointCloud<pcl::Normal>);
+
+  pcl::RegionGrowing<pcl::PointXYZ> rg;
+  rg.setCloud(empty_cloud);
+  rg.setNormals(empty_normals);
+
+  int num_of_segments = rg.segmentPoints();
+  EXPECT_EQ(0, num_of_segments);
+
+  std::vector<std::list<int>> segments;
+  segments = rg.getSegments();
+  EXPECT_EQ(0, segments.size());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+TEST (RegionGrowingTest, SegmentWithDifferentNormalAndCloudSize)
+{
+  pcl::RegionGrowing<pcl::PointXYZ> rg;
+  rg.setCloud(another_cloud_);
+  rg.setNormals(normals_);
+
+  int first_cloud_size = cloud_->points.size();
+  int second_cloud_size = another_cloud_->points.size();
+  ASSERT_NE(first_cloud_size, second_cloud_size);
+
+  int num_of_segments = rg.segmentPoints();
+  EXPECT_EQ(0, num_of_segments);
+
+  std::vector<std::list<int>> segments;
+  segments = rg.getSegments();
+  EXPECT_EQ(0, segments.size());
+
+  rg.setCloud(cloud_);
+  rg.setNormals(another_normals_);
+
+  num_of_segments = rg.segmentPoints();
+  EXPECT_EQ(0, num_of_segments);
+
+  segments = rg.getSegments();
+  EXPECT_EQ(0, segments.size());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+TEST (RegionGrowingTest, SegmentWithWrongThresholdParameters)
+{
+  pcl::RegionGrowing<pcl::PointXYZ> rg;
+  rg.setCloud(cloud_);
+  rg.setNormals(normals_);
+
+  rg.setNumberOfNeighbours(0);
+
+  int num_of_segments = rg.segmentPoints();
+  EXPECT_EQ(0, num_of_segments);
+
+  std::vector<std::list<int>> segments;
+  segments = rg.getSegments();
+  EXPECT_EQ(0, segments.size());
+
+  rg.setNumberOfNeighbours(30);
+  rg.setResidualThreshold(-10.0);
+
+  num_of_segments = rg.segmentPoints();
+  EXPECT_EQ(0, num_of_segments);
+
+  segments = rg.getSegments();
+  EXPECT_EQ(0, segments.size());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+TEST (RegionGrowingTest, SegmentFromPoint)
+{
+  pcl::RegionGrowing<pcl::PointXYZ> rg;
+
+  std::list<int> segment = rg.getSegmentFromPoint(0);
+  EXPECT_EQ(0, segment.size());
+
+  rg.setCloud(cloud_);
+  rg.setNormals(normals_);
+  segment = rg.getSegmentFromPoint(0);
+  EXPECT_NE(0, segment.size());
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 TEST (SegmentDifferences, Segmentation)
@@ -108,15 +244,21 @@ TEST (ExtractPolygonalPrism, Segmentation)
 int
   main (int argc, char** argv)
 {
-  if (argc < 2)
+  if (argc < 3)
   {
     std::cerr << "No test file given. Please download `bun0.pcd` and pass its path to the test." << std::endl;
+    std::cerr << "The test needs two files. Please, pass two paths of different pcd files." << std::endl;
     return (-1);
   }
 
   // Load a standard PCD file from disk
-  PointCloud<PointXYZ> cloud, cloud_t;
+  PointCloud<PointXYZ> cloud, cloud_t, another_cloud;
   if (loadPCDFile (argv[1], cloud) < 0)
+  {
+    std::cerr << "Failed to read test file. Please download `bun0.pcd` and pass its path to the test." << std::endl;
+    return (-1);
+  }
+  if (pcl::io::loadPCDFile (argv[2], another_cloud) < 0)
   {
     std::cerr << "Failed to read test file. Please download `bun0.pcd` and pass its path to the test." << std::endl;
     return (-1);
@@ -129,6 +271,18 @@ int
 
   cloud_   = cloud.makeShared ();
   cloud_t_ = cloud_t.makeShared ();
+
+  another_cloud_ = another_cloud.makeShared();
+  normals_ = (new pcl::PointCloud<pcl::Normal>)->makeShared();
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
+  normal_estimator.setInputCloud(cloud_);
+  normal_estimator.setKSearch(30);
+  normal_estimator.compute(*normals_);
+
+  another_normals_ = (new pcl::PointCloud<pcl::Normal>)->makeShared();
+  normal_estimator.setInputCloud(another_cloud_);
+  normal_estimator.setKSearch(30);
+  normal_estimator.compute(*another_normals_);
 
   testing::InitGoogleTest (&argc, argv);
   return (RUN_ALL_TESTS ());
