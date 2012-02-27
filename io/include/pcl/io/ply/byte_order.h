3056dabc09d592f17995a183aa829933620f40c4
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-02-27 22:25:17 +0000

    define Android as little endian
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4797 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- io/include/pcl/io/ply/byte_order.h
+++ io/include/pcl/io/ply/byte_order.h
@@ -59,7 +59,7 @@ namespace pcl
 #if (defined (__powerpc) || defined (__powerpc__) || defined (__POWERPC__) || defined (__ppc__) || defined (_M_PPC) || defined (__ARCH_PPC))
 #  define PLY_BIG_ENDIAN
 #elif (defined (i386) || defined (__i386__) || defined (__i386) || defined (_M_IX86) || defined (_X86_) || defined (__THW_INTEL__) || defined (__I86__) || defined (__INTEL__)) \
-  || (defined (__amd64__) || defined (__amd64) || defined (__x86_64__) || defined (__x86_64) || defined (_M_X64))
+  || (defined (__amd64__) || defined (__amd64) || defined (__x86_64__) || defined (__x86_64) || defined (_M_X64) || defined (ANDROID))
 #  define PLY_LITTLE_ENDIAN
 #else
 #  error
