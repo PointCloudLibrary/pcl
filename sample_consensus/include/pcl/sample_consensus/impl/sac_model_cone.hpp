74f6109f01906ef6b648c2971128b0455d754c49
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-02-27 04:46:45 +0000

    sync-ing with trunk
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/branches/pcl-1.x@4786 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- sample_consensus/include/pcl/sample_consensus/impl/sac_model_cone.hpp
+++ sample_consensus/include/pcl/sample_consensus/impl/sac_model_cone.hpp
@@ -165,7 +165,7 @@ pcl::SampleConsensusModelCone<PointT, PointNT>::getDistancesToModel (
 
     // Calculate the actual radius of the cone at the level of the projected point
     Eigen::Vector4f height = apex - pt_proj;
-    double actual_cone_radius = tan(opening_angle) * height.norm ();
+    double actual_cone_radius = tan (opening_angle) * height.norm ();
     height.normalize ();
 
     // Calculate the cones perfect normals
@@ -459,11 +459,11 @@ pcl::SampleConsensusModelCone<PointT, PointNT>::doSamplesVerifyModel (
 
     // Calculate the actual radius of the cone at the level of the projected point
     Eigen::Vector4f height = apex - pt_proj;
-    double actual_cone_radius = tan(openning_angle) * height.norm ();
+    double actual_cone_radius = tan (openning_angle) * height.norm ();
 
     // Aproximate the distance from the point to the cone as the difference between
     // dist(point,cone_axis) and actual cone radius
-    if (fabs (pointToAxisDistance (pt, model_coefficients) - actual_cone_radius > threshold))
+    if (fabs (static_cast<double>(pointToAxisDistance (pt, model_coefficients) - actual_cone_radius) > threshold))
       return (false);
   }
 
