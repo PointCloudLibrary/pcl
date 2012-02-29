c981b738cf857b353cb513ab7f6c201362793466
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-02-29 00:20:47 +0000

    * added a new @PlanarPolygon@ class for representing 2D planar polygon regions
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4831 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ common/include/pcl/planar_polygon.h
@@ -0,0 +1,62 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: planar_polygon.h 4696 2012-02-23 06:12:55Z rusu $
+ *
+ */
+
+#ifndef PCL_PLANAR_POLYGON_H_
+#define PCL_PLANAR_POLYGON_H_
+
+namespace pcl
+{
+  /** \brief PlanarPolygon represents a 2D planar polygon.
+    * \author Alex Trevor 
+    */ 
+  template <typename PointT>
+  struct PlanarPolygon
+  {
+    /** \brief A list of points on the boundary/contour of the planar region. */
+    std::vector<PointT, Eigen::aligned_allocator<PointT> > contour;
+
+    /** \brief A list of model coefficients (a,b,c,d). */
+    Eigen::Vector4f coefficients;
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  };
+}
+
+#endif  //#ifndef PCL_PLANAR_POLYGON_H_
+
