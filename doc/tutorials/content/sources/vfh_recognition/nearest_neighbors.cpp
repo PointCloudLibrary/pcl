65ee6a8296d82757cce632b3b09a48615939c855
Mourad Boufarguine <mourad@boufarguine.name>
2012-03-08 15:31:40 +0000

    fix mac os build
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@4975 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- doc/tutorials/content/sources/vfh_recognition/nearest_neighbors.cpp
+++ doc/tutorials/content/sources/vfh_recognition/nearest_neighbors.cpp
@@ -29,7 +29,7 @@ loadHist (const boost::filesystem::path &path, vfh_model &vfh)
     Eigen::Vector4f origin;
     Eigen::Quaternionf orientation;
     pcl::PCDReader r;
-    int type; int idx;
+    unsigned int type; int idx;
     r.readHeader (path.string (), cloud, origin, orientation, version, type, idx);
 
     vfh_idx = pcl::getFieldIndex (cloud, "vfh");
