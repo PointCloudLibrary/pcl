3b6c9a4a2c65f0aad518a86cde8a40b6a288b84c
Nizar Sallem <nksallem@laas.fr>
2012-03-17 00:50:27 +0000

    Add: implementation of 2D corner detectors
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5145 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ keypoints/include/pcl/keypoints/harris_keypoint2D.h
@@ -0,0 +1,179 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef PCL_HARRIS_KEYPOINT_2D_H_
+#define PCL_HARRIS_KEYPOINT_2D_H_
+
+#include <pcl/keypoints/keypoint.h>
+#include <pcl/keypoints/sift_keypoint.h>
+
+namespace pcl
+{
+  /** \brief HarrisKeypoint2D detects Harris corners family points
+    *
+    * \author Nizar Sallem
+    * \ingroup keypoints
+    */
+  template <typename PointInT, typename PointOutT>
+  class HarrisKeypoint2D : public Keypoint<PointInT, PointOutT>
+  {
+    public:
+      typedef typename Keypoint<PointInT, PointOutT>::PointCloudIn PointCloudIn;
+      typedef typename Keypoint<PointInT, PointOutT>::PointCloudOut PointCloudOut;
+      typedef typename Keypoint<PointInT, PointOutT>::KdTree KdTree;
+      typedef typename PointCloudIn::ConstPtr PointCloudInConstPtr;
+
+      using Keypoint<PointInT, PointOutT>::name_;
+      using Keypoint<PointInT, PointOutT>::input_;
+      using Keypoint<PointInT, PointOutT>::indices_;
+
+      typedef enum {HARRIS = 1, NOBLE, LOWE, TOMASI} ResponseMethod;
+
+      /** \brief Constructor
+        * \param[in] method the method to be used to determine the corner responses
+        * \param[in] radius the radius for normal estimation as well as for non maxima suppression
+        * \param[in] threshold the threshold to filter out weak corners
+        */
+      HarrisKeypoint2D (ResponseMethod method = HARRIS, int window_width = 3, int window_height = 3, int min_distance = 5, float threshold = 0.0)
+      : threshold_ (threshold)
+      , refine_ (false)
+      , nonmax_ (true)
+      , method_ (method)
+      , threads_ (1)
+      , response_ (new pcl::PointCloud<PointOutT> ())
+      , window_width_ (window_width)
+      , window_height_ (window_height)
+      , skipped_pixels_ (0)
+      , min_distance_ (min_distance_)
+      {
+        name_ = "HarrisKeypoint2D";
+      }
+
+      /** \brief set the method of the response to be calculated.
+        * \param[in] type
+        */
+      void setMethod (ResponseMethod type);
+
+      ///Set window width
+      void setWindowWidth (int window_width);
+
+      ///Set window height
+      void setWindowHeight (int window_height);      
+
+      ///Set number of pixels to skip
+      void setSkippedPixels (int skipped_pixels);
+
+      ///Set minimal distance between candidate keypoints
+      void setMinimalDistance (int min_distance);
+      
+      /** \brief set the threshold value for detecting corners. This is only evaluated if non maxima suppression is turned on.
+        * \brief note non maxima suppression needs to be activated in order to use this feature.
+        * \param[in] threshold 
+        */
+      void setThreshold (float threshold);
+
+      /** \brief whether non maxima suppression should be applied or the response for each point should be returned
+        * \note this value needs to be turned on in order to apply thresholding and refinement
+        * \param[in] nonmax default is false
+        */
+      void setNonMaxSupression (bool = false);
+
+      /** \brief whether the detected key points should be refined or not. If turned of, the key points are a subset of 
+        * the original point cloud. Otherwise the key points may be arbitrary.
+        * \brief note non maxima supression needs to be on in order to use this feature.
+        * \param[in] do_refine
+        */
+      void setRefine (bool do_refine);
+
+      /** \brief Initialize the scheduler and set the number of threads to use.
+        * \param nr_threads the number of hardware threads to use (-1 sets the value back to automatic)
+        */
+      inline void
+      setNumberOfThreads (int nr_threads)
+      {
+        if (nr_threads == 0)
+          nr_threads = 1;
+        threads_ = nr_threads;
+      }
+
+    protected:
+      bool initCompute ();
+      void detectKeypoints (PointCloudOut &output);
+      /** \brief gets the corner response for valid input points*/
+      void responseHarris (PointCloudOut &output, float& highest_response) const;
+      void responseNoble (PointCloudOut &output, float& highest_response) const;
+      void responseLowe (PointCloudOut &output, float& highest_response) const;
+      void responseTomasi (PointCloudOut &output, float& highest_response) const;
+//      void refineCorners (PointCloudOut &corners) const;
+      /** \brief calculates the upper triangular part of unnormalized 
+        * covariance matrix over intensities given by the 2D coordinates 
+        * and window_width_ and window_height_
+        */
+      void calculateIntensityCovar (std::size_t pos, float* coefficients) const;
+
+      float threshold_;
+      bool refine_;
+      bool nonmax_;
+      ResponseMethod method_;
+      int threads_;      
+
+    private:
+      Eigen::MatrixXf derivatives_rows_;
+      Eigen::MatrixXf derivatives_cols_;
+      boost::shared_ptr<pcl::PointCloud<PointOutT> > response_;
+      bool greaterIntensityAtIndices (int a, int b) const
+      {
+        return (response_->at (a).intensity > response_->at (b).intensity);
+      }      
+      /// Window width ; after initCompute stores half window width
+      int window_width_;
+      /// Window height ; after initCompute stores half window height
+      int window_height_;
+      int half_window_width_;
+      int half_window_height_;
+      int skipped_pixels_;
+      int min_distance_;
+      SIFTKeypointFieldSelector<PointInT> intensity_;
+  };
+}
+
+#include "pcl/keypoints/impl/harris_keypoint2D.hpp"
+
+#endif // #ifndef PCL_HARRIS_KEYPOINT_2D_H_
+
---
+++ keypoints/include/pcl/keypoints/impl/harris_keypoint2D.hpp
@@ -0,0 +1,501 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef PCL_HARRIS_KEYPOINT_2D_IMPL_H_
+#define PCL_HARRIS_KEYPOINT_2D_IMPL_H_
+
+#include "pcl/keypoints/harris_keypoint2D.h"
+#include <pcl/common/io.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/integral_image_normal.h>
+#include <pcl/common/time.h>
+#include <pcl/common/centroid.h>
+
+#ifdef __SSE__
+#include <xmmintrin.h>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::setMethod (ResponseMethod method)
+{
+  method_ = method;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::setThreshold (float threshold)
+{
+  threshold_= threshold;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::setRefine (bool do_refine)
+{
+  refine_ = do_refine;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::setNonMaxSupression (bool nonmax)
+{
+  nonmax_ = nonmax;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::setWindowWidth (int window_width)
+{
+  window_width_= window_width;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::setWindowHeight (int window_height)
+{
+  window_height_= window_height;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::setSkippedPixels (int skipped_pixels)
+{
+  skipped_pixels_= skipped_pixels;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::calculateIntensityCovar (std::size_t index, float* coefficients) const
+{
+  int x = static_cast<int> (index % input_->width);
+  int y = static_cast<int> (index / input_->width);
+  unsigned count = 0;
+  // indices        0   1   2
+  // coefficients: ixix  ixiy  iyiy
+  memset (coefficients, 0, sizeof (float) * 3);
+
+  int endx = std::min ((int)input_->width, x + half_window_width_);
+  int endy = std::min ((int)input_->height, y + half_window_height_);
+  for (int xx = std::max (0, x - half_window_width_); xx < endx; ++xx)
+    for (int yy = std::max (0, y - half_window_height_); yy < endy; ++yy)
+    {
+      const float& ix = derivatives_rows_ (xx,yy);
+      const float& iy = derivatives_cols_ (xx,yy);
+      coefficients[0]+= ix * ix;
+      coefficients[1]+= ix * iy;
+      coefficients[2]+= iy * iy;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> bool
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::initCompute ()
+{
+
+  if (!pcl::Keypoint<PointInT, PointOutT>::initCompute ())
+  {
+    PCL_ERROR ("[pcl::HarrisKeypoint2D::initCompute] init failed.");
+    return (false);
+  }
+  
+
+  if (!input_->isOrganized ())
+  {    
+    PCL_ERROR ("[pcl::HarrisKeypoint2D::initCompute] HarrisKeypoint2D doesn't support non organized clouds.");
+    return (false);
+  }
+  
+  if (indices_->size () != input_->size ())
+  {
+    PCL_ERROR ("[pcl::HarrisKeypoint2D::initCompute] HarrisKeypoint2D doesn't support setting indices.");
+    return (false);
+  }
+  
+  if ((window_height_%2) == 0)
+  {
+    PCL_ERROR ("[pcl::HarrisKeypoint2D::initCompute] Window height must be odd");
+    return (false);
+  }
+
+  if ((window_width_%2) == 0)
+  {
+    PCL_ERROR ("[pcl::HarrisKeypoint2D::initCompute] Window width must be odd");
+    return (false);
+  }
+
+  if (window_height_ < 3 || window_width_ < 3)
+  {
+    PCL_ERROR ("[pcl::HarrisKeypoint2D::initCompute] Window size must be >= 3x3");
+    return (false);
+  }
+  
+  half_window_width_ = window_width_ / 2;
+  half_window_height_ = window_height_ / 2;
+
+  return (true);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::detectKeypoints (PointCloudOut &output)
+{
+  derivatives_cols_.resize (input_->width, input_->height);
+  derivatives_rows_.resize (input_->width, input_->height);
+  //Compute cloud intensities first derivatives along columns and rows
+  //!!! nsallem 20120220 : we don't test here for density so if one term in nan the result is nan
+  int w = static_cast<int> (input_->width) - 1;
+  int h = static_cast<int> (input_->height) - 1;
+  // j = 0 --> j-1 out of range ; use 0
+  // i = 0 --> i-1 out of range ; use 0
+  derivatives_cols_(0,0) = (intensity_ ((*input_) (0,1)) - intensity_ ((*input_) (0,0))) * 0.5;
+  derivatives_rows_(0,0) = (intensity_ ((*input_) (1,0)) - intensity_ ((*input_) (0,0))) * 0.5;
+
+// #ifdef HAVE_OPENMP
+// //#pragma omp parallel for shared (derivatives_cols_, input_) num_threads (threads_)
+// #pragma omp parallel for num_threads (threads_)
+// #endif
+  for(int i = 1; i < w; ++i)
+	{
+		derivatives_cols_(i,0) = (intensity_ ((*input_) (i,1)) - intensity_ ((*input_) (i,0))) * 0.5;
+	}
+
+  derivatives_rows_(w,0) = (intensity_ ((*input_) (w,0)) - intensity_ ((*input_) (w-1,0))) * 0.5;
+  derivatives_cols_(w,0) = (intensity_ ((*input_) (w,1)) - intensity_ ((*input_) (w,0))) * 0.5;
+
+// #ifdef HAVE_OPENMP
+// //#pragma omp parallel for shared (derivatives_cols_, derivatives_rows_, input_) num_threads (threads_)
+// #pragma omp parallel for num_threads (threads_)
+// #endif
+  for(int j = 1; j < h; ++j)
+  {
+    // i = 0 --> i-1 out of range ; use 0
+		derivatives_rows_(0,j) = (intensity_ ((*input_) (1,j)) - intensity_ ((*input_) (0,j))) * 0.5;
+    for(int i = 1; i < w; ++i)
+    {
+      // derivative with respect to rows
+      derivatives_rows_(i,j) = (intensity_ ((*input_) (i+1,j)) - intensity_ ((*input_) (i-1,j))) * 0.5;
+
+      // derivative with respect to cols
+      derivatives_cols_(i,j) = (intensity_ ((*input_) (i,j+1)) - intensity_ ((*input_) (i,j-1))) * 0.5;
+    }
+    // i = w --> w+1 out of range ; use w
+    derivatives_rows_(w,j) = (intensity_ ((*input_) (w,j)) - intensity_ ((*input_) (w-1,j))) * 0.5;
+  }
+
+  // j = h --> j+1 out of range use h
+  derivatives_cols_(0,h) = (intensity_ ((*input_) (0,h)) - intensity_ ((*input_) (0,h-1))) * 0.5;
+  derivatives_rows_(0,h) = (intensity_ ((*input_) (1,h)) - intensity_ ((*input_) (0,h))) * 0.5;
+
+// #ifdef HAVE_OPENMP
+// //#pragma omp parallel for shared (derivatives_cols_, input_) num_threads (threads_)
+// #pragma omp parallel for num_threads (threads_)
+// #endif
+  for(int i = 1; i < w; ++i)
+	{
+    derivatives_cols_(i,h) = (intensity_ ((*input_) (i,h)) - intensity_ ((*input_) (i,h-1))) * 0.5;
+	}
+  derivatives_rows_(w,h) = (intensity_ ((*input_) (w,h)) - intensity_ ((*input_) (w-1,h))) * 0.5;
+  derivatives_cols_(w,h) = (intensity_ ((*input_) (w,h)) - intensity_ ((*input_) (w,h-1))) * 0.5;
+
+  // std::ofstream out_cols ("derivatives_cols.dat");
+  // out_cols << derivatives_cols_;
+  // out_cols.close ();
+  
+  // std::ofstream out_rows ("derivatives_rows.dat");
+  // out_rows << derivatives_rows_;
+  // out_rows.close ();
+  float highest_response_;
+  
+  switch (method_)
+  {
+    case HARRIS:
+      responseHarris(*response_, highest_response_);
+      break;
+    case NOBLE:
+      responseNoble(*response_, highest_response_);
+      break;
+    case LOWE:
+      responseLowe(*response_, highest_response_);
+      break;
+    case TOMASI:
+      responseTomasi(*response_, highest_response_);
+      break;
+  }
+  
+  if (!nonmax_)
+    output = *response_;
+  else
+  {    
+    threshold_*= highest_response_;
+
+    std::sort (indices_->begin (), indices_->end (), 
+               boost::bind (&HarrisKeypoint2D::greaterIntensityAtIndices, this, _1, _2));
+    
+    output.clear ();
+    output.reserve (response_->size());
+    std::vector<bool> occupency_map (response_->size (), false);
+    
+    int width = static_cast<int> (response_->width);
+    int height = static_cast<int> (response_->height);
+#ifdef HAVE_OPENMP
+#pragma omp parallel for shared (output, occupency_map) private (width, height) num_threads(threads_)   
+#endif
+    for (size_t idx = 0; idx < occupency_map.size (); ++idx)
+    {
+      if (occupency_map[idx] || response_->points [indices_->at (idx)].intensity < threshold_ || !isFinite (response_->points[idx]))
+        continue;
+        
+#ifdef HAVE_OPENMP
+#pragma omp critical
+#endif
+      output.push_back (response_->at (indices_->at (idx)));
+      
+			int u_end = std::min (width, indices_->at (idx) % width + min_distance_);
+			int v_end = std::min (height, indices_->at (idx) / width + min_distance_);
+      for(int u = std::max (0, indices_->at (idx) % width - min_distance_); u < u_end; ++u)
+        for(int v = std::max (0, indices_->at (idx) / width - min_distance_); v < v_end; ++v)
+          occupency_map[v*input_->width+u] = true;
+    }
+
+    // if (refine_)
+    //   refineCorners (output);
+
+    output.height = 1;
+    output.width = static_cast<uint32_t> (output.size());
+    std::cout << output.size () << std::endl;
+  }
+
+  // we don not change the denseness
+  output.is_dense = input_->is_dense;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::responseHarris (PointCloudOut &output, float& highest_response) const
+{
+  PCL_ALIGN (16) float covar [3];
+  output.clear ();
+  output.resize (input_->size ());
+  std::cout << output.size () << "X" << input_->size () << std::endl;
+  highest_response = std::numeric_limits<float>::min ();
+
+#ifdef HAVE_OPENMP
+  #pragma omp parallel for shared (output, highest_response) private (covar) num_threads(threads_)
+#endif
+
+  for (size_t index = 0; index < output.size (); ++index)
+  {
+    PointOutT& out_point = output.points [index];
+    const PointInT &in_point = (*input_).points [index];
+    out_point.intensity = 0;
+    out_point.x = in_point.x;
+    out_point.y = in_point.y;
+    out_point.z = in_point.z;
+    if (isFinite (in_point))
+    {
+      calculateIntensityCovar (index, covar);
+      float trace = covar [0] + covar [2];
+      if (trace != 0.f)
+      {
+        float det = covar[0] * covar[2] - covar[1] * covar[1];
+        out_point.intensity = 0.04f + det - 0.04f * trace * trace;
+        highest_response =  (out_point.intensity > highest_response) ? out_point.intensity : highest_response;
+      }
+    }
+  }
+
+  output.height = input_->height;
+  output.width = input_->width;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::responseNoble (PointCloudOut &output, float& highest_response) const
+{
+  highest_response = std::numeric_limits<float>::min ();
+  PCL_ALIGN (16) float covar [3];
+  output.resize (input_->size ());
+
+// #ifdef HAVE_OPENMP
+// #pragma omp parallel for shared (output, highest_response) private (covar) num_threads(threads_)
+// #endif
+
+  for (size_t index = 0; index < output.size (); ++index)
+  {
+    PointOutT &out_point = output.points [index];
+    const PointInT &in_point = input_->points [index];
+    out_point.x = in_point.x;
+    out_point.y = in_point.y;
+    out_point.z = in_point.z;
+    out_point.intensity = 0;
+    if (isFinite (in_point))
+    {    
+      calculateIntensityCovar (index, covar);
+      float trace = covar [0] + covar [2];
+      if (trace != 0)
+      {
+        float det = covar[0] * covar[2] - covar[1] * covar[1];
+        out_point.intensity = det / trace;
+        highest_response =  (out_point.intensity > highest_response) ? out_point.intensity : highest_response;
+      }
+    }    
+  }
+  
+  output.height = input_->height;
+  output.width = input_->width;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::responseLowe (PointCloudOut &output, float& highest_response) const
+{
+  highest_response = std::numeric_limits<float>::min ();
+  PCL_ALIGN (16) float covar [3];
+  output.resize (input_->size ());
+
+// #ifdef HAVE_OPENMP
+// #pragma omp parallel for shared (output, highest_response) private (covar) num_threads(threads_)
+// #endif
+  for (size_t index = 0; index < output.size (); ++index)      
+  {
+    PointOutT &out_point = output.points [index];
+    const PointInT &in_point = input_->points [index];
+    out_point.x = in_point.x;
+    out_point.y = in_point.y;
+    out_point.z = in_point.z;
+    out_point.intensity = 0;
+    if (isFinite (in_point))
+    {    
+      calculateIntensityCovar (index, covar);
+      float trace = covar [0] + covar [2];
+      if (trace != 0)
+      {
+        float det = covar[0] * covar[2] - covar[1] * covar[1];
+        out_point.intensity = det / (trace * trace);
+        highest_response =  (out_point.intensity > highest_response) ? out_point.intensity : highest_response;
+      }
+    }
+  }
+
+  output.height = input_->height;
+  output.width = input_->width;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT> void
+pcl::HarrisKeypoint2D<PointInT, PointOutT>::responseTomasi (PointCloudOut &output, float& highest_response) const
+{
+  highest_response = std::numeric_limits<float>::min ();
+  PCL_ALIGN (16) float covar [3];
+  output.resize (input_->size ());
+
+// #ifdef HAVE_OPENMP
+// #pragma omp parallel for shared (output, highest_response) private (covar) num_threads(threads_)
+// #endif
+  for (size_t index = 0; index < output.size (); ++index)
+  {
+    PointOutT &out_point = output.points [index];
+    const PointInT &in_point = input_->points [index];
+    out_point.x = in_point.x;
+    out_point.y = in_point.y;
+    out_point.z = in_point.z;
+    out_point.intensity = 0;
+    if (isFinite (in_point))
+    {      
+      calculateIntensityCovar (index, covar);
+      // min egenvalue
+      out_point.intensity = ((covar[0] + covar[2] - sqrt((covar[0] - covar[2])*(covar[0] - covar[2]) + 4 * covar[1] * covar[1])) /2.0f);
+      highest_response =  (out_point.intensity > highest_response) ? out_point.intensity : highest_response;
+    }    
+  }
+  
+  output.height = input_->height;
+  output.width = input_->width;
+}
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// template <typename PointInT, typename PointOutT> void
+// pcl::HarrisKeypoint2D<PointInT, PointOutT>::refineCorners (PointCloudOut &corners) const
+// {
+//   std::vector<int> nn_indices;
+//   std::vector<float> nn_dists;
+
+//   Eigen::Matrix2f nnT;
+//   Eigen::Matrix2f NNT;
+//   Eigen::Matrix2f NNTInv;
+//   Eigen::Vector2f NNTp;
+//   float diff;
+//   const unsigned max_iterations = 10;
+// #ifdef HAVE_OPENMP
+//   #pragma omp parallel for shared (corners) private (nnT, NNT, NNTInv, NNTp, diff, nn_indices, nn_dists) num_threads(threads_)
+// #endif
+//   for (int cIdx = 0; cIdx < static_cast<int> (corners.size ()); ++cIdx)
+//   {
+//     unsigned iterations = 0;
+//     do {
+//       NNT.setZero();
+//       NNTp.setZero();
+//       PointInT corner;
+//       corner.x = corners[cIdx].x;
+//       corner.y = corners[cIdx].y;
+//       corner.z = corners[cIdx].z;
+//       tree_->radiusSearch (corner, search_radius_, nn_indices, nn_dists);
+//       for (std::vector<int>::const_iterator iIt = nn_indices.begin(); iIt != nn_indices.end(); ++iIt)
+//       {
+//         if (!pcl_isfinite (normals_->points[*iIt].normal_x))
+//           continue;
+
+//         nnT = normals_->points[*iIt].getNormalVector3fMap () * normals_->points[*iIt].getNormalVector3fMap ().transpose();
+//         NNT += nnT;
+//         NNTp += nnT * surface_->points[*iIt].getVector3fMap ();
+//       }
+//       if (invert3x3SymMatrix (NNT, NNTInv) != 0)
+//         corners[cIdx].getVector3fMap () = NNTInv * NNTp;
+
+//       diff = (corners[cIdx].getVector3fMap () - corner.getVector3fMap()).squaredNorm ();
+//     } while (diff > 1e-6 && ++iterations < max_iterations);
+//   }
+// }
+
+#define PCL_INSTANTIATE_HarrisKeypoint2D(T,U,N) template class PCL_EXPORTS pcl::HarrisKeypoint2D<T,U,N>;
+#endif // #ifndef PCL_HARRIS_KEYPOINT_2D_IMPL_H_
+
