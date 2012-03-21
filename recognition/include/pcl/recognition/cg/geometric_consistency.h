03e6343f11cb8f5c26c7080af5aafe48bc6fcd33
Tommaso Cavallari <tommaso@tommasocavallari.com>
2012-03-21 17:25:36 +0000

    added @GeometricConsistencyGrouping@ clustering class
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5216 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ recognition/include/pcl/recognition/cg/geometric_consistency.h
@@ -0,0 +1,142 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *  
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef PCL_RECOGNITION_GEOMETRIC_CONSISTENCY_H_
+#define PCL_RECOGNITION_GEOMETRIC_CONSISTENCY_H_
+
+#include <pcl/recognition/cg/correspondence_grouping.h>
+#include <Eigen/StdVector>
+
+namespace pcl
+{
+ 
+  /** \brief Class implementing a 3D correspondence grouping enforcing geometric consistency among feature correspondences
+    *
+    * \author Federico Tombari, Tommaso Cavallari, Aitor Aldoma
+    * \ingroup recognition
+    */
+  template<typename PointModelT, typename PointSceneT>
+  class GeometricConsistencyGrouping : public CorrespondenceGrouping<PointModelT, PointSceneT>
+  {
+    public:
+      typedef typename pcl::PCLBase<PointModelT>::PointCloudConstPtr PointCloudConstPtr;
+      typedef typename pcl::PCLBase<PointModelT>::PointCloudPtr PointCloudPtr;
+      typedef typename pcl::CorrespondenceGrouping<PointModelT, PointSceneT>::SceneCloudConstPtr SceneCloudConstPtr;
+
+      /** \brief Constructor */
+      GeometricConsistencyGrouping () 
+        : gc_threshold_ (3)
+        , gc_size_ (1.0)
+        , found_transformations_ ()
+      {}
+
+      
+      /** \brief Sets the minimum cluster size
+        * \param[in] threshold the minimum cluster size 
+        */
+      inline void
+      setGCThreshold (int threshold)
+      {
+        gc_threshold_ = threshold;
+      }
+
+      /** \brief Gets the minimum cluster size.
+        * 
+        * \return the minimum cluster size used by GC.
+        */
+      inline double
+      getGCThreshold () const
+      {
+        return (gc_threshold_);
+      }
+
+      /** \brief Sets the consensus set resolution. This should be in metric units.
+        * 
+        * \param[in] gc_size consensus set resolution.
+        */
+      inline void
+      setGCSize (double gc_size)
+      {
+        gc_size_ = gc_size;
+      }
+
+      /** \brief Gets the consensus set resolution.
+        * 
+        * \return the consensus set resolution.
+        */
+      inline double
+      getGCSize () const
+      {
+        return (gc_size_);
+      }
+
+      /** \brief The main function, recognizes instances of the model into the scene set by the user.
+        * 
+        * \param[out] transformations a vector containing one transformation matrix for each instance of the model recognized into the scene.
+        *
+        * \return true if the recognition had been successful or false if errors have occurred.
+        */
+      bool
+      recognize (std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > &transformations);
+
+    protected:
+      using CorrespondenceGrouping<PointModelT, PointSceneT>::input_;
+      using CorrespondenceGrouping<PointModelT, PointSceneT>::scene_;
+      using CorrespondenceGrouping<PointModelT, PointSceneT>::model_scene_corrs_;
+
+      /** \brief Minimum cluster size. It shouldn't be less than 3, since at least 3 correspondences are needed to compute the 6DOF pose */
+      double gc_threshold_;
+
+      /** \brief Resolution of the consensus set used to cluster correspondences together*/
+      double gc_size_;
+
+      /** \brief Transformations found by clusterCorrespondences method. */
+      std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > found_transformations_;
+
+      /** \brief Cluster the input correspondences in order to distinguish between different instances of the model into the scene.
+        * 
+        * \param[out] model_instances a vector containing the clustered correspondences for each model found on the scene.
+        * \return true if the clustering had been successful or false if errors have occurred.
+        */ 
+      void
+      clusterCorrespondences (std::vector<Correspondences> &model_instances);
+  };
+}
+
+#endif // PCL_RECOGNITION_GEOMETRIC_CONSISTENCY_H_
---
+++ recognition/include/pcl/recognition/impl/cg/geometric_consistency.hpp
@@ -0,0 +1,171 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *  
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef PCL_RECOGNITION_GEOMETRIC_CONSISTENCY_IMPL_H_
+#define PCL_RECOGNITION_GEOMETRIC_CONSISTENCY_IMPL_H_
+
+#include <pcl/recognition/cg/geometric_consistency.h>
+#include <pcl/registration/correspondence_types.h>
+#include <pcl/registration/correspondence_rejection_sample_consensus.h>
+#include <pcl/common/io.h>
+
+bool
+gcCorrespSorter (pcl::Correspondence i, pcl::Correspondence j) 
+{ 
+  return (i.distance < j.distance); 
+}
+
+template<typename PointModelT, typename PointSceneT> void
+pcl::GeometricConsistencyGrouping<PointModelT, PointSceneT>::clusterCorrespondences (std::vector<Correspondences> &model_instances)
+{
+  model_instances.clear ();
+  found_transformations_.clear ();
+
+  if (!model_scene_corrs_)
+  {
+    PCL_ERROR(
+      "[pcl::GeometricConsistencyGrouping::clusterCorrespondences()] Error! Correspondences not set, please set them before calling again this function.\n");
+    return;
+  }
+
+  CorrespondencesPtr sorted_corrs (new Correspondences (*model_scene_corrs_));
+
+  std::sort (sorted_corrs->begin (), sorted_corrs->end (), gcCorrespSorter);
+
+  model_scene_corrs_ = sorted_corrs;
+
+  std::vector<int> consensus_set;
+  std::vector<bool> taken_corresps (model_scene_corrs_->size ());
+
+  Eigen::Vector3f dist_ref, dist_trg;
+
+  //temp copy of scene cloud with the type cast to ModelT in order to use Ransac
+  PointCloudPtr temp_scene_cloud_ptr (new PointCloud ());
+  pcl::copyPointCloud<PointSceneT, PointModelT> (*scene_, *temp_scene_cloud_ptr);
+
+  pcl::registration::CorrespondenceRejectorSampleConsensus<PointModelT> corr_rejector;
+  corr_rejector.setMaxIterations (10000);
+  corr_rejector.setInlierThreshold (gc_size_);
+  corr_rejector.setInputCloud (input_);
+  corr_rejector.setTargetCloud (temp_scene_cloud_ptr);
+
+  for (size_t i = 0; i < model_scene_corrs_->size (); ++i)
+  {
+    if (taken_corresps[i])
+      continue;
+
+    consensus_set.clear ();
+    consensus_set.push_back (i);
+    
+    for (size_t j = 0; j < model_scene_corrs_->size (); ++j)
+    {
+      if ( j != i &&  !taken_corresps[j])
+      {
+        //Let's check if j fits into the current consensus set
+        bool is_a_good_candidate = true;
+        for (size_t k = 0; k < consensus_set.size (); ++k)
+        {
+          int scene_index_k = model_scene_corrs_->at (k).index_match;
+          int model_index_k = model_scene_corrs_->at (k).index_query;
+          int scene_index_j = model_scene_corrs_->at (j).index_match;
+          int model_index_j = model_scene_corrs_->at (j).index_query;
+          
+          const Eigen::Vector3f& scene_point_k = scene_->at (scene_index_k).getVector3fMap ();
+          const Eigen::Vector3f& model_point_k = input_->at (model_index_k).getVector3fMap ();
+          const Eigen::Vector3f& scene_point_j = scene_->at (scene_index_j).getVector3fMap ();
+          const Eigen::Vector3f& model_point_j = input_->at (model_index_j).getVector3fMap ();
+
+          dist_ref = scene_point_k - scene_point_j;
+          dist_trg = model_point_k - model_point_j;
+
+          double distance = abs (dist_ref.norm () - dist_trg.norm ());
+
+          if (distance > gc_size_)
+          {
+            is_a_good_candidate = false;
+            break;
+          }
+        }
+        if (is_a_good_candidate)
+          consensus_set.push_back (j);
+      }
+    }
+    
+    if (consensus_set.size () > gc_threshold_)
+    {
+      Correspondences temp_corrs, filtered_corrs;
+      for (size_t j = 0; j < consensus_set.size (); j++)
+      {
+        temp_corrs.push_back (model_scene_corrs_->at (consensus_set[j]));
+        taken_corresps[ consensus_set[j] ] = true;
+      }
+      //ransac filtering
+      corr_rejector.getRemainingCorrespondences (temp_corrs, filtered_corrs);
+      //save transformations for recognize
+      found_transformations_.push_back (corr_rejector.getBestTransformation ());
+
+      model_instances.push_back (filtered_corrs);
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename PointModelT, typename PointSceneT> bool
+pcl::GeometricConsistencyGrouping<PointModelT, PointSceneT>::recognize (std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > &transformations)
+{
+  transformations.clear ();
+  if (!this->initCompute ())
+  {
+    PCL_ERROR(
+      "[pcl::GeometricConsistencyGrouping::recognize()] Error! Model cloud or Scene cloud not set, please set them before calling again this function.\n");
+    return (false);
+  }
+
+  std::vector<pcl::Correspondences> model_instances;
+  clusterCorrespondences (model_instances);
+
+  transformations = found_transformations_;
+
+  this->deinitCompute ();
+  return (true);
+}
+
+#define PCL_INSTANTIATE_GeometricConsistencyGrouping(T,ST) template class PCL_EXPORTS pcl::GeometricConsistencyGrouping<T,ST>;
+
+#endif // PCL_RECOGNITION_GEOMETRIC_CONSISTENCY_IMPL_H_
