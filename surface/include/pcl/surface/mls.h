ecccce3331b2eb0c7b9427b620a34465e42deb09
Nizar Sallem <nksallem@laas.fr>
2012-03-27 01:35:14 +0000

    Add reference paper
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5317 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- surface/include/pcl/surface/mls.h
+++ surface/include/pcl/surface/mls.h
@@ -54,9 +54,12 @@
 
 namespace pcl
 {
-  /** \brief MovingLeastSquares represent an implementation of the MLS (Moving Least Squares) algorithm for data
-    * smoothing and improved normal estimation. It also contains methods for upsampling the resulting cloud based
-    * on the parametric fit.
+  /** \brief MovingLeastSquares represent an implementation of the MLS (Moving Least Squares) algorithm 
+    * for data smoothing and improved normal estimation. It also contains methods for upsampling the 
+    * resulting cloud based on the parametric fit.
+    * Reference paper: "Computing and Rendering Point Set Surfaces" by Marc Alexa, Johannes Behr, 
+    * Daniel Cohen-Or, Shachar Fleishman, David Levin and Claudio T. Silva
+    * www.sci.utah.edu/~shachar/Publications/crpss.pdf
     * \author Zoltan Csaba Marton, Radu B. Rusu, Alexandru E. Ichim, Suat Gedikli
     * \ingroup surface
     */
