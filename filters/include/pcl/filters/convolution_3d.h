f6ff4e768c53317804871aa4c95da6f0d64077cf
Nizar Sallem <nksallem@laas.fr>
2012-03-28 06:19:47 +0000

    Add new class to handle convolution in 3D space for radial basis kernel
    There is still a bug in the color component, otherwise look fine
    
    
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5369 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ filters/include/pcl/filters/convolution_3d.h
@@ -0,0 +1,265 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef PCL_FILTERS_CONVOLUTION_3D_H
+#define PCL_FILTERS_CONVOLUTION_3D_H
+
+#include <pcl/pcl_base.h>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <pcl/search/pcl_search.h>
+#include <boost/optional.hpp>
+
+namespace pcl
+{
+  namespace filters
+  {
+    /** \brief Class ConvolvingKernel base class for all convolving kernels
+      * \ingroup filters
+      */
+    template<typename PointInT, typename PointOutT>
+    class ConvolvingKernel
+    {
+      public:
+        typedef boost::shared_ptr<ConvolvingKernel<PointInT, PointOutT> > Ptr;
+        typedef boost::shared_ptr<ConvolvingKernel<PointInT, PointOutT> > ConstPtr;
+        typedef typename PointCloud<PointInT>::ConstPtr PointCloudInConstPtr;
+
+        /// \brief empty constructor
+        ConvolvingKernel () {}
+
+        /// \brief empty destructor
+        virtual ~ConvolvingKernel () {}
+
+        /** \brief Set input cloud
+          * \param[in] input source point cloud
+          */
+        void
+        setInputCloud (const PointCloudInConstPtr& input) { input_ = input; }
+
+        /** \brief Convolve point at the center of this local information
+          * \param[in] indices indices of the point in the source point cloud
+          * \param[in] distances euclidean distance squared from the query point
+          * \return the convolved point
+          */
+        virtual PointOutT
+        operator() (const std::vector<int>& indices, const std::vector<float>& distances) = 0;
+
+        /** \brief Must call this methode before doing any computation
+          * \note make sure to override this with at least
+          * \code
+          * bool initCompute ()
+          * {
+          *   return (true);
+          * }
+          * \endcode
+          * in your kernel interface, else you are going nowhere!
+          */
+        virtual bool
+        initCompute () { return false; }
+
+        /** \brief Utility function that annihilates a point making it fail the \ref pcl::isFinite test
+          * \param[in/out] p point to annihilate
+          */
+        static void
+        makeInfinite (PointOutT& p)
+        {
+          p.x = p.y = p.z = std::numeric_limits<float>::quiet_NaN ();
+        }
+
+      protected:
+        /// source cloud
+        PointCloudInConstPtr input_;
+    };
+
+    /** \brief Gaussian kernel implementation interface
+      * Use this as implementation reference
+      * \ingroup filters
+      */
+    template<typename PointInT, typename PointOutT>
+    class GaussianKernel : public ConvolvingKernel <PointInT, PointOutT>
+    {
+      public:
+        using ConvolvingKernel<PointInT, PointOutT>::initCompute;
+        using ConvolvingKernel<PointInT, PointOutT>::input_;
+        using ConvolvingKernel<PointInT, PointOutT>::operator ();
+        using ConvolvingKernel<PointInT, PointOutT>::makeInfinite;
+        typedef boost::shared_ptr<GaussianKernel<PointInT, PointOutT> > Ptr;
+        typedef boost::shared_ptr<GaussianKernel<PointInT, PointOutT> > ConstPtr;
+
+        /** Default constructor */
+        GaussianKernel ()
+          : ConvolvingKernel <PointInT, PointOutT> ()
+          , sigma_ (0)
+          , threshold_ (std::numeric_limits<float>::infinity ())
+        {}
+
+        ~GaussianKernel () {}
+
+        /** Set the sigma parameter of the Gaussian
+          * \param[in] sigma
+          */
+        inline void
+        setSigma (float sigma) { sigma_ = sigma; }
+
+        /** Set the distance threshold relative to a sigma factor i.e. points such as
+          * ||pi - q|| > sigma_coefficient^2 * sigma^2 are not considered.
+          */
+        inline void
+        setThresholdRelativeToSigma (float sigma_coefficient)
+        {
+          sigma_coefficient_.reset (sigma_coefficient);
+        }
+
+        /** Set the distance threshold such as pi, ||pi - q|| > threshold are not considered. */
+        inline void
+        setThreshold (float threshold) { threshold_ = threshold; }
+
+        /** Must call this methode before doing any computation */
+        bool initCompute ();
+
+        PointOutT
+        operator() (const std::vector<int>& indices, const std::vector<float>& distances);
+
+      private:
+        float sigma_;
+        float sigma_sqr_;
+        float threshold_;
+        boost::optional<float> sigma_coefficient_;
+    };
+
+    /** Convolution3D handles the non organized case where width and height are unknown or if you
+      * are only interested in convolving based on local neighborhood information.
+      * The convolving kernel MUST be a radial symmetric and implement \ref ConvolvingKernel
+      * interface.
+      */
+    template <typename PointIn, typename PointOut, typename KernelT>
+    class Convolution3D : public pcl::PCLBase <PointIn>
+    {
+      public:
+        typedef typename pcl::PointCloud<PointIn> PointCloudIn;
+        typedef typename PointCloudIn::ConstPtr PointCloudInConstPtr;
+        typedef typename pcl::search::Search<PointIn> KdTree;
+        typedef typename pcl::search::Search<PointIn>::Ptr KdTreePtr;
+        typedef typename pcl::PointCloud<PointOut> PointCloudOut;
+        typedef boost::shared_ptr<Convolution3D<PointIn, PointOut, KernelT> > Ptr;
+        typedef boost::shared_ptr<Convolution3D<PointIn, PointOut, KernelT> > ConstPtr;
+
+        using pcl::PCLBase<PointIn>::indices_;
+        using pcl::PCLBase<PointIn>::input_;
+
+        /** \brief Constructor */
+        Convolution3D ();
+
+        /** \brief Empty destructor */
+        ~Convolution3D () {}
+
+        /** \brief Initialize the scheduler and set the number of threads to use.
+          * \param nr_threads the number of hardware threads to use (-1 sets the value back to automatic)
+          */
+        inline void
+        setNumberOfThreads (unsigned int nr_threads) { threads_ = (nr_threads == 0) ? 1 : nr_threads; }
+
+        /** \brief Set convolving kernel
+          * \param[in] kernel convolving element
+          */
+        inline void
+        setKernel (const KernelT& kernel) { kernel_ = kernel; }
+
+        /** \brief Provide a pointer to the input dataset that we need to estimate features at every point for.
+          * \param cloud the const boost shared pointer to a PointCloud message
+          */
+        inline void
+        setSearchSurface (const PointCloudInConstPtr &cloud) { surface_ = cloud; }
+
+        /** \brief Get a pointer to the surface point cloud dataset. */
+        inline PointCloudInConstPtr
+        getSearchSurface () { return (surface_); }
+
+        /** \brief Provide a pointer to the search object.
+          * \param tree a pointer to the spatial search object.
+          */
+        inline void
+        setSearchMethod (const KdTreePtr &tree) { tree_ = tree; }
+
+        /** \brief Get a pointer to the search method used. */
+        inline KdTreePtr
+        getSearchMethod () { return (tree_); }
+
+        /** \brief Set the sphere radius that is to be used for determining the nearest neighbors
+          * \param radius the sphere radius used as the maximum distance to consider a point a neighbor
+          */
+        inline void
+        setRadiusSearch (double radius) { search_radius_ = radius; }
+
+        /** \brief Get the sphere radius used for determining the neighbors. */
+        inline double
+        getRadiusSearch () { return (search_radius_); }
+
+        /** Convolve point cloud.
+          * \param[out] output the convolved cloud
+          */
+        void
+        convolve (PointCloudOut& output);
+
+      protected:
+        /** \brief initialize computation */
+        bool initCompute ();
+
+        /** \brief An input point cloud describing the surface that is to be used for nearest neighbors estimation. */
+        PointCloudInConstPtr surface_;
+
+        /** \brief A pointer to the spatial search object. */
+        KdTreePtr tree_;
+
+        /** \brief The nearest neighbors search radius for each point. */
+        double search_radius_;
+
+        /** \brief number of threads */
+        int threads_;
+
+        /** \brief convlving kernel */
+        KernelT kernel_;
+    };
+  }
+}
+
+#include <pcl/filters/impl/convolution_3d.hpp>
+
+#endif // PCL_FILTERS_CONVOLUTION_3D_H
---
+++ filters/include/pcl/filters/impl/convolution_3d.hpp
@@ -0,0 +1,217 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef PCL_FILTERS_CONVOLUTION_3D_IMPL_HPP
+#define PCL_FILTERS_CONVOLUTION_3D_IMPL_HPP
+
+#include <pcl/point_types.h>
+#include <pcl/common/point_operators.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+namespace pcl
+{
+  namespace filters
+  {
+    template <typename PointT>
+    class ConvolvingKernel<PointT, pcl::Normal>
+    {
+      void
+      makeInfinite (pcl::Normal& n)
+      {
+        n.normal_x = n.normal_y = n.normal_z = std::numeric_limits<float>::quiet_NaN ();
+      }
+    };
+
+    template <typename PointT> class
+    ConvolvingKernel<PointT, pcl::PointXY>
+    {
+      void
+      makeInfinite (pcl::Normal& n)
+      {
+        n.normal_x = n.normal_y = std::numeric_limits<float>::quiet_NaN ();
+      }
+    };
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename PointInT, typename PointOutT> bool
+pcl::filters::GaussianKernel<PointInT, PointOutT>::initCompute ()
+{
+  if (sigma_ == 0)
+  {
+    PCL_ERROR ("Sigma is not set or equal to 0!\n", sigma_);
+    return (false);
+  }
+  sigma_sqr_ = sigma_ * sigma_;
+
+  if (sigma_coefficient_)
+  {
+    if ((*sigma_coefficient_) > 6 || (*sigma_coefficient_) < 3)
+    {
+      PCL_ERROR ("Sigma coefficient (%f) out of [3..6]!\n", (*sigma_coefficient_));
+      return (false);
+    }
+    else
+      threshold_ = (*sigma_coefficient_) * (*sigma_coefficient_) * sigma_sqr_;
+  }
+
+  return (true);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename PointInT, typename PointOutT> PointOutT
+pcl::filters::GaussianKernel<PointInT, PointOutT>::operator() (const std::vector<int>& indices,
+                                                               const std::vector<float>& distances)
+{
+  using namespace pcl::common;
+  PointOutT result;
+  float total_weight = 0;
+  std::vector<float>::const_iterator dist_it = distances.begin ();
+
+  for (std::vector<int>::const_iterator idx_it = indices.begin ();
+       idx_it != indices.end ();
+       ++idx_it, ++dist_it)
+  {
+    if (*dist_it <= threshold_ && isFinite (input_->at (*idx_it)))
+    {
+      float weight = expf (-0.5f * (*dist_it) / sigma_sqr_);
+      result += weight * (input_->at (*idx_it));
+      total_weight += weight;
+    }
+  }
+  if (total_weight != 0)
+    result /= (total_weight * 2 * float (M_PI) * sigma_sqr_);
+  else
+    makeInfinite (result);
+
+  return (result);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT, typename KernelT>
+pcl::filters::Convolution3D<PointInT, PointOutT, KernelT>::Convolution3D ()
+  : PCLBase <PointInT> ()
+  , surface_ ()
+  , tree_ ()
+  , search_radius_ (0)
+{}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT, typename KernelT> bool
+pcl::filters::Convolution3D<PointInT, PointOutT, KernelT>::initCompute ()
+{
+  if (!PCLBase<PointInT>::initCompute ())
+  {
+    PCL_ERROR ("[pcl::filters::Convlution3D::initCompute] init failed!\n");
+    return (false);
+  }
+  // Initialize the spatial locator
+  if (!tree_)
+  {
+    if (input_->isOrganized ())
+      tree_.reset (new pcl::search::OrganizedNeighbor<PointInT> ());
+    else
+      tree_.reset (new pcl::search::KdTree<PointInT> (false));
+  }
+  // If no search surface has been defined, use the input dataset as the search surface itself
+  if (!surface_)
+    surface_ = input_;
+  // Send the surface dataset to the spatial locator
+  tree_->setInputCloud (surface_);
+  // Do a fast check to see if the search parameters are well defined
+  if (search_radius_ <= 0.0)
+  {
+    PCL_ERROR ("[pcl::filters::Convlution3D::initCompute] search radius (%f) must be > 0",
+               search_radius_);
+    return (false);
+  }
+  // Make sure the provided kernel implements the required interface
+  if (dynamic_cast<ConvolvingKernel<PointInT, PointOutT>* > (&kernel_) == 0)
+  {
+    PCL_ERROR ("[pcl::filters::Convlution3D::initCompute] init failed");
+    PCL_ERROR ("kernel_ must implement ConvolvingKernel interface\n!");
+    return (false);
+  }
+  kernel_.setInputCloud (surface_);
+  // Initialize convolving kernel
+  if (!kernel_.initCompute ())
+  {
+    PCL_ERROR ("[pcl::filters::Convlution3D::initCompute] kernel initialization failed!\n");
+    return (false);
+  }
+  return (true);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointInT, typename PointOutT, typename KernelT> void
+pcl::filters::Convolution3D<PointInT, PointOutT, KernelT>::convolve (PointCloud<PointOutT>& output)
+{
+  if (!initCompute ())
+  {
+    PCL_ERROR ("[pcl::filters::Convlution3D::convolve] init failed!\n");
+    return;
+  }
+  output.resize (surface_->size ());
+  output.width = surface_->width;
+  output.height = surface_->height;
+  output.is_dense = surface_->is_dense;
+  std::vector<int> nn_indices;
+  std::vector<float> nn_distances;
+  std::cout << "size set " << output.width << "x" << output.height << std::endl;
+
+  #pragma omp parallel for shared (output) private (nn_indices, nn_distances) num_threads (threads_)
+
+  for (std::size_t point_idx = 0; point_idx < surface_->size (); ++point_idx)
+  {
+    const PointInT& point_in = surface_->points [point_idx];
+    PointOutT& point_out = output [point_idx];
+    if (isFinite (point_in) &&
+        tree_->radiusSearch (point_in, search_radius_, nn_indices, nn_distances))
+    {
+      point_out = kernel_ (nn_indices, nn_distances);
+    }
+    else
+    {
+      kernel_.makeInfinite (point_out);
+      output.is_dense = false;
+    }
+  }
+}
+
+#endif
