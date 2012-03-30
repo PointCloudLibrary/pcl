fec7d1531f82a85fee3866f932aa801ec0465b81
Stephen D. Fox <stfox88@gmail.com>
2012-03-30 03:34:46 +0000

    fixed broken gcc. didnt like floating point initilization for const int
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5423 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- outofcore/include/pcl/outofcore/impl/octree_base.hpp
+++ outofcore/include/pcl/outofcore/impl/octree_base.hpp
@@ -75,7 +75,7 @@ namespace pcl
     const std::string octree_base<Container, PointT>::TREE_EXTENSION_ = ".octree";
 
     template<typename Container, typename PointT>
-    const int octree_base<Container, PointT>::OUTOFCORE_VERSION_ ( 3.0 );
+    const int octree_base<Container, PointT>::OUTOFCORE_VERSION_ ( 3 );
 
     template<typename Container, typename PointT>
     const uint64_t octree_base<Container, PointT>::LOAD_COUNT_ ( 2e9 );
