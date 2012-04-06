4be243cc8c44002dc268b4d27248a99cc2adf9f5
Stefan Holzer <holzers@in.tum.de>
2012-04-06 22:34:20 +0000

    added machine learning library and base files for decision tree learning
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5527 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ ml/include/pcl/ml/dt/feature_handler.h
@@ -0,0 +1,95 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+  
+#ifndef PCL_ML_DT_FEATURE_HANDLER
+#define PCL_ML_DT_FEATURE_HANDLER
+
+#include <pcl/common/common.h>
+
+#include <vector>
+#include <ostream>
+
+namespace pcl
+{
+
+  /** \brief Utility class interface which is used for creating and evaluating features. */
+  template <
+    class FeatureType,
+    class DataSet,
+    class ExampleIndex >
+  class PCL_EXPORTS FeatureHandler
+  {
+    public:
+
+      /** \brief Destructor. */
+      virtual 
+      ~FeatureHandler () {};
+
+      /** \biref Creates random features.
+        * \param[in] num_of_features The number of random features to create.
+        * \param[out] features The destination for the created features.
+        */
+      virtual void 
+      createRandomFeatures (const size_t num_of_features, std::vector<FeatureType> & features) = 0;
+
+      /** \brief Evaluates a feature on the specified data. 
+        * \param[in] features The features to evaluate.
+        * \param[in] data_set The data set on which the feature is evaluated.
+        * \param[in] examples The examples which specify on which parts of the data set the feature is evaluated.
+        * \param[out] results The destination for the results of the feature evaluation.
+        * \param[out] flags Flags that are supplied together with the results.
+        */
+      virtual void 
+      evaluateFeature (const FeatureType & feature,
+                       DataSet & data_set,
+                       std::vector<ExampleIndex> & examples,
+                       std::vector<float> & results,
+                       std::vector<unsigned char> & flags ) const = 0;
+
+      /** \brief Generates evaluation code for the specified feature and writes it to the specified stream.
+        * \param[in] feature The feature for which code is generated.
+        * \param[out] stream The destionation for the code.
+        */
+      virtual void 
+      generateCodeForEvaluation (const FeatureType & feature,
+                                 ::std::ostream & stream ) const = 0;
+
+  };
+
+}
+
+#endif
---
+++ ml/include/pcl/ml/impl/dt/decision_tree_learner.hpp
@@ -0,0 +1,293 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+  
+#define DT_DEBUG_OUTPUT
+//#define DT_DEBUG_OUTPUT2
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class FeatureType, class DataSet, class LabelType, class ExampleIndex, class NodeType>
+pcl::DecisionTreeLearner<FeatureType, DataSet, LabelType, ExampleIndex, NodeType>::DecisionTreeLearner ()
+  : max_tree_depth_ (15)
+  , num_of_features_ (1000)
+  , num_of_thresholds_ (10)
+  , feature_handler_ (NULL)
+  , stats_estimator_ (NULL)
+  , data_set_ ()
+  , label_data_ ()
+  , examples_ ()
+{
+  
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class FeatureType, class DataSet, class LabelType, class ExampleIndex, class NodeType>
+pcl::DecisionTreeLearner<FeatureType, DataSet, LabelType, ExampleIndex, NodeType>::~DecisionTreeLearner ()
+{
+  
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class FeatureType, class DataSet, class LabelType, class ExampleIndex, class NodeType>
+void
+pcl::DecisionTreeLearner<FeatureType, DataSet, LabelType, ExampleIndex, NodeType>::train (
+  pcl::DecisionTree<NodeType> & tree)
+{
+  // create random features
+  std::vector<FeatureType> features;
+  feature_handler_->createRandomFeatures (num_of_features_, features);
+
+  // recursively build decision tree
+  NodeType root_node; 
+  tree.setRoot (root_node);
+
+  trainDecisionTreeNode (features, examples_, label_data_, max_tree_depth_, tree.getRoot ());
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class FeatureType, class DataSet, class LabelType, class ExampleIndex, class NodeType>
+void
+pcl::DecisionTreeLearner<FeatureType, DataSet, LabelType, ExampleIndex, NodeType>::trainDecisionTreeNode (
+  std::vector<FeatureType> & features,
+  std::vector<ExampleIndex> & examples,
+  std::vector<LabelType> & label_data,
+  const size_t max_depth,
+  NodeType & node)
+{
+  const size_t num_of_examples = examples.size ();
+  if (num_of_examples == 0)
+  {
+    PCL_ERROR ("Reached invalid point in decision tree training");
+    return;
+  };
+
+  if (max_depth == 0)
+  {
+    stats_estimator_->computeAndSetNodeStats(data_set_, examples, label_data, node);
+    return;
+  };
+
+
+  std::vector<float> feature_results;
+  std::vector<unsigned char> flags;
+
+  feature_results.reserve (num_of_examples);
+  flags.reserve (num_of_examples);
+
+#ifdef DT_DEBUG_OUTPUT
+  ::std::cerr << "maxDepth: " << max_depth << ", examples: " << num_of_examples << ", ";
+#endif
+
+  // find best feature for split
+  int best_feature_index = -1;
+  float best_feature_threshold = 0.0f;
+  float best_feature_information_gain = 0.0f;
+
+  const size_t num_of_features = features.size ();
+  for (size_t feature_index = 0; feature_index < num_of_features; ++feature_index)
+  {
+#ifdef DT_DEBUG_OUTPUT2
+    ::std::cerr << "maxDepth: " << maxDepth << ", examples: " << numOfExamples << ", featureIndex: " << featureIndex << " / " << numOfFeatures << "     \r" << std::endl;
+#endif
+
+    // evaluate features
+    std::vector<float> feature_results;
+    std::vector<unsigned char> flags;
+    feature_results.reserve (num_of_examples);
+    flags.reserve (num_of_examples);
+
+    feature_handler_->evaluateFeature (features[feature_index],
+                                       data_set_,
+                                       examples,
+                                       feature_results,
+                                       flags );
+
+    // get list of thresholds
+    std::vector<float> thresholds;
+    thresholds.reserve (num_of_thresholds_);
+    createThresholdsUniform (num_of_thresholds_, feature_results, thresholds);
+
+
+#ifdef DT_DEBUG_OUTPUT2
+    ::std::cerr << "thresholds: ";
+#endif
+    for (size_t threshold_index = 0; threshold_index < num_of_thresholds_; ++threshold_index)
+    {
+      const float threshold = thresholds[threshold_index];
+
+#ifdef DT_DEBUG_OUTPUT2
+      ::std::cerr << threshold << "(";
+#endif
+
+      const float information_gain = stats_estimator_->computeInformationGain (data_set_,
+                                                                               examples,
+                                                                               label_data,
+                                                                               feature_results,
+                                                                               flags,
+                                                                               threshold);
+
+#ifdef DT_DEBUG_OUTPUT2
+      ::std::cerr << informationGain << "), ";
+#endif
+
+      if (information_gain > best_feature_information_gain)
+      {
+        best_feature_information_gain = information_gain;
+        best_feature_index = static_cast<int> (feature_index);
+        best_feature_threshold = threshold;
+      }
+    }
+#ifdef DT_DEBUG_OUTPUT2
+    ::std::cerr << ::std::endl;
+
+    ::std::cerr << "bestFeatureInformationGain: " << bestFeatureInformationGain << ", bestFeatureIndex: " << bestFeatureIndex << ", bestFeatureThreshold: " << bestFeatureThreshold << ::std::endl;
+#endif
+  }
+
+#ifdef DT_DEBUG_OUTPUT
+  ::std::cerr << ::std::endl;
+#endif
+
+  if (best_feature_index == -1)
+  {
+    stats_estimator_->computeAndSetNodeStats (data_set_, examples, label_data, node);
+    return;
+  }
+
+  // get branch indices for best feature and best threshold
+  std::vector<unsigned char> branch_indices;
+  branch_indices.reserve (num_of_examples);
+  {
+    feature_handler_->evaluateFeature (features[best_feature_index],
+                                       data_set_,
+                                       examples,
+                                       feature_results,
+                                       flags );
+
+    stats_estimator_->computeBranchIndices (feature_results,
+                                            flags,
+                                            best_feature_threshold,
+                                            branch_indices);
+ } 
+
+  stats_estimator_->computeAndSetNodeStats (data_set_, examples, label_data, node);
+
+  // seperate data
+  {
+    const size_t num_of_branches = stats_estimator_->getNumOfBranches ();
+
+    std::vector<size_t> branch_counts (num_of_branches, 0);
+    for (size_t example_index = 0; example_index < num_of_examples; ++example_index)
+    {
+      ++branch_counts[branch_indices[example_index]];
+    }
+
+    node.feature = features[best_feature_index];
+    node.threshold = best_feature_threshold;
+    //node.num_of_sub_nodes = num_of_branches;
+    node.sub_nodes.resize (num_of_branches);
+
+    for (size_t branch_index = 0; branch_index < num_of_branches; ++branch_index)
+    {
+      if (branch_counts[branch_index] == 0)
+      {
+        NodeType branch_node;
+        stats_estimator_->computeAndSetNodeStats (data_set_, examples, label_data, branch_node);
+        //branch_node->num_of_sub_nodes = 0;
+
+        node.sub_nodes[branch_index] = branch_node;
+
+        continue;
+      }
+
+      std::vector<LabelType> branch_labels;
+      std::vector<ExampleIndex> branch_examples;
+      branch_labels.reserve (branch_counts[branch_index]);
+      branch_examples.reserve (branch_counts[branch_index]);
+
+      //size_t branch_example_index = 0;
+      for (size_t example_index = 0; example_index < num_of_examples; ++example_index)
+      {
+        if (branch_indices[example_index] == branch_index)
+        {
+          branch_examples.push_back (examples[example_index]);
+          branch_labels.push_back (label_data[example_index]);
+
+          //++branch_example_index;
+        }
+      }
+
+      trainDecisionTreeNode (features, branch_examples, branch_labels, max_depth-1, node.sub_nodes[branch_index]);
+    }
+  }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class FeatureType, class DataSet, class LabelType, class ExampleIndex, class NodeType>
+void
+pcl::DecisionTreeLearner<FeatureType, DataSet, LabelType, ExampleIndex, NodeType>::createThresholdsUniform (
+  const size_t num_of_thresholds,
+  std::vector<float> & values,
+  std::vector<float> & thresholds)
+{
+  // estimate range of values
+  float min_value = ::std::numeric_limits<float>::max();
+  float max_value = -::std::numeric_limits<float>::max();
+
+  const size_t num_of_values = values.size ();
+  for (int value_index = 0; value_index < num_of_values; ++value_index)
+  {
+    const float value = values[value_index];
+
+    if (value < min_value) min_value = value;
+    if (value > max_value) max_value = value;
+  }
+
+  const float range = max_value - min_value;
+  const float step = range / (num_of_thresholds+2);
+
+  // compute thresholds
+  thresholds.resize (num_of_thresholds);
+
+  for (int threshold_index = 0; threshold_index < num_of_thresholds; ++threshold_index)
+  {
+    thresholds[threshold_index] = min_value + step*(threshold_index+1);
+  }
+}
+  
+
