17fc9957644618df49e7718fa8ebc0555b233471
Gioia Ballin <gioia.ballin@gmail.com>
2012-05-15 00:20:25 +0000

    implemented feature #644 with a related unit test
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5738 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/src/intersections.cpp
+++ common/src/intersections.cpp
@@ -56,7 +56,7 @@ pcl::lineWithLineIntersection (const Eigen::VectorXf &line_a,
   return (false);
 }
 
-inline bool
+bool
 pcl::lineWithLineIntersection (const pcl::ModelCoefficients &line_a, 
                                const pcl::ModelCoefficients &line_b, 
                                Eigen::Vector4f &point, double sqr_eps)
@@ -66,3 +66,49 @@ pcl::lineWithLineIntersection (const pcl::ModelCoefficients &line_a,
   return (lineWithLineIntersection (coeff1, coeff2, point, sqr_eps));
 }
 
+bool 
+pcl::planeWithPlaneIntersection (const Eigen::Vector4f &plane_a, 
+                                 const Eigen::Vector4f &plane_b,
+                                 Eigen::VectorXf &line,
+                                 double angular_tolerance)
+{
+  //planes shouldn't be parallel
+  double test_cosine = plane_a.head<3>().dot(plane_b.head<3>());
+  double upper_limit = 1 + angular_tolerance;
+  double lower_limit = 1 - angular_tolerance;
+
+  if ((test_cosine < upper_limit) && (test_cosine > lower_limit))
+  {
+      PCL_ERROR ("Plane A and Plane B are Parallel");
+      return (false);
+  }
+
+  if ((test_cosine > -upper_limit) && (test_cosine < -lower_limit))
+  {
+      PCL_ERROR ("Plane A and Plane B are Parallel");
+      return (false);
+  }
+
+  Eigen::Vector4f line_direction = plane_a.cross3(plane_b);
+  line_direction.normalized();
+
+  //construct system of equations using lagrange multipliers with one objective function and two constraints
+  Eigen::MatrixXf langegrange_coefs(5,5);
+  langegrange_coefs << 2,0,0,plane_a[0],plane_b[0],  0,2,0,plane_a[1],plane_b[1],  0,0,2, plane_a[2], plane_b[2], plane_a[0], plane_a[1] , plane_a[2], 0,0, plane_b[0], plane_b[1], plane_b[2], 0,0;
+
+  Eigen::VectorXf b;
+  b.resize(5);
+  b << 0, 0, 0, -plane_a[3], -plane_b[3];
+
+  //solve for the lagrange Multipliers
+  Eigen::VectorXf x;
+  x.resize(5);
+  x = langegrange_coefs.colPivHouseholderQr().solve(b);
+
+  line.resize(6);
+  line.head<3>() = x.head<3>(); // the x[3] and x[4] are the values of the lagrange multipliers and are neglected
+  line[3] = line_direction[0];
+  line[4] = line_direction[1];
+  line[5] = line_direction[2];
+  return true;
+} 
