e736a59685aa45067613522c4bce8c92a65799d3
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-05-30 00:20:35 +0000

    * fixed a grave bug where @pcl::removeNaNFromPointCloud@ was not copying @width/height@ if the dataset was dense
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5806 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- filters/include/pcl/filters/impl/filter.hpp
+++ filters/include/pcl/filters/impl/filter.hpp
@@ -48,7 +48,7 @@ pcl::removeNaNFromPointCloud (const pcl::PointCloud<PointT> &cloud_in, pcl::Poin
   // If the clouds are not the same, prepare the output
   if (&cloud_in != &cloud_out)
   {
-    cloud_out.header   = cloud_in.header;
+    cloud_out.header = cloud_in.header;
     cloud_out.points.resize (cloud_in.points.size ());
   }
   // Reserve enough space for the indices
@@ -58,11 +58,10 @@ pcl::removeNaNFromPointCloud (const pcl::PointCloud<PointT> &cloud_in, pcl::Poin
   // If the data is dense, we don't need to check for NaN
   if (cloud_in.is_dense)
   {
-    cloud_out.points = cloud_in.points;
+    // Simply copy the data
+    cloud_out = cloud_in;
     for (j = 0; j < cloud_out.points.size (); ++j)
-    {
       index[j] = static_cast<int>(j);
-    }
   }
   else
   {
