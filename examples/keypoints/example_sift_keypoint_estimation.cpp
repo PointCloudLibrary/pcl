4d0a22e3eb9811677c261728a47711c91d793150
Karthik Desingh <desinghkar@gmail.com>
2012-06-08 12:49:11 +0000

    Adding keypoints folder to examples and adding sift_keypoint_estimation example
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5864 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ examples/keypoints/example_sift_keypoint_estimation.cpp
@@ -0,0 +1,111 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Point Cloud Library (PCL) - www.pointclouds.org
+ * Copyright (c) 2009-2011, Willow Garage, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ * * Neither the name of Willow Garage, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: example_rift_estimation.cpp 5172 2012-03-18 10:43:15Z desinghkar $
+ *
+ *
+ */
+
+//STL
+#include <iostream>
+
+//PCL
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/io.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <pcl/keypoints/impl/sift_keypoint.hpp>
+#include <pcl/features/normal_3d.h>
+//#include <pcl/visualization/pcl_visualizer.h>
+	
+int
+main(int, char** argv)
+{
+  std::string filename = argv[1];
+  std::cout << "Reading " << filename << std::endl;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+  if(pcl::io::loadPCDFile<pcl::PointXYZRGB> (filename, *cloud) == -1) // load the file
+  {
+  PCL_ERROR ("Couldn't read file");
+  return -1;
+  }
+  std::cout << "points: " << cloud->points.size () <<std::endl;
+  
+  // Parameters for sift computation
+  const float min_scale = 0.1;
+  const int n_octaves = 6;
+  const int n_scales_per_octave = 10;
+  const float min_contrast = 0.5;
+  
+  
+  // Estimate the sift interest points using Intensity values from RGB values
+  pcl::SIFTKeypoint<pcl::PointXYZRGB, pcl::PointWithScale> sift;
+  pcl::PointCloud<pcl::PointWithScale> result;
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB> ());
+  sift.setSearchMethod(tree);
+  sift.setScales(min_scale, n_octaves, n_scales_per_octave);
+  sift.setMinimumContrast(min_contrast);
+  sift.setInputCloud(cloud);
+  sift.compute(result);
+  
+  // Copying the pointwithscale to pointxyz so as visualize the cloud
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_temp (new pcl::PointCloud<pcl::PointXYZ>);
+  copyPointCloud(result, *cloud_temp);
+
+  // Saving the resultant cloud 
+  std::cout << "Resulting sift points are of size: " << cloud_temp->points.size () <<std::endl;
+  pcl::io::savePCDFileASCII("sift_points.pcd", *cloud_temp);
+
+  
+/*  
+  // Visualization of keypoints along with the original cloud
+  pcl::visualization::PCLVisualizer viewer("PCL Viewer");
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> keypoints_color_handler (cloud_temp, 0, 255, 0);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> cloud_color_handler (cloud, 255, 255, 0);
+  viewer.setBackgroundColor( 0.0, 0.0, 0.0 );
+  viewer.addPointCloud(cloud, "cloud");
+  viewer.addPointCloud(cloud_temp, keypoints_color_handler, "keypoints");
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 7, "keypoints");
+  
+  while(!viewer.wasStopped ())
+  {
+  viewer.spinOnce ();
+  }
+*/  
+
+  
+  return 0;
+  
+}
