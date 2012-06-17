897cbfa17cb92c8456d04b6dbd49407d0117d462
Jochen Sprickerhof <jochen@sprickerhof.de>
2012-06-17 21:41:03 +0000

    Add QVTK hint for Apple to FindQVTK.cmake (thanks Bill Lorensen)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@5938 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- cmake/Modules/FindQVTK.cmake
+++ cmake/Modules/FindQVTK.cmake
@@ -8,7 +8,7 @@
 # if QVTK_FOUND then QVTK_INCLUDE_DIR is appended to VTK_INCLUDE_DIRS and 
 # QVTK_LIBRARY is appended to QVTK_LIBRARY_DIR
 
-find_library (QVTK_LIBRARY QVTK HINT ${VTK_DIR})
+find_library (QVTK_LIBRARY QVTK HINTS ${VTK_DIR} ${VTK_DIR}/bin)
 find_path (QVTK_INCLUDE_DIR QVTKWidget.h HINT ${VTK_INCLUDE_DIRS})
 find_package_handle_standard_args(QVTK DEFAULT_MSG
   QVTK_LIBRARY QVTK_INCLUDE_DIR)
