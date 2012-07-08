0137f804dfb156b4df7a916d211a4868f6ad7c64
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-07-08 22:49:16 +0000

    nm
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@6243 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- examples/segmentation/example_extract_clusters_normals.cpp
+++ examples/segmentation/example_extract_clusters_normals.cpp
@@ -53,27 +53,27 @@
 
 
 int 
-main(int argc, char **argv)
+main (int, char **argv)
 {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr (new pcl::PointCloud<pcl::PointXYZ> ());
   pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal> ());
   pcl::PCDWriter writer;
 	
-  if(pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *cloud_ptr) == -1)
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *cloud_ptr) == -1)
   {
     cout<<"Couldn't read the file "<<argv[1]<<endl;
-    return -1;
+    return (-1);
   }
   std::cout << "Loaded pcd file " << argv[1] << " with " << cloud_ptr->points.size () << std::endl;
 
   // Normal estimation
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-  ne.setInputCloud(cloud_ptr);
+  ne.setInputCloud (cloud_ptr);
 
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree_n (new pcl::search::KdTree<pcl::PointXYZ>());
-  ne.setSearchMethod(tree_n);
-  ne.setRadiusSearch(0.03);
-  ne.compute(*cloud_normals);
+  ne.setSearchMethod (tree_n);
+  ne.setRadiusSearch (0.03);
+  ne.compute (*cloud_normals);
   std::cout << "Estimated the normals" << std::endl;
 
   // Creating the kdtree object for the search method of the extraction
@@ -84,11 +84,10 @@ main(int argc, char **argv)
   std::vector<int> indices;
   std::vector<pcl::PointIndices> cluster_indices;
   const float tolerance = 0.5f; // 50cm tolerance in (x, y, z) coordinate system
-  const double eps_angle = 5*(M_PI/180); // 5degree tolerance in normals
+  const double eps_angle = 5 * (M_PI / 180.0); // 5degree tolerance in normals
   const unsigned int min_cluster_size = 50;
  
-  pcl::extractEuclideanClusters(*cloud_ptr, *cloud_normals, tolerance, tree_ec, cluster_indices, eps_angle, min_cluster_size);
-
+  pcl::extractEuclideanClusters (*cloud_ptr, *cloud_normals, tolerance, tree_ec, cluster_indices, eps_angle, min_cluster_size);
 
   std::cout << "No of clusters formed are " << cluster_indices.size () << std::endl;
 
@@ -110,5 +109,5 @@ main(int argc, char **argv)
     j++;
   }
 
-  return 0;
+  return (0);
 }
