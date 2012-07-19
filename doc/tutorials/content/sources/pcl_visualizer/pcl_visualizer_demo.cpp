987518706f5c7e8e4757dd4bea59d8b06802cd98
Jochen Sprickerhof <jochen@sprickerhof.de>
2012-07-19 08:29:21 +0000

    Remove superfluous header, thx ewjoachim
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@6472 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- doc/tutorials/content/sources/pcl_visualizer/pcl_visualizer_demo.cpp
+++ doc/tutorials/content/sources/pcl_visualizer/pcl_visualizer_demo.cpp
@@ -5,7 +5,6 @@
 
 #include <boost/thread/thread.hpp>
 #include <pcl/common/common_headers.h>
-#include <pcl/common/common_headers.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
