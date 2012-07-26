0710b622a840842f1e0f42fce4aef029f300fc08
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-07-26 00:20:43 +0000

    added an example for the new registration pipeline usig correspondences
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@6541 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ doc/tutorials/content/sources/registration_api/example1.cpp
@@ -0,0 +1,241 @@
+#include <fstream>
+#include <pcl/common/angles.h>
+#include <pcl/console/parse.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_representation.h>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/keypoints/uniform_sampling.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/fpfh.h>
+#include <pcl/registration/correspondence_estimation.h>
+#include <pcl/registration/correspondence_estimation_normal_shooting.h>
+#include <pcl/registration/correspondence_rejection_median_distance.h>
+#include <pcl/registration/correspondence_rejection_surface_normal.h>
+#include <pcl/registration/transformation_estimation_svd.h>
+#include <pcl/registration/transformation_estimation_point_to_plane_lls.h>
+#include <pcl/registration/default_convergence_criteria.h>
+
+#include <pcl/visualization/pcl_visualizer.h>
+
+using namespace std;
+using namespace sensor_msgs;
+using namespace pcl;
+using namespace pcl::io;
+using namespace pcl::console;
+using namespace pcl::registration;
+using namespace pcl::visualization;
+
+typedef PointNormal PointT;
+typedef PointCloud<PointT> Cloud;
+typedef Cloud::Ptr CloudPtr;
+typedef Cloud::ConstPtr CloudConstPtr;
+
+CloudPtr src, tgt;
+
+bool rejection = true;
+bool visualize = false;
+
+boost::shared_ptr<PCLVisualizer> vis;
+
+////////////////////////////////////////////////////////////////////////////////
+void
+findCorrespondences (const CloudPtr &src,
+                     const CloudPtr &tgt,
+                     Correspondences &all_correspondences)
+{
+  CorrespondenceEstimationNormalShooting<PointT, PointT, PointT> est;
+  //CorrespondenceEstimation<PointT, PointT> est;
+  est.setInputSource (src);
+  est.setSourceNormals (src);
+
+  est.setInputTarget (tgt);
+  est.setTargetNormals (tgt);
+  est.setKSearch (10);
+  //est.determineCorrespondences (all_correspondences);
+  est.determineReciprocalCorrespondences (all_correspondences);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+rejectBadCorrespondences (const CorrespondencesPtr &all_correspondences,
+                          const CloudPtr &src,
+                          const CloudPtr &tgt,
+                          Correspondences &remaining_correspondences)
+{
+  CorrespondenceRejectorMedianDistance rej;
+  rej.setMedianFactor (8.79241104);
+  rej.setInputCorrespondences (all_correspondences);
+
+  rej.getCorrespondences (remaining_correspondences);
+  return;
+  
+  CorrespondencesPtr remaining_correspondences_temp (new Correspondences);
+  rej.getCorrespondences (*remaining_correspondences_temp);
+  PCL_DEBUG ("[rejectBadCorrespondences] Number of correspondences remaining after rejection: %d\n", remaining_correspondences_temp->size ());
+
+  // Reject if the angle between the normals is really off
+  CorrespondenceRejectorSurfaceNormal rej_normals;
+  rej_normals.setThreshold (acos (deg2rad (45.0)));
+  rej_normals.initializeDataContainer<PointT, PointT> ();
+  rej_normals.setInputCloud<PointT> (src);
+  rej_normals.setInputNormals<PointT, PointT> (src);
+  rej_normals.setInputTarget<PointT> (tgt);
+  rej_normals.setTargetNormals<PointT, PointT> (tgt);
+  rej_normals.setInputCorrespondences (remaining_correspondences_temp);
+  rej_normals.getCorrespondences (remaining_correspondences);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+findTransformation (const CloudPtr &src,
+                    const CloudPtr &tgt,
+                    const CorrespondencesPtr &correspondences,
+                    Eigen::Matrix4f &transform)
+{
+  TransformationEstimationPointToPlaneLLS<PointT, PointT> trans_est;
+  trans_est.estimateRigidTransformation (*src, *tgt, *correspondences, transform);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+view (const CloudConstPtr &src, const CloudConstPtr &tgt, const CorrespondencesPtr &correspondences)
+{
+  if (!visualize || !vis) return;
+  PointCloudColorHandlerCustom<PointT> green (tgt, 0, 255, 0);
+  if (!vis->updatePointCloud<PointT> (src, "source"))
+  {
+    vis->addPointCloud<PointT> (src, "source");
+    vis->resetCameraViewpoint ("source");
+  }
+  if (!vis->updatePointCloud<PointT> (tgt, green, "target")) vis->addPointCloud<PointT> (tgt, green, "target");
+  vis->setPointCloudRenderingProperties (PCL_VISUALIZER_OPACITY, 0.5, "source");
+  vis->setPointCloudRenderingProperties (PCL_VISUALIZER_OPACITY, 0.7, "target");
+  vis->setPointCloudRenderingProperties (PCL_VISUALIZER_POINT_SIZE, 6, "source");
+ // if (!vis->updateCorrespondences<PointT> (src, tgt, *correspondences, 10)) 
+  TicToc tt;
+  tt.tic ();
+  vis->removeCorrespondences ();
+  vis->addCorrespondences<PointT> (src, tgt, *correspondences, 10, "correspondences");
+  tt.toc_print ();
+  vis->setShapeRenderingProperties (PCL_VISUALIZER_LINE_WIDTH, 4, "correspondences");
+  vis->spin ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+compute (const PointCloud<PointT>::Ptr &src, 
+         const PointCloud<PointT>::Ptr &tgt,
+         Eigen::Matrix4f &transform)
+{
+  CorrespondencesPtr all_correspondences (new Correspondences), 
+                     good_correspondences (new Correspondences);
+
+  PointCloud<PointT>::Ptr output (new PointCloud<PointT>);
+  *output = *src;
+
+  Eigen::Matrix4f final_transform (Eigen::Matrix4f::Identity ());
+
+  int iterations = 0;
+  DefaultConvergenceCriteria converged (iterations, transform, *good_correspondences);
+
+  // ICP loop
+  do
+  {
+    // Find correspondences
+    findCorrespondences (output, tgt, *all_correspondences);
+    PCL_DEBUG ("Number of correspondences found: %d\n", all_correspondences->size ());
+
+    if (rejection)
+    {
+      // Reject correspondences
+      rejectBadCorrespondences (all_correspondences, output, tgt, *good_correspondences);
+      PCL_DEBUG ("Number of correspondences remaining after rejection: %d\n", good_correspondences->size ());
+    }
+    else
+      *good_correspondences = *all_correspondences;
+
+    // Find transformation
+    findTransformation (output, tgt, good_correspondences, transform);
+
+    //std::cerr << transform << std::endl;
+    // Transform the data
+    transformPointCloud (*output, *output, transform);
+  
+    // Obtain the final transformation    
+    final_transform = transform * final_transform;
+
+    // Check if convergence has been reached
+    ++iterations;
+    
+    view (output, tgt, good_correspondences);
+  }
+  while (!converged);
+  transform = final_transform;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+saveTransform (const std::string &file, const Eigen::Matrix4f &transform)
+{
+  ofstream ofs;
+  ofs.open (file.c_str (), ios::trunc | ios::binary);
+  for (int i = 0; i < 4; ++i)
+    for (int j = 0; j < 4; ++j)
+      ofs.write (reinterpret_cast<const char*>(&transform (i, j)), sizeof (float));  
+  ofs.close ();
+}
+
+/* ---[ */
+int
+main (int argc, char** argv)
+{
+  // Check whether we want to enable debug mode
+  bool debug = false;
+  parse_argument (argc, argv, "-debug", debug);
+  if (debug)
+    setVerbosityLevel (L_DEBUG);
+
+  parse_argument (argc, argv, "-rejection", rejection);
+  parse_argument (argc, argv, "-visualization", visualize);
+  if (visualize)
+    vis.reset (new PCLVisualizer ("Registration example"));
+
+  // Parse the command line arguments for .pcd and .transform files
+  std::vector<int> p_pcd_file_indices, p_tr_file_indices;
+  p_pcd_file_indices = parse_file_extension_argument (argc, argv, ".pcd");
+  if (p_pcd_file_indices.size () != 2)
+  {
+    print_error ("Need one input source PCD file and one input target PCD file to continue.\n");
+    print_error ("Example: %s source.pcd target.pcd output.transform\n", argv[0]);
+    return (-1);
+  }
+  p_tr_file_indices = parse_file_extension_argument (argc, argv, ".transform");
+  if (p_tr_file_indices.size () != 1)
+  {
+    print_error ("Need one output transform file to continue.\n");
+    print_error ("Example: %s source.pcd target.pcd output.transform\n", argv[0]);
+    return (-1);
+  }
+  
+  // Load the files
+  print_info ("Loading %s as source and %s as target...\n", argv[p_pcd_file_indices[0]], argv[p_pcd_file_indices[1]]);
+  src.reset (new PointCloud<PointT>);
+  tgt.reset (new PointCloud<PointT>);
+  if (loadPCDFile (argv[p_pcd_file_indices[0]], *src) == -1 || loadPCDFile (argv[p_pcd_file_indices[1]], *tgt) == -1)
+  {
+    print_error ("Error reading the input files!\n");
+    return (-1);
+  }
+
+  // Compute the best transformtion
+  Eigen::Matrix4f transform;
+  compute (src, tgt, transform);
+
+  saveTransform (argv[p_tr_file_indices[0]], transform);
+
+  cerr.precision (15);
+  std::cerr << transform << std::endl;
+}
+/* ]--- */
