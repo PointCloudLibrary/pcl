f85a8824409214ae0200bccd42d8b6262125e584
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-07-27 21:02:45 +0000

    fix
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@6585 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- tools/icp2d.cpp
+++ tools/icp2d.cpp
@@ -97,8 +97,8 @@ main (int argc, char **argv)
 
     pcl::IterativeClosestPointNonLinear<PointType, PointType> icp;
 
-    boost::shared_ptr<pcl::WarpPointRigid3D<PointType, PointType> > warp_fcn 
-      (new pcl::WarpPointRigid3D<PointType, PointType>);
+    boost::shared_ptr<pcl::registration::WarpPointRigid3D<PointType, PointType> > warp_fcn 
+      (new pcl::registration::WarpPointRigid3D<PointType, PointType>);
 
     // Create a TransformationEstimationLM object, and set the warp to it
     boost::shared_ptr<pcl::registration::TransformationEstimationLM<PointType, PointType> > te (new pcl::registration::TransformationEstimationLM<PointType, PointType>);
