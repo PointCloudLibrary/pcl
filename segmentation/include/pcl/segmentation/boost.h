bd8cb5ddae287d1baa9dde1f8b05a8f2e96af3c0
Robert Huitl <pcl@huitl.de>
2012-09-10 16:16:29 +0000

    Don't try to include boost/graph/boykov_kolmogorov_max_flow.hpp when Boost version < 1.44 (segmentation/CMakeLists.txt excludes min_cut_segmentation, however the missing header breaks compilation)
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7108 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- segmentation/include/pcl/segmentation/boost.h
+++ segmentation/include/pcl/segmentation/boost.h
@@ -47,7 +47,9 @@
 
 // Marking all Boost headers as system headers to remove warnings
 #include <boost/make_shared.hpp>
-#include <boost/graph/boykov_kolmogorov_max_flow.hpp>
+#if (BOOST_VERSION >= 104400) 
+#include <boost/graph/boykov_kolmogorov_max_flow.hpp> 
+#endif 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/shared_ptr.hpp>
 
