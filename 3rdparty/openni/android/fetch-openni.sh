8fddf3e450134fab43c53104d426ceb4ff945ce9
Roman Donchenko <roman.donchenko@itseez.com>
2012-09-28 13:39:38 +0000

    Patched another bug in OpenNI, added credit for patches originally from ROS, and moved all patches to Git format (for ease of use with StGit).
    Reworked the fetch script for OpenNI to get an archive of the needed
    revision instead of the whole repository.
    
    
    
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7329 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- 3rdparty/openni/android/fetch-openni.sh
+++ 3rdparty/openni/android/fetch-openni.sh
@@ -5,12 +5,27 @@ set -e
 . ./normalize-eol.sh
 
 rm -rf OpenNI 2> /dev/null
-git clone git://github.com/OpenNI/OpenNI.git OpenNI
+
+echo 'Downloading OpenNI...'
+
+mkdir OpenNI
 cd OpenNI
-git checkout Stable-1.5.2.23
+wget -vO- http://github.com/OpenNI/OpenNI/tarball/Stable-1.5.2.23 | tar -xz --strip-components=1
+
+echo 'Normalizing EOL characters to LF...'
 
 normalize_eol
 
+echo 'Patching...'
+
 cat ../openni-patches/series | while read patchname; do
-  patch -p1 -i ../openni-patches/$patchname
+  case "$patchname" in
+    \#*)
+      ;; # ignore comments
+    *)
+      patch -p1 -i ../openni-patches/$patchname
+      ;;
+  esac
 done
+
+echo 'Done.'
