eb9db796bde6ede54ede7309f4544ed7983b582e
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-10-01 19:03:19 +0000

    nm
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7406 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/include/pcl/common/eigen.h
+++ common/include/pcl/common/eigen.h
@@ -50,8 +50,6 @@
 #  pragma GCC system_header
 #elif defined __SUNPRO_CC
 #  pragma disable_warn
-#elif defined _MSC_VER
-#  pragma warning(push, 1)
 #endif
 
 #include <cmath>
@@ -869,8 +867,6 @@ namespace pcl
 
 #if defined __SUNPRO_CC
 #  pragma enable_warn
-#elif defined _MSC_VER
-#  pragma warning(pop)
 #endif
 
 #endif  //#ifndef PCL_EIGEN_H_
