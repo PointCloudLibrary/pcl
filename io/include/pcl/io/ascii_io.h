2e5faa49debbdce0af836c2a4e58a50e35473e3b
Adam Stambler <adasta@gmail.com>
2012-10-03 04:23:31 +0000

    added a generic ascii point file reader to read  delimited  text files of points
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7435 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ io/include/pcl/io/ascii_io.h
@@ -0,0 +1,155 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef ASCII_IO_H_
+#define ASCII_IO_H_
+
+#include <pcl/io/file_io.h>
+#include <sensor_msgs/PointField.h>
+#include <pcl/common/io.h>
+
+
+namespace pcl{
+/** \brief Ascii Point Cloud Reader.
+  * Read any ASCII file by setting the separating characters and input point fields.
+  *
+  * \author Adam Stambler (adasta@gmail.com)
+  * \ingroup io
+  */
+  class ASCIIReader : public FileReader {
+
+  public:
+    ASCIIReader();
+    virtual ~ASCIIReader();
+    using   FileReader::read ;
+
+/* Load only the meta information (number of points, their types, etc),
+  * and not the points themselves, from a given FILE file. Useful for fast
+  * evaluation of the underlying data structure.
+  *
+  * Returns:
+  *  * < 0 (-1) on error
+  *  * > 0 on success
+  * \param[in] file_name the name of the file to load
+  * \param[out] cloud the resultant point cloud dataset (only the header will be filled)
+  * \param[out] origin the sensor acquisition origin (only for > FILE_V7 - null if not present)
+  * \param[out] orientation the sensor acquisition orientation (only for > FILE_V7 - identity if not present)
+  * \param[out] file_version the FILE version of the file (either FILE_V6 or FILE_V7)
+  * \param[out] data_type the type of data (binary data=1, ascii=0, etc)
+  * \param[out] data_idx the offset of cloud data within the file
+  * \param[in] offset the offset in the file where to expect the true header to begin.
+  * One usage example for setting the offset parameter is for reading
+  * data from a TAR "archive containing multiple files: TAR files always
+  * add a 512 byte header in front of the actual file, so set the offset
+  * to the next byte after the header (e.g., 513).
+  */
+    virtual int
+    readHeader (const std::string &file_name, sensor_msgs::PointCloud2 &cloud,
+                Eigen::Vector4f &origin, Eigen::Quaternionf &orientation,
+                int &file_version, int &data_type, unsigned int &data_idx, const int offset = 0) ;
+
+
+  /** \brief Read a point cloud data from a FILE file and store it into a sensor_msgs/PointCloud2.
+    * \param[in] file_name the name of the file containing the actual PointCloud data
+    * \param[out] cloud the resultant PointCloud message read from disk
+    * \param[out] origin the sensor acquisition origin (only for > FILE_V7 - null if not present)
+    * \param[out] orientation the sensor acquisition orientation (only for > FILE_V7 - identity if not present)
+    * \param[out] file_version the FILE version of the file (either FILE_V6 or FILE_V7)
+    * \param[in] offset the offset in the file where to expect the true header to begin.
+    * One usage example for setting the offset parameter is for reading
+    * data from a TAR "archive containing multiple files: TAR files always
+    * add a 512 byte header in front of the actual file, so set the offset
+    * to the next byte after the header (e.g., 513).
+    */
+    virtual int
+    read (const std::string &file_name, sensor_msgs::PointCloud2 &cloud,
+          Eigen::Vector4f &origin, Eigen::Quaternionf &orientation, int &file_version,
+          const int offset = 0);
+
+  /** \brief Set the ascii file point fields using a list of fields.
+    * \param[in] fields  is a list of point fields, in order, in the input ascii file
+    */
+    void setInputFields(const std::vector<sensor_msgs::PointField>& fields );
+
+
+  /** \brief Set the ascii file point fields using a point type.
+    * \param[in] p  a point type
+    */
+    template<typename PointT>
+    void setInputFields( const PointT p=PointT() );
+
+
+  /** \brief Set the Separting characters for the ascii point fields 2.
+    * \param[in] chars string of separating characters
+    *  Sets the separating characters for the point fields.  The
+    *  default separating characters are " \n\t,"
+    */
+    void setSepChars(std::string chars);
+
+  /** \brief Set the extension of the ascii point file type.
+    * \param[in] ext   extension (example :  ".txt" or ".xyz" )
+    */
+    void setExtension(std::string ext){extension_ = ext;}
+  protected:
+    std::string sep_chars_;
+    std::string extension_;
+    std::vector<sensor_msgs::PointField> fields_;
+    std::string name_;
+
+
+    /** \brief Parses token based on field type.
+    * \param[in] token   string representation of point fields
+    * \param[in] field   token point field type
+    * \param[out] data_target  address that the point field data should be assigned to
+    *  returns the size of the parsed point field in bytes
+    */
+    int parse( const std::string& token, const sensor_msgs::PointField& field, uint8_t* data_target );
+
+    /** \brief Returns the size in bytes of a point field type.
+    * \param[in] type   point field type
+    *  returns the size of the type in bytes
+    */
+    uint32_t typeSize( int type);
+};
+}
+
+template<typename PointT> void
+pcl::ASCIIReader::setInputFields(const PointT p) {
+  pcl::getFields<PointT>(fields_);
+}
+
+#endif /* PTS_IO_H_ */
---
+++ io/src/ascii_io.cpp
@@ -0,0 +1,226 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <pcl/io/ascii_io.h>
+#include <istream>
+#include <fstream>
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+
+pcl::ASCIIReader::ASCIIReader() {
+
+  extension_ = ".txt";
+  sep_chars_ = ", \n\r\t";
+  name_ = "AsciiReader";
+
+  {
+  sensor_msgs::PointField f;
+  f.datatype = sensor_msgs::PointField::FLOAT32;
+  f.count= 1;
+  f.name="x";
+  fields_.push_back(f);
+  }
+
+  {
+  sensor_msgs::PointField f;
+  f.datatype = sensor_msgs::PointField::FLOAT32;
+  f.count= 1;
+  f.name="y";
+  f.offset =4;
+  fields_.push_back(f);
+  }
+
+  {
+  sensor_msgs::PointField f;
+  f.datatype = sensor_msgs::PointField::FLOAT32;
+  f.count= 1;
+  f.name="z";
+  f.offset =8;
+  fields_.push_back(f);
+  }
+}
+
+pcl::ASCIIReader::~ASCIIReader() {
+
+}
+
+int
+pcl::ASCIIReader::readHeader (const std::string& file_name,
+  sensor_msgs::PointCloud2& cloud, Eigen::Vector4f& origin,
+  Eigen::Quaternionf& orientation, int& file_version, int& data_type,
+  unsigned int& data_idx, const int offset)
+{
+	(void)offset; //offset is not used for ascii file implementation
+	
+  boost::filesystem::path fpath  = file_name;
+
+  if ( !boost::filesystem::exists(fpath) )
+  {
+    pcl::console::print_error("[%s] File %s does not exist.\n", name_.c_str(), file_name.c_str());
+    return -1;
+  }
+  if ( fpath.extension().string() != extension_)
+  {
+    pcl::console::print_error("[%s] File does not have %s extension. \n", name_.c_str(), extension_.c_str());
+    return -1;
+  }
+
+  cloud.fields = fields_;
+  cloud.point_step =0;
+  for(int i=0; i< fields_.size(); i++) cloud.point_step +=  typeSize(cloud.fields[i].datatype);
+
+  std::fstream ifile(file_name.c_str(), std::fstream::in);
+  std::string line;
+  int total=0;
+  while(std::getline(ifile,line))  total++;
+
+  origin = Eigen::Vector4f::Zero();
+  orientation = Eigen::Quaternionf();
+  cloud.width = total;
+  cloud.height=1;
+  file_version =-1;
+  data_type=0;
+  data_idx =0;
+  return total;
+}
+
+int
+pcl::ASCIIReader::read(const std::string& file_name,
+  sensor_msgs::PointCloud2& cloud, Eigen::Vector4f& origin,
+  Eigen::Quaternionf& orientation, int& file_version, const int offset) {
+
+  int  data_type;
+  unsigned int data_idx;
+  if ( this->readHeader(file_name,cloud,origin,orientation,file_version, data_type, data_idx, offset) <0 ) return -1;
+  cloud.data.resize(cloud.height*cloud.width*cloud.point_step);
+
+  std::string line;
+  std::fstream ifile(file_name.c_str(), std::fstream::in);
+
+    int total=0;
+
+    uint8_t* data = cloud.data.data();
+    while(std::getline(ifile,line))
+    {
+      boost::algorithm::trim(line);
+      if (line.find_first_not_of("#") !=0 ) continue;   //skip comment lines
+
+     std::vector<std::string> tokens;
+     boost::algorithm::split(tokens, line,boost::algorithm::is_any_of(sep_chars_), boost::algorithm::token_compress_on);
+
+     if (tokens.size()!= fields_.size()) continue;
+
+     uint32_t offset =0;
+     try{
+       for(int i=0; i< fields_.size(); i++) offset +=  parse(tokens[i], fields_[i], data+offset);
+     }
+     catch (std::exception& e){
+       continue;
+     }
+     data += offset;
+     total++;
+    }
+  cloud.data.resize(total*cloud.point_step);
+  return (cloud.width*cloud.height);
+}
+
+void
+pcl::ASCIIReader::setInputFields(
+		const std::vector<sensor_msgs::PointField>& fields) {
+	fields_ = fields;
+}
+
+void
+pcl::ASCIIReader::setSepChars(std::string chars) {
+	sep_chars_ =chars;
+}
+
+int
+pcl::ASCIIReader::parse(const std::string& token,
+		const sensor_msgs::PointField& field, uint8_t* data_target) {
+
+  switch ( field.datatype){
+    case sensor_msgs::PointField::INT8:
+      *(reinterpret_cast<int8_t*>( data_target) ) = boost::lexical_cast<int8_t>(token);
+      return 1;
+    case sensor_msgs::PointField::UINT8:
+      *(reinterpret_cast<uint8_t*>( data_target) ) = boost::lexical_cast<uint8_t>(token);
+      return 1;
+    case sensor_msgs::PointField::INT16:
+      *(reinterpret_cast<int16_t*>( data_target)) = boost::lexical_cast<int16_t>(token);
+      return 2;
+    case sensor_msgs::PointField::UINT16:
+      *(reinterpret_cast<uint16_t*>( data_target)) = boost::lexical_cast<uint16_t>(token);
+      return 2;
+    case sensor_msgs::PointField::INT32:
+      *(reinterpret_cast<int32_t*>( data_target)) = boost::lexical_cast<int32_t>(token);
+      return 4;
+    case sensor_msgs::PointField::UINT32:
+      *(reinterpret_cast<uint32_t*>( data_target)) = boost::lexical_cast<uint32_t>(token);
+      return 4;
+    case sensor_msgs::PointField::FLOAT32:
+      *(reinterpret_cast<float*>( data_target)) = boost::lexical_cast<float>(token);
+      return 4;
+    case sensor_msgs::PointField::FLOAT64:
+      *(reinterpret_cast<double*>( data_target)) = boost::lexical_cast<double>(token);
+      return 8;
+  }
+  return 0;
+}
+
+uint32_t
+pcl::ASCIIReader::typeSize(int type) {
+	switch ( type){
+  case sensor_msgs::PointField::INT8:
+    return 1;
+  case sensor_msgs::PointField::UINT8:
+    return 1;
+  case sensor_msgs::PointField::INT16:
+    return 2;
+  case sensor_msgs::PointField::UINT16:
+    return 2;
+  case sensor_msgs::PointField::INT32:
+    return 4;
+  case sensor_msgs::PointField::UINT32:
+    return 4;
+  case sensor_msgs::PointField::FLOAT32:
+    return 4;
+  case sensor_msgs::PointField::FLOAT64:
+    return 8;
+	}
+	return 0;
+}
