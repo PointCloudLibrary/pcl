821644eb09b304200c469fac94572b204b6f900f
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-10-03 18:30:55 +0000

    * added VTK to PCD command line tool
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7441 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ tools/vtk2pcd.cpp
@@ -0,0 +1,117 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: pcd2vtk.cpp 6697 2012-08-04 02:41:10Z rusu $
+ *
+ */
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/console/print.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/time.h>
+
+using namespace pcl;
+using namespace pcl::io;
+using namespace pcl::console;
+
+void
+printHelp (int, char **argv)
+{
+  print_error ("Syntax is: %s input.vtk output.pcd\n", argv[0]);
+}
+
+bool
+loadCloud (const std::string &filename, vtkSmartPointer<vtkPolyData> &cloud)
+{
+  TicToc tt;
+  print_highlight ("Loading "); print_value ("%s ", filename.c_str ());
+
+  tt.tic ();
+  vtkSmartPointer<vtkPolyDataReader> reader;
+  reader->SetFileName (filename.c_str ());
+  cloud = reader->GetOutput ();
+  print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", cloud->GetNumberOfPoints ()); print_info (" points]\n");
+
+  return (true);
+}
+
+void
+saveCloud (const std::string &filename, const pcl::PointCloud<pcl::PointXYZ> &cloud)
+{
+  TicToc tt;
+  tt.tic ();
+
+  print_highlight ("Saving "); print_value ("%s ", filename.c_str ());
+  
+  PCDWriter w;
+  w.writeBinaryCompressed (filename, cloud);
+  
+  print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", cloud.width * cloud.height); print_info (" points]\n");
+}
+
+/* ---[ */
+int
+main (int argc, char** argv)
+{
+  print_info ("Convert a VTK file to PCD format. For more information, use: %s -h\n", argv[0]);
+
+  if (argc < 3)
+  {
+    printHelp (argc, argv);
+    return (-1);
+  }
+
+  // Parse the command line arguments for .pcd and .vtk files
+  std::vector<int> pcd_file_indices = parse_file_extension_argument (argc, argv, ".pcd");
+  std::vector<int> vtk_file_indices = parse_file_extension_argument (argc, argv, ".vtk");
+  if (pcd_file_indices.size () != 1 || vtk_file_indices.size () != 1)
+  {
+    print_error ("Need one input VTK file and one output PCD file.\n");
+    return (-1);
+  }
+
+  // Load the first file
+  vtkSmartPointer<vtkPolyData> polydata;
+  if (!loadCloud (argv[vtk_file_indices[0]], polydata)) 
+    return (-1);
+
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  vtkPolyDataToPointCloud (polydata, cloud);
+
+  // Convert to pcd and save
+  saveCloud (argv[pcd_file_indices[0]], cloud);
+}
+
