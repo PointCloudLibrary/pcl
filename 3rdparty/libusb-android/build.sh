de8b5bba6ffb5401cf6d9eb020975d23107d641b
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-10-04 04:54:08 +0000

    example build
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7446 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- 3rdparty/libusb-android/build.sh
+++ 3rdparty/libusb-android/build.sh
@@ -4,6 +4,10 @@ type arm-linux-androideabi-gcc > /dev/null || {
   cat << EOF
 Please create a standalone toolchain first and add it to your PATH. See
 STANDALONE-TOOLCHAIN.html in Android NDK documentation.
+
+For example:
+$  export CC="${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-gcc --sysroot=$SYSROOT"
+
 EOF
   exit 1
 }
