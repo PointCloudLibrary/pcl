18bf61d32133488bc2cfb511913c4ca0a7cd3b0b
Roman Donchenko <roman.donchenko@itseez.com>
2012-10-19 08:33:19 +0000

    Fixed an integer overflow in Stopwatch.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7594 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- mobile_apps/android/BodyParts/lib/stopwatch.cpp
+++ mobile_apps/android/BodyParts/lib/stopwatch.cpp
@@ -6,7 +6,7 @@ long long Stopwatch::getTime()
 {
   timespec ts;
   clock_gettime (CLOCK_MONOTONIC, &ts);
-  return ts.tv_sec * 1000000000 + ts.tv_nsec;
+  return ts.tv_sec * 1000000000LL + ts.tv_nsec;
 }
 
 Stopwatch::Stopwatch()
