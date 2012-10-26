ccdbbb7181ea6e47e0776310b5a52c3d71a7d1c1
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-10-26 16:50:29 +0000

    One more fix for #824. Thanks Eric!
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7728 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- io/src/pcd_io.cpp
+++ io/src/pcd_io.cpp
@@ -1131,10 +1131,9 @@ pcl::PCDReader::read (const std::string &file_name, sensor_msgs::PointCloud2 &cl
       PCL_DEBUG ("[pcl::PCDReader::read] Read a binary compressed file with %u bytes compressed and %u original.\n", compressed_size, uncompressed_size);
       // For all those weird situations where the compressed data is actually LARGER than the uncompressed one
       // (we really ought to check this in the compressor and copy the original data in those cases)
-
-      if (data_size < compressed_size)
+      if (data_size < compressed_size || uncompressed_size < compressed_size)
       {
-        PCL_DEBUG ("[pcl::PCDReader::read] Allocated data size (%zu) smaller than compressed size (%u). Need to remap.\n", data_size, compressed_size);
+        PCL_DEBUG ("[pcl::PCDReader::read] Allocated data size (%zu) or uncompressed size (%zu) smaller than compressed size (%u). Need to remap.\n", data_size, uncompressed_size, compressed_size);
 #if _WIN32
         UnmapViewOfFile (map);
         data_size = compressed_size + data_idx + 8;
