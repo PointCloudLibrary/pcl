d4789aef9a520b1519bf396678113002f6dfdb2d
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-11-04 01:21:13 +0000

    test
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7823 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- recognition/include/pcl/recognition/ransac_based/aux.h
+++ recognition/include/pcl/recognition/ransac_based/aux.h
@@ -2,7 +2,7 @@
  * Software License Agreement (BSD License)
  *
  *  Point Cloud Library (PCL) - www.pointclouds.org
- *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *  Copyright (c) 2012-, Open Perception, Inc.
  *
  *  All rights reserved.
  *
@@ -16,7 +16,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *   * Neither the name of the copyright holder(s) nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -33,18 +33,10 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *
- *
  */
 
-/*
- * aux.h
- *
- *  Created on: Oct 30, 2012
- *      Author: papazov
- */
-
-#ifndef AUX_H_
-#define AUX_H_
+#ifndef PCL_RECOGNITION_RANSAC_BASED_AUX_H_
+#define PCL_RECOGNITION_RANSAC_BASED_AUX_H_
 
 #include <cmath>
 
