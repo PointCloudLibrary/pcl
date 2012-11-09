1cb0aecd1c337bfb04995d1a1b1ff557fd0d97d3
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-11-09 00:09:02 +0000

    force to target 14
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7868 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- mobile_apps/android/ONIRecorder/build.sh
+++ mobile_apps/android/ONIRecorder/build.sh
@@ -2,5 +2,5 @@
 export NDK_MODULE_PATH=$(cd ../../../3rdparty/android/ndk-modules; pwd)
 
 ndk-build
-android update project -p . -n ONIRecorder
+android update project -p . -n ONIRecorder --target android-14
 ant debug
