50ff6a895b3ca8683475054c8396f821d3dc6117
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-11-13 01:26:45 +0000

    fixed a compiler error
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@7905 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- outofcore/include/pcl/outofcore/impl/octree_base_node.hpp
+++ outofcore/include/pcl/outofcore/impl/octree_base_node.hpp
@@ -1065,7 +1065,7 @@ namespace pcl
     }
 
     template<typename Container, typename PointT> void
-    OutofcoreOctreeBaseNode<Container, PointT>::queryFrustum (const double planes[24], std::list<std::string>& file_names, const boost::uint32_t query_depth, const bool skip_vfc_check=false)
+    OutofcoreOctreeBaseNode<Container, PointT>::queryFrustum (const double planes[24], std::list<std::string>& file_names, const boost::uint32_t query_depth, const bool skip_vfc_check)
     {
 
       enum {INSIDE, INTERSECT, OUTSIDE};
@@ -1214,7 +1214,7 @@ namespace pcl
 ////////////////////////////////////////////////////////////////////////////////
 
     template<typename Container, typename PointT> void
-    OutofcoreOctreeBaseNode<Container, PointT>::queryFrustum (const double planes[24], Eigen::Vector3d eye, Eigen::Matrix4d view_projection_matrix, std::list<std::string>& file_names, const boost::uint32_t query_depth, const bool skip_vfc_check=false)
+    OutofcoreOctreeBaseNode<Container, PointT>::queryFrustum (const double planes[24], Eigen::Vector3d eye, Eigen::Matrix4d view_projection_matrix, std::list<std::string>& file_names, const boost::uint32_t query_depth, const bool skip_vfc_check)
     {
 
       // If we're above our query depth
