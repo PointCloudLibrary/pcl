c635111004d7f317a0bf25c9a82220bb07d29f2e
Roman Donchenko <roman.donchenko@itseez.com>
2012-11-19 10:23:14 +0000

    ONIRecorder: add APP_PLATFORM to Application.mk, otherwise ndk-build complains.
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@8000 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- mobile_apps/android/ONIRecorder/jni/Application.mk
+++ mobile_apps/android/ONIRecorder/jni/Application.mk
@@ -2,3 +2,4 @@ APP_ABI := armeabi-v7a
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti
 APP_MODULES := onirec OpenNI.jni nimCodecs nimRecorder nimMockNodes XnDeviceSensorV2
+APP_PLATFORM := android-12
