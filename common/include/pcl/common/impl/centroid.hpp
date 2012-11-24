0369a3ac2fa182b5d81b6efbfea323238cc701fe
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-11-24 22:25:37 +0000

    fixed a few compiler warnings
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@8050 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/include/pcl/common/impl/centroid.hpp
+++ common/include/pcl/common/impl/centroid.hpp
@@ -670,9 +670,9 @@ pcl::demeanPointCloud (const pcl::PointCloud<PointT> &cloud_in,
   // Subtract the centroid from cloud_in
   for (size_t i = 0; i < cloud_in.points.size (); ++i)
   {
-    cloud_out[i].x -= centroid[0];
-    cloud_out[i].y -= centroid[1];
-    cloud_out[i].z -= centroid[2];
+    cloud_out[i].x -= static_cast<float> (centroid[0]);
+    cloud_out[i].y -= static_cast<float> (centroid[1]);
+    cloud_out[i].z -= static_cast<float> (centroid[2]);
   }
 }
 
@@ -700,9 +700,9 @@ pcl::demeanPointCloud (const pcl::PointCloud<PointT> &cloud_in,
   // Subtract the centroid from cloud_in
   for (size_t i = 0; i < indices.size (); ++i)
   {
-    cloud_out[i].x = cloud_in[indices[i]].x - centroid[0];
-    cloud_out[i].y = cloud_in[indices[i]].y - centroid[1];
-    cloud_out[i].z = cloud_in[indices[i]].z - centroid[2];
+    cloud_out[i].x = static_cast<float> (cloud_in[indices[i]].x - centroid[0]);
+    cloud_out[i].y = static_cast<float> (cloud_in[indices[i]].y - centroid[1]);
+    cloud_out[i].z = static_cast<float> (cloud_in[indices[i]].z - centroid[2]);
   }
 }
 
