8e930daadd5cbb095bfd297abdb54784b3c314c1
Alexandru Eugen Ichim <alex.e.ichim@gmail.com>
2012-11-30 03:48:20 +0000

    stub for correspondence rejector unit tests
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@8153 a9d63959-f2ad-4865-b262-bf0e56cfafb6

---
+++ test/registration/test_correspondence_rejectors.cpp
@@ -0,0 +1,72 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <pcl/registration/correspondence_rejection_median_distance.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (CorrespondenceRejectors, CorrespondenceRejectionMedianDistance)
+{
+  pcl::CorrespondencesPtr corresps (new pcl::Correspondences ());
+  for (int i = 0; i <= 10; ++i)
+  {
+    pcl::Correspondence c;
+    c.distance = static_cast<float> (i * i);
+    corresps->push_back (c);
+  }
+
+  pcl::registration::CorrespondenceRejectorMedianDistance rejector;
+  rejector.setInputCorrespondences (corresps);
+  rejector.setMedianFactor (2.0);
+
+  pcl::Correspondences corresps_filtered;
+  rejector.getCorrespondences (corresps_filtered);
+
+  EXPECT_EQ (corresps_filtered.size (), 8);
+  for (int i = 0; i < 8; ++i)
+    EXPECT_NEAR (corresps_filtered[i].distance, static_cast<float> (i * i), 1e-5);
+}
+
+
+/* ---[ */
+int
+  main (int argc, char** argv)
+{
+  testing::InitGoogleTest (&argc, argv);
+  return (RUN_ALL_TESTS ());
+}
+/* ]--- */
