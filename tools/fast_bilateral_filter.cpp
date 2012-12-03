132163c9e72fadf87c266798739bf5d45d31fb80
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-12-03 13:03:05 +0000

    changed the @fast_bilateral_filter@ command line tool
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@8176 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- tools/fast_bilateral_filter.cpp
+++ tools/fast_bilateral_filter.cpp
@@ -1,131 +1,152 @@
 /*
  * Software License Agreement (BSD License)
  *
- * Point Cloud Library (PCL) - www.pointclouds.org
- * Copyright (c) 2009-2012, Willow Garage, Inc.
- * Copyright (c) 2012-, Open Perception, Inc.
-
- * All rights reserved.
-
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *  
+ *  All rights reserved.
  *
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials provided
- *    with the distribution.
- * * Neither the name of the copyright holder(s) nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
- * $Id$
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
-#include <pcl/filters/fast_bilateral.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/filters/fast_bilateral.h>
 #include <pcl/console/print.h>
 #include <pcl/console/parse.h>
 #include <pcl/console/time.h>
 
+using namespace std;
 using namespace pcl;
 using namespace pcl::io;
 using namespace pcl::console;
 
-float default_sigma_s = 15.0f;
-float default_sigma_r = 0.05f;
-bool default_early_division = false;
+float default_sigma_s = 5.0f;
+float default_sigma_r = 0.03f;
 
 void
 printHelp (int, char **argv)
 {
-  print_info ("Note: only works with organized point clouds\n");
-  print_error ("Syntax is: %s input.pcd output.pcd <options>\n", argv[0]);
+  print_error ("Syntax is: %s input.pcd output.pcd <options> [optional_arguments]\n", argv[0]);
   print_info ("  where options are:\n");
-  print_info ("                     -sigma_s X = size of the filter window (default: ");
+  print_info ("                     -sigma_s X = use a sigma S value of X (default: "); 
   print_value ("%f", default_sigma_s); print_info (")\n");
-  print_info ("                     -sigma_r X = standard deviation of the filter (default: ");
+  print_info ("                     -sigma_r X = use a sigma R value of X (default: "); 
   print_value ("%f", default_sigma_r); print_info (")\n");
-  print_info ("                     -early_division X = early division (default: ");
-  print_value ("%d", default_early_division); print_info (")\n");
+  print_info ("\nOptional arguments are:\n");
+  print_info ("                     -input_dir X  = batch process all PCD files found in input_dir\n");
+  print_info ("                     -output_dir X = save the processed files from input_dir in this directory\n");
 }
 
 bool
-loadCloud (const std::string &filename, sensor_msgs::PointCloud2 &cloud)
+loadCloud (const string &filename, sensor_msgs::PointCloud2 &cloud,
+           Eigen::Vector4f &translation, Eigen::Quaternionf &orientation)
 {
-  TicToc tt;
-  print_highlight ("Loading "); print_value ("%s ", filename.c_str ());
-
-  tt.tic ();
-  if (loadPCDFile (filename, cloud) < 0)
+  if (loadPCDFile (filename, cloud, translation, orientation) < 0)
     return (false);
-  print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", cloud.width * cloud.height); print_info (" points]\n");
-  print_info ("Available dimensions: "); print_value ("%s\n", pcl::getFieldsList (cloud).c_str ());
 
   return (true);
 }
 
-
 void
 compute (const sensor_msgs::PointCloud2::ConstPtr &input, sensor_msgs::PointCloud2 &output,
-         float sigma_s, float sigma_r, bool early_division)
+         float sigma_s = 5.f, float sigma_r = 0.03f)
 {
-  PointCloud<PointXYZRGBA>::Ptr cloud (new PointCloud<PointXYZRGBA> ());
-  fromROSMsg (*input, *cloud);
-
-  FastBilateralFilter<PointXYZRGBA> filter;
-  filter.setSigmaS (sigma_s);
-  filter.setSigmaR (sigma_r);
-  filter.setEarlyDivision (early_division);
-  filter.setInputCloud (cloud);
-
-  PointCloud<PointXYZRGBA>::Ptr out_cloud (new PointCloud<PointXYZRGBA> ());
-
+  // Convert data to PointCloud<T>
+  PointCloud<PointXYZ>::Ptr xyz (new PointCloud<PointXYZ>);
+  fromROSMsg (*input, *xyz);
 
   TicToc tt;
   tt.tic ();
-  filter.filter (*out_cloud);
-  print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", out_cloud->width * out_cloud->height); print_info (" points]\n");
 
-  toROSMsg (*out_cloud, output);
+  // Apply the filter
+  FastBilateralFilter<PointXYZ> fbf;
+  fbf.setInputCloud (xyz);
+  fbf.setSigmaS (sigma_s);
+  fbf.setSigmaR (sigma_r);
+  PointCloud<PointXYZ> xyz_filtered;
+  fbf.filter (xyz_filtered);
+
+  print_highlight ("Filtered data in "); print_value ("%g", tt.toc ()); print_info (" ms for "); print_value ("%zu", xyz_filtered.size ()); print_info (" points.\n");
+
+  // Convert data back
+  sensor_msgs::PointCloud2 output_xyz;
+  toROSMsg (xyz_filtered, output_xyz);
+  concatenateFields (*input, output_xyz, output);
 }
 
 void
-saveCloud (const std::string &filename, const sensor_msgs::PointCloud2 &output)
+saveCloud (const string &filename, const sensor_msgs::PointCloud2 &output,
+           const Eigen::Vector4f &translation, const Eigen::Quaternionf &orientation)
 {
-  TicToc tt;
-  tt.tic ();
+  PCDWriter w;
+  w.writeBinaryCompressed (filename, output, translation, orientation);
+}
 
-  print_highlight ("Saving "); print_value ("%s ", filename.c_str ());
+int
+batchProcess (const vector<string> &pcd_files, string &output_dir, float sigma_s, float sigma_r)
+{
+#if _OPENMP
+#pragma omp parallel for
+#endif
+  for (int i = 0; i < int (pcd_files.size ()); ++i)
+  {
+    // Load the first file
+    Eigen::Vector4f translation;
+    Eigen::Quaternionf rotation;
+    sensor_msgs::PointCloud2::Ptr cloud (new sensor_msgs::PointCloud2);
+    if (!loadCloud (pcd_files[i], *cloud, translation, rotation)) 
+      continue;
 
-  pcl::io::savePCDFile (filename, output,  Eigen::Vector4f::Zero (),
-      Eigen::Quaternionf::Identity (), true);
+    // Perform the feature estimation
+    sensor_msgs::PointCloud2 output;
+    compute (cloud, output, sigma_s, sigma_r);
 
-  print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", output.width * output.height); print_info (" points]\n");
+    // Prepare output file name
+    string filename = pcd_files[i];
+    boost::trim (filename);
+    vector<string> st;
+    boost::split (st, filename, boost::is_any_of ("/\\"), boost::token_compress_on);
+    
+    // Save into the second file
+    stringstream ss;
+    ss << output_dir << "/" << st.at (st.size () - 1);
+    saveCloud (ss.str (), output, translation, rotation);
+  }
+  return (0);
 }
 
 /* ---[ */
 int
 main (int argc, char** argv)
 {
-  print_info ("Moving Least Squares smoothing of a point cloud. For more information, use: %s -h\n", argv[0]);
+  print_info ("Smooth depth data using a FastBilateralFilter. For more information, use: %s -h\n", argv[0]);
 
   if (argc < 3)
   {
@@ -133,33 +154,77 @@ main (int argc, char** argv)
     return (-1);
   }
 
-  // Parse the command line arguments for .pcd files
-  std::vector<int> p_file_indices;
-  p_file_indices = parse_file_extension_argument (argc, argv, ".pcd");
-  if (p_file_indices.size () != 2)
-  {
-    print_error ("Need one input PCD file and one output PCD file to continue.\n");
-    return (-1);
-  }
+  bool batch_mode = false;
 
   // Command line parsing
   float sigma_s = default_sigma_s;
   float sigma_r = default_sigma_r;
-  bool early_division = default_early_division;
-
   parse_argument (argc, argv, "-sigma_s", sigma_s);
   parse_argument (argc, argv, "-sigma_r", sigma_r);
-  parse_argument (argc, argv, "-early_division", early_division);
+  string input_dir, output_dir;
+  if (parse_argument (argc, argv, "-input_dir", input_dir) != -1)
+  {
+    PCL_INFO ("Input directory given as %s. Batch process mode on.\n", input_dir.c_str ());
+    if (parse_argument (argc, argv, "-output_dir", output_dir) == -1)
+    {
+      PCL_ERROR ("Need an output directory! Please use -output_dir to continue.\n");
+      return (-1);
+    }
 
-  // Load the first file
-  sensor_msgs::PointCloud2::Ptr cloud (new sensor_msgs::PointCloud2);
-  if (!loadCloud (argv[p_file_indices[0]], *cloud))
-    return (-1);
+    // Both input dir and output dir given, switch into batch processing mode
+    batch_mode = true;
+  }
 
-  // Apply the filter
-  sensor_msgs::PointCloud2 output;
-  compute (cloud, output, sigma_s, sigma_r, early_division);
+  if (!batch_mode)
+  {
+    // Parse the command line arguments for .pcd files
+    vector<int> p_file_indices;
+    p_file_indices = parse_file_extension_argument (argc, argv, ".pcd");
+    if (p_file_indices.size () != 2)
+    {
+      print_error ("Need one input PCD file and one output PCD file to continue.\n");
+      return (-1);
+    }
 
-  // Save into the second file
-  saveCloud (argv[p_file_indices[1]], output);
+    print_info ("Smoothing data with a sigma S/R of: "); 
+    print_value ("%f / %f\n", sigma_s, sigma_r);
+
+    // Load the first file
+    Eigen::Vector4f translation;
+    Eigen::Quaternionf rotation;
+    sensor_msgs::PointCloud2::Ptr cloud (new sensor_msgs::PointCloud2);
+    if (!loadCloud (argv[p_file_indices[0]], *cloud, translation, rotation)) 
+      return (-1);
+
+    // Perform the feature estimation
+    sensor_msgs::PointCloud2 output;
+    compute (cloud, output, sigma_s, sigma_r);
+
+    // Save into the second file
+    saveCloud (argv[p_file_indices[1]], output, translation, rotation);
+  }
+  else
+  {
+    if (input_dir != "" && boost::filesystem::exists (input_dir))
+    {
+      vector<string> pcd_files;
+      boost::filesystem::directory_iterator end_itr;
+      for (boost::filesystem::directory_iterator itr (input_dir); itr != end_itr; ++itr)
+      {
+        // Only add PCD files
+        if (!is_directory (itr->status ()) && boost::algorithm::to_upper_copy (boost::filesystem::extension (itr->path ())) == ".PCD" )
+        {
+          pcd_files.push_back (itr->path ().string ());
+          PCL_INFO ("[Batch processing mode] Added %s for processing.\n", itr->path ().string ().c_str ());
+        }
+      }
+      batchProcess (pcd_files, output_dir, sigma_s, sigma_r);
+    }
+    else
+    {
+      PCL_ERROR ("Batch processing mode enabled, but invalid input directory (%s) given!\n", input_dir.c_str ());
+      return (-1);
+    }
+  }
 }
+
