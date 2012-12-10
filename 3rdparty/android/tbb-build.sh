2cb605a41cf6cb757d328ba3d104a3e76c34c8d5
Radu B. Rusu <radu.b.rusu@gmail.com>
2012-12-10 01:24:09 +0000

    moved 3rdparty android make files to the mobile repository
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@8209 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- 3rdparty/android/tbb-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-(cd src/tbb && make ANDROID=1)
-
-rm -r ndk-mobules/tbb/include/* 2> /dev/null
-rm -r ndk-modules/tbb/lib/armeabi-v7a/* 2> /dev/null
-
-cp -a src/tbb/include/tbb ndk-modules/tbb/include
-cp src/tbb/build/android_arm_gcc_whatever_release/*.so ndk-modules/tbb/lib/armeabi-v7a
