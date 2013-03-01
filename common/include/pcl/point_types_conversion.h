319cb82e685db5ab34de60db832fe6f6a1740066
Koen Buys <buys.koen@gmail.com>
2013-03-01 09:22:39 +0000

    update for PointCloudXYZRGBAtoHSV method
    git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@8750 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- common/include/pcl/point_types_conversion.h
+++ common/include/pcl/point_types_conversion.h
@@ -131,8 +131,44 @@ namespace pcl
     if (out.h < 0.f) out.h += 360.f;
   }
 
+  /** \brief Convert a XYZRGB point type to a XYZHSV
+    * \param[in] in the input XYZRGB point
+    * \param[out] out the output XYZHSV point
+    * \todo include the A parameter but how?
+    */
+  inline void
+  PointXYZRGBAtoXYZHSV (PointXYZRGBA& in,
+                        PointXYZHSV& out)
+  {
+    const unsigned char max = std::max (in.r, std::max (in.g, in.b));
+    const unsigned char min = std::min (in.r, std::min (in.g, in.b));
 
-  /** \brief Convert a XYZHSV point type to a XYZRGB
+    out.v = static_cast <float> (max) / 255.f;
+
+    if (max == 0) // division by zero
+    {
+      out.s = 0.f;
+      out.h = 0.f; // h = -1.f;
+      return;
+    }
+
+    const float diff = static_cast <float> (max - min);
+    out.s = diff / static_cast <float> (max);
+
+    if (min == max) // diff == 0 -> division by zero
+    {
+      out.h = 0;
+      return;
+    }
+
+    if      (max == in.r) out.h = 60.f * (      static_cast <float> (in.g - in.b) / diff);
+    else if (max == in.g) out.h = 60.f * (2.f + static_cast <float> (in.b - in.r) / diff);
+    else                  out.h = 60.f * (4.f + static_cast <float> (in.r - in.g) / diff); // max == b
+
+    if (out.h < 0.f) out.h += 360.f;
+  }
+
+  /* \brief Convert a XYZHSV point type to a XYZRGB
     * \param[in] in the input XYZHSV point 
     * \param[out] out the output XYZRGB point
     */
@@ -268,6 +304,24 @@ namespace pcl
     {
       PointXYZHSV p;
       PointXYZRGBtoXYZHSV (in.points[i], p);
+      out.points.push_back (p);
+    }
+  }
+
+  /** \brief Convert a XYZRGB point cloud to a XYZHSV
+    * \param[in] in the input XYZRGB point cloud
+    * \param[out] out the output XYZHSV point cloud
+    */
+  inline void
+  PointCloudXYZRGBAtoXYZHSV (PointCloud<PointXYZRGBA>& in,
+                             PointCloud<PointXYZHSV>& out)
+  {
+    out.width   = in.width;
+    out.height  = in.height;
+    for (size_t i = 0; i < in.points.size (); i++)
+    {
+      PointXYZHSV p;
+      PointXYZRGBAtoXYZHSV (in.points[i], p);
       out.points.push_back (p);
     }
   }
