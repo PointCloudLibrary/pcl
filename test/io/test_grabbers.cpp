89e77539d1a48404a496d3e6372cfbd2075e0b55
sdmiller <sdmiller@stanford.edu>
2013-05-21 17:40:41 -0700

    Fixed other test_grabbers bug

--- test/io/test_grabbers.cpp
+++ test/io/test_grabbers.cpp
@@ -378,7 +378,11 @@ TEST (PCL, ImageGrabberSetIntrinsicsTIFF)
   double cx_multiplier = 0.8;
   double cy_multiplier = 1.3;
   double fx_old, fy_old, cx_old, cy_old;
-  grabber.getCameraIntrinsics (fx_old, fy_old, cx_old, cy_old);
+  //grabber.getCameraIntrinsics (fx_old, fy_old, cx_old, cy_old); Need to use old intrinsics, can't trust defaults
+  fx_old = 525;
+  fy_old = 525;
+  cx_old = 320;
+  cy_old = 240;
   double fx_new = fx_multiplier * fx_old;
   double fy_new = fy_multiplier * fy_old;
   double cx_new = cx_multiplier * cx_old;
