52d17f559e1ee7185ad6c3b7c450196d9e4e06f6
Jochen Sprickerhof <jochen@sprickerhof.de>
2013-06-23 11:53:51 +0200

    Add PointXYZRGBNormal to PCL_ONLY_CORE_POINT_TYPES
    Needed since 96f1c162e7f6dfb2aa6da7c2bd0b4fd22ae615f4

--- sample_consensus/src/sac_model_circle.cpp
+++ sample_consensus/src/sac_model_circle.cpp
@@ -43,7 +43,7 @@
 #include <pcl/point_types.h>
 // Instantiations of specific point types
 #ifdef PCL_ONLY_CORE_POINT_TYPES
-  PCL_INSTANTIATE(SampleConsensusModelCircle2D, (pcl::PointXYZ)(pcl::PointXYZI)(pcl::PointXYZRGBA)(pcl::PointXYZRGB))
+  PCL_INSTANTIATE(SampleConsensusModelCircle2D, (pcl::PointXYZ)(pcl::PointXYZI)(pcl::PointXYZRGBA)(pcl::PointXYZRGB)(pcl::PointXYZRGBNormal))
 #else
   PCL_INSTANTIATE(SampleConsensusModelCircle2D, PCL_XYZ_POINT_TYPES)
 #endif
