6b7a62d356474120d545971fd50ed514e74e9a87
nizar-sallem <nksallem@gmail.com>
2013-10-11 11:45:08 +0200

    Cosmetics: clean up

--- cmake/Modules/FindFZAPI.cmake
+++ cmake/Modules/FindFZAPI.cmake
@@ -6,26 +6,19 @@
 # FZAPI_INCLUDE_DIRS - Directories containing the FZAPI include files.
 # FZAPI_LIBRARIES - Libraries needed to use FZAPI.
  
-#MESSAGE("Searching for Fotonic in: ${FZ_API_DIR}")
-
 if(FZ_API_DIR)
   # Find include dirs
-  #MESSAGE("Searching Fotonic includes: ${FZ_API_DIR}")
   find_path(FZAPI_INCLUDE_DIR fz_api.h
     PATHS "${FZ_API_DIR}" NO_DEFAULT_PATH
     DOC "Fotonic include directories")
 	
   # Find libraries
-  #MESSAGE("Searching Fotonic libs: ${FZ_API_DIR}/Release")
   find_library(FZAPI_LIBS fotonic_fz_api
     HINTS "${FZ_API_DIR}/Release" NO_DEFAULT_PATH
     DOC "Fotonic libraries")
 else()
   set(FZ_API_DIR "default value" CACHE FILEPATH "directory of Fotonic API")	
 endif()
-
-#MESSAGE("FZAPI_INCLUDE_DIR: ${FZAPI_INCLUDE_DIR}")
-#MESSAGE("FZAPI_LIBS: ${FZAPI_LIBS}")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FZAPI DEFAULT_MSG
