1a219500f12567fd29be20ffe4e13a0f026a84e9
Andrei Zaharescu <cooperz@gmail.com>
2013-10-31 11:43:21 -0400

    fixes to make it work on mac os x 10.9 mavericks

--- common/include/pcl/impl/point_types.hpp
+++ common/include/pcl/impl/point_types.hpp
@@ -350,6 +350,7 @@ namespace pcl
       intensity = 0;
     }
   
+    operator unsigned char() const { return intensity; }
     friend std::ostream& operator << (std::ostream& os, const Intensity8u& p);
   };
 
@@ -1082,7 +1083,7 @@ namespace pcl
   struct Boundary
   {
     uint8_t boundary_point;
-  
+    operator unsigned char() const { return boundary_point; }
     friend std::ostream& operator << (std::ostream& os, const Boundary& p);
   };
 
