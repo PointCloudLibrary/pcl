7e62e3655a7349e36c70772d3bcb09d5a0a25f37
nizar-sallem <nksallem@gmail.com>
2013-11-28 11:16:41 +0100

    Fix: response_ instead of response

--- keypoints/include/pcl/keypoints/impl/harris_2d.hpp
+++ keypoints/include/pcl/keypoints/impl/harris_2d.hpp
@@ -252,7 +252,7 @@ pcl::HarrisKeypoint2D<PointInT, PointOutT, IntensityT>::detectKeypoints (PointCl
   if (!nonmax_)
   {
     output = *response_;
-    for (size_t i = 0; i < response->size (); ++i)
+    for (size_t i = 0; i < response_->size (); ++i)
       keypoints_indices_->indices.push_back (i);
   }
   else
