9b0887ac426793029bdfe53627234789faac72d8
Victor Lamoine <victor.lamoine@gmail.com>
2014-02-19 08:45:44 +0100

    Make camera parameters human readable

--- visualization/src/interactor_style.cpp
+++ visualization/src/interactor_style.cpp
@@ -527,9 +527,13 @@ pcl::visualization::PCLVisualizerInteractorStyle::OnKeyDown ()
       cam->GetViewUp (view);
       int *win_pos = Interactor->GetRenderWindow ()->GetPosition ();
       int *win_size = Interactor->GetRenderWindow ()->GetSize ();
-      ofs_cam << clip[0]  << "," << clip[1]  << "/" << focal[0] << "," << focal[1] << "," << focal[2] << "/" <<
-                 pos[0]   << "," << pos[1]   << "," << pos[2]   << "/" << view[0]  << "," << view[1]  << "," << view[2] << "/" <<
-                 cam->GetViewAngle () / 180.0 * M_PI  << "/" << win_size[0] << "," << win_size[1] << "/" << win_pos[0] << "," << win_pos[1]
+      ofs_cam <<	"Clipping plane: near["  << clip[0] << "] far[" << clip[1] << "]" << endl <<
+					"Focal point: x[" << focal[0] << "] y[" << focal[1] << "] z[" << focal[2] << "]" << endl <<
+					"Position: x[" << pos[0] << "] y[" << pos[1] << "] z[" << pos[2] << "]" << endl <<
+					"View (see doc: Up vector of the camera) x[" << view[0]  << "] y[" << view[1]  << "] z[" << view[2] << "]" << endl <<
+                 	"Camera view angle: [" <<	cam->GetViewAngle () / 180.0 * M_PI  << "]" << endl <<
+					"Window size: x[" << win_size[0] << "] y[" << win_size[1] << "]" << endl <<
+					"Window position: x[" << win_pos[0] << "] y[" << win_pos[1] << "]"
               << endl;
       ofs_cam.close ();
 
@@ -547,10 +551,14 @@ pcl::visualization::PCLVisualizerInteractorStyle::OnKeyDown ()
       cam->GetViewUp (view);
       int *win_pos = Interactor->GetRenderWindow ()->GetPosition ();
       int *win_size = Interactor->GetRenderWindow ()->GetSize ();
-      std::cerr << clip[0]  << "," << clip[1]  << "/" << focal[0] << "," << focal[1] << "," << focal[2] << "/" <<
-                   pos[0]   << "," << pos[1]   << "," << pos[2]   << "/" << view[0]  << "," << view[1]  << "," << view[2] << "/" <<
-                   cam->GetViewAngle () / 180.0 * M_PI  << "/" << win_size[0] << "," << win_size[1] << "/" << win_pos[0] << "," << win_pos[1]
-                << endl;
+      std::cerr <<	"Clipping plane: near["  << clip[0] << "] far[" << clip[1] << "]" << endl <<
+					"Focal point: x[" << focal[0] << "] y[" << focal[1] << "] z[" << focal[2] << "]" << endl <<
+					"Position: x[" << pos[0] << "] y[" << pos[1] << "] z[" << pos[2] << "]" << endl <<
+					"View (see doc: Up vector of the camera) x[" << view[0]  << "] y[" << view[1]  << "] z[" << view[2] << "]" << endl <<
+                 	"Camera view angle: [" <<	cam->GetViewAngle () / 180.0 * M_PI  << "]" << endl <<
+					"Window size: x[" << win_size[0] << "] y[" << win_size[1] << "]" << endl <<
+					"Window position: x[" << win_pos[0] << "] y[" << win_pos[1] << "]"
+              << endl;
       break;
     }
     case '=':
