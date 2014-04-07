dd7f6df44208171743d7a1702cf5fe13ec9c9e39
Victor Lamoine <victor.lamoine@gmail.com>
2014-04-07 18:47:36 +0200

    Matrix transform tutorial update

--- doc/tutorials/content/matrix_transform.rst
+++ doc/tutorials/content/matrix_transform.rst
@@ -30,16 +30,13 @@ Now, let's break down the code piece by piece.
    :lines: 1-8
 
 We include all the headers we will make use of.
-**#include <pcl/registration/ia_ransac.h>** allows us to use **pcl::transformPointCloud** function.
-
-
+**#include <pcl/common/transforms.h>** allows us to use **pcl::transformPointCloud** function.
 
 .. literalinclude:: sources/matrix_transform/matrix_transform.cpp
    :language: cpp
    :lines: 10-17
 
 This function display the help in case the user didn't provide expected arguments.
-
 
 
 .. literalinclude:: sources/matrix_transform/matrix_transform.cpp
@@ -50,14 +47,12 @@ We parse the arguments on the command line, either using **-h** or **--help** wi
 display the help. This terminates the program
 
 
-
 .. literalinclude:: sources/matrix_transform/matrix_transform.cpp
    :language: cpp
    :lines: 30-45
 
 We look for .ply or .pcd filenames in the arguments. If not found; terminate the program.
 The bool **file_is_pcd** will help us choose between loading PCD or PLY file.
-
 
 
 .. literalinclude:: sources/matrix_transform/matrix_transform.cpp
@@ -68,22 +63,32 @@ We now load the PCD/PLY file and check if the file was loaded successfuly. Other
 the program.
 
 
-
 .. literalinclude:: sources/matrix_transform/matrix_transform.cpp
    :language: cpp
-   :lines: 64-81
+   :lines: 64-75
 
-The interesting part: We define a 4x4 matrix for our transformation. We ititialize the matrix 
-to indentity::
+This is a first approach to create a transformation. This will help you understand how transformation matrices work.
+We initialize a 4x4 matrix to identity;  ::
 
           |  1  0  0  0  |
       i = |  0  1  0  0  |
           |  0  0  1  0  |
           |  0  0  0  1  |
 
-This means no transformation (null rotation and translation). We do not use the 
-last row of the matrix. The first 3 rows and colums (top left) components are the rotation
+.. note::
+
+  The identity matrix is the equivalent of "1" when multiplying numbers; it changes nothing.
+  It is a square matrix with ones on the main diagonal and zeros elsewhere.
+
+This means no transformation (no rotation and no translation). We do not use the 
+last row of the matrix.
+
+The first 3 rows and colums (top left) components are the rotation
 matrix. The first 3 rows of the last column is the translation.
+
+.. literalinclude:: sources/matrix_transform/matrix_transform.cpp
+   :language: cpp
+   :lines: 77-90
 
 Here we defined a 45° (PI/4) rotation around the Z axis and a translation on the X axis.
 This is the transformation we just defined ::
@@ -94,27 +99,24 @@ This is the transformation we just defined ::
 
       t = < 2.5, 0.0, 0.0 >
 
+.. literalinclude:: sources/matrix_transform/matrix_transform.cpp
+   :language: cpp
+   :lines: 92-105
+
+This second approach is easier to understand and is less error prone. 
+Be carefull if you want to apply several rotations; rotations are not commutative ! This means than in most cases :
+rotA * rotB != rotB * rotA.
 
 .. literalinclude:: sources/matrix_transform/matrix_transform.cpp
    :language: cpp
-   :lines: 83-89
-
-Here we display the rotation matrix and the translation vector.
-
-
-
-.. literalinclude:: sources/matrix_transform/matrix_transform.cpp
-   :language: cpp
-   :lines: 91-92
+   :lines: 107-110
 
 Now we apply this matrix on the point cloud **source_cloud** and we save the result in the
- newly created **transformed_cloud**.
-
-
+newly created **transformed_cloud**.
 
 .. literalinclude:: sources/matrix_transform/matrix_transform.cpp
    :language: cpp
-   :lines: 95-113
+   :lines: 112-135
 
 We then visualize the result using the **PCLVisualizer**. The original point cloud will be
 displayed white and the transformed one in red. The coordoniates axis will be displayed.
@@ -136,25 +138,47 @@ After you have made the executable, you can run it. Simply do::
 You will see something similar to this::
 
   ./matrix_transform cube.ply 
-  [pcl::PLYReader] cube.ply:12: property 'list uint8 uint32 vertex_indices' of element 'face' is not handled
-  [pcl::PLYReader] cube.ply:26: property 'float32 focal' of element 'camera' is not handled
-  [pcl::PLYReader] cube.ply:27: property 'float32 scalex' of element 'camera' is not handled
-  [pcl::PLYReader] cube.ply:28: property 'float32 scaley' of element 'camera' is not handled
-  [pcl::PLYReader] cube.ply:29: property 'float32 centerx' of element 'camera' is not handled
-  [pcl::PLYReader] cube.ply:30: property 'float32 centery' of element 'camera' is not handled
-  [pcl::PLYReader] cube.ply:33: property 'float32 k1' of element 'camera' is not handled
-  [pcl::PLYReader] cube.ply:34: property 'float32 k2' of element 'camera' is not handled
+  [pcl::PLYReader] /home/victor/cube.ply:12: property 'list uint8 uint32 vertex_indices' of element 'face' is not handled
+  Method #1: using a Matrix4f
+   0.707107 -0.707107         0       2.5
+   0.707107  0.707107         0         0
+          0         0         1         0
+          0         0         0         1
   
-  This is the rotation matrix :
-      |    0.7   -0.7    0.0 | 
-  R = |    0.7    0.7    0.0 | 
-      |    0.0    0.0    1.0 | 
+  Method #2: using an Affine3f
+   0.707107 -0.707107         0       2.5
+   0.707107  0.707107         0         0
+          0         0         1         0
+          0         0         0         1
   
-  This is the translation vector :
-  t = <    2.5,    0.0,    0.0 >
-  
-  Point cloud colors :	black	= original point cloud
-  			red	= transformed point cloud
+  Point cloud colors :	white	= original point cloud
+                         red	= transformed point cloud
 
 .. image:: images/matrix_transform/cube_big.png
   :height: 614
+
+More about transformations
+--------------------------
+
+| So now you successfully transformed a point cloud using a transformation matrix.
+| What if you want to transform a single point ? A vector ?
+
+| A point is defined in 3D space with its three coordinates; x,y,z (in a cartesian coordinate system).
+| How can you multiply a vector (with 3 coordinates) with a 4x4 matrix ? You simply can't ! If you don't know why please refer to `matrix multiplications on wikipedia <https://en.wikipedia.org/wiki/Matrix_multiplication>`_.
+
+We need a vector with 4 components. What do you put in the last component ? It depends on what you want to do :
+
+* If you want to transform a point: put 1 at the end of the vector so that the translation is taken in account.
+* If you want to transform the direction of a vector: put 0 at the end of the vector to ignore the translation.
+
+| Here's a quick example, we want to transform the following vector : 
+| [10.0, 0.0, 0.0, 1.0, 0.0, 0.0]
+
+| Where the first 3 components defines the origin coordinates and the last 3 components the direction.
+| This vector starts at point 10.0, 5.0, 0.0 and ends at 11.0, 5.0, 0.0.
+
+This is what you need to do to transform the vector : ::
+
+  [10.0, 5.0, 0.0, 1.0] * 4x4_transformation_matrix
+  [1.0, 0.0, 0.0, 0.0] * 4x4_transformation_matrix
+
--- doc/tutorials/content/sources/matrix_transform/matrix_transform.cpp
+++ doc/tutorials/content/sources/matrix_transform/matrix_transform.cpp
@@ -4,16 +4,16 @@
 #include <pcl/io/ply_io.h>
 #include <pcl/point_cloud.h>
 #include <pcl/console/parse.h>
-#include <pcl/registration/ia_ransac.h>
+#include <pcl/common/transforms.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
 // This function displays the help
 void
 showHelp(char * program_name)
 {
-	std::cout << std::endl;
-	std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
-	std::cout << "-h:	Show this help." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
+  std::cout << "-h:  Show this help." << std::endl;
 }
 
 // This is the main function
@@ -21,97 +21,117 @@ int
 main (int argc, char** argv)
 {
 
-	// Show help
-	if (pcl::console::find_switch (argc, argv, "-h") || pcl::console::find_switch (argc, argv, "--help")) {
-		showHelp (argv[0]);
-		return 0;
-	}
+  // Show help
+  if (pcl::console::find_switch (argc, argv, "-h") || pcl::console::find_switch (argc, argv, "--help")) {
+    showHelp (argv[0]);
+    return 0;
+  }
 
-	// Fetch point cloud filename in arguments | Works with PCD and PLY files
-	std::vector<int> filenames;
-	bool file_is_pcd = false;
+  // Fetch point cloud filename in arguments | Works with PCD and PLY files
+  std::vector<int> filenames;
+  bool file_is_pcd = false;
 
-	filenames = pcl::console::parse_file_extension_argument (argc, argv, ".ply");
+  filenames = pcl::console::parse_file_extension_argument (argc, argv, ".ply");
 
-	if (filenames.size () != 1)	{
-		filenames = pcl::console::parse_file_extension_argument (argc, argv, ".pcd");
+  if (filenames.size () != 1)  {
+    filenames = pcl::console::parse_file_extension_argument (argc, argv, ".pcd");
 
-		if (filenames.size () != 1) {
-			showHelp (argv[0]);
-			return -1;
-		} else {
-			file_is_pcd = true;
-		}
-	}
+    if (filenames.size () != 1) {
+      showHelp (argv[0]);
+      return -1;
+    } else {
+      file_is_pcd = true;
+    }
+  }
 
-	// Loading file | Works with PCD and PLY files
-	pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+  // Load file | Works with PCD and PLY files
+  pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
 
-	if (file_is_pcd) {
-		if (pcl::io::loadPCDFile (argv[filenames[0]], *source_cloud) < 0)	{
-			std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
-			showHelp (argv[0]);
-			return -1;
-		}
-	} else {
-		if (pcl::io::loadPLYFile (argv[filenames[0]], *source_cloud) < 0)	{
-			std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
-			showHelp (argv[0]);
-			return -1;
-		}
-	}
+  if (file_is_pcd) {
+    if (pcl::io::loadPCDFile (argv[filenames[0]], *source_cloud) < 0)  {
+      std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+      showHelp (argv[0]);
+      return -1;
+    }
+  } else {
+    if (pcl::io::loadPLYFile (argv[filenames[0]], *source_cloud) < 0)  {
+      std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+      showHelp (argv[0]);
+      return -1;
+    }
+  }
 
-	// Defining rotation matrix and translation vector
-	Eigen::Matrix4f transformation_matrix = Eigen::Matrix4f::Identity(); // We initialize this matrix to a null transformation.
+  /* Reminder: how transformation matrices work :
 
-   	//			|-------> This column is the translation
-	//	| 1 0 0 x |  \
-	//	| 0 1 0 y |   }-> The identity 3x3 matrix (no rotation)
-	//	| 0 0 1 z |  /
-	//	| 0 0 0 1 |    -> We do not use this line
+           |-------> This column is the translation
+    | 1 0 0 x |  \
+    | 0 1 0 y |   }-> The identity 3x3 matrix (no rotation) on the left
+    | 0 0 1 z |  /
+    | 0 0 0 1 |    -> We do not use this line (and it has to stay 0,0,0,1)
 
-	// Defining a rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
-	float theta = M_PI/4; // The angle of rotation in radians
-	transformation_matrix (0,0) = cos(theta);
-	transformation_matrix (0,1) = -sin(theta);
-	transformation_matrix (1,0) = sin(theta);
-	transformation_matrix (1,1) = cos(theta);
+    METHOD #1: Using a Matrix4f
+    This is the "manual" method, perfect to understand but error prone !
+  */
+  Eigen::Matrix4f transform_1 = Eigen::Matrix4f::Identity();
 
-	// Defining a translation of 2 meters on the x axis.
-	transformation_matrix (0,3) = 2.5;
+  // Define a rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
+  float theta = M_PI/4; // The angle of rotation in radians
+  transform_1 (0,0) = cos (theta);
+  transform_1 (0,1) = -sin(theta);
+  transform_1 (1,0) = sin (theta);
+  transform_1 (1,1) = cos (theta);
+  //    (row, column)
 
-	// Printing the transformation
-	printf ("\nThis is the rotation matrix :\n");
-	printf ("    | %6.1f %6.1f %6.1f | \n", transformation_matrix (0,0), transformation_matrix (0,1), transformation_matrix (0,2));
-	printf ("R = | %6.1f %6.1f %6.1f | \n", transformation_matrix (1,0), transformation_matrix (1,1), transformation_matrix (1,2));
-	printf ("    | %6.1f %6.1f %6.1f | \n", transformation_matrix (2,0), transformation_matrix (2,1), transformation_matrix (2,2));
-	printf ("\nThis is the translation vector :\n");
-	printf ("t = < %6.1f, %6.1f, %6.1f >\n", transformation_matrix (0,3), transformation_matrix (1,3), transformation_matrix (2,3));
+  // Define a translation of 2.5 meters on the x axis.
+  transform_1 (0,3) = 2.5;
 
-	// Executing the transformation
-	pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());	// A pointer on a new cloud
-	pcl::transformPointCloud (*source_cloud, *transformed_cloud, transformation_matrix);
+  // Print the transformation
+  printf ("Method #1: using a Matrix4f\n");
+  std::cout << transform_1 << std::endl;
 
-	// Visualization
-	printf("\nPoint cloud colors :	black	= original point cloud\n			red	= transformed point cloud\n");
-	pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+  /*  METHOD #2: Using a Affine3f
+    This method is easier and less error prone
+  */
+  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
 
-	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (source_cloud, 0, 0, 0); // Where 255,255,255 are R,G,B colors
-	viewer.addPointCloud (source_cloud, source_cloud_color_handler, "original_cloud");	// We add the point cloud to the viewer and pass the color handler
+  // Define a translation of 2.5 meters on the x axis.
+  transform_2.translation() << 2.5, 0.0, 0.0;
 
-	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler (transformed_cloud, 230, 20, 20);
-	viewer.addPointCloud (transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
+  // The same rotation matrix as before; tetha radians arround Z axis
+  transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
 
-	viewer.addCoordinateSystem (1.0, "cloud", 0);
-	viewer.setBackgroundColor(0.95, 0.95, 0.95, 0); // Setting background to a dark grey
-	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
-	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
-	//viewer.setPosition(800, 400); // Setting visualiser window position
+  // Print the transformation
+  printf ("\nMethod #2: using an Affine3f\n");
+  std::cout << transform_2.matrix() << std::endl;
 
-	while (!viewer.wasStopped ()) { // Display the visualiser untill 'q' key is pressed
-		viewer.spinOnce ();
-	}
+  // Executing the transformation
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+  // You can either apply transform_1 or transform_2; they are the same
+  pcl::transformPointCloud (*source_cloud, *transformed_cloud, transform_2);
 
-	return 0;
+  // Visualization
+  printf(  "\nPoint cloud colors :  white  = original point cloud\n"
+      "                        red  = transformed point cloud\n");
+  pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+
+   // Define R,G,B colors for the point cloud
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (source_cloud, 255, 255, 255);
+  // We add the point cloud to the viewer and pass the color handler
+  viewer.addPointCloud (source_cloud, source_cloud_color_handler, "original_cloud");
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler (transformed_cloud, 230, 20, 20); // Red
+  viewer.addPointCloud (transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
+
+  viewer.addCoordinateSystem (1.0, "cloud", 0);
+  viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
+  //viewer.setPosition(800, 400); // Setting visualiser window position
+
+  while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
+    viewer.spinOnce ();
+  }
+
+  return 0;
 }
 
