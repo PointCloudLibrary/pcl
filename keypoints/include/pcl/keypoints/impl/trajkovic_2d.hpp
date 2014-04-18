be64c8fa74e838f15b33d2e9bcf043c40e4151be
Nizar Sallem <nksallem@gmail.com>
2014-04-18 14:19:46 +0200

    Fix: OpenMP with current GCC version doesn't like inherited members

--- keypoints/include/pcl/keypoints/impl/trajkovic_2d.hpp
+++ keypoints/include/pcl/keypoints/impl/trajkovic_2d.hpp
@@ -219,7 +219,7 @@ pcl::TrajkovicKeypoint2D<PointInT, PointOutT, IntensityT>::detectKeypoints (Poin
   const int occupency_map_size (indices.size ());
 
 #ifdef _OPENMP
-#pragma omp parallel for shared (output, keypoints_indices_) num_threads (threads_)
+#pragma omp parallel for shared (output) num_threads (threads_)
 #endif
   for (int i = 0; i < indices.size (); ++i)
   {
