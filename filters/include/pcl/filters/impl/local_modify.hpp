b6729290843bf16b76eb65b194b871cbeb0e99e8
Whitney O'Meara <womeara@radiantblue.com>
2014-05-09 11:46:57 -0400

    Updated local_modify filter to fix an issue with box search

--- filters/include/pcl/filters/impl/local_modify.hpp
+++ filters/include/pcl/filters/impl/local_modify.hpp
@@ -220,8 +220,7 @@ pcl::LocalModify<PointT>::applyLocalFilter (PointCloud &output)
   // Initialize the search class
   if (locality_type_ == LT_BOX)
   {
-    if (!octree_)
-      octree_.reset (new pcl::octree::OctreePointCloudSearch<PointT> (resolution_));
+    octree_.reset (new pcl::octree::OctreePointCloudSearch<PointT> (resolution_));
 
     octree_->setInputCloud (cloud_projected);
     octree_->addPointsFromInputCloud ();
