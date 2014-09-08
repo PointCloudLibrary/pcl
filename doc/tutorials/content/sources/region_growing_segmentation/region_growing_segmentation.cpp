1c82cbc7848fa5048d64db11ec1c0f15df97bdef
Victor Lamoine <victor.lamoine@gmail.com>
2014-09-08 09:04:14 +0200

    Re-format code

--- doc/tutorials/content/sources/region_growing_segmentation/region_growing_segmentation.cpp
+++ doc/tutorials/content/sources/region_growing_segmentation/region_growing_segmentation.cpp
@@ -10,24 +10,25 @@
 #include <pcl/segmentation/region_growing.h>
 
 int
-main (int argc, char** argv)
+main (int argc,
+      char** argv)
 {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  if ( pcl::io::loadPCDFile <pcl::PointXYZ> ("region_growing_tutorial.pcd", *cloud) == -1)
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("region_growing_tutorial.pcd", *cloud) == -1)
   {
     std::cout << "Cloud reading failed." << std::endl;
     return (-1);
   }
 
   pcl::search::Search<pcl::PointXYZ>::Ptr tree = boost::shared_ptr<pcl::search::Search<pcl::PointXYZ> > (new pcl::search::KdTree<pcl::PointXYZ>);
-  pcl::PointCloud <pcl::Normal>::Ptr normals (new pcl::PointCloud <pcl::Normal>);
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
   normal_estimator.setSearchMethod (tree);
   normal_estimator.setInputCloud (cloud);
   normal_estimator.setKSearch (50);
   normal_estimator.compute (*normals);
 
-  pcl::IndicesPtr indices (new std::vector <int>);
+  pcl::IndicesPtr indices (new std::vector<int>);
   pcl::PassThrough<pcl::PointXYZ> pass;
   pass.setInputCloud (cloud);
   pass.setFilterFieldName ("z");
@@ -45,13 +46,12 @@ main (int argc, char** argv)
   reg.setSmoothnessThreshold (3.0 / 180.0 * M_PI);
   reg.setCurvatureThreshold (1.0);
 
-  std::vector <pcl::PointIndices> clusters;
+  std::vector<pcl::PointIndices> clusters;
   reg.extract (clusters);
 
   std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
   std::cout << "First cluster has " << clusters[0].indices.size () << " points." << endl;
-  std::cout << "These are the indices of the points of the initial" <<
-    std::endl << "cloud that belong to the first cluster:" << std::endl;
+  std::cout << "These are the indices of the points of the initial" << std::endl << "cloud that belong to the first cluster:" << std::endl;
   int counter = 0;
   while (counter < 5 || counter > clusters[0].indices.size ())
   {
@@ -59,12 +59,13 @@ main (int argc, char** argv)
     counter++;
   }
 
-  pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
   pcl::visualization::CloudViewer viewer ("Cluster viewer");
-  viewer.showCloud(colored_cloud);
+  viewer.showCloud (colored_cloud);
   while (!viewer.wasStopped ())
   {
   }
 
   return (0);
 }
+
