765c1be99760b2feaf0186da7588f5ce529e99a6
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2015-08-25 23:13:25 +0200

    Only initialize covariances if they are empty
    Adopted from 3ee92d922308cdfd78e59d8f326383432e46dc4a

--- registration/src/gicp6d.cpp
+++ registration/src/gicp6d.cpp
@@ -191,10 +191,18 @@ namespace pcl
     mahalanobis_.resize (N, Eigen::Matrix3d::Identity ());
 
     // Compute target cloud covariance matrices
-    computeCovariances<PointTarget> (target_, tree_, *target_covariances_);
+    if ((!target_covariances_) || (target_covariances_->empty ()))
+    {
+      target_covariances_.reset (new MatricesVector);
+      computeCovariances<PointTarget> (target_, tree_, *target_covariances_);
+    }
     // Compute input cloud covariance matrices
-    computeCovariances<PointSource> (input_, tree_reciprocal_,
-        *input_covariances_);
+    if ((!input_covariances_) || (input_covariances_->empty ()))
+    {
+      input_covariances_.reset (new MatricesVector);
+      computeCovariances<PointSource> (input_, tree_reciprocal_,
+          *input_covariances_);
+    }
 
     base_transformation_ = guess;
     nr_iterations_ = 0;
