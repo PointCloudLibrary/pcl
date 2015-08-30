20835db9296735b4fb83f13c0477fbd116da109a
Chen Feng <simbaforrest at gmail dot com>
2015-08-30 13:12:37 -0400

    Fixed compile error related to example projects

--- examples/features/example_difference_of_normals.cpp
+++ examples/features/example_difference_of_normals.cpp
@@ -24,6 +24,11 @@
 
 #include <pcl/features/don.h>
 
+#ifdef PCL_ONLY_CORE_POINT_TYPES
+#include <pcl/features/impl/normal_3d_omp.hpp>
+#include <pcl/segmentation/impl/extract_clusters.hpp>
+#endif
+
 using namespace pcl;
 using namespace std;
 
