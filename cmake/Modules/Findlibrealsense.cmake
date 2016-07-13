2911593fd89979d77fea0a5dea861f079f056607
lebronzhang <lebron.zhang@intel.com>
2016-07-14 06:29:36 +0800

    Add static link support to librealsense

--- cmake/Modules/Findlibrealsense.cmake
+++ cmake/Modules/Findlibrealsense.cmake
@@ -13,19 +13,40 @@ if (LIBREALSENSE_LIBRARIES AND LIBREALSENSE_INCLUDE_DIRS)
   # in cache already
   set(LIBREALSENSE_FOUND TRUE)
 else (LIBREALSENSE_LIBRARIES AND LIBREALSENSE_INCLUDE_DIRS)
+  find_path(LIBREALSENSE_DIR include/librealsense
+            PATHS $ENV{LIBREALSENSE_DIR} NO_DEFAULT_PATH)
+  if (WIN32)
+    set(LIBREALSENSE_RELEASE_NAME realsense.lib)
+    set(LIBREALSENSE_DEBUG_NAME realsense-d.lib)
+    set(LIBREALSENSE_SUFFIX Win32)
+    if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+      set(LIBREALSENSE_SUFFIX x64)
+    endif ()
+  else (WIN32)
+    set(LIBREALSENSE_RELEASE_NAME librealsense.so)
+    set(LIBREALSENSE_DEBUG_NAME librealsense.a)
+  endif (WIN32)
+
 find_path(LIBREALSENSE_INCLUDE_DIR
           NAMES librealsense
-          PATHS /usr/local/include /usr/include /opt/local/include /sw/include)
+          PATHS /usr/local/include /usr/include /opt/local/include /sw/include ${LIBREALSENSE_DIR}/include NO_DEFAULT_PATH)
 
 find_library(LIBREALSENSE_LIBRARY
-             NAMES librealsense.so
-             PATHS /usr/local/lib /usr/lib /opt/local/lib /sw/lib)
+             NAMES ${LIBREALSENSE_RELEASE_NAME}
+             PATHS ${LIBREALSENSE_DIR}/bin/ /usr/local/lib /usr/lib /opt/local/lib /sw/lib ${LIBREALSENSE_DIR}/lib/ NO_DEFAULT_PATH
+             PATH_SUFFIXES ${LIBREALSENSE_SUFFIX})
+find_library(LIBREALSENSE_LIBRARY_DEBUG
+             NAMES ${LIBREALSENSE_DEBUG_NAME}
+             PATHS ${LIBREALSENSE_DIR}/bin/ ${LIBREALSENSE_DIR}/bin/debug/ NO_DEFAULT_PATH
+             PATH_SUFFIXES ${LIBREALSENSE_SUFFIX})
+if (NOT LIBREALSENSE_LIBRARY_DEBUG)
+  set(LIBREALSENSE_LIBRARY_DEBUG ${LIBREALSENSE_LIBRARY})
+endif ()
 
+set(LIBREALSENSE_LIBRARIES optimized ${LIBREALSENSE_LIBRARY} debug ${LIBREALSENSE_LIBRARY_DEBUG})
 set(LIBREALSENSE_INCLUDE_DIRS ${LIBREALSENSE_INCLUDE_DIR})
-set(LIBREALSENSE_LIBRARIES ${LIBREALSENSE_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LIBREALSENSE LIBREALSENSE_LIBRARY LIBREALSENSE_INCLUDE_DIR)
+find_package_handle_standard_args(LIBREALSENSE LIBREALSENSE_LIBRARIES LIBREALSENSE_INCLUDE_DIRS)
 
-mark_as_advanced(LIBREALSENSE_INCLUDE_DIRS LIBREALSENSE_LIBRARIES)
 endif (LIBREALSENSE_LIBRARIES AND LIBREALSENSE_INCLUDE_DIRS)
