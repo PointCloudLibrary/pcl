a2cf3aa984ceaafc0f7110631612f2614be189d2
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-09-20 19:51:56 +0200

    fix in toPCLPointCloud2 if input cloud has no points

--- common/include/pcl/conversions.h
+++ common/include/pcl/conversions.h
@@ -254,7 +254,11 @@ namespace pcl
     // Fill point cloud binary data (padding and all)
     size_t data_size = sizeof (PointT) * cloud.points.size ();
     msg.data.resize (data_size);
-    memcpy (&msg.data[0], &cloud.points[0], data_size);
+    if (data_size)
+    {
+      //data[0] and points[0] only available for data_size > 0.
+      memcpy(&msg.data[0], &cloud.points[0], data_size);
+    }
 
     // Fill fields metadata
     msg.fields.clear ();
