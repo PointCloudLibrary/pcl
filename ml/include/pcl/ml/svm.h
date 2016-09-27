bb97b085ba4459ca3105d0082fff984bd8ff0a8e
bellonemauro <bellonemauro@gmail.com>
2016-09-27 22:21:00 +0200

    added EXPORTS for dlls

--- ml/include/pcl/ml/svm.h
+++ ml/include/pcl/ml/svm.h
@@ -45,6 +45,12 @@
 #ifdef __cplusplus
 extern "C"
 {
+#if (defined WIN32 || defined _WIN32 || defined WINCE) 
+    #define SVMLIB_EXPORTS __declspec(dllexport)
+#else
+    #define SVMLIB_EXPORTS
+#endif
+
 #endif
 
   extern int libsvm_version;
@@ -135,27 +141,27 @@ extern "C"
   struct svm_model *svm_train (const struct svm_problem *prob, const struct svm_parameter *param);
   void svm_cross_validation (const struct svm_problem *prob, const struct svm_parameter *param, int nr_fold, double *target);
 
-  int svm_save_model (const char *model_file_name, const struct svm_model *model);
+  SVMLIB_EXPORTS int svm_save_model (const char *model_file_name, const struct svm_model *model);
 
   struct svm_model *svm_load_model (const char *model_file_name);
 
   int svm_get_svm_type (const struct svm_model *model);
-  int svm_get_nr_class (const struct svm_model *model);
-  void svm_get_labels (const struct svm_model *model, int *label);
+  SVMLIB_EXPORTS int svm_get_nr_class (const struct svm_model *model);
+  SVMLIB_EXPORTS void svm_get_labels (const struct svm_model *model, int *label);
   double svm_get_svr_probability (const struct svm_model *model);
 
   double svm_predict_values (const struct svm_model *model, const struct svm_node *x, double* dec_values);
   double svm_predict (const struct svm_model *model, const struct svm_node *x);
   double svm_predict_probability (const struct svm_model *model, const struct svm_node *x, double* prob_estimates);
 
-  void svm_free_model_content (struct svm_model *model_ptr);
-  void svm_free_and_destroy_model (struct svm_model **model_ptr_ptr);
-  void svm_destroy_param (struct svm_parameter *param);
+  SVMLIB_EXPORTS void svm_free_model_content (struct svm_model *model_ptr);
+  SVMLIB_EXPORTS void svm_free_and_destroy_model (struct svm_model **model_ptr_ptr);
+  SVMLIB_EXPORTS void svm_destroy_param (struct svm_parameter *param);
 
   const char *svm_check_parameter (const struct svm_problem *prob, const struct svm_parameter *param);
   int svm_check_probability_model (const struct svm_model *model);
 
-  void svm_set_print_string_function (void (*print_func) (const char *));
+  SVMLIB_EXPORTS void svm_set_print_string_function (void (*print_func) (const char *));
 
 #ifdef __cplusplus
 }
