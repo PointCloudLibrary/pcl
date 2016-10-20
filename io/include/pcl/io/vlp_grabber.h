0dc4b892bfead02d0ddac0bea513a9cdf652e202
Tsukasa Sugiura <t.sugiura0204@gmail.com>
2016-10-20 21:12:33 +0900

    Add support pcl::PointXYZRGBA to pcl::VLPGrabber
    Add support pcl::PointXYZRGBA to pcl::VLPGrabber.
    This change will be able to attach color to point cloud using
    pcl::visualization::PointCloudColorHandlerRGBField.

--- io/include/pcl/io/vlp_grabber.h
+++ io/include/pcl/io/vlp_grabber.h
@@ -68,7 +68,7 @@ namespace pcl
        * \param[in] port UDP Port that should be used to listen for VLP packets
        */
       VLPGrabber (const boost::asio::ip::address& ipAddress,
-                  const unsigned short port);
+                  const uint16_t port);
 
       /** \brief virtual Destructor inherited from the Grabber interface. It never throws. */
       virtual
@@ -80,12 +80,27 @@ namespace pcl
       virtual std::string
       getName () const;
 
+      /** \brief Allows one to customize the colors used for each of the lasers. */
+      void
+      setLaserColorRGB (const pcl::RGB& color,
+                        uint8_t laserNumber);
+
+    protected:
+      static const uint8_t VLP_MAX_NUM_LASERS = 16;
+      static const uint8_t VLP_DUAL_MODE = 0x39;
+
     private:
+      /** \brief Lookup table of Colors for coloring each of the lasers. */
+      pcl::RGB laser_rgb_mapping_[VLP_MAX_NUM_LASERS];
+
       virtual void
       toPointClouds (HDLDataPacket *dataPacket);
 
       boost::asio::ip::address
       getDefaultNetworkAddress ();
+
+      void
+      initializeLaserMapping ();
 
       void
       loadVLP16Corrections ();
