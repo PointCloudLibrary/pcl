25f92b3e6ae73bc26265103745065025fb424853
Sérgio Agostinho <sergio.r.agostinho@gmail.com>
2017-06-25 02:39:07 +0100

    Enabled osx buils in Travis

--- .travis.sh
+++ .travis.sh
@@ -3,6 +3,7 @@
 PCL_DIR=`pwd`
 BUILD_DIR=$PCL_DIR/build
 DOC_DIR=$BUILD_DIR/doc/doxygen/html
+GTEST_DIR=$HOME/gtest
 
 TUTORIALS_DIR=$BUILD_DIR/doc/tutorials/html
 ADVANCED_DIR=$BUILD_DIR/doc/advanced/html
@@ -17,30 +18,83 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   fi
 fi
 
+function before_install_linux ()
+{
+  if [ "$CC" == "clang" ]; then
+    sudo ln -s ../../bin/ccache /usr/lib/ccache/clang
+    sudo ln -s ../../bin/ccache /usr/lib/ccache/clang++
+    export CCACHE_CPP2=yes
+  fi 
+}
+
+function before_install_osx ()
+{
+  brew tap homebrew/science
+  brew install --only-dependencies --with-openni pcl
+  brew install ccache
+  export PATH="/usr/local/opt/ccache/libexec:$PATH"
+  export CCACHE_CPP2=yes
+  CMAKE_ARGS="  -DCMAKE_C_COMPILER=/usr/local/opt/ccache/libexec/cc \
+                -DCMAKE_CXX_COMPILER=/usr/local/opt/ccache/libexec/c++"
+
+  echo 'Which $CC:'
+  which cc
+  
+  if [[ "$TASK" == "test-core" || "$TASK" == "test-ext-1" || "$TASK" == "test-ext-2" ]] ; then
+    mkdir -p $GTEST_DIR
+    git clone https://github.com/google/googletest.git $GTEST_DIR
+    CMAKE_ARGS="$CMAKE_ARGS \
+                -DGTEST_SRC_DIR=$GTEST_DIR/googletest \
+                -DGTEST_INCLUDE_DIR=$GTEST_DIR/googletest/include"
+  elif [ "$TASK" == "doc" ]; then
+    brew install doxygen
+    export PATH=$HOME/Library/Python/2.7/lib/python/site-packages:$HOME/Library/Python/2.7/bin:$PATH
+  fi
+}
+
 function before_install ()
 {
-  if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    if [ "$CC" == "clang" ]; then
-      sudo ln -s ../../bin/ccache /usr/lib/ccache/clang
-      sudo ln -s ../../bin/ccache /usr/lib/ccache/clang++
-    fi
-  fi
+  case $TRAVIS_OS_NAME in
+    linux ) before_install_linux;;
+    osx ) before_install_osx;;
+  esac
+
+  echo "CCache Info:"
+  which ccache
+  ccache --version
+  ccache --max-size=2G
+  export CCACHE_SLOPPINESS=pch_defines,time_macros
+  ccache -s
+  ls -l ~/.ccache
+
+  echo "Echo PATH:"
+  echo $PATH
+
+  echo "Envs:"
+  env
 }
 
 function build ()
 {
-  case $CC in
-    clang ) build_lib;;
-    gcc ) build_lib_core;;
-  esac
+  # case $CC in
+  #   clang ) build_lib;;
+  #   gcc ) build_lib_core;;
+  # esac
+  build_lib
 }
 
 function build_lib ()
 {
   # A complete build
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//                 TASK: BUILD all PCL modules                   //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DPCL_QT_VERSION=4 \
         -DBUILD_simulation=ON \
@@ -48,12 +102,6 @@ function build_lib ()
         -DBUILD_examples=OFF \
         -DBUILD_tools=OFF \
         -DBUILD_apps=OFF \
-        -DBUILD_apps_3d_rec_framework=OFF \
-        -DBUILD_apps_cloud_composer=OFF \
-        -DBUILD_apps_in_hand_scanner=OFF \
-        -DBUILD_apps_modeler=OFF \
-        -DBUILD_apps_optronic_viewer=OFF \
-        -DBUILD_apps_point_cloud_editor=OFF \
         $PCL_DIR
   # Build
   make -j2
@@ -62,16 +110,43 @@ function build_lib ()
 function build_examples ()
 {
   # A complete build
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//                 TASK: BUILD PCL EXAMPLES                      //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DPCL_QT_VERSION=4 \
-        -DBUILD_simulation=ON \
         -DBUILD_global_tests=OFF \
         -DBUILD_examples=ON \
         -DBUILD_tools=OFF \
         -DBUILD_apps=OFF \
+        -DBUILD_2d=ON \
+        -DBUILD_features=ON \
+        -DBUILD_filters=ON \
+        -DBUILD_geometry=ON \
+        -DBUILD_io=ON \
+        -DBUILD_kdtree=ON \
+        -DBUILD_keypoints=ON \
+        -DBUILD_ml=ON \
+        -DBUILD_octree=ON \
+        -DBUILD_outofcore=ON \
+        -DBUILD_people=OFF \
+        -DBUILD_recognition=OFF \
+        -DBUILD_registration=OFF \
+        -DBUILD_sample_consensus=ON \
+        -DBUILD_search=ON \
+        -DBUILD_segmentation=ON \
+        -DBUILD_simulation=OFF \
+        -DBUILD_stereo=ON \
+        -DBUILD_surface=ON \
+        -DBUILD_tools=OFF \
+        -DBUILD_tracking=OFF \
+        -DBUILD_visualization=ON \
         $PCL_DIR
   # Build
   make -j2
@@ -80,16 +155,26 @@ function build_examples ()
 function build_tools ()
 {
   # A complete build
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//                 TASK: BUILD PCL TOOLS                         //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DPCL_QT_VERSION=4 \
-        -DBUILD_simulation=ON \
         -DBUILD_global_tests=OFF \
         -DBUILD_examples=OFF \
         -DBUILD_tools=ON \
         -DBUILD_apps=OFF \
+        -DBUILD_outofcore=OFF \
+        -DBUILD_people=OFF \
+        -DBUILD_simulation=OFF \
+        -DBUILD_stereo=OFF \
+        -DBUILD_tracking=OFF \
         $PCL_DIR
   # Build
   make -j2
@@ -98,9 +183,15 @@ function build_tools ()
 function build_apps ()
 {
   # A complete build
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//                 TASK: BUILD PCL APPS                          //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DPCL_QT_VERSION=4 \
         -DBUILD_simulation=OFF \
@@ -124,9 +215,15 @@ function build_apps ()
 function build_lib_core ()
 {
   # A reduced build, only pcl_common
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//                 TASK: BUILD pcl_common                        //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DBUILD_2d=OFF \
         -DBUILD_features=OFF \
@@ -157,9 +254,16 @@ function build_lib_core ()
 
 function test_core ()
 {
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//              TASK: BUILD and RUN PCL TESTS CORE               //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
+
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DPCL_NO_PRECOMPILE=ON \
         -DBUILD_tools=OFF \
@@ -212,9 +316,15 @@ function test_core ()
 
 function test_ext_1 ()
 {
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//              TASK: BUILD and RUN PCL EXTended 1               //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DPCL_NO_PRECOMPILE=ON \
         -DBUILD_tools=OFF \
@@ -267,9 +377,15 @@ function test_ext_1 ()
 
 function test_ext_2 ()
 {
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//              TASK: BUILD and RUN PCL EXTended 2               //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Configure
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        $CMAKE_ARGS \
         -DPCL_ONLY_CORE_POINT_TYPES=ON \
         -DPCL_NO_PRECOMPILE=ON \
         -DBUILD_tools=OFF \
@@ -322,6 +438,11 @@ function test_ext_2 ()
 
 function doc ()
 {
+  echo "///////////////////////////////////////////////////////////////////"
+  echo "//                                                               //"
+  echo "//                 TASK: BUILD PCL DOCS                          //"
+  echo "//                                                               //"
+  echo "///////////////////////////////////////////////////////////////////"
   # Do not generate documentation for pull requests
   if [[ $TRAVIS_PULL_REQUEST != 'false' ]]; then exit; fi
   # Install sphinx
@@ -371,6 +492,8 @@ function doc ()
 case $1 in
   before-install ) before_install;;
   build ) build;;
+  build-lib-core ) build_lib_core;;
+  build-lib ) build_lib;;
   build-examples ) build_examples;;
   build-tools ) build_tools;;
   build-apps ) build_apps;;
