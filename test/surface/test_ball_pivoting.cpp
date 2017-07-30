2530b4049386f86eeada8213c05d788d497bf206
Tongxi Lou <tongxi.lou@tum.de>
2017-07-30 22:57:21 +0200

    clean unit test and fix one logic error

--- test/surface/test_ball_pivoting.cpp
+++ test/surface/test_ball_pivoting.cpp
@@ -40,25 +40,12 @@
 #include <gtest/gtest.h>
 
 #include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-//#include <pcl/features/normal_3d.h>
 #include <pcl/surface/ball_pivoting.h>
 #include <pcl/common/common.h>
 
 using namespace pcl;
 using namespace pcl::io;
 using namespace std;
-
-/*PointCloud<PointXYZ>::Ptr cloud (new PointCloud<PointXYZ>);
-PointCloud<PointNormal>::Ptr cloud_with_normals (new PointCloud<PointNormal>);
-search::KdTree<PointXYZ>::Ptr tree;
-search::KdTree<PointNormal>::Ptr tree2;
-
-// add by ktran to test update functions
-PointCloud<PointXYZ>::Ptr cloud1 (new PointCloud<PointXYZ>);
-PointCloud<PointNormal>::Ptr cloud_with_normals1 (new PointCloud<PointNormal>);
-search::KdTree<PointXYZ>::Ptr tree3;
-search::KdTree<PointNormal>::Ptr tree4;*/
 
 // custom cloud and tree
 PointCloud<PointNormal>::Ptr cloud_custom (new PointCloud<PointNormal>);
@@ -74,38 +61,43 @@ TEST (PCL, BallPivotingTest)
   pivoter.setInputCloud (cloud_custom);
   pivoter.setSearchMethod (tree_custom);
 
+  // reconstruction with custom radius
   pivoter.setSearchRadius (0.02);
   pivoter.reconstruct (mesh_fixed_radius);
 
+  // reconstruction with estimated radius
   pivoter.setEstimatedRadius (100, 5, 0.95f); 
   pivoter.reconstruct (mesh_found_radius);
 
+  // test reconstruction with custom radius
   const std::vector<Vertices> &polygons_fixed_radius = mesh_fixed_radius.polygons;
-  EXPECT_EQ (polygons_fixed_radius[100].vertices[0], 99);
-  EXPECT_EQ (polygons_fixed_radius[100].vertices[1], 102);
-  EXPECT_EQ (polygons_fixed_radius[100].vertices[2], 101);
-  EXPECT_EQ (polygons_fixed_radius[200].vertices[0], 199);
-  EXPECT_EQ (polygons_fixed_radius[200].vertices[1], 202);
-  EXPECT_EQ (polygons_fixed_radius[200].vertices[2], 201);
+  EXPECT_EQ (polygons_fixed_radius[100].vertices[0], 101);
+  EXPECT_EQ (polygons_fixed_radius[100].vertices[1], 100);
+  EXPECT_EQ (polygons_fixed_radius[100].vertices[2], 102);
+  EXPECT_EQ (polygons_fixed_radius[200].vertices[0], 201);
+  EXPECT_EQ (polygons_fixed_radius[200].vertices[1], 200);
+  EXPECT_EQ (polygons_fixed_radius[200].vertices[2], 202);
 
+  // test reconstruction with estimated radius
   const std::vector<Vertices> &polygons_found_radius = mesh_found_radius.polygons;
-  EXPECT_EQ (polygons_found_radius[100].vertices[0], 99);
-  EXPECT_EQ (polygons_found_radius[100].vertices[1], 102);
-  EXPECT_EQ (polygons_found_radius[100].vertices[2], 101);
-  EXPECT_EQ (polygons_found_radius[200].vertices[0], 199);
-  EXPECT_EQ (polygons_found_radius[200].vertices[1], 202);
-  EXPECT_EQ (polygons_found_radius[200].vertices[2], 201);
+  EXPECT_EQ (polygons_found_radius[100].vertices[0], 101);
+  EXPECT_EQ (polygons_found_radius[100].vertices[1], 100);
+  EXPECT_EQ (polygons_found_radius[100].vertices[2], 102);
+  EXPECT_EQ (polygons_found_radius[200].vertices[0], 201);
+  EXPECT_EQ (polygons_found_radius[200].vertices[1], 200);
+  EXPECT_EQ (polygons_found_radius[200].vertices[2], 202);
 }
 
 /**
  * return a point cloud similar to triangle strip in computer graphics, 
- * it generally extends to +X direction, with normal in +Z and points on Z=0.
+ * it generally extends to +X direction, with normal in +Z and points on z=0.
  */
 pcl::PointCloud<pcl::PointNormal>::Ptr
 compose_strip_cloud (const size_t size_cloud)
 {
   const Eigen::Vector3f normal (0.0f, 0.0f, 1.0f);
-  const Eigen::Vector3f odd_shift = Eigen::Vector3f (0.5f, std::sqrt (3.0f) / 2.0f, 0.0f) / 100.0f;
+  const Eigen::Vector3f odd_shift = 
+    Eigen::Vector3f (0.5f, std::sqrt (3.0f) / 2.0f, 0.0f) / 100.0f;
   const Eigen::Vector3f even_shift = Eigen::Vector3f (1.0f, 0.0f, 0.0f) / 100.0f;
   Eigen::Vector3f base = Eigen::Vector3f::Zero ();
 
@@ -119,13 +111,8 @@ compose_strip_cloud (const size_t size_cloud)
     pcl::PointNormal point;
     if (id_point % 2 == 1)
     {
-      // 1,3,5,7,9...-th points
-      position += odd_shift;
-    }
-    else
-    {
-      // 0,2,4,6,8...-th points
-      base += even_shift;
+      position += odd_shift; // shift in this iteration
+      base += even_shift; // shift in following iterations
     }
     point.getVector3fMap () = position;
     point.getNormalVector3fMap () = normal;
@@ -139,65 +126,9 @@ compose_strip_cloud (const size_t size_cloud)
 int
 main (int argc, char** argv)
 {
-/*  if (argc < 2)
-  {
-    std::cerr << "No test file given. Please download `bun0.pcd` and pass its path to the test." << std::endl;
-    return (-1);
-  }
-
-  // Load file
-  pcl::PCLPointCloud2 cloud_blob;
-  loadPCDFile (argv[1], cloud_blob);
-  fromPCLPointCloud2 (cloud_blob, *cloud);
-
-  // Create search tree
-  tree.reset (new search::KdTree<PointXYZ> (false));
-  tree->setInputCloud (cloud);
-
-  // Normal estimation
-  NormalEstimation<PointXYZ, Normal> n;
-  PointCloud<Normal>::Ptr normals (new PointCloud<Normal> ());
-  n.setInputCloud (cloud);
-  //n.setIndices (indices[B);
-  n.setSearchMethod (tree);
-  n.setKSearch (20);
-  n.compute (*normals);
-
-  // Concatenate XYZ and normal information
-  pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
-      
-  // Create search tree
-  tree2.reset (new search::KdTree<PointNormal>);
-  tree2->setInputCloud (cloud_with_normals);
-
-  // Process for update cloud
-  if(argc == 3){
-    pcl::PCLPointCloud2 cloud_blob1;
-    loadPCDFile (argv[2], cloud_blob1);
-    fromPCLPointCloud2 (cloud_blob1, *cloud1);
-    // Create search tree
-    tree3.reset (new search::KdTree<PointXYZ> (false));
-    tree3->setInputCloud (cloud1);
-
-    // Normal estimation
-    NormalEstimation<PointXYZ, Normal> n1;
-    PointCloud<Normal>::Ptr normals1 (new PointCloud<Normal> ());
-    n1.setInputCloud (cloud1);
-
-    n1.setSearchMethod (tree3);
-    n1.setKSearch (20);
-    n1.compute (*normals1);
-
-    // Concatenate XYZ and normal information
-    pcl::concatenateFields (*cloud1, *normals1, *cloud_with_normals1);
-    // Create search tree
-    tree4.reset (new search::KdTree<PointNormal>);
-    tree4->setInputCloud (cloud_with_normals1);
-  }*/
-
-  // use composed point cloud for unit test
+  // use composed point cloud (strip) for unit test
   cloud_custom= compose_strip_cloud (1000);
-  tree_custom.reset (new search::KdTree<PointNormal>);
+  tree_custom.reset (new search::KdTree<PointNormal> ());
   tree_custom->setInputCloud (cloud_custom);
 
   // Testing
