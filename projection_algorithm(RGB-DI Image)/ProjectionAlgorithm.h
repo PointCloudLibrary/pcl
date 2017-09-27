6d4288f2020b70c360aab10a3f009b252dcabb93
DS <ds110293@163.com>
2017-09-27 21:44:01 +0800

    add

---
+++ projection_algorithm(RGB-DI Image)/ProjectionAlgorithm.h
@@ -0,0 +1,24 @@
+#ifndef __ProjectionAlgorithm
+#define __ProjectionAlgorithm
+#include"header.h"
+class ProjectionAlgorithm
+{
+public:
+	ProjectionAlgorithm();
+	~ProjectionAlgorithm();
+	void fun0(const string &LaserPoints6D);
+	void fun1(const string &LaserPoints6D);
+	void fun2(const string &LaserPoints6D);
+	//x upward -0,x upward -1,x upward -2
+	void PointsFilter6D(const string &LaserPoints6D,int flag);
+	float standardization(float a, float b, float c, float d, float x);
+	void ProjectionMirror6D();
+    
+private:
+	vector<vector<float>> m_feapicked;
+	int imgRows;
+	int imgClos;
+	float m_bx; float m_by; float m_bz;
+};
+
+#endif
