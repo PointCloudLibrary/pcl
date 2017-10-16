f265affbda0b162387d8bc46faca168bb9b4817c
Bo Wang <david.b.wang@gmail.com>
2017-10-16 14:54:38 -0700

    Remove indention

--- io/include/pcl/io/boost.h
+++ io/include/pcl/io/boost.h
@@ -41,46 +41,45 @@
 #if defined __GNUC__
 #  pragma GCC system_header 
 #endif
-
 //https://bugreports.qt-project.org/browse/QTBUG-22829
 #ifndef Q_MOC_RUN
-  #ifndef __CUDACC__
-    #include <boost/version.hpp>
-    #include <boost/numeric/conversion/cast.hpp>
-    #include <boost/thread/mutex.hpp>
-    #include <boost/thread/condition.hpp>
-    #include <boost/thread.hpp>
-    #include <boost/thread/thread.hpp>
-    #include <boost/filesystem.hpp>
-    #include <boost/bind.hpp>
-    #include <boost/cstdint.hpp>
-    #include <boost/function.hpp>
-    #include <boost/tuple/tuple.hpp>
-    #include <boost/shared_ptr.hpp>
-    #include <boost/weak_ptr.hpp>
-    #include <boost/mpl/fold.hpp>
-    #include <boost/mpl/inherit.hpp>
-    #include <boost/mpl/inherit_linearly.hpp>
-    #include <boost/mpl/joint_view.hpp>
-    #include <boost/mpl/transform.hpp>
-    #include <boost/mpl/vector.hpp>
-    #include <boost/date_time/posix_time/posix_time.hpp>
-    #if BOOST_VERSION >= 104700
-      #include <boost/chrono.hpp>
-    #endif /* BOOST_VERSION >= 104700 */
-    #include <boost/tokenizer.hpp>
-    #include <boost/foreach.hpp>
-    #include <boost/shared_array.hpp>
-    #if BOOST_VERSION >= 104900
-      #include <boost/interprocess/permissions.hpp>
-    #endif /* BOOST_VERSION >= 104900 */
-    #include <boost/iostreams/device/mapped_file.hpp>
-    #define BOOST_PARAMETER_MAX_ARITY 7
-    #include <boost/signals2.hpp>
-    #include <boost/signals2/slot.hpp>
-  #endif /* __CUDACC__*/
-  #include <boost/algorithm/string.hpp>
-  #include <boost/interprocess/sync/file_lock.hpp>
-#endif /* Q_MOC_RUN */
-
+#ifndef __CUDACC__
+#include <boost/version.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/bind.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/function.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/mpl/fold.hpp>
+#include <boost/mpl/inherit.hpp>
+#include <boost/mpl/inherit_linearly.hpp>
+#include <boost/mpl/joint_view.hpp>
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#if BOOST_VERSION >= 104700
+#include <boost/chrono.hpp>
+#endif
+#include <boost/tokenizer.hpp>
+#include <boost/foreach.hpp>
+#include <boost/shared_array.hpp>
+#if BOOST_VERSION >= 104900
+#include <boost/interprocess/permissions.hpp>
+#endif
+#include <boost/iostreams/device/mapped_file.hpp>
+#define BOOST_PARAMETER_MAX_ARITY 7
+#include <boost/signals2.hpp>
+#include <boost/signals2/slot.hpp>
+#endif
+#include <boost/algorithm/string.hpp>
+#include <boost/interprocess/sync/file_lock.hpp>
+#endif
 #endif    // _PCL_IO_BOOST_H_
+
