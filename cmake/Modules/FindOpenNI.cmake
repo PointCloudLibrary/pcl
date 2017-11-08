8b027d955e1c7a4a261635b3ef03cb92d163d351
Sergey Alexandrov <alexandrov88@gmail.com>
2017-11-08 13:51:44 +0100

    Update finder scripts for Ensenso, OpenNI, and OpenNI2
    * Make sure XXX_LIBRARIES and XXX_INCLUDE_DIRS variables are not defined
      if the packages are not found
    * Make XXX_LIBRARIES and XXX_INCLUDE_DIRS advanced options
    * Some reformatting
    
    Fixes #2059

--- cmake/Modules/FindOpenNI.cmake
+++ cmake/Modules/FindOpenNI.cmake
@@ -1,13 +1,14 @@
 ###############################################################################
 # Find OpenNI
 #
-# This sets the following variables:
-# OPENNI_FOUND - True if OPENNI was found.
-# OPENNI_INCLUDE_DIRS - Directories containing the OPENNI include files.
-# OPENNI_LIBRARIES - Libraries needed to use OPENNI.
-# OPENNI_DEFINITIONS - Compiler flags for OPENNI.
+#     find_package(OpenNI)
 #
-# For libusb-1.0, add USB_10_ROOT if not found
+# Variables defined by this module:
+#
+#  OPENNI_FOUND                True if OpenNI was found
+#  OPENNI_INCLUDE_DIRS         The location(s) of OpenNI headers
+#  OPENNI_LIBRARIES            Libraries needed to use OpenNI
+#  OPENNI_DEFINITIONS          Compiler flags for OpenNI
 
 find_package(PkgConfig QUIET)
 
@@ -47,31 +48,53 @@ if(WIN32 AND CMAKE_SIZEOF_VOID_P EQUAL 8)
   set(OPENNI_SUFFIX 64)
 endif(WIN32 AND CMAKE_SIZEOF_VOID_P EQUAL 8)
 
-#add a hint so that it can find it without the pkg-config
+# Add a hint so that it can find it without the pkg-config
 find_path(OPENNI_INCLUDE_DIR XnStatus.h
-          HINTS ${PC_OPENNI_INCLUDEDIR} ${PC_OPENNI_INCLUDE_DIRS} /usr/include/openni /usr/include/ni /opt/local/include/ni "${OPENNI_ROOT}" "$ENV{OPENNI_ROOT}"
+          HINTS ${PC_OPENNI_INCLUDEDIR}
+                ${PC_OPENNI_INCLUDE_DIRS}
+                /usr/include/openni
+                /usr/include/ni
+                /opt/local/include/ni
+                "${OPENNI_ROOT}"
+                "$ENV{OPENNI_ROOT}"
           PATHS "$ENV{OPEN_NI_INSTALL_PATH${OPENNI_SUFFIX}}/Include"
           PATH_SUFFIXES openni include Include)
-#add a hint so that it can find it without the pkg-config
+
+# Add a hint so that it can find it without the pkg-config
 find_library(OPENNI_LIBRARY
              NAMES OpenNI${OPENNI_SUFFIX}
-             HINTS ${PC_OPENNI_LIBDIR} ${PC_OPENNI_LIBRARY_DIRS} /usr/lib "${OPENNI_ROOT}" "$ENV{OPENNI_ROOT}"
+             HINTS ${PC_OPENNI_LIBDIR}
+                   ${PC_OPENNI_LIBRARY_DIRS}
+                   /usr/lib
+                   "${OPENNI_ROOT}"
+                   "$ENV{OPENNI_ROOT}"
              PATHS "$ENV{OPEN_NI_LIB${OPENNI_SUFFIX}}"
              PATH_SUFFIXES lib Lib Lib64)
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-  set(OPENNI_LIBRARIES ${OPENNI_LIBRARY} ${LIBUSB_1_LIBRARIES})
-else()
-  set(OPENNI_LIBRARIES ${OPENNI_LIBRARY})
+if(OPENNI_INCLUDE_DIR AND OPENNI_LIBRARY)
+
+  # Include directories
+  set(OPENNI_INCLUDE_DIRS ${OPENNI_INCLUDE_DIR})
+  unset(OPENNI_INCLUDE_DIR)
+  mark_as_advanced(OPENNI_INCLUDE_DIRS)
+
+  # Libraries
+  if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    set(OPENNI_LIBRARIES ${OPENNI_LIBRARY} ${LIBUSB_1_LIBRARIES})
+  else()
+    set(OPENNI_LIBRARIES ${OPENNI_LIBRARY})
+  endif()
+  unset(OPENNI_LIBRARY)
+  mark_as_advanced(OPENNI_LIBRARIES)
+
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OpenNI DEFAULT_MSG OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
-
-mark_as_advanced(OPENNI_LIBRARY OPENNI_INCLUDE_DIR)
+find_package_handle_standard_args(OpenNI
+  FOUND_VAR OPENNI_FOUND
+  REQUIRED_VARS OPENNI_LIBRARIES OPENNI_INCLUDE_DIRS
+)
 
 if(OPENNI_FOUND)
-  # Add the include directories
-  set(OPENNI_INCLUDE_DIRS ${OPENNI_INCLUDE_DIR})
-  message(STATUS "OpenNI found (include: ${OPENNI_INCLUDE_DIRS}, lib: ${OPENNI_LIBRARY})")
-endif(OPENNI_FOUND)
+  message(STATUS "OpenNI found (include: ${OPENNI_INCLUDE_DIRS}, lib: ${OPENNI_LIBRARIES})")
+endif()
