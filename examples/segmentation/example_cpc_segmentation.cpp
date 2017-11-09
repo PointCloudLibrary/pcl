4a8efc289b0b4d84f3ca629ee8858a96fa456aa0
yongduek <yongduek.seo@gmail.com>
2017-11-09 22:28:26 +0900

    Update example_cpc_segmentation.cpp

--- examples/segmentation/example_cpc_segmentation.cpp
+++ examples/segmentation/example_cpc_segmentation.cpp
@@ -489,12 +489,12 @@ CPCSegmentation Parameters: \n\
           color = concave_color;
 
         // two times since we add also two points per edge
-#if (VTK_MAJOR_VERSION < 7) || (VTK_MAJOR_VERSION==7 && VTK_MINOR_VERSION==0)
+#if (VTK_MAJOR_VERSION < 7) || (VTK_MAJOR_VERSION == 7 && VTK_MINOR_VERSION == 0)
         colors->InsertNextTupleValue (color);
         colors->InsertNextTupleValue (color);
 #else       
-        colors->InsertNextTypedTuple(color);
-        colors->InsertNextTypedTuple(color);
+        colors->InsertNextTypedTuple (color);
+        colors->InsertNextTypedTuple (color);
 #endif      
         pcl::Supervoxel<PointT>::Ptr supervoxel = supervoxel_clusters.at (sv_label);
         pcl::PointXYZRGBA vert_curr = supervoxel->centroid_;
