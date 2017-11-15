bed968c1821c0ff7c340282ca8601af8b3d819b7
Alex Moriarty <alexander.moriarty@gmail.com>
2017-11-15 17:31:18 +0100

    [c++] replace auto_ptr with boost::scoped_ptr
    auto_ptr is removed with C++17.
    
    C++11 std::unique_ptr and boost::move::unique_ptr are not old enough for all platforms supported by PCL.

--- visualization/include/pcl/visualization/cloud_viewer.h
+++ visualization/include/pcl/visualization/cloud_viewer.h
@@ -37,7 +37,7 @@
 #define PCL_CLOUD_VIEWER_H_
 
 #include <pcl/visualization/pcl_visualizer.h> //pcl vis
-#include <boost/move/unique_ptr.hpp> // unique ptr for pre-C++11
+#include <boost/scoped_ptr.hpp> // scoped_ptr for pre-C++11
 
 #include <string>
 
@@ -200,7 +200,7 @@ namespace pcl
       private:
         /** \brief Private implementation. */
         struct CloudViewer_impl;
-	boost::movelib::unique_ptr<CloudViewer_impl> impl_;
+	boost::scoped_ptr<CloudViewer_impl> impl_;
         
         boost::signals2::connection 
         registerMouseCallback (boost::function<void (const pcl::visualization::MouseEvent&)>);
