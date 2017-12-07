1fdd3db128a1e1e5fa69a08fa74aa731078314bd
Sérgio Agostinho <sergio.r.agostinho@gmail.com>
2017-12-07 23:06:58 +0000

    Fix covariance calculation in PCA

--- common/include/pcl/common/impl/pca.hpp
+++ common/include/pcl/common/impl/pca.hpp
@@ -68,7 +68,8 @@ pcl::PCA<PointT>::initCompute ()
   demeanPointCloud (*input_, *indices_, mean_, cloud_demean);
   assert (cloud_demean.cols () == int (indices_->size ()));
   // Compute the product cloud_demean * cloud_demean^T
-  Eigen::Matrix3f alpha = static_cast<Eigen::Matrix3f> (cloud_demean.topRows<3> () * cloud_demean.topRows<3> ().transpose ());
+  const Eigen::Matrix3f alpha = (1.f / (float (indices_->size ()) - 1.f))
+                                  * cloud_demean.topRows<3> () * cloud_demean.topRows<3> ().transpose ();
   
   // Compute eigen vectors and values
   Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> evd (alpha);
