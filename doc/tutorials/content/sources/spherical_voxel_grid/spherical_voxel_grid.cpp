e30328966a9a36dc72a85b4db25753c714f79f53
Spencer Davis <dsdavis1@crimson.ua.edu>
2018-02-03 22:30:37 -0600

    Correct tutorial

--- doc/tutorials/content/sources/spherical_voxel_grid/spherical_voxel_grid.cpp
+++ doc/tutorials/content/sources/spherical_voxel_grid/spherical_voxel_grid.cpp
@@ -4,24 +4,24 @@
 #include <pcl/filters/spherical_voxel_grid.h>
 
 int
-main (int argc, char** argv)
+main ()
 {
-  pcl::PointCloud<pcl::PointXYZI>::Ptr input(new pcl::PointCloud<pcl::PointXYZI>);
-  pcl::PointCloud<pcl::PointXYZI>::Ptr output(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr input (new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::PointCloud<pcl::PointXYZI> output;
 
   pcl::io::loadPCDFile<pcl::PointXYZI> ("spherical_voxel_grid_example.pcd", *input);
 
-  std::cout << "Size before downsample: " << input->points.size() << std::endl;
+  std::cout << "Size before downsample: " << input->size () << std::endl;
 
   // Create the filtering object
   pcl::SphericalVoxelGrid<pcl::PointXYZI> voxel;
   voxel.setInputCloud (input);
   voxel.setLeafSize (0.1, 90, 300);
-  voxel.filter (*output);
+  voxel.filter (output);
 
-  std::cout << "Size after downsample: " << output->points.size() << std::endl;
+  std::cout << "Size after downsample: " << output.size () << std::endl;
 
-  pcl::io::savePCDFileASCII ("spherical_voxel_grid_example_downsampled.pcd", *output);
+  pcl::io::savePCDFileASCII ("spherical_voxel_grid_example_downsampled.pcd", output);
 
   return (0);
 }
