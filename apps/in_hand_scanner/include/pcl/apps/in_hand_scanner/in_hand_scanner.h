3c4c73cfb2b15b6f51358db0d32d2a49c93cac44
Daniel Packard <pack3754@gmail.com>
2018-02-14 12:15:37 -0600

    Patch out crs-develop changes to in_hand_scanner

--- apps/in_hand_scanner/include/pcl/apps/in_hand_scanner/in_hand_scanner.h
+++ apps/in_hand_scanner/include/pcl/apps/in_hand_scanner/in_hand_scanner.h
@@ -49,8 +49,6 @@
 #include <pcl/apps/in_hand_scanner/common_types.h>
 #include <pcl/apps/in_hand_scanner/opengl_viewer.h>
 
-#include <pcl/io/real_sense_2_grabber.h>
-
 ////////////////////////////////////////////////////////////////////////////////
 // Forward declarations
 ////////////////////////////////////////////////////////////////////////////////
@@ -210,8 +208,7 @@ namespace pcl
         typedef pcl::ihs::MeshPtr      MeshPtr;
         typedef pcl::ihs::MeshConstPtr MeshConstPtr;
 
-        //typedef pcl::OpenNIGrabber                Grabber;
-        typedef RealSense2Grabber                 Grabber;
+        typedef pcl::OpenNIGrabber                Grabber;
         typedef boost::shared_ptr <Grabber>       GrabberPtr;
         typedef boost::shared_ptr <const Grabber> GrabberConstPtr;
 
