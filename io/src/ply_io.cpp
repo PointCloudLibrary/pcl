dcd83238e38c43e42b3f9cd829096f37094458ef
You Yue <youy0004123@gmail.com>
2018-03-05 11:31:22 +0100

    Change boolean to int output

--- io/src/ply_io.cpp
+++ io/src/ply_io.cpp
@@ -98,8 +98,7 @@ pcl::PLYReader::endHeaderCallback ()
   std::cout << "cloud_->point_step * cloud_->width * cloud_->height" << cloud_->point_step * cloud_->width * cloud_->height << std::endl;
   std::cout << "static_cast<size_t>(cloud_->point_step) * cloud_->width * cloud_->height" << static_cast<size_t>(cloud_->point_step) * cloud_->width * cloud_->height << std::endl;
   cloud_->data.resize (static_cast<size_t>(cloud_->point_step) * cloud_->width * cloud_->height);
-  std::cout << "Before " << cloud_->data.size () == cloud_->point_step * cloud_->width * cloud_->height << std::endl;
-  std::cout << "After " << cloud_->data.size () == static_cast<size_t>(cloud_->point_step) * cloud_->width * cloud_->height << std::endl;
+  std::cout << "cloud_->data.size ()" << cloud_->data.size () << std::endl;
 
   return (cloud_->data.size () == static_cast<size_t>(cloud_->point_step) * cloud_->width * cloud_->height);
 }
