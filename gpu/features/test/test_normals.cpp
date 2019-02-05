97f7b5cdf2afb635cbc17896c53401790917a09b
Heiko Thiel <heiko.thiel@hpi.de>
2019-02-05 12:11:00 +0100

    Remove GTEST_USE_OWN_TR1_TUPLE, since this define is not needed anymore with C++14 support and even not anymore used in current version of GoogleTest

--- gpu/features/test/test_normals.cpp
+++ gpu/features/test/test_normals.cpp
@@ -34,14 +34,6 @@
 *  Author: Anatoly Baskeheev, Itseez Ltd, (myname.mysurname@mycompany.com)
 */
 
-#if (defined(__GNUC__) && !defined(__CUDACC__) && (GTEST_GCC_VER_ >= 40000)) 
-    #define GTEST_USE_OWN_TR1_TUPLE 0
-#endif
-
-#if defined(_MSC_VER)
-    #define GTEST_USE_OWN_TR1_TUPLE 0
-#endif
-
 #include <gtest/gtest.h>
 #include "data_source.hpp"
 #include <iostream>
