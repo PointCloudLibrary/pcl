851c29c4ea90f6eb3b89213c0ac151e7eb03bfac
Heiko Thiel <heiko.thiel@hpi.de>
2019-04-18 18:38:47 +0200

    Changes are done by run-clang-tidy -header-filter='.*' -checks='-*,modernize-use-nullptr' -fix

--- features/src/range_image_border_extractor.cpp
+++ features/src/range_image_border_extractor.cpp
@@ -55,9 +55,9 @@ namespace pcl
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 RangeImageBorderExtractor::RangeImageBorderExtractor(const RangeImage* range_image) : BaseClass(),
   parameters_ (), range_image_(range_image), range_image_size_during_extraction_(0),
-  border_scores_left_(NULL), border_scores_right_(NULL), border_scores_top_(NULL), border_scores_bottom_(NULL),
-  surface_structure_(NULL), border_descriptions_(NULL), shadow_border_informations_(NULL), border_directions_(NULL),
-  surface_change_scores_(NULL), surface_change_directions_(NULL)
+  border_scores_left_(nullptr), border_scores_right_(nullptr), border_scores_top_(nullptr), border_scores_bottom_(nullptr),
+  surface_structure_(nullptr), border_descriptions_(nullptr), shadow_border_informations_(nullptr), border_directions_(nullptr),
+  surface_change_scores_(nullptr), surface_change_directions_(nullptr)
 {
 }
 
@@ -79,34 +79,34 @@ RangeImageBorderExtractor::setRangeImage (const RangeImage* range_image)
 void 
 RangeImageBorderExtractor::clearData ()
 {
-  delete[] border_scores_left_;    border_scores_left_   = NULL;
-  delete[] border_scores_right_;   border_scores_right_  = NULL;
-  delete[] border_scores_top_;     border_scores_top_    = NULL;
-  delete[] border_scores_bottom_;  border_scores_bottom_ = NULL;
+  delete[] border_scores_left_;    border_scores_left_   = nullptr;
+  delete[] border_scores_right_;   border_scores_right_  = nullptr;
+  delete[] border_scores_top_;     border_scores_top_    = nullptr;
+  delete[] border_scores_bottom_;  border_scores_bottom_ = nullptr;
   //cout << PVARC(range_image_size_during_extraction_)<<PVARN((void*)this);
   for (int i=0; i<range_image_size_during_extraction_; ++i)
   {
-    if (surface_structure_!=NULL)
+    if (surface_structure_!=nullptr)
       delete surface_structure_[i];
-    if (shadow_border_informations_!=NULL)
+    if (shadow_border_informations_!=nullptr)
       delete shadow_border_informations_[i];
-    if (border_directions_!=NULL)
+    if (border_directions_!=nullptr)
       delete border_directions_[i];
   }
-  delete[] surface_structure_; surface_structure_ = NULL;
-  delete border_descriptions_; border_descriptions_ = NULL;
-  delete[] shadow_border_informations_; shadow_border_informations_ = NULL;
-  delete[] border_directions_; border_directions_ = NULL;
+  delete[] surface_structure_; surface_structure_ = nullptr;
+  delete border_descriptions_; border_descriptions_ = nullptr;
+  delete[] shadow_border_informations_; shadow_border_informations_ = nullptr;
+  delete[] border_directions_; border_directions_ = nullptr;
   
-  delete[] surface_change_scores_;  surface_change_scores_ = NULL;
-  delete[] surface_change_directions_;  surface_change_directions_ = NULL;
+  delete[] surface_change_scores_;  surface_change_scores_ = nullptr;
+  delete[] surface_change_directions_;  surface_change_directions_ = nullptr;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void 
 RangeImageBorderExtractor::extractLocalSurfaceStructure ()
 {
-  if (surface_structure_ != NULL)
+  if (surface_structure_ != nullptr)
     return;
   //cerr << __PRETTY_FUNCTION__<<" called (this="<<(void*)this<<").\n";
   //MEASURE_FUNCTION_TIME;
@@ -127,7 +127,7 @@ RangeImageBorderExtractor::extractLocalSurfaceStructure ()
     {
       int index = y*width + x;
       LocalSurface*& local_surface = surface_structure_[index];
-      local_surface = NULL;
+      local_surface = nullptr;
       if (!range_image_->isValid(index))
         continue;
       local_surface = new LocalSurface;
@@ -141,7 +141,7 @@ RangeImageBorderExtractor::extractLocalSurfaceStructure ()
                                   &local_surface->neighborhood_mean, &local_surface->eigen_values))
       {
         delete local_surface;
-        local_surface = NULL;
+        local_surface = nullptr;
       }
       
       //cout << x<<","<<y<<": ("<<local_surface->normal_no_jumps[0]<<","<<local_surface->normal_no_jumps[1]<<","<<local_surface->normal_no_jumps[2]<<")\n";
@@ -153,7 +153,7 @@ RangeImageBorderExtractor::extractLocalSurfaceStructure ()
 void 
 RangeImageBorderExtractor::extractBorderScoreImages ()
 {
-  if (border_scores_left_ != NULL)
+  if (border_scores_left_ != nullptr)
     return;
   
   extractLocalSurfaceStructure();
@@ -175,7 +175,7 @@ RangeImageBorderExtractor::extractBorderScoreImages ()
       float& left=border_scores_left_[index]; float& right=border_scores_right_[index];
       float& top=border_scores_top_[index]; float& bottom=border_scores_bottom_[index]; 
       LocalSurface* local_surface_ptr = surface_structure_[index];
-      if (local_surface_ptr==NULL)
+      if (local_surface_ptr==nullptr)
       {
         left=right=top=bottom = 0.0f;
         continue;
@@ -227,10 +227,10 @@ RangeImageBorderExtractor::updateScoresAccordingToNeighborValues ()
 void 
 RangeImageBorderExtractor::findAndEvaluateShadowBorders ()
 {
-  if (shadow_border_informations_ != NULL)
+  if (shadow_border_informations_ != nullptr)
     return;
   
-  if (border_scores_left_==NULL)
+  if (border_scores_left_==nullptr)
   {
     std::cerr << __PRETTY_FUNCTION__<<": border score images not available!\n";
   }
@@ -246,27 +246,27 @@ RangeImageBorderExtractor::findAndEvaluateShadowBorders ()
     {
       int index = y*width+x;
       ShadowBorderIndices*& shadow_border_indices = shadow_border_informations_[index];
-      shadow_border_indices = NULL;
+      shadow_border_indices = nullptr;
       int shadow_border_idx;
       
       if (changeScoreAccordingToShadowBorderValue(x, y, -1, 0, border_scores_left_, border_scores_right_, shadow_border_idx))
       {
-        shadow_border_indices = (shadow_border_indices==NULL ? new ShadowBorderIndices : shadow_border_indices);
+        shadow_border_indices = (shadow_border_indices==nullptr ? new ShadowBorderIndices : shadow_border_indices);
         shadow_border_indices->left = shadow_border_idx;
       }
       if (changeScoreAccordingToShadowBorderValue(x, y, 1, 0, border_scores_right_, border_scores_left_, shadow_border_idx))
       {
-        shadow_border_indices = (shadow_border_indices==NULL ? new ShadowBorderIndices : shadow_border_indices);
+        shadow_border_indices = (shadow_border_indices==nullptr ? new ShadowBorderIndices : shadow_border_indices);
         shadow_border_indices->right = shadow_border_idx;
       }
       if (changeScoreAccordingToShadowBorderValue(x, y, 0, -1, border_scores_top_, border_scores_bottom_, shadow_border_idx))
       {
-        shadow_border_indices = (shadow_border_indices==NULL ? new ShadowBorderIndices : shadow_border_indices);
+        shadow_border_indices = (shadow_border_indices==nullptr ? new ShadowBorderIndices : shadow_border_indices);
         shadow_border_indices->top = shadow_border_idx;
       }
       if (changeScoreAccordingToShadowBorderValue(x, y, 0, 1, border_scores_bottom_, border_scores_top_, shadow_border_idx))
       {
-        shadow_border_indices = (shadow_border_indices==NULL ? new ShadowBorderIndices : shadow_border_indices);
+        shadow_border_indices = (shadow_border_indices==nullptr ? new ShadowBorderIndices : shadow_border_indices);
         shadow_border_indices->bottom = shadow_border_idx;
       }
     }
@@ -292,7 +292,7 @@ RangeImageBorderExtractor::getAnglesImageForBorderDirections ()
       float& angle = angles_image[index];
       angle = -std::numeric_limits<float>::infinity ();
       const Eigen::Vector3f* border_direction_ptr = border_directions_[index];
-      if (border_direction_ptr == NULL)
+      if (border_direction_ptr == nullptr)
         continue;
       const Eigen::Vector3f& border_direction = *border_direction_ptr;
       const PointWithRange& point = range_image_->getPoint(index);
@@ -354,7 +354,7 @@ RangeImageBorderExtractor::getAnglesImageForSurfaceChangeDirections ()
 void 
 RangeImageBorderExtractor::classifyBorders ()
 {
-  if (border_descriptions_ != NULL)
+  if (border_descriptions_ != nullptr)
     return;
   
   // Get local plane approximations
@@ -392,7 +392,7 @@ RangeImageBorderExtractor::classifyBorders ()
       BorderTraits& border_traits = border_description.traits;
       
       ShadowBorderIndices* shadow_border_indices = shadow_border_informations_[index];
-      if (shadow_border_indices == NULL)
+      if (shadow_border_indices == nullptr)
         continue;
       
       int shadow_border_index = shadow_border_indices->left;
@@ -461,7 +461,7 @@ RangeImageBorderExtractor::classifyBorders ()
 void 
 RangeImageBorderExtractor::calculateBorderDirections ()
 {
-  if (border_directions_!=NULL)
+  if (border_directions_!=nullptr)
     return;
   classifyBorders();
   
@@ -489,9 +489,9 @@ RangeImageBorderExtractor::calculateBorderDirections ()
     {
       int index = y*width + x;
       Eigen::Vector3f*& average_border_direction = average_border_directions[index];
-      average_border_direction = NULL;
+      average_border_direction = nullptr;
       const Eigen::Vector3f* border_direction = border_directions_[index];
-      if (border_direction==NULL)
+      if (border_direction==nullptr)
         continue;
       average_border_direction = new Eigen::Vector3f(*border_direction);
       float weight_sum = 1.0f;
@@ -501,7 +501,7 @@ RangeImageBorderExtractor::calculateBorderDirections ()
         {
           int index2 = y2*width + x2;
           const Eigen::Vector3f* neighbor_border_direction = border_directions_[index2];
-          if (neighbor_border_direction==NULL || index2==index)
+          if (neighbor_border_direction==nullptr || index2==index)
             continue;
           
           // Opposite directions?
@@ -526,7 +526,7 @@ RangeImageBorderExtractor::calculateBorderDirections ()
       if (pcl_lrint (weight_sum) < minimum_weight)
       {
         delete average_border_direction;
-        average_border_direction=NULL;
+        average_border_direction=nullptr;
       }
       else
         average_border_direction->normalize();
@@ -543,7 +543,7 @@ RangeImageBorderExtractor::calculateBorderDirections ()
 void 
 RangeImageBorderExtractor::calculateSurfaceChanges ()
 {
-  if (surface_change_scores_!=NULL)
+  if (surface_change_scores_!=nullptr)
     return;
   
   calculateBorderDirections();
@@ -569,7 +569,7 @@ RangeImageBorderExtractor::calculateSurfaceChanges ()
       const BorderTraits& border_traits = border_descriptions_->points[index].traits;
       if (border_traits[BORDER_TRAIT__VEIL_POINT] || border_traits[BORDER_TRAIT__SHADOW_BORDER])
         continue;
-      if (border_directions_[index]!=NULL)
+      if (border_directions_[index]!=nullptr)
       {
         surface_change_score = 1.0f;
         surface_change_direction = *border_directions_[index];
@@ -664,7 +664,7 @@ RangeImageBorderExtractor::computeFeature (PointCloudOut& output)
     return;
   }
   
-  if (range_image_==NULL)
+  if (range_image_==nullptr)
   {
     std::cerr << __PRETTY_FUNCTION__
               << ": RangeImage is not set. Sorry, the border extraction works on range images, not on normal point clouds."
