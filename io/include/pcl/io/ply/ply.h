3cfccb8f494de69437973f2e35134630acbff34a
YoshuaNava <yoshua.nava.chocron@gmail.com>
2019-04-25 10:24:22 +0200

    [io] Removed trailing semicolons after macro calls

--- io/include/pcl/io/ply/ply.h
+++ io/include/pcl/io/ply/ply.h
@@ -82,14 +82,14 @@ namespace pcl
         static const char* old_name () { return OLD_NAME; } \
       };
 
-      PLY_TYPE_TRAITS(int8, int16, "int8", "char");
-      PLY_TYPE_TRAITS(int16, int16, "int16", "short");
-      PLY_TYPE_TRAITS(int32, int32, "int32", "int");
-      PLY_TYPE_TRAITS(uint8, uint16, "uint8", "uchar");
-      PLY_TYPE_TRAITS(uint16, uint16, "uint16", "ushort");
-      PLY_TYPE_TRAITS(uint32, uint32, "uint32", "uint");
-      PLY_TYPE_TRAITS(float32, float32, "float32", "float");
-      PLY_TYPE_TRAITS(float64, float64, "float64", "double");
+      PLY_TYPE_TRAITS(int8, int16, "int8", "char")
+      PLY_TYPE_TRAITS(int16, int16, "int16", "short")
+      PLY_TYPE_TRAITS(int32, int32, "int32", "int")
+      PLY_TYPE_TRAITS(uint8, uint16, "uint8", "uchar")
+      PLY_TYPE_TRAITS(uint16, uint16, "uint16", "ushort")
+      PLY_TYPE_TRAITS(uint32, uint32, "uint32", "uint")
+      PLY_TYPE_TRAITS(float32, float32, "float32", "float")
+      PLY_TYPE_TRAITS(float64, float64, "float64", "double")
 
       
 #undef PLY_TYPE_TRAITS
