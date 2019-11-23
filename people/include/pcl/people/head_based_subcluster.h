682b1acbf683d5d03ae17eff48794c69e1dd50a5
Sérgio Agostinho <sergio.r.agostinho@gmail.com>
2019-11-23 23:18:46 +0100

    [people]  Encapsulate further boost::shared_ptr uses

--- people/include/pcl/people/head_based_subcluster.h
+++ people/include/pcl/people/head_based_subcluster.h
@@ -60,8 +60,8 @@ namespace pcl
     public:
 
       using PointCloud = pcl::PointCloud<PointT>;
-      using PointCloudPtr = boost::shared_ptr<PointCloud>;
-      using PointCloudConstPtr = boost::shared_ptr<const PointCloud>;
+      using PointCloudPtr = typename PointCloud::Ptr;
+      using PointCloudConstPtr = typename PointCloud::ConstPtr;
 
       /** \brief Constructor. */
       HeadBasedSubclustering ();
