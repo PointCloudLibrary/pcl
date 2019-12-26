5415f0e0c10b0234fccb10c2ae947f4bbdc6dcca
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2019-12-26 18:32:29 +0100

    Make parallel RANSAC selectable from SACSegmentation

--- segmentation/include/pcl/segmentation/sac_segmentation.h
+++ segmentation/include/pcl/segmentation/sac_segmentation.h
@@ -95,6 +95,7 @@ namespace pcl
         , eps_angle_ (0.0)
         , axis_ (Eigen::Vector3f::Zero ())
         , max_iterations_ (50)
+        , threads_ (-1)
         , probability_ (0.99)
         , random_ (random)
       {
@@ -160,6 +161,13 @@ namespace pcl
       /** \brief Get the probability of choosing at least one sample free from outliers. */
       inline double 
       getProbability () const { return (probability_); }
+
+      /** \brief Set the number of threads to use or turn off parallelization.
+        * \param[in] nr_threads the number of hardware threads to use (0 sets the value automatically, a negative number turns parallelization off)
+        * \note Not all SAC methods have a parallel implementation. Some will ignore this setting.
+        */
+      inline void
+      setNumberOfThreads (const int nr_threads = -1) { threads_ = nr_threads; }
 
       /** \brief Set to true if a coefficient refinement is required.
         * \param[in] optimize true for enabling model coefficient refinement, false otherwise
@@ -290,6 +298,9 @@ namespace pcl
 
       /** \brief Maximum number of iterations before giving up (user given parameter). */
       int max_iterations_;
+
+      /** \brief The number of threads the scheduler should use, or a negative number if no parallelization is wanted. */
+      int threads_;
 
       /** \brief Desired probability of choosing at least one sample free from outliers (user given parameter). */
       double probability_;
