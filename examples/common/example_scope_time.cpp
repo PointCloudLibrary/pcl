268b89ec8344a9d9461ea8eedb7b1f1321962c7d
Chaytanya <50200796+chaytanyasinha@users.noreply.github.com>
2020-02-24 18:54:48 +0530

    Update example_scope_time.cpp

--- examples/common/example_scope_time.cpp
+++ examples/common/example_scope_time.cpp
@@ -48,11 +48,7 @@ main ()
 {
   pcl::ScopeTime scope_time ("Test loop");
   {
-    float total = 0.0f;
     for (std::size_t i = 0; i < 1e4; ++i)
-    {
-      total += static_cast<float> (i);
-    }
   }
   std::cout << "Done." << std::endl;
 
