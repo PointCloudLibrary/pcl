70af9939565021b095203068b1c05424832cbb8e
Divyanshu Madan <iamdm99@gmail.com>
2020-03-28 01:25:57 +0530

    clang-formatted tracking impl files
    clang-formatted impl files [2]
    
    clang formatted impl files [3]
    
    clang formatted impl files [4]

--- tracking/include/pcl/tracking/impl/distance_coherence.hpp
+++ tracking/include/pcl/tracking/impl/distance_coherence.hpp
@@ -4,21 +4,21 @@
 #include <Eigen/Dense>
 #include <pcl/tracking/distance_coherence.h>
 
-namespace pcl
+namespace pcl {
+namespace tracking {
+template <typename PointInT>
+double
+DistanceCoherence<PointInT>::computeCoherence(PointInT& source, PointInT& target)
 {
-  namespace tracking
-  {
-    template <typename PointInT> double
-    DistanceCoherence<PointInT>::computeCoherence (PointInT &source, PointInT &target)
-    {
-       Eigen::Vector4f p = source.getVector4fMap ();
-       Eigen::Vector4f p_dash = target.getVector4fMap ();
-       double d = (p - p_dash).norm ();
-       return 1.0 / (1.0 + d * d * weight_);
-    }
-  }
+  Eigen::Vector4f p = source.getVector4fMap();
+  Eigen::Vector4f p_dash = target.getVector4fMap();
+  double d = (p - p_dash).norm();
+  return 1.0 / (1.0 + d * d * weight_);
 }
+} // namespace tracking
+} // namespace pcl
 
-#define PCL_INSTANTIATE_DistanceCoherence(T) template class PCL_EXPORTS pcl::tracking::DistanceCoherence<T>;
+#define PCL_INSTANTIATE_DistanceCoherence(T)                                           \
+  template class PCL_EXPORTS pcl::tracking::DistanceCoherence<T>;
 
 #endif
