4f37afdda4c2347c60cd523bffa46cb5ca164502
Divyanshu Madan <iamdm99@gmail.com>
2020-05-20 00:50:51 +0530

    [py_point_types.cpp] corrected for namespaces and operators

--- bindings/python/common/py_point_types.cpp
+++ bindings/python/common/py_point_types.cpp
@@ -4,14 +4,16 @@
 namespace py = pybind11;
 PYBIND11_MODULE(pcl, m)
 {
-  py::class_<_PointXYZ>(m, "_PointXYZ");
-  py::class_<PointXYZ, public _PointXYZ>(m, "PointXYZ")
+  py::class_<pcl::_PointXYZ>(m, "_PointXYZ");
+  py::class_<pcl::PointXYZ, public _PointXYZ>(m, "PointXYZ")
       .def(py::init<const _PointXYZ & p>())
       .def(py::init<>())
-      .def(py::init<float _x, float _y, float _z>());
-  py::class_<_RGB>(m, "_RGB");
-  py::class_<RGB, public _RGB>(m, "RGB")
+      .def(py::init<float _x, float _y, float _z>())
+      .def(py::self << py::self);
+  py::class_<pcl::_RGB>(m, "_RGB");
+  py::class_<pcl::RGB, public _RGB>(m, "RGB")
       .def(py::init<const _RGB & p>())
       .def(py::init<>())
-      .def(py::init<std::uint8_t _r, std::uint8_t _g, std::uint8_t _b>());
+      .def(py::init<std::uint8_t _r, std::uint8_t _g, std::uint8_t _b>())
+      .def(py::self << py::self);
 }
