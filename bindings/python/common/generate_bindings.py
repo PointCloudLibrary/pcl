65f3d19801a78f99f2da97a0f060f790268154f5
Divyanshu Madan <iamdm99@gmail.com>
2020-05-20 00:32:47 +0530

    [generate_binding.py] ultra rough

--- bindings/python/common/generate_bindings.py
+++ bindings/python/common/generate_bindings.py
@@ -6,7 +6,15 @@ def read_json(filename):
         return json.load(f)
 
 
+def write_to_cpp(filename, linelist):
+    with open(filename, 'w') as f:
+        for line in linelist:
+            f.writelines(line)
+            f.writelines("\n")
+
+
 namespace = None
+in_struct = False
 module_linelist = []
 
 
@@ -15,16 +23,17 @@ def check_namespace(item):
         item["identifier"] = f"{namespace}::{item['identifier']}"
 
 
-def handle_final(module):
+def handle_final(filename, module_name):
     linelist = []
-    linelist.append("#include <point_types.h>")
+    linelist.append(f"#include <{filename}>")
     linelist.append("#include <pybind11/pybind11.h>")
     linelist.append("namespace py = pybind11;")
-    linelist.append(f"PYBIND11_MODULE({module}, m)")
+    linelist.append(f"PYBIND11_MODULE({module_name}, m)")
     linelist.append("{")
     for line in module_linelist:
         linelist.append(line)
     linelist.append("}")
+    return linelist
 
 
 def handle_include(item):
@@ -36,44 +45,60 @@ def handle_alias(item):
 
 
 def handle_constructor(item):
-    """
-    @TODO
-    """
+    parameters = ""
+    parameters = ",".join(params for params in item["parameters"])
+    module_linelist.append(f".def(py::init<{parameters}>())")
 
+
+def handle_operator(item):
+    if in_struct:
+        module_linelist.append(f'.def(py::self {item["identifier"]} py::self)')
 
 
 def handle_namespace(item):
     namespace = item["identifier"]
     for sub_item in item["members"]:
-        type_functions[sub_item["type"]]
+        type_functions[sub_item["type"]](sub_item)
     namespace = None
 
 
 def handle_struct(item):
-    name = item["identifier"]
+    in_struct=True
+    name_decl = item["identifier"]
     check_namespace(item)
-    module_linelist.append(f'py::class_<{name}>(m, "{name}")')
-    """
-    @TODO
-    Think about parent classes, which are not implementations
-    """
+    name_impl = item["identifier"]
+    if "parent" in item.keys():
+        name_impl = f'{item["identifier"]}, {item["parent"]}'
+
+    module_linelist.append(f'py::class_<{name_impl}>(m, "{name_decl}")')
     for sub_item in item["members"]:
-        type_functions[sub_item["type"]]
+        type_functions[sub_item["type"]](sub_item)
+    module_linelist.append(";")
+    in_struct=False
+
+
+def handle_macro(item):
+    pass
 
 
 type_functions = {
-    "include": handle_include(item),
-    "namespace": handle_namespace(item),
-    "alias": handle_alias(item),
-    "struct": handle_struct(struct),
-    "constructor": handle_constructor(item),
-    "operator": handle_operator(item),
+    "include": handle_include,
+    "namespace": handle_namespace,
+    "alias": handle_alias,
+    "struct": handle_struct,
+    "constructor": handle_constructor,
+    "operator": handle_operator,
+    "macro": handle_macro,
 }
 
 
 def main():
-    handle_final(module="pcl")
     header_info = read_json("point_types.json")
     for item in header_info:
-        for key, value in item.items():
-            print(key, value)
+        type_functions[item["type"]](item)
+    lines_to_write=handle_final(filename="pcl/point_types.h", module_name="pcl")
+    write_to_cpp(filename="py_point_types.cpp", linelist=lines_to_write)
+
+
+if __name__ == "__main__":
+    main()
