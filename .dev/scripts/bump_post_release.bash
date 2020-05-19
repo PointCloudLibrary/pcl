05f654a8d85cc1414d241c5a269e86d31886f29d
Kunal Tyagi <tyagi.kunal@live.com>
2020-05-19 21:54:07 +0900

    Fixing mistake

--- .dev/scripts/bump_post_release.bash
+++ .dev/scripts/bump_post_release.bash
@@ -3,7 +3,7 @@
 new_version=$(git tag | sort -rV | head -1 | cut -d- -f 2).99
 
 if ls | grep README -q; then
-    gsed -i "s,VERSION [0-9.]*),VERSION ${new_version})," CMakeLists.txt
+    sed -i "s,VERSION [0-9.]*),VERSION ${new_version})," CMakeLists.txt
 else
     echo "Don't think this is the root directory" 1>&2
     exit 4
