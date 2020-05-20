0978fc1185d53255d11c4295167e1b1b9ab14ed8
zarathustra <jin_wu_uestc@hotmail.com>
2020-05-20 15:10:23 +0800

    faster rigid registration

---
+++ registration/src/transformation_estimation_fs3r.cpp
@@ -0,0 +1,4 @@
+#include <pcl/registration/transformation_estimation_fs3r.h>
+#include <pcl/registration/impl/transformation_estimation_fs3r.hpp>
+
+template class pcl::registration::TransformationEstimationFS3R<pcl::PointXYZRGBNormal, pcl::PointXYZRGBNormal>;
