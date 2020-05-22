b432122f6b4835da878a9e75f85b4c6f8db283e6
Divyanshu Madan <iamdm99@gmail.com>
2020-05-23 00:46:32 +0530

    [py_point_types.cpp] after some fixes

--- bindings/python/common/py_point_types.cpp
+++ bindings/python/common/py_point_types.cpp
@@ -2,18 +2,18 @@
 
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
+namespace pcl {
 PYBIND11_MODULE(pcl, m)
 {
-  py::class_<pcl::_PointXYZ>(m, "_PointXYZ");
-  py::class_<pcl::PointXYZ, public _PointXYZ>(m, "PointXYZ")
-      .def(py::init<const _PointXYZ & p>())
+  py::class_<_PointXYZ>(m, "_PointXYZ");
+  py::class_<PointXYZ, _PointXYZ>(m, "PointXYZ")
+      .def(py::init<const _PointXYZ&>())
       .def(py::init<>())
-      .def(py::init<float _x, float _y, float _z>())
-      .def(py::self << py::self);
-  py::class_<pcl::_RGB>(m, "_RGB");
-  py::class_<pcl::RGB, public _RGB>(m, "RGB")
-      .def(py::init<const _RGB & p>())
+      .def(py::init<float, float, float>());
+  py::class_<_RGB>(m, "_RGB");
+  py::class_<RGB, _RGB>(m, "RGB")
+      .def(py::init<const _RGB>())
       .def(py::init<>())
-      .def(py::init<std::uint8_t _r, std::uint8_t _g, std::uint8_t _b>())
-      .def(py::self << py::self);
+      .def(py::init<std::uint8_t, std::uint8_t, std::uint8_t>());
 }
+} // namespace pcl
