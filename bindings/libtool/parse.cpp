fedded1e755343d08eac9d646a85f6388c77a65b
Divyanshu Madan <iamdm99@gmail.com>
2020-06-02 05:30:19 +0530

    [parse.cpp] minimum compiling solution

---
+++ bindings/libtool/parse.cpp
@@ -0,0 +1,8 @@
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Support/CommandLine.h"
+
+using namespace clang::tooling;
+
+int main(){;}
