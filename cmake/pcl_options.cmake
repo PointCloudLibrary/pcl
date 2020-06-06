3ec2691e0f07ea957ceeea591e07a99ec5f461c2
haritha-j <haritha.16@cse.mrt.ac.lk>
2020-06-06 16:47:54 +0530

    change default behalt behaviour

--- cmake/pcl_options.cmake
+++ cmake/pcl_options.cmake
@@ -66,7 +66,7 @@ option(BUILD_tools "Useful PCL-based command line tools" ON)
 option(WITH_DOCS "Build doxygen documentation" OFF)
 
 # set index size
-set(PCL_INDEX_SIZE 32 CACHE STRING "Set index size. Only available from PCL 1.12 or greater. Available options are: 8 16 32 64")
+set(PCL_INDEX_SIZE -1 CACHE STRING "Set index size. Available options are: 8 16 32 64")
 
 #set whether indices are signed or unsigned
 set(PCL_INDEX_SIGNED true CACHE BOOL "Set whether indices need to be signed or unsigned. Signed by default.")
