ea668feab081c9c925cb633d716fe63d1e99e140
Aditya Mulampally <aditya.mulampally@gmail.com>
2020-06-07 23:50:09 -0500

    Fixed Indentations

--- visualization/include/pcl/visualization/pcl_visualizer.h
+++ visualization/include/pcl/visualization/pcl_visualizer.h
@@ -1975,42 +1975,41 @@ namespace pcl
       protected:
         /** \brief The render window interactor. */
         vtkSmartPointer<vtkRenderWindowInteractor> interactor_;
-		
-		 /** \brief Removes a vtk actor from the screen.
+
+		    /** \brief Removes a vtk actor from the screen.
           * \param[in] actor a pointer to the vtk actor object
           * \param[in] viewport the view port where the actor should be removed from (default: all)
           */
         bool
-		removeCustomActorFromRenderer(const vtkSmartPointer<vtkLODActor> &actor,
+		    removeCustomActorFromRenderer(const vtkSmartPointer<vtkLODActor> &actor,
                                  int viewport = 0)
-		{
-			removeActorFromRenderer (actor,viewport);
-						 
-		}
-        
+        {
+          removeActorFromRenderer (actor,viewport);
+		    }
+
 
         /** \brief Removes a vtk actor from the screen.
           * \param[in] actor a pointer to the vtk actor object
           * \param[in] viewport the view port where the actor should be removed from (default: all)
           */
         bool
-		removeCustomActorFromRenderer (const vtkSmartPointer<vtkActor> &actor,
+		    removeCustomActorFromRenderer (const vtkSmartPointer<vtkActor> &actor,
                                  int viewport = 0)
-		{
-        removeActorFromRenderer (actor,viewport);
-		}
-		
-		 /** \brief Adds a vtk actor to screen.
+		    {
+          removeActorFromRenderer (actor,viewport);
+		    }
+
+		    /** \brief Adds a vtk actor to screen.
           * \param[in] actor a pointer to the vtk actor object
           * \param[in] viewport the view port where the actor should be added to (default: all)
           */
         bool
         removeCustomActorFromRenderer (const vtkSmartPointer<vtkProp> &actor,
                                  int viewport = 0)
-		{
-			removeActorFromRenderer (actor,viewport); 
-		}
-		
+		    {
+			    removeActorFromRenderer (actor,viewport);
+		    }
+
         /** \brief  Adds a vtk actor to screen.
           * \param[in] actor a pointer to the vtk actor object
           * \param[in] viewport port where the actor should be added to (default: 0/all)
@@ -2019,21 +2018,21 @@ namespace pcl
           * renders. To select a specific viewport use an integer between 1 and N.
           */
         void
-		addCustomActorToRenderer(const vtkSmartPointer<vtkProp> &actor,
+		    addCustomActorToRenderer(const vtkSmartPointer<vtkProp> &actor,
                             int viewport = 0)
-		{
-        addActorToRenderer (actor, viewport);
-		}
-       
+		    {
+          addActorToRenderer (actor, viewport);
+		    }
 
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
       private:
         /** \brief Internal function for renderer setup
          * \param[in] vtk renderer
