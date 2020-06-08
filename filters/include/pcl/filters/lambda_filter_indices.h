d76ecdec02904d95f6913ae0fbf09284a1905c7d
Kunal Tyagi <tyagi.kunal@live.com>
2020-06-09 07:34:11 +0900

    minor improvements, less license

--- filters/include/pcl/filters/lambda_filter_indices.h
+++ filters/include/pcl/filters/lambda_filter_indices.h
@@ -1,40 +1,10 @@
 /*
- * Software License Agreement (BSD License)
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  *  Point Cloud Library (PCL) - www.pointclouds.org
  *  Copyright (c) 2020-, Open Perception
  *
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the copyright holder(s) nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * $Id$
- *
+ *  All rights reserved
  */
 
 #pragma once
@@ -42,8 +12,6 @@
 #include <pcl/filters/filter_indices.h>
 #include <pcl/pcl_macros.h>
 #include <pcl/type_traits.h> // for is_invocable
-
-#include <utility>
 
 namespace pcl {
 namespace detail {
@@ -74,9 +42,8 @@ private:
 
 protected:
   using FilterIndices<PointT>::PointCloud;
-  using PointCloudPtr = typename PointCloud::Ptr;
-  using PointCloudConstPtr = typename PointCloud::ConstPtr;
-  using FieldList = typename pcl::traits::fieldList<PointT>::type;
+  using FilterIndices<PointT>::PointCloudPtr;
+  using FilterIndices<PointT>::PointCloudComnstPtr;
 
   using Base::extract_removed_indices_;
   using Base::negative_;
@@ -127,7 +94,7 @@ private:
 public:
   using FunctorT = Functor;
   // using in type would complicate signature
-  static_assert(is_lambda_point_filter_v<PointT, FunctorT>,
+  static_assert(detail::is_lambda_point_filter_v<PointT, FunctorT>,
                 "Functor signature must be similar to `bool(const PointT&, index_t)`");
 
 protected:
