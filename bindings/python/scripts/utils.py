0ca9cbc5ae842db92c3589c49012fce718f61379
Divyanshu Madan <iamdm99@gmail.com>
2020-07-05 20:19:43 +0530

    [utils.py] add utils

---
+++ bindings/python/scripts/utils.py
@@ -0,0 +1,43 @@
+import os
+import json
+import argparse
+
+
+def get_realpath(path):
+    return os.path.realpath(path)
+
+
+def ensure_dir_exists(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+def get_dirname(path):
+    return get_realpath(os.path.dirname(path))
+
+
+def get_json_output_path(source, output_dir):
+    x_list = source.split("pcl/", 1)[-1]
+    x_list = x_list.split("/")
+    extra = ["pcl", "include"]
+
+    filename = x_list[-1].split(".")[0]
+    relative_dir = "/".join(x for x in x_list[:-1] if x not in extra)
+    dir = os.path.join(output_dir, relative_dir)
+
+    ensure_dir_exists(dir)
+
+    return get_realpath(f"{dir}/{filename}.json")
+
+
+def dump_json(filepath, info):
+    with open(filepath, "w") as f:
+        json.dump(info, f, indent=2)
+
+
+def parse_arguments(args, script):
+    if script == "parse.py":
+        parser = argparse.ArgumentParser(description="C++ libclang parser")
+        parser.add_argument("files", nargs="+", help="The source files to parse")
+        return parser.parse_args(args)
+
