70690dbb69078a74ad2b57332d115f6ec3852df0
Divyanshu Madan <iamdm99@gmail.com>
2020-07-17 17:21:34 +0530

    [utils.py] modify to add generate.py functionalities

--- bindings/python/scripts/utils.py
+++ bindings/python/scripts/utils.py
@@ -20,7 +20,7 @@ def join_path(*args):
     return os.path.join(*args)
 
 
-def get_json_output_path(source, output_dir):
+def get_output_path(source, output_dir, split_from, extension):
     """
     Returns json output path after manipulation of the source file's path
 
@@ -33,15 +33,15 @@ def get_json_output_path(source, output_dir):
     """
 
     # pcl_path: contains the path as seen in the pcl directory
-    _, pcl_path = source.split(f"pcl{os.sep}", 1)
+    _, split_path = source.split(f"{split_from}{os.sep}", 1)
 
     # relative_dir: contains the relative output path for the json file
     # source_filename: contains the source's file name
-    relative_dir, source_filename = os.path.split(pcl_path)
+    relative_dir, source_filename = os.path.split(split_path)
 
     # filename: contains the output json's file name
     filename, _ = source_filename.split(".")
-    filename = f"{filename}.json"
+    filename = f"{filename}.{extension}"
 
     # dir: final output path
     dir = join_path(output_dir, relative_dir)
@@ -49,14 +49,26 @@ def get_json_output_path(source, output_dir):
     # make the output directory if it doesn't exist
     ensure_dir_exists(dir)
 
-    json_output_path = get_realpath(join_path(dir, filename))
+    output_path = get_realpath(join_path(dir, filename))
 
-    return json_output_path
+    return output_path
 
 
 def dump_json(filepath, info):
     with open(filepath, "w") as f:
         json.dump(info, f, indent=2)
+
+
+def read_json(filename):
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+def write_to_file(filename, linelist):
+    with open(filename, "w") as f:
+        for line in linelist:
+            f.writelines(line)
+            f.writelines("\n")
 
 
 def parse_arguments(script):
@@ -83,6 +95,18 @@ def parse_arguments(script):
             help="Output path for generated json",
         )
         parser.add_argument("files", nargs="+", help="The source files to parse")
-        args = parser.parse_args()
 
-        return args
+    if script == "generate":
+        parser = argparse.ArgumentParser(description="JSON to pybind11 generation")
+        parser.add_argument("files", nargs="+", help="JSON input")
+        parser.add_argument(
+            "--pybind11_output_path",
+            default=get_parent_directory(file=__file__),
+            help="Output path for generated cpp",
+        )
+
+    else:
+        args = None
+
+    args = parser.parse_args()
+    return args
