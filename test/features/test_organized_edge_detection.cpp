31c25675e85f9727eb66b29a47f0c4fb2eaceb67
Matt Middleton <mmiddleton@wynright.com>
2020-07-21 05:14:13 -0500

    Use proper image conventions Detail how test works Use proper cloud constructor Remove magic numbers

--- test/features/test_organized_edge_detection.cpp
+++ test/features/test_organized_edge_detection.cpp
@@ -17,11 +17,14 @@ namespace
   class OrganizedPlaneDetectionTestFixture : public ::testing::Test
   {
    protected:
-     static const int kExpectedOccludingEdgePoints = 395;
-     static const int kExpectedOccludedEdgePoints = 401;
-     static const int kSyntheticCloudDisparity = .03;
-     static const int kSyntheticCloudResolution = .005;
-     static const int kSyntheticCloudInnerBoxWidth = 50;
+     const int kSyntheticCloudInnerSquareEdgeLength = 124;
+     const int kSyntheticCloudOuterSquareEdgeLength = kSyntheticCloudInnerSquareEdgeLength * 2;
+
+     const int kExpectedOccludingEdgePoints = kSyntheticCloudInnerSquareEdgeLength * 4 - 8;  // Empirically determined
+     const int kExpectedOccludedEdgePoints = kSyntheticCloudInnerSquareEdgeLength * 4;       // Each edge pixed of inner square should generate an occluded point
+
+     const float kSyntheticCloudDisparity = .03f;
+     const float kSyntheticCloudResolution = 0.01f;
 
      pcl::PointCloud <pcl::PointXYZRGBA>::Ptr cloud_;
      pcl::PointIndicesPtr indices_;
@@ -40,66 +43,81 @@ namespace
    private:
      pcl::PointCloud<pcl::PointXYZRGBA>::Ptr GenerateSyntheticEdgeDetectionCloud(const float &disparity)
      {
-	auto organized_test_cloud = pcl::make_shared<pcl::PointCloud<pcl::PointXYZRGBA>>();
-	organized_test_cloud->width = kSyntheticCloudInnerBoxWidth * 4;
-	organized_test_cloud->height = kSyntheticCloudInnerBoxWidth * 4;
-	organized_test_cloud->is_dense = false;
-	organized_test_cloud->points.resize(organized_test_cloud->height * organized_test_cloud->width);
+	auto organized_test_cloud = pcl::make_shared<pcl::PointCloud<pcl::PointXYZRGBA>>(kSyntheticCloudOuterSquareEdgeLength, kSyntheticCloudOuterSquareEdgeLength);
+        organized_test_cloud->is_dense = true;
 
-	for(std::size_t i = 0; i < kSyntheticCloudInnerBoxWidth * 2; i++)
+        const auto kBaseDepth = 2.0f;
+
+        // Draw a smaller red square in front of a larger green square both centered on the view axis to generate synthetic occluding and occluded edges based on depth disparity between neighboring pixels
+	for(auto row = 0; row < kSyntheticCloudOuterSquareEdgeLength; row++)
 	{
-		for(std::size_t j = 0; j < kSyntheticCloudInnerBoxWidth * 2; j++)
-		{
-			organized_test_cloud->at(j, i).x = i * kSyntheticCloudResolution - (kSyntheticCloudInnerBoxWidth / 2 * kSyntheticCloudResolution);
-			organized_test_cloud->at(j, i).y = j * kSyntheticCloudResolution;
-			organized_test_cloud->at(j, i).z = 2.0 + (disparity * 4);
-			organized_test_cloud->at(j, i).r = 128;
-			organized_test_cloud->at(j, i).g = 128;
-			organized_test_cloud->at(j, i).b = 128;
-			organized_test_cloud->at(j, i).a = 255;
+		for(auto col = 0; col < kSyntheticCloudOuterSquareEdgeLength; col++)
+                {
+                        float x = col - (kSyntheticCloudOuterSquareEdgeLength / 2);
+                        float y = row - (kSyntheticCloudOuterSquareEdgeLength / 2);
+
+                        const auto outer_square_ctr = kSyntheticCloudOuterSquareEdgeLength / 2;
+                        const auto inner_square_ctr = kSyntheticCloudInnerSquareEdgeLength / 2;
+
+                        auto depth = kBaseDepth + (disparity * 2);
+                        auto r = 0;
+                        auto g = 255;
+
+                        // If pixels correspond to smaller box, then set depth and color appropriately
+                        if (col > outer_square_ctr - inner_square_ctr && col < outer_square_ctr + inner_square_ctr)
+                        {
+                          if (row > outer_square_ctr - inner_square_ctr && row < outer_square_ctr + inner_square_ctr)
+                          {
+                            depth = kBaseDepth;
+                            r = 255;
+                            g = 0;
+                          }
+                        }
+
+			organized_test_cloud->at(col, row).x = x * kSyntheticCloudResolution;
+			organized_test_cloud->at(col, row).y = y * kSyntheticCloudResolution;
+			organized_test_cloud->at(col, row).z = depth;
+			organized_test_cloud->at(col, row).r = r;
+			organized_test_cloud->at(col, row).g = g;
+			organized_test_cloud->at(col, row).b = 0;
+			organized_test_cloud->at(col, row).a = 255;
 		}
 	}
 
-	for(std::size_t i = kSyntheticCloudInnerBoxWidth / 2; i < kSyntheticCloudInnerBoxWidth / 2 + kSyntheticCloudInnerBoxWidth; i++)
-	{
-		for(std::size_t j = kSyntheticCloudInnerBoxWidth / 2; j < kSyntheticCloudInnerBoxWidth / 2 + kSyntheticCloudInnerBoxWidth; j++)
-		{
-			organized_test_cloud->at(j, i).x = i * kSyntheticCloudResolution - (kSyntheticCloudInnerBoxWidth / 2 * kSyntheticCloudResolution);
-			organized_test_cloud->at(j, i).y = j * kSyntheticCloudResolution;
-			organized_test_cloud->at(j, i).z = 2.0;
-			organized_test_cloud->at(j, i).r = 192;
-			organized_test_cloud->at(j, i).g = 192;
-			organized_test_cloud->at(j, i).b = 192;
-			organized_test_cloud->at(j, i).a = 255;
-		}
-	}
+
 	return organized_test_cloud;
      }
-
-
   };
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+This test is designed to ensure that the organized edge detection appropriately classifies occluding and occluding 
+edges and to speifically detect the type of regression detailed in PR 4275
+(https://github.com/PointCloudLibrary/pcl/pull/4275).  This test works by generating a synthentic cloud of one 
+square slightly in front of another square, so that occluding edges and occluded edges are generated.  The 
+regression introduced in PCL 1.10.1 was a logic bug that caused both occluding and occluded edges to erroneously 
+be categorized as occluding edges.  This test should catch this and similar bugs.
+*/
 TEST_F (OrganizedPlaneDetectionTestFixture, OccludedAndOccludingEdges)
 {
-  auto MaxSearchNeighbors = 50;
-  auto DepthDiscontinuity = .02f;
+  const auto kMaxSearchNeighbors = 50;
+  const auto kDepthDiscontinuity = .02f;
 
   auto oed = pcl::OrganizedEdgeFromRGB<pcl::PointXYZRGBA, pcl::Label>();
 
   oed.setEdgeType(oed.EDGELABEL_OCCLUDING | oed.EDGELABEL_OCCLUDED);
   oed.setInputCloud(cloud_);
-  oed.setDepthDisconThreshold(DepthDiscontinuity);
-  oed.setMaxSearchNeighbors(MaxSearchNeighbors);
+  oed.setDepthDisconThreshold(kDepthDiscontinuity);
+  oed.setMaxSearchNeighbors(kMaxSearchNeighbors);
 
   auto labels = pcl::PointCloud<pcl::Label>();
   auto label_indices = std::vector<pcl::PointIndices>();
 
   oed.compute(labels, label_indices);
 
-  EXPECT_EQ (395, label_indices[1].indices.size());  // Occluding Edges Indices Size should be 395 for synthetic cloud
-  EXPECT_EQ (401, label_indices[2].indices.size());  // Occluded Edge Indices should be 401 for synthentic cloud
+  EXPECT_EQ (kExpectedOccludingEdgePoints, label_indices[1].indices.size());  // Occluding Edges Indices Size should be 395 for synthetic cloud
+  EXPECT_EQ (kExpectedOccludedEdgePoints, label_indices[2].indices.size());  // Occluded Edge Indices should be 401 for synthentic cloud
 }
 
 
