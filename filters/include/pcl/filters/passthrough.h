f4d803c304328a158b2f55da22994b8e60f18102
Shrijit Singh <shrijitsingh99@gmail.com>
2020-07-23 13:21:54 +0530

    Switch to SPDX style license

--- filters/include/pcl/filters/passthrough.h
+++ filters/include/pcl/filters/passthrough.h
@@ -3,7 +3,7 @@
  *
  *  Point Cloud Library (PCL) - www.pointclouds.org
  *  Copyright (c) 2010-2012, Willow Garage, Inc.
- *  Copyright (c) 2020-, Open Perception
+ *  Copyright (c) 2012-, Open Perception
  *
  *  All rights reserved
  */
