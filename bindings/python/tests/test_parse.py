f53e3e91cf9f0487f3744ae0dd85d369edbaed23
Divyanshu Madan <iamdm99@gmail.com>
2020-07-26 23:53:19 +0530

    [test_parse.py] add struct based tests

--- bindings/python/tests/test_parse.py
+++ bindings/python/tests/test_parse.py
@@ -120,23 +120,6 @@ def test_field_decl(tmp_path):
     assert field_decl["name"] == "aClassMember"
 
 
-def test_parm_decl(tmp_path):
-    file_contents = """
-    struct AStruct {
-        AStruct(int aFunctionParameter) {}
-    };
-    """
-    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
-
-    struct_decl = parsed_info["members"][0]
-    constructor = struct_decl["members"][0]
-    parm_decl = constructor["members"][0]
-
-    assert parm_decl["kind"] == "PARM_DECL"
-    assert parm_decl["element_type"] == "Int"
-    assert parm_decl["name"] == "aFunctionParameter"
-
-
 def test_parsed_info_structure(tmp_path):
     file_contents = ""
     parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
@@ -163,3 +146,162 @@ def test_call_expr(tmp_path):
 
     assert var_decl["name"] == "anInt"
 
+
+def test_struct_decl(tmp_path):
+    file_contents = "struct AStruct {};"
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+
+    assert struct_decl["kind"] == "STRUCT_DECL"
+    assert struct_decl["name"] == "AStruct"
+
+
+def test_cxx_base_specifier(tmp_path):
+    file_contents = """
+    struct BaseStruct {};
+    struct DerivedStruct: public BaseStruct {};
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    child_struct_decl = parsed_info["members"][1]
+    cxx_base_specifier = child_struct_decl["members"][0]
+
+    assert cxx_base_specifier["kind"] == "CXX_BASE_SPECIFIER"
+    assert cxx_base_specifier["access_specifier"] == "PUBLIC"
+    assert cxx_base_specifier["name"] == "struct BaseStruct"
+
+
+def test_cxx_method(tmp_path):
+    file_contents = """
+    struct AStruct {
+        void aMethod() {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    cxx_method = struct_decl["members"][0]
+
+    assert cxx_method["kind"] == "CXX_METHOD"
+    assert cxx_method["result_type"] == "void"
+    assert cxx_method["name"] == "aMethod"
+
+
+def test_type_ref(tmp_path):
+    file_contents = """
+    struct AStruct { 
+        int aClassMember;
+        void aMethod(AStruct& aClassMember) {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    cxx_method = struct_decl["members"][1]
+    l_value_ref = cxx_method["members"][0]
+    type_ref = l_value_ref["members"][0]
+
+    assert type_ref["kind"] == "TYPE_REF"
+    assert type_ref["name"] == "struct AStruct"
+
+
+def test_constructor(tmp_path):
+    file_contents = """
+    struct AStruct {
+        AStruct() {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    constructor = struct_decl["members"][0]
+
+    assert constructor["kind"] == "CONSTRUCTOR"
+    assert constructor["access_specifier"] == "PUBLIC"
+    assert constructor["name"] == "AStruct"
+
+
+def test_parm_decl(tmp_path):
+    file_contents = """
+    struct AStruct {
+        AStruct(int aFunctionArgument) {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    constructor = struct_decl["members"][0]
+    parm_decl = constructor["members"][0]
+
+    assert parm_decl["kind"] == "PARM_DECL"
+    assert parm_decl["element_type"] == "Int"
+    assert parm_decl["name"] == "aFunctionArgument"
+
+
+def test_unexposed_expr(tmp_path):
+    file_contents = """
+    struct AStruct {
+        int aClassMember;
+        AStruct(int aFunctionArgument) : aClassMember(aFunctionArgument) {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    constructor = struct_decl["members"][1]
+    unexposed_expr = constructor["members"][2]
+
+    assert unexposed_expr["kind"] == "UNEXPOSED_EXPR"
+    assert unexposed_expr["name"] == "aFunctionArgument"
+
+
+# @TODO: Not sure how to reproduce. Maybe later.
+# def test_member_ref_expr(tmp_path):
+
+
+def test_decl_ref_expr(tmp_path):
+    file_contents = """
+    struct AStruct {
+        int firstMember, secondMember;
+        AStruct(int firstFunctionArgument, int secondFunctionArgument)
+        : firstMember(secondFunctionArgument), secondMember(firstFunctionArgument)
+        {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    constructor = struct_decl["members"][2]
+    unexposed_expr_1 = constructor["members"][3]
+    unexposed_expr_2 = constructor["members"][5]
+    decl_ref_expr_1 = unexposed_expr_1["members"][0]
+    decl_ref_expr_2 = unexposed_expr_2["members"][0]
+
+    assert decl_ref_expr_1["kind"] == "DECL_REF_EXPR"
+    assert decl_ref_expr_2["kind"] == "DECL_REF_EXPR"
+    assert decl_ref_expr_1["name"] == "secondFunctionArgument"
+    assert decl_ref_expr_2["name"] == "firstFunctionArgument"
+
+
+def test_member_ref(tmp_path):
+    file_contents = """
+    struct AStruct {
+        int firstMember, secondMember;
+        AStruct(int firstFunctionArgument, int secondFunctionArgument)
+        : firstMember(secondFunctionArgument), secondMember(firstFunctionArgument)
+        {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+    struct_decl = parsed_info["members"][0]
+    constructor = struct_decl["members"][2]
+    member_ref_1 = constructor["members"][2]
+    member_ref_2 = constructor["members"][4]
+
+    assert member_ref_1["kind"] == "MEMBER_REF"
+    assert member_ref_2["kind"] == "MEMBER_REF"
+    assert member_ref_1["element_type"] == "Int"
+    assert member_ref_2["element_type"] == "Int"
+    assert member_ref_1["name"] == "firstMember"
+    assert member_ref_2["name"] == "secondMember"
