24e9d30a8fb5388ea52c3d8c74ce16c8af6d1a9e
Divyanshu Madan <iamdm99@gmail.com>
2020-07-30 16:14:54 +0530

    add empty name

--- bindings/python/tests/test_parse.py
+++ bindings/python/tests/test_parse.py
@@ -46,16 +46,16 @@ def test_anonymous_decls(tmp_path):
 
     union_decl = parsed_info["members"][0]
 
-    assert union_decl["kind"] == "UNION_DECL"
+    assert union_decl["kind"] == "ANONYMOUS_UNION_DECL"
     assert union_decl["name"] == ""
 
     struct_decl = union_decl["members"][0]
 
-    assert struct_decl["kind"] == "STRUCT_DECL"
+    assert struct_decl["kind"] == "ANONYMOUS_STRUCT_DECL"
     assert struct_decl["name"] == ""
 
     enum_decl = struct_decl["members"][0]
 
-    assert enum_decl["kind"] == "ENUM_DECL"
+    assert enum_decl["kind"] == "ANONYMOUS_ENUM_DECL"
     assert enum_decl["name"] == ""
 
