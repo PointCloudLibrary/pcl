2995ba699203117b9dd8587a914b63d69f6629ea
Divyanshu Madan <iamdm99@gmail.com>
2020-07-30 16:29:43 +0530

    [generate.py] messy fix for anonymous gen and member definition

--- bindings/python/scripts/generate.py
+++ bindings/python/scripts/generate.py
@@ -27,6 +27,23 @@ class bind:
             "CLASS_TEMPLATE": self.skip,  # self.handle_class_template
             "TEMPLATE_NON_TYPE_PARAMETER": self.skip,
             "FUNCTION_TEMPLATE": self.skip,
+            "ANONYMOUS_UNION_DECL": self.skip,
+            "ALIGNED_ATTR": self.skip,
+            "INTEGER_LITERAL": self.skip,
+            "ANONYMOUS_STRUCT_DECL": self.skip,
+            "COMPOUND_STMT": self.skip,
+            "FLOATING_LITERAL": self.skip,
+            "BINARY_OPERATOR": self.skip,
+            "ARRAY_SUBSCRIPT_EXPR": self.skip,
+            "CXX_THROW_EXPR": self.skip,
+            "FRIEND_DECL": self.skip,
+            "FUNCTION_DECL": self.skip,
+            "INIT_LIST_EXPR": self.skip,
+            "RETURN_STMT": self.skip,
+            "OVERLOADED_DECL_REF": self.skip,
+            "UNARY_OPERATOR": self.skip,
+            "IF_STMT": self.skip,
+            "OBJ_BOOL_LITERAL_EXPR": self.skip,
         }
 
         self.handle_node(root)
@@ -58,7 +75,7 @@ class bind:
     def handle_node(self, item):
         self.item = item
         self.kind = self.item["kind"]
-        self.name = self.item["name"]
+        self.name = self.item["name"] if "name" in self.item else ""
         self.members = self.item["members"]
         self.depth = self.item["depth"]
 
@@ -79,6 +96,16 @@ class bind:
     def handle_namespace(self):
         self.linelist.append(f"namespace {self.name}" + "{")
 
+    def get_fields_from_anonymous(self, item):
+        fields = []
+        for sub_item in item["members"]:
+            if sub_item["kind"] in ["ANONYMOUS_UNION_DECL", "ANONYMOUS_STRUCT_DECL"]:
+                for field in self.get_fields_from_anonymous(sub_item):
+                    fields.append(field)
+            if sub_item["kind"] == "FIELD_DECL":
+                fields.append(sub_item)
+        return fields
+
     def handle_struct_decl(self):
         cxx_base_specifier_list = [
             sub_item["name"]
@@ -88,10 +115,24 @@ class bind:
         if cxx_base_specifier_list:
             cxx_base_specifier_list = ",".join(cxx_base_specifier_list)
             self.linelist.append(
-                f'py::class_<{self.name}, {cxx_base_specifier_list}>(m, "{self.name}")'
+                f'py::class_<{self.name}, {cxx_base_specifier_list.replace("struct ", "").replace("pcl::", "")}>(m, "{self.name}")'
             )
         else:
             self.linelist.append(f'py::class_<{self.name}>(m, "{self.name}")')
+
+
+        # handle anonymous
+        for sub_item in self.members:
+            fields = self.get_fields_from_anonymous(sub_item)
+            for field in fields:
+                if field["element_type"] == "ConstantArray":
+                    self.linelist.append(
+                        f'.def_property_readonly("{field["name"]}", []({self.name}& obj) {{return obj.{field["name"]}; }})'  # float[ ' + f'obj.{sub_item["name"]}' + '.size()];} )'
+                    )
+                else:
+                    self.linelist.append(
+                        f'.def_readwrite("{field["name"]}", &{self.name}::{field["name"]})'
+                    )
 
         for sub_item in self.members:
             if sub_item["kind"] == "FIELD_DECL":
@@ -201,11 +242,12 @@ def main():
             lines_to_write = bind_object.handle_final(
                 filename="pcl/point_types.h", module_name="pcl"
             )
+            print(bind_object._skipped)
             output_filepath = utils.get_output_path(
                 source=source,
                 output_dir=utils.join_path(args.pybind11_output_path, "pybind11"),
                 split_from="json",
-                extension="cpp",
+                extension=".cpp",
             )
             utils.write_to_file(filename=output_filepath, linelist=lines_to_write)
 
