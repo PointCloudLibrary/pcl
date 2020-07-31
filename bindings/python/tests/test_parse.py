7f5245409e2305df8db9566703839d3d4d85c221
Divyanshu Madan <iamdm99@gmail.com>
2020-07-31 07:18:27 +0530

    [test_parse.py] ready tests

--- bindings/python/tests/test_parse.py
+++ bindings/python/tests/test_parse.py
@@ -59,3 +59,79 @@ def test_anonymous_decls(tmp_path):
     assert enum_decl["kind"] == "ANONYMOUS_ENUM_DECL"
     assert enum_decl["name"] == ""
 
+
+def test_translation_unit(tmp_path):
+    file_contents = ""
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    assert parsed_info["kind"] == "TRANSLATION_UNIT"
+    assert parsed_info["depth"] == 0
+    assert parsed_info["name"] == str(tmp_path / "file.hpp")
+
+
+def test_namespace(tmp_path):
+    file_contents = "namespace a_namespace {}"
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    namespace = parsed_info["members"][0]
+
+    assert namespace["kind"] == "NAMESPACE"
+    assert namespace["name"] == "a_namespace"
+
+
+def test_namespace_ref(tmp_path):
+    file_contents = """
+    #include <ostream>
+    std::ostream anOstream;
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    var_decl = parsed_info["members"][0]
+    namespace_ref = var_decl["members"][0]
+
+    assert namespace_ref["kind"] == "NAMESPACE_REF"
+    assert namespace_ref["name"] == "std"
+
+
+def test_var_decl(tmp_path):
+    file_contents = "int anInt = 1;"
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    var_decl = parsed_info["members"][0]
+
+    assert var_decl["kind"] == "VAR_DECL"
+    assert var_decl["element_type"] == "Int"
+    assert var_decl["name"] == "anInt"
+
+
+def test_field_decl(tmp_path):
+    file_contents = """
+    struct AStruct {
+        int aClassMember;
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    field_decl = struct_decl["members"][0]
+
+    assert field_decl["kind"] == "FIELD_DECL"
+    assert field_decl["element_type"] == "Int"
+    assert field_decl["name"] == "aClassMember"
+
+
+def test_parm_decl(tmp_path):
+    file_contents = """
+    struct AStruct {
+        AStruct(int aFunctionParameter) {}
+    };
+    """
+    parsed_info = get_parsed_info(tmp_path=tmp_path, file_contents=file_contents)
+
+    struct_decl = parsed_info["members"][0]
+    constructor = struct_decl["members"][0]
+    parm_decl = constructor["members"][0]
+
+    assert parm_decl["kind"] == "PARM_DECL"
+    assert parm_decl["element_type"] == "Int"
+    assert parm_decl["name"] == "aFunctionParameter"
