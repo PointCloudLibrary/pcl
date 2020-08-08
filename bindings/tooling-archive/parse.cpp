e5fbc9cf3f2ea70c2f5a72dfbf424639a0d9b5bd
Divyanshu Madan <iamdm99@gmail.com>
2020-08-08 16:05:58 +0530

    remove unnecessary; cleanup

--- bindings/tooling-archive/parse.cpp
+++
@@ -1,8 +0,0 @@
-#include "clang/Frontend/FrontendActions.h"
-#include "clang/Tooling/CommonOptionsParser.h"
-#include "clang/Tooling/Tooling.h"
-#include "llvm/Support/CommandLine.h"
-
-using namespace clang::tooling;
-
-int main(){;}
