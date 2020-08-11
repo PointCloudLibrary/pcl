66fa90b18bbbbc021fb1b01bfc9246560845a30d
haritha-j <haritha.16@cse.mrt.ac.lk>
2020-08-11 10:18:45 +0530

    add warning for assertion

--- gpu/octree/src/cuda/knn_search.cu
+++ gpu/octree/src/cuda/knn_search.cu
@@ -232,7 +232,7 @@ namespace pcl { namespace device { namespace knn_search
 
           // find minimum distance among warp threads
           constexpr unsigned FULL_MASK = 0xFFFFFFFF;
-          static_assert(KernelPolicy::WARP_SIZE <= 8*sizeof(unsigned int));
+          static_assert(KernelPolicy::WARP_SIZE <= 8*sizeof(unsigned int), "WARP_SIZE exceeds size of bit_offset.");
 
           for (unsigned int bit_offset = KernelPolicy::WARP_SIZE / 2; bit_offset > 0;
                bit_offset /= 2) {
