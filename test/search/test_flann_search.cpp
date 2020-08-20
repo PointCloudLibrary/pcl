5b74733e80e1f662f50f130a03ba1da4e0531c5d
Kunal Tyagi <tyagi.kunal@live.com>
2020-08-20 10:23:09 +0900

    emplace_back isn't a drop in replacement

--- test/search/test_flann_search.cpp
+++ test/search/test_flann_search.cpp
@@ -60,14 +60,14 @@ init ()
   cloud.width = cloud.size ();
   cloud.height = 1;
 
-  cloud_big.width = 640;
-  cloud_big.height = 480;
   srand (int (time (nullptr)));
   // Randomly create a new point cloud
   for (std::size_t i = 0; i < cloud_big.width * cloud_big.height; ++i)
     cloud_big.emplace_back(static_cast<float>(1024 * rand() / (RAND_MAX + 1.0)),
-                                  float(1024 * rand() / (RAND_MAX + 1.0)),
-                                  float(1024 * rand() / (RAND_MAX + 1.0)));
+                           static_cast<float>(1024 * rand() / (RAND_MAX + 1.0)),
+                           static_cast<float>(1024 * rand() / (RAND_MAX + 1.0)));
+  cloud_big.width = 640;
+  cloud_big.height = 480;
 }
 
 
