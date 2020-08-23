28a16733f1c48cdd3879620ae13425f36ed9e549
Divyanshu Madan <iamdm99@gmail.com>
2020-08-23 21:00:26 +0530

    [test_generate.py] updates
    - rename `get_character_only_string` -> `remove_whitespace`
    - functional changes to `remove_whitespace`
    - rename `get_binded_code` -> `generate_bindings`
    - tmp_path change order and comment updation
    - dump_json: separators for json size reduction
    - include: remove pcl
    - pybind_linelist -> initial_pybind_lines
    - default constructor

--- bindings/python/tests/test_generate.py
+++ bindings/python/tests/test_generate.py
@@ -4,25 +4,20 @@ import scripts.utils as utils
 import test_parse
 
 
-def get_character_only_string(word):
+def remove_whitespace(string):
     """
-    Returns extracted characters from a word by excluding whitespaces (' '), newlines ('\n') and tabs ('\t').
+    Removes whitespace from the string.
 
     Parameters:
-        - word (str)
+        - string (str)
     
-    Returns:
-        - chars (str): The stripped off string
+    Returns: The stripped off string.
     """
 
-    chars = []
-    for c in word:
-        if c not in (" ", "\n", "\t"):
-            chars += c
-    return "".join(chars)
+    return str([x for x in string if not x.isspace()])
 
 
-def get_binded_code(tmp_path, cpp_code_block):
+def generate_bindings(cpp_code_block, tmp_path):
     """
     Returns binded code for a cpp code block
     - Steps:
@@ -31,7 +26,7 @@ def get_binded_code(tmp_path, cpp_code_block):
         3. Convert the list to a string and then return the stripped off string.
 
     Parameters:
-        - tmp_path (Path): The tmp_path for test folder
+        - tmp_path (pathlib.PosixPath): The tmp_path for test folder
         - cpp_code_block (str): The cpp code block to generate bindings for
 
     Returns:
@@ -45,7 +40,7 @@ def get_binded_code(tmp_path, cpp_code_block):
 
     # JSON dump the parsed info
     json_path = tmp_path / "file.json"
-    utils.dump_json(filepath=json_path, info=parsed_info)
+    utils.dump_json(filepath=json_path, info=parsed_info, indent=None, separators=(',', ':'))
 
     # Get the binded code
     binded_code = generate.generate(source=json_path)
@@ -53,12 +48,12 @@ def get_binded_code(tmp_path, cpp_code_block):
     # List to string
     binded_code = "".join(binded_code)
 
-    return get_character_only_string(binded_code)
+    return remove_whitespace(binded_code)
 
 
 def get_expected_string(expected_module_code):
     """
-    Returns expected output string after combining inclusions and pybind11 linelist.
+    Returns expected output string after combining inclusions and pybind11's initial lines.
 
     Parameters:
         - expected_module_code (str): Module code to be combined.
@@ -67,14 +62,13 @@ def get_expected_string(expected_module_code):
         - expected_output (str): The stripped off, combined code.
     """
 
-    file_inclusion = "#include <pcl/file.cpp>"
+    file_include = "#include <file.cpp>"
 
-    # Get pybind11 linelist in the form of a string
-    pybind_linelist = generate.bind.pybind_linelist
-    pybind_linelist = "".join(pybind_linelist)
+    # Get pybind11's intial lines in the form of a string
+    initial_pybind_lines = "".join(generate.bind.initial_pybind_lines)
 
-    expected_output = get_character_only_string(
-        file_inclusion + pybind_linelist + expected_module_code
+    expected_output = remove_whitespace(
+        file_include + initial_pybind_lines + expected_module_code
     )
 
     return expected_output
@@ -82,11 +76,12 @@ def get_expected_string(expected_module_code):
 
 def test_case1(tmp_path):
     cpp_code_block = "struct AStruct {};"
-    output = get_binded_code(tmp_path=tmp_path, cpp_code_block=cpp_code_block)
+    output = generate_bindings(tmp_path=tmp_path, cpp_code_block=cpp_code_block)
 
     expected_module_code = """
     PYBIND11_MODULE(pcl, m){
-        py::class_<AStruct>(m, "AStruct");
+        py::class_<AStruct>(m, "AStruct")
+        .def(py::init<>());
     }
     """
 
@@ -99,11 +94,12 @@ def test_case2(tmp_path):
         int aMember;
     };
     """
-    output = get_binded_code(tmp_path=tmp_path, cpp_code_block=cpp_code_block)
+    output = generate_bindings(tmp_path=tmp_path, cpp_code_block=cpp_code_block)
 
     expected_module_code = """
     PYBIND11_MODULE(pcl, m){
         py::class_<AStruct>(m, "AStruct")
+        .def(py::init<>())
         .def_readwrite("aMember", &AStruct::aMember);
     }
     """
