e88ed6f10d2d9e51239f05a2f018f5ff7e172e0e
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2020-11-11 10:48:41 +0100

    Add google benchmark for NormalEstimation

---
+++ benchmarks/features.cpp
@@ -0,0 +1,29 @@
+#include <benchmark/benchmark.h>
+#include <pcl/features/normal_3d.h> // for NormalEstimation
+#include <pcl/io/pcd_io.h> // for PCDReader
+
+static void BM_NormalEstimation(benchmark::State& state, const std::string& file) {
+  // Perform setup here
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PCDReader reader;
+  reader.read (file, *cloud);
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+  ne.setInputCloud (cloud);
+  ne.setKSearch (100);
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+  for (auto _ : state) {
+    // This code gets timed
+    ne.compute (*cloud_normals);
+  }
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2)
+  {
+    std::cerr << "No test file given. Please download `table_scene_mug_stereo_textured.pcd` and pass its path to the test." << std::endl;
+    return (-1);
+  }
+  benchmark::RegisterBenchmark("BM_NormalEstimation", BM_NormalEstimation, argv[1]);
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+}
