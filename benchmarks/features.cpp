51ef38fed3b9a081b22144b37ab6a93422330775
Lars Glud <larshg@gmail.com>
2020-11-16 11:45:38 +0100

    Add NormalEstimationOMP

--- benchmarks/features.cpp
+++ benchmarks/features.cpp
@@ -1,5 +1,8 @@
 #include <benchmark/benchmark.h>
 #include <pcl/features/normal_3d.h> // for NormalEstimation
+#ifdef _OPENMP
+  #include <pcl/features/normal_3d_omp.h> // for NormalEstimationOMP
+#endif
 #include <pcl/io/pcd_io.h> // for PCDReader
 
 static void BM_NormalEstimation(benchmark::State& state, const std::string& file) {
@@ -16,6 +19,22 @@ static void BM_NormalEstimation(benchmark::State& state, const std::string& file
     ne.compute (*cloud_normals);
   }
 }
+#ifdef _OPENMP
+static void BM_NormalEstimationOMP(benchmark::State& state, const std::string& file) {
+  // Perform setup here
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PCDReader reader;
+  reader.read(file, *cloud);
+  pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> ne;
+  ne.setInputCloud(cloud);
+  ne.setKSearch(100);
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
+  for (auto _ : state) {
+    // This code gets timed
+    ne.compute(*cloud_normals);
+  }
+}
+#endif
 
 int main(int argc, char** argv) {
   if (argc < 2)
@@ -24,6 +43,9 @@ int main(int argc, char** argv) {
     return (-1);
   }
   benchmark::RegisterBenchmark("BM_NormalEstimation", BM_NormalEstimation, argv[1])->Unit(benchmark::kMillisecond);
+#ifdef _OPENMP
+  benchmark::RegisterBenchmark("BM_NormalEstimationOMP", BM_NormalEstimationOMP, argv[1])->Unit(benchmark::kMillisecond);
+#endif
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
 }
