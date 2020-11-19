77864baef048930dadd3bb605d1417b863188322
Kunal Tyagi <tyagi.kunal@live.com>
2020-11-19 17:37:14 +0900

    Hotfix

--- common/include/pcl/point_cloud.h
+++ common/include/pcl/point_cloud.h
@@ -592,7 +592,7 @@ namespace pcl
           PCL_WARN("Mismatch in assignment. Requested width (%zu) doesn't divide "
                    "provided size (%zu) cleanly. Setting height to 1",
                    static_cast<std::size_t>(width),
-                   static_cast<std::size>(size()));
+                   static_cast<std::size_t>(size()));
           width = size();
           height = 1;
         }
