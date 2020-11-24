7e3529db04bf6986184c748901b4995f38650f59
AdamKorcz <adam@adalogics.com>
2020-11-24 11:46:26 +0000

    Changed license headers

--- test/fuzz/build.sh
+++ test/fuzz/build.sh
@@ -1,19 +1,13 @@
 #!/bin/bash -eu
-# Copyright 2020 Google Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: BSD-3-Clause
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#  Point Cloud Library (PCL) - www.pointclouds.org
+#  Copyright (c) 2020-, Open Perception
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#  All rights reserved
 #
-################################################################################
+
 
 # Build PCL
 cd pcl
