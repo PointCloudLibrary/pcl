4f06cc0993054b554fe27e4f932fc7c4302137cf
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2020-11-24 14:59:44 +0100

    Add more benchmark variants for NormalEstimation

--- benchmarks/features/normal_3d.cpp
+++ benchmarks/features/normal_3d.cpp
@@ -12,7 +12,7 @@ static void BM_NormalEstimation(benchmark::State& state, const std::string& file
   reader.read (file, *cloud);
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
   ne.setInputCloud (cloud);
-  ne.setKSearch (100);
+  ne.setKSearch (state.range(0));
   pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
   for (auto _ : state) {
     // This code gets timed
@@ -37,12 +37,13 @@ static void BM_NormalEstimationOMP(benchmark::State& state, const std::string& f
 #endif
 
 int main(int argc, char** argv) {
-  if (argc < 2)
+  if (argc < 3)
   {
-    std::cerr << "No test file given. Please download `table_scene_mug_stereo_textured.pcd` and pass its path to the test." << std::endl;
+    std::cerr << "No test files given. Please download `table_scene_mug_stereo_textured.pcd` and `milk_cartoon_all_small_clorox.pcd`, and pass their paths to the test." << std::endl;
     return (-1);
   }
-  benchmark::RegisterBenchmark("BM_NormalEstimation", BM_NormalEstimation, argv[1])->Unit(benchmark::kMillisecond);
+  benchmark::RegisterBenchmark("BM_NormalEstimation_mug", BM_NormalEstimation, argv[1])->Arg(50)->Arg(100)->Unit(benchmark::kMillisecond);
+  benchmark::RegisterBenchmark("BM_NormalEstimation_milk", BM_NormalEstimation, argv[2])->Arg(50)->Arg(100)->Unit(benchmark::kMillisecond);
 #ifdef _OPENMP
   benchmark::RegisterBenchmark("BM_NormalEstimationOMP", BM_NormalEstimationOMP, argv[1])->Unit(benchmark::kMillisecond);
 #endif
