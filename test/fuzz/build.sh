3e36075bea07f777dda601ab4b06a2f3fa0f2d56
AdamKorcz <adam@adalogics.com>
2020-12-08 16:39:37 +0000

    Removed installation of non-needed modules

--- test/fuzz/build.sh
+++ test/fuzz/build.sh
@@ -1,20 +1,53 @@
 #!/bin/bash -eu
+# Copyright 2018 Google Inc.
 #
-# SPDX-License-Identifier: BSD-3-Clause
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#  Point Cloud Library (PCL) - www.pointclouds.org
-#  Copyright (c) 2020-, Open Perception
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-#  All rights reserved
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
+################################################################################
 
+mkdir pcl/test/fuzz
+cp $SRC/read_fuzzer.cc pcl/test/fuzz/
 
 # Build PCL
 cd pcl
 mkdir build && cd build
-cmake -DWITH_OPENGL=FALSE  -DWITH_PCAP=FALSE \
-        -DWITH_VTK=FALSE -DPCL_SHARED_LIBS:BOOL=OFF ..
-make
+pwd
+cmake -DWITH_OPENGL=FALSE \
+      -DWITH_PCAP=FALSE \
+      -DWITH_VTK=FALSE \
+      -DPCL_SHARED_LIBS:BOOL=OFF \
+      -DWITH_LIBUSB=FALSE \
+      -DWITH_QT=FALSE \
+      -DBUILD_features=OFF \
+      -DBUILD_filters=OFF \
+      -DBUILD_geometry=OFF \
+      -DBUILD_kdtree=OFF \
+      -DBUILD_keypoints=OFF \
+      -DBUILD_ml=OFF \
+      -DBUILD_outofcore=OFF \
+      -DBUILD_people=OFF \
+      -DBUILD_recognition=OFF \
+      -DBUILD_registration=OFF \
+      -DBUILD_sample_consensus=OFF \
+      -DBUILD_search=OFF \
+      -DBUILD_segmentation=OFF \
+      -DBUILD_stereo=OFF \
+      -DBUILD_surface=OFF \
+      -DBUILD_tracking=OFF \
+      -DBUILD_visualization=OFF \
+      ..
+
+make -j$(nproc)
 
 # Build fuzzers
 cd ../test/fuzz
