8af2f4d41a51d26fb43b3dde9a0aac8da86ca664
Heiko Thiel <heiko.thiel@hpi.de>
2020-12-21 20:23:58 +0100

    Use UNKNOWN IMPORTED instead of INTERFACE IMPORTED, as we targeting to a artifact in case we get values not from PkgConfig. Therefore PkgConfig is only used to get a hint where to find libusb, but ignoring compiler flags.

--- cmake/Modules/Findlibusb.cmake
+++ cmake/Modules/Findlibusb.cmake
@@ -41,35 +41,33 @@ endif()
 
 find_package(PkgConfig QUIET)
 if(libusb_FIND_VERSION)
-  pkg_check_modules(PC_LIBUSB libusb-1.0>=${libusb_FIND_VERSION})
+  pkg_check_modules(PC_libusb libusb-1.0>=${libusb_FIND_VERSION})
 else()
-  pkg_check_modules(PC_LIBUSB libusb-1.0)
+  pkg_check_modules(PC_libusb libusb-1.0)
 endif()
 
-if(PC_LIBUSB_FOUND)
-  set(LIBUSB_INCLUDE_DIR ${PC_LIBUSB_INCLUDEDIR})
-  set(LIBUSB_LIBRARIES ${PC_LIBUSB_LIBRARIES})
-else()
-  find_path(LIBUSB_INCLUDE_DIR
-    NAMES
-      libusb-1.0/libusb.h
-  )
-  find_library(LIBUSB_LIBRARIES
-    NAMES
-      usb-1.0
-      libusb
-  )
-endif()
+find_path(libusb_INCLUDE_DIR
+  NAMES
+    libusb-1.0/libusb.h
+  PATHS
+    ${PC_libusb_INCLUDEDIR}
+)
+
+find_library(libusb_LIBRARIES
+  NAMES
+    usb-1.0
+    libusb
+  PATHS
+    ${PC_libusb_LIBRARY_DIRS}
+)
   
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LIBUSB DEFAULT_MSG LIBUSB_LIBRARIES LIBUSB_INCLUDE_DIR)
+find_package_handle_standard_args(libusb DEFAULT_MSG libusb_LIBRARIES libusb_INCLUDE_DIR)
 
-mark_as_advanced(LIBUSB_INCLUDE_DIRS LIBUSB_LIBRARIES)
+mark_as_advanced(libusb_INCLUDE_DIRS libusb_LIBRARIES)
 
 if(LIBUSB_FOUND)
-  add_library(libusb::libusb INTERFACE IMPORTED)
-  set_target_properties(libusb::libusb PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LIBUSB_INCLUDE_DIR}")
-  set_target_properties(libusb::libusb PROPERTIES INTERFACE_COMPILE_DEFINITIONS "${PC_LIBUSB_CFLAGS_OTHER}")
-  set_target_properties(libusb::libusb PROPERTIES INTERFACE_LINK_OPTIONS "${PC_LIBUSB_LDFLAGS_OTHER}")
-  set_target_properties(libusb::libusb PROPERTIES INTERFACE_LINK_LIBRARIES "${LIBUSB_LIBRARIES}")
+  add_library(libusb::libusb UNKNOWN IMPORTED)
+  set_target_properties(libusb::libusb PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${libusb_INCLUDE_DIR}")
+  set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION "${libusb_LIBRARIES}")
 endif()
