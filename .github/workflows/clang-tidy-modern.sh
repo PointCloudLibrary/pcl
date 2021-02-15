c4b5911c3b91de2c37c7cd319048568109aa6f80
Norm Evangelista <norm.evangelista@gmail.com>
2021-02-15 09:34:34 -0800

    Made changes to scripts per review [PCL-2731]
    Signed-off-by: Norm Evangelista <norm.evangelista@gmail.com>

--- .github/workflows/clang-tidy-modern.sh
+++ .github/workflows/clang-tidy-modern.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# kiss_fft ignored for now
+# Will be used in future PR as clang-tidy is applied up the PCL dependency graph
+# Tracked in issue #2731
 run-clang-tidy-10.py -config='' -header-filter='^((?!.*kiss_fft\.h.*).)*$,.*' -fix
