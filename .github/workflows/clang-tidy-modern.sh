da1cfd686872d0073b8009e1570db6d8964b1d97
Norm Evangelista <norm.evangelista@gmail.com>
2021-02-17 08:27:13 -0800

    Removed GitHub workflows for clang-tidy by request [PCL-2731]
    Signed-off-by: Norm Evangelista <norm.evangelista@gmail.com>

--- .github/workflows/clang-tidy-modern.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# kiss_fft ignored for now
-# Will be used in future PR as clang-tidy is applied up the PCL dependency graph
-# Tracked in issue #2731
-run-clang-tidy-10.py -config='' -header-filter='^((?!.*kiss_fft\.h.*).)*$,.*' -fix
