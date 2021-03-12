4bc502b98831cbe66119f2b9c9d10c4fc2e2f9e8
Norm Evangelista <norm.evangelista@gmail.com>
2021-03-12 08:21:36 -0800

    Reverted clang-tidy changes for C header [PCL-2731]
    Signed-off-by: Norm Evangelista <norm.evangelista@gmail.com>

--- outofcore/include/pcl/outofcore/cJSON.h
+++ outofcore/include/pcl/outofcore/cJSON.h
@@ -44,37 +44,23 @@ extern "C"
 #define cJSON_IsReference 256
 
 /* The cJSON structure: */
-using cJSON = struct cJSON {
-
+typedef struct cJSON {
 	struct cJSON *next,*prev;	/* next/prev allow you to walk array/object chains. Alternatively, use GetArraySize/GetArrayItem/GetObjectItem */
-
 	struct cJSON *child;		/* An array or object item will have a child pointer pointing to a chain of the items in the array/object. */
-
-
 
 	int type;					/* The type of the item, as above. */
 
-
-
 	char *valuestring;			/* The item's string, if type==cJSON_String */
-
 	int valueint;				/* The item's number, if type==cJSON_Number */
-
 	double valuedouble;			/* The item's number, if type==cJSON_Number */
 
-
-
 	char *string;				/* The item's name string, if this item is the child of, or is in the list of subitems of an object. */
+} cJSON;
 
-};
-
-using cJSON_Hooks = struct cJSON_Hooks {
-
+typedef struct cJSON_Hooks {
       void *(*malloc_fn)(std::size_t sz);
-
       void (*free_fn)(void *ptr);
-
-};
+} cJSON_Hooks;
 
 /* Supply malloc, realloc and free functions to cJSON */
 PCLAPI(void) cJSON_InitHooks(cJSON_Hooks* hooks);
