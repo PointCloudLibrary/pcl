6b928dc4492917b4c8d5662f7c216ca00f25281f
AdamKorcz <adam@adalogics.com>
2021-03-15 13:30:26 +0000

    Spaces instead of tabs

--- test/fuzz/ply_reader_fuzzer.cpp
+++ test/fuzz/ply_reader_fuzzer.cpp
@@ -6,18 +6,18 @@
 #include <pcl/point_types.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-	pcl::PCLPointCloud2 cloud_blob;
-	pcl::PLYReader reader;
-  	char filename[256];
-	sprintf(filename, "/tmp/libfuzzer.%d", getpid());
+    pcl::PCLPointCloud2 cloud_blob;
+    pcl::PLYReader reader;
+    char filename[256];
+    sprintf(filename, "/tmp/libfuzzer.%d", getpid());
 
-	FILE *fp = fopen(filename, "wb");
-	if (!fp)
-		return 0;
-	fwrite(data, size, 1, fp);
-	fclose(fp);
+    FILE *fp = fopen(filename, "wb");
+    if (!fp)
+        return 0;
+    fwrite(data, size, 1, fp);
+    fclose(fp);
 
-	reader.read (filename, cloud_blob);	
-	unlink(filename);
-	return 0;
+    reader.read (filename, cloud_blob); 
+    unlink(filename);
+    return 0;
 }
