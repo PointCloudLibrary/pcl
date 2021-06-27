9072aee26075bf02ea6e01b43dc225aee2f923dc
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2021-06-27 10:19:14 +0200

    Improve formatting

--- benchmarks/features/normal_3d.cpp
+++ benchmarks/features/normal_3d.cpp
@@ -1,26 +1,30 @@
-#include <benchmark/benchmark.h>
-#include <pcl/features/normal_3d.h> // for NormalEstimation
-#ifdef _OPENMP
-  #include <pcl/features/normal_3d_omp.h> // for NormalEstimationOMP
-#endif
-#include <pcl/io/pcd_io.h> // for PCDReader
+#include <pcl/features/normal_3d.h>     // for NormalEstimation
+#include <pcl/features/normal_3d_omp.h> // for NormalEstimationOMP
+#include <pcl/io/pcd_io.h>              // for PCDReader
 
-static void BM_NormalEstimation(benchmark::State& state, const std::string& file) {
+#include <benchmark/benchmark.h>
+
+static void
+BM_NormalEstimation(benchmark::State& state, const std::string& file)
+{
   // Perform setup here
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PCDReader reader;
-  reader.read (file, *cloud);
+  reader.read(file, *cloud);
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-  ne.setInputCloud (cloud);
-  ne.setKSearch (state.range(0));
-  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+  ne.setInputCloud(cloud);
+  ne.setKSearch(state.range(0));
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
   for (auto _ : state) {
     // This code gets timed
-    ne.compute (*cloud_normals);
+    ne.compute(*cloud_normals);
   }
 }
+
 #ifdef _OPENMP
-static void BM_NormalEstimationOMP(benchmark::State& state, const std::string& file) {
+static void
+BM_NormalEstimationOMP(benchmark::State& state, const std::string& file)
+{
   // Perform setup here
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PCDReader reader;
@@ -36,16 +40,29 @@ static void BM_NormalEstimationOMP(benchmark::State& state, const std::string& f
 }
 #endif
 
-int main(int argc, char** argv) {
-  if (argc < 3)
-  {
-    std::cerr << "No test files given. Please download `table_scene_mug_stereo_textured.pcd` and `milk_cartoon_all_small_clorox.pcd`, and pass their paths to the test." << std::endl;
+int
+main(int argc, char** argv)
+{
+  if (argc < 3) {
+    std::cerr
+        << "No test files given. Please download `table_scene_mug_stereo_textured.pcd` "
+           "and `milk_cartoon_all_small_clorox.pcd`, and pass their paths to the test."
+        << std::endl;
     return (-1);
   }
-  benchmark::RegisterBenchmark("BM_NormalEstimation_mug", &BM_NormalEstimation, argv[1])->Arg(50)->Arg(100)->Unit(benchmark::kMillisecond);
-  benchmark::RegisterBenchmark("BM_NormalEstimation_milk", &BM_NormalEstimation, argv[2])->Arg(50)->Arg(100)->Unit(benchmark::kMillisecond);
+  benchmark::RegisterBenchmark("BM_NormalEstimation_mug", &BM_NormalEstimation, argv[1])
+      ->Arg(50)
+      ->Arg(100)
+      ->Unit(benchmark::kMillisecond);
+  benchmark::RegisterBenchmark(
+      "BM_NormalEstimation_milk", &BM_NormalEstimation, argv[2])
+      ->Arg(50)
+      ->Arg(100)
+      ->Unit(benchmark::kMillisecond);
 #ifdef _OPENMP
-  benchmark::RegisterBenchmark("BM_NormalEstimationOMP", &BM_NormalEstimationOMP, argv[1])->Unit(benchmark::kMillisecond);
+  benchmark::RegisterBenchmark(
+      "BM_NormalEstimationOMP", &BM_NormalEstimationOMP, argv[1])
+      ->Unit(benchmark::kMillisecond);
 #endif
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
