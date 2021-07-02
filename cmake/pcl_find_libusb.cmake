c8249f3e41f79ca6e4273f6ca16d25dadc6a01a4
Lars Glud <larshg@gmail.com>
2021-07-02 17:44:13 +0200

    Additional commets about VCPKG.

--- cmake/pcl_find_libusb.cmake
+++ cmake/pcl_find_libusb.cmake
@@ -1,8 +1,13 @@
+#
+#pcl_find_libusb is used due to VCPKG making impossible to use local findXX modules.
+# and VCPKG findlibusb doesn't create the libusb targets.
+
 # Find and set libusb
 find_package(libusb)
 
 if(TARGET libusb::libusb)
-  #libusb target is found by the find_package script. VCPKG skip PCLs findlibusb and sets its own variables which is handled below.
+  #libusb target is found by the find_package script. 
+  #VCPKG skip PCLs findlibusb and sets its own variables which is handled below.
   return()
 endif()
 
@@ -16,13 +21,19 @@ if(libusb_FOUND)
   add_library(libusb::libusb UNKNOWN IMPORTED)
   set_target_properties(libusb::libusb PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LIBUSB_INCLUDE_DIRS}")
   
-  foreach(LIBRARY_CONFIG ${LIBUSB_LIBRARIES})
-   if(LIBRARY_CONFIG MATCHES "^(debug|optimized)$")
-     set(_CONF ${LIBRARY_CONFIG})
-   elseif(_CONF MATCHES "^debug$")
-     set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION_DEBUG "${LIBRARY_CONFIG}")
-   else()
-     set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION "${LIBRARY_CONFIG}")
-   endif()
- endforeach()
+  if(LINUX)
+    set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION "${libusb_LIBRARIES}")
+  endif()
+  
+  if(WIN32)
+    foreach(LIBRARY_CONFIG ${LIBUSB_LIBRARIES})
+     if(LIBRARY_CONFIG MATCHES "^(debug|optimized)$")
+       set(_CONF ${LIBRARY_CONFIG})
+     elseif(_CONF MATCHES "^debug$")
+       set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION_DEBUG "${LIBRARY_CONFIG}")
+     else()
+       set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION "${LIBRARY_CONFIG}")
+     endif()
+    endforeach()
+  endif()
 endif()
