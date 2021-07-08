b382fb230c19ea4c6b81641c6f9360022df1e003
tin1254 <tin-1254@hotmail.com>
2021-07-08 02:26:44 +0200

    Speed up new VoxelGrid by caching options

--- filters/include/pcl/filters/experimental/transform_filter.h
+++ filters/include/pcl/filters/experimental/transform_filter.h
@@ -171,6 +171,7 @@ protected:
       return;
     }
 
+    output.clear();
     output.height = 1;      // downsampling breaks the organized structure
     output.is_dense = true; // we filter out invalid points
 
