d7e50b08c804f4c3733fc098c2ab23e93e23dc3b
tin1254 <tin-1254@hotmail.com>
2021-07-08 13:23:56 +0200

    Add benchmark for new VoxelGrid

--- benchmarks/filters/voxel_grid.cpp
+++ benchmarks/filters/voxel_grid.cpp
@@ -1,4 +1,5 @@
 #include <pcl/filters/approximate_voxel_grid.h>
+#include <pcl/filters/experimental/voxel_grid.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/io/pcd_io.h> // for PCDReader
 
@@ -44,6 +45,26 @@ BM_ApproxVoxelGrid(benchmark::State& state, const std::string& file)
   }
 }
 
+static void
+BM_Experimental_VoxelGrid(benchmark::State& state, const std::string& file)
+{
+  // Perform setup here
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PCDReader reader;
+  reader.read(file, *cloud);
+
+  pcl::experimental::VoxelGrid<pcl::PointXYZ> vg;
+  vg.setLeafSize(0.01, 0.01, 0.01);
+  vg.setInputCloud(cloud);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxelized(
+      new pcl::PointCloud<pcl::PointXYZ>);
+  for (auto _ : state) {
+    // This code gets timed
+    vg.filter(*cloud_voxelized);
+  }
+}
+
 int
 main(int argc, char** argv)
 {
@@ -60,11 +81,17 @@ main(int argc, char** argv)
   benchmark::RegisterBenchmark(
       "BM_ApproximateVoxelGrid_milk", &BM_ApproxVoxelGrid, argv[2])
       ->Unit(benchmark::kMillisecond);
+  benchmark::RegisterBenchmark(
+      "BM_Experimental_VoxelGrid_milk", &BM_Experimental_VoxelGrid, argv[2])
+      ->Unit(benchmark::kMillisecond);
 
   benchmark::RegisterBenchmark("BM_VoxelGrid_mug", &BM_VoxelGrid, argv[1])
       ->Unit(benchmark::kMillisecond);
   benchmark::RegisterBenchmark(
       "BM_ApproximateVoxelGrid_mug", &BM_ApproxVoxelGrid, argv[1])
+      ->Unit(benchmark::kMillisecond);
+  benchmark::RegisterBenchmark(
+      "BM_Experimental_VoxelGrid_mug", &BM_Experimental_VoxelGrid, argv[1])
       ->Unit(benchmark::kMillisecond);
 
   benchmark::Initialize(&argc, argv);
