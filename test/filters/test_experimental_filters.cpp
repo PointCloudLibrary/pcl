8800b44f4d9cd9c7d635fa6926f6eda1577c3202
tin1254 <tin-1254@hotmail.com>
2021-08-11 02:27:22 +0200

    Implement PassThrough with FunctorFilter
    * Implement experimental::PassThrough
    
    * Copy set up the same PassThrough unit tests

--- test/filters/test_experimental_filters.cpp
+++ test/filters/test_experimental_filters.cpp
@@ -9,8 +9,8 @@
 
 #include <pcl/common/eigen.h>
 #include <pcl/filters/experimental/crop_box.h>
-#include <pcl/filters/crop_box.h>
-#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/experimental/passthrough.h>
+#include <pcl/io/pcd_io.h> // for loadPCDFile
 #include <pcl/test/gtest.h>
 #include <pcl/pcl_base.h> // for pcl::Indices
 #include <pcl/pcl_tests.h>
@@ -18,6 +18,8 @@
 
 using namespace pcl;
 using namespace Eigen;
+
+PointCloud<PointXYZ>::Ptr cloud(new PointCloud<PointXYZ>);
 
 TEST(ExperimentalCropBox, Filters)
 {
@@ -373,9 +375,148 @@ TEST(ExperimentalCropBox, Filters)
   EXPECT_VECTOR_DOES_NOT_CONTAIN(pcl::Indices({0, 4}), indices);
 }
 
+TEST(ExperimentalPassThrough, Filters)
+{
+  // Test the PointCloud<PointT> method
+  PointCloud<PointXYZ> output;
+  experimental::PassThrough<PointXYZ> pt;
+
+  pt.setInputCloud(cloud);
+  pt.filter(output);
+
+  EXPECT_EQ(output.size(), cloud->size());
+  EXPECT_EQ(output.width, cloud->width);
+  EXPECT_EQ(output.height, cloud->height);
+
+  pt.setFilterFieldName("z");
+  pt.setFilterLimits(0.05f, 0.1f);
+  pt.filter(output);
+
+  EXPECT_EQ(output.size(), 42);
+  EXPECT_EQ(output.width, 42);
+  EXPECT_EQ(output.height, 1);
+  EXPECT_TRUE(output.is_dense);
+
+  EXPECT_NEAR(output[0].x, -0.074556, 1e-5);
+  EXPECT_NEAR(output[0].y, 0.13415, 1e-5);
+  EXPECT_NEAR(output[0].z, 0.051046, 1e-5);
+
+  EXPECT_NEAR(output[41].x, -0.030331, 1e-5);
+  EXPECT_NEAR(output[41].y, 0.039749, 1e-5);
+  EXPECT_NEAR(output[41].z, 0.052133, 1e-5);
+
+  pt.setNegative(true);
+  pt.filter(output);
+
+  EXPECT_EQ(output.size(), 355);
+  EXPECT_EQ(output.width, 355);
+  EXPECT_EQ(output.height, 1);
+  EXPECT_TRUE(output.is_dense);
+
+  EXPECT_NEAR(output[0].x, 0.0054216, 1e-5);
+  EXPECT_NEAR(output[0].y, 0.11349, 1e-5);
+  EXPECT_NEAR(output[0].z, 0.040749, 1e-5);
+
+  EXPECT_NEAR(output[354].x, -0.07793, 1e-5);
+  EXPECT_NEAR(output[354].y, 0.17516, 1e-5);
+  EXPECT_NEAR(output[354].z, -0.0444, 1e-5);
+
+  experimental::PassThrough<PointXYZ> pt_(true);
+
+  pt_.setInputCloud(cloud);
+  pt_.filter(output);
+
+  EXPECT_EQ(pt_.getRemovedIndices()->size(), 0);
+  EXPECT_EQ(output.size(), cloud->size());
+  EXPECT_EQ(output.width, cloud->width);
+  EXPECT_EQ(output.height, cloud->height);
+
+  pt_.setFilterFieldName("z");
+  pt_.setFilterLimits(0.05f, 0.1f);
+  pt_.filter(output);
+
+  EXPECT_EQ(output.size(), 42);
+  EXPECT_EQ(output.width, 42);
+  EXPECT_EQ(output.height, 1);
+  EXPECT_TRUE(output.is_dense);
+  EXPECT_EQ(output.size(), cloud->size() - pt_.getRemovedIndices()->size());
+
+  EXPECT_NEAR(output[0].x, -0.074556, 1e-5);
+  EXPECT_NEAR(output[0].y, 0.13415, 1e-5);
+  EXPECT_NEAR(output[0].z, 0.051046, 1e-5);
+
+  EXPECT_NEAR(output[41].x, -0.030331, 1e-5);
+  EXPECT_NEAR(output[41].y, 0.039749, 1e-5);
+  EXPECT_NEAR(output[41].z, 0.052133, 1e-5);
+
+  pt_.setNegative(true);
+  pt_.filter(output);
+
+  EXPECT_EQ(output.size(), 355);
+  EXPECT_EQ(output.width, 355);
+  EXPECT_EQ(output.height, 1);
+  EXPECT_TRUE(output.is_dense);
+  EXPECT_EQ(output.size(), cloud->size() - pt_.getRemovedIndices()->size());
+
+  EXPECT_NEAR(output[0].x, 0.0054216, 1e-5);
+  EXPECT_NEAR(output[0].y, 0.11349, 1e-5);
+  EXPECT_NEAR(output[0].z, 0.040749, 1e-5);
+
+  EXPECT_NEAR(output[354].x, -0.07793, 1e-5);
+  EXPECT_NEAR(output[354].y, 0.17516, 1e-5);
+  EXPECT_NEAR(output[354].z, -0.0444, 1e-5);
+
+  // Test the keep organized structure
+  pt.setUserFilterValue(std::numeric_limits<float>::quiet_NaN());
+  pt.setFilterFieldName("");
+  pt.filter(output);
+
+  EXPECT_EQ(output.size(), cloud->size());
+  EXPECT_EQ(output.width, cloud->width);
+  EXPECT_EQ(output.height, cloud->height);
+  EXPECT_EQ(output.is_dense, cloud->is_dense);
+  EXPECT_NEAR(output[0].x, (*cloud)[0].x, 1e-5);
+  EXPECT_NEAR(output[output.size() - 1].x, (*cloud)[cloud->size() - 1].x, 1e-5);
+
+  pt.setFilterFieldName("z");
+  pt.setNegative(false);
+  pt.setKeepOrganized(true);
+  pt.filter(output);
+
+  EXPECT_EQ(output.size(), cloud->size());
+  EXPECT_EQ(output.width, cloud->width);
+  EXPECT_EQ(output.height, cloud->height);
+  EXPECT_FALSE(output.is_dense); // NaN was set as a user filter value
+
+  EXPECT_TRUE(std::isnan(output[0].x));
+  EXPECT_TRUE(std::isnan(output[0].y));
+  EXPECT_TRUE(std::isnan(output[0].z));
+  EXPECT_TRUE(std::isnan(output[41].x));
+  EXPECT_TRUE(std::isnan(output[41].y));
+  EXPECT_TRUE(std::isnan(output[41].z));
+
+  pt.setNegative(true);
+  pt.filter(output);
+
+  EXPECT_EQ(output.size(), cloud->size());
+  EXPECT_EQ(output.width, cloud->width);
+  EXPECT_EQ(output.height, cloud->height);
+  EXPECT_FALSE(output.is_dense); // NaN was set as a user filter value
+
+  EXPECT_NEAR(output[0].x, (*cloud)[0].x, 1e-5);
+  EXPECT_NEAR(output[0].y, (*cloud)[0].y, 1e-5);
+  EXPECT_NEAR(output[0].z, (*cloud)[0].z, 1e-5);
+
+  EXPECT_NEAR(output[41].x, (*cloud)[41].x, 1e-5);
+  EXPECT_NEAR(output[41].y, (*cloud)[41].y, 1e-5);
+  EXPECT_NEAR(output[41].z, (*cloud)[41].z, 1e-5);
+}
+
 int
 main(int argc, char** argv)
 {
+  io::loadPCDFile(argv[1], *cloud);
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
