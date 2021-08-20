3e34a6322856e2f0a55ddad59d1456d26c1ce97f
tin1254 <tin-1254@hotmail.com>
2021-08-20 19:26:19 +0200

    Implement benchmark for functor PassThrough

---
+++ benchmarks/filters/passthrough.cpp
@@ -0,0 +1,92 @@
+
+#include <pcl/filters/experimental/passthrough.h>
+#include <pcl/filters/passthrough.h>
+
+#include <benchmark/benchmark.h>
+
+#include <array>
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace pcl;
+
+template <typename PointT, template <typename> class Filter>
+static void
+BM_passthrough(benchmark::State& state, const typename PointCloud<PointT>::Ptr cloud)
+{
+  Filter<PointT> filter;
+  typename pcl::PointCloud<PointT> out_cloud;
+
+  filter.setFilterLimits(-10., 10.);
+  filter.setInputCloud(cloud);
+  filter.setFilterFieldName("x");
+
+  for (auto _ : state)
+    filter.filter(out_cloud);
+}
+
+template <typename PointT, template <typename> class T>
+void
+BM_register(const std::string& class_name,
+            const typename PointCloud<PointT>::Ptr dataset,
+            const std::string& dataset_name,
+            const benchmark::TimeUnit unit)
+{
+  const std::string bm_name = dataset_name + "_" + class_name;
+  benchmark::RegisterBenchmark(bm_name.c_str(), &BM_passthrough<PointT, T>, dataset)
+      ->Unit(unit);
+};
+
+template <typename PointT, template <typename> class... Ts, std::size_t ArraySize>
+bool
+BM_registration(const std::array<std::string, ArraySize>& classes_name,
+                const std::vector<typename PointCloud<PointT>::Ptr>& datasets,
+                const std::vector<std::string>& datasets_name,
+                const benchmark::TimeUnit unit = benchmark::kMillisecond)
+{
+  static_assert(sizeof...(Ts) == ArraySize,
+                "Number of template classes and classes name are different.\n");
+
+  if (datasets.size() != datasets_name.size()) {
+    PCL_ERROR("[Benchmark] Number of datasets and datasets name are different.\n");
+    return false;
+  }
+
+  for (std::size_t i = 0; i < datasets.size(); ++i) {
+    typename std::array<std::string, ArraySize>::const_iterator name_it =
+        classes_name.begin();
+    const int res[] = {(BM_register<PointT, Ts>(
+                            *(name_it++), datasets.at(i), datasets_name.at(i), unit),
+                        0)...};
+    (void)res; // suppress warning
+  }
+
+  return true;
+}
+
+int
+main(int argc, char** argv)
+{
+  std::vector<PointCloud<PointXYZ>::Ptr> datasets;
+  std::vector<std::string> datasets_name;
+  for (std::size_t i = 2; i <= 5; ++i) {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    const std::size_t num_pt = std::pow(8, i);
+    cloud->resize(num_pt);
+    for (auto& pt : *cloud)
+      pt = pcl::PointXYZ{0., 0., 0.};
+
+    datasets.push_back(std::move(cloud));
+    datasets_name.push_back(std::to_string(num_pt));
+  }
+  const std::array<std::string, 2> filters_name{"PassThrough", "FunctorPassThrough"};
+
+  if (BM_registration<PointXYZ, PassThrough, experimental::PassThrough>(
+          filters_name, datasets, datasets_name, benchmark::kMicrosecond)) {
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+  }
+
+  return 0;
+}
