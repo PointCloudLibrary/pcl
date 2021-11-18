abd2f8fca3acad28634bc16450357baaaecee70d
herrwusel <herrwusel@users.noreply.github.com>
2021-11-18 21:22:32 +0100

    PLY parser: accept files with whitespace at line end (#5033)
    * PLY parser: remove undesired whitespace
    
    If element in the header contains whitespaces after the count, remove
    them.
    
    * Consume whitespaces in ply headers
    
    If stream eof is not true, try to consume whitespaces
    
    * Fix indentation

--- io/src/ply/ply_parser.cpp
+++ io/src/ply/ply_parser.cpp
@@ -113,6 +113,11 @@ bool pcl::io::ply::ply_parser::parse (const std::string& filename)
       std::string format_string, version;
       char space_format_format_string, space_format_string_version;
       stringstream >> space_format_format_string >> std::ws >> format_string >> space_format_string_version >> std::ws >> version;
+      if (!stringstream.eof ())
+      {
+        stringstream >> std::ws;
+        warning_callback_ (line_number_, "parse warning: trailing whitespaces in the header");
+      }
       if (!stringstream ||
           !stringstream.eof () ||
           !isspace (space_format_format_string) ||
@@ -158,6 +163,11 @@ bool pcl::io::ply::ply_parser::parse (const std::string& filename)
       std::size_t count;
       char space_element_name, space_name_count;
       stringstream >> space_element_name >> std::ws >> name >> space_name_count >> std::ws >> count;
+      if (!stringstream.eof ())
+      {
+        stringstream >> std::ws;
+        warning_callback_ (line_number_, "parse warning: trailing whitespaces in the header");
+      }
       if (!stringstream ||
           !stringstream.eof () ||
           !isspace (space_element_name) ||
