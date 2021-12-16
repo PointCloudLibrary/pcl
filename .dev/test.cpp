3209938ff912049274ce8a552ad0c426a0269b07
Kunal Tyagi <tyagi.kunal@live.com>
2021-12-16 16:44:51 +0900

    First working model

---
+++ .dev/test.cpp
@@ -0,0 +1,25 @@
+#if 0
+/**
+ * @file test.cpp
+ * @author your name (you@domain.com)
+ * @brief Something @details Very very long here is alright with this
+ * @version 0.1
+ * @date 2021-12-16
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#endif
+
+int x = 0;
+/** @brief @cite
+ */
+
+namespace {}
+namespace {
+/**
+ * @brief \code
+ *
+ */
+class A {};
+} // namespace
