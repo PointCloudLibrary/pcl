72f642aa43e8c5d3814170c36f9a710bcfb73d0e
Lars Glud <larshg@gmail.com>
2022-02-04 22:05:47 +0100

    Fix VS2022 error C3052: 'k': variable doesn't appear in a data-sharing clause under a default(none) clause

--- common/src/fft/kiss_fft.c
+++ common/src/fft/kiss_fft.c
@@ -260,11 +260,13 @@ void kf_work(
 #if (defined _OPENMP && (_OPENMP <= 201307)) || (defined __GNUC__ && (__GNUC__ >= 6 && __GNUC__ < 9))
 #pragma omp parallel for \
   default(none) \
-  shared(f, factors, Fout, in_stride)
+  shared(f, factors, Fout, in_stride) \
+  private(k)
 #else
 #pragma omp parallel for \
   default(none) \
-  shared(f, factors, Fout, fstride, in_stride, m, p, st)
+  shared(f, factors, Fout, fstride, in_stride, m, p, st) \
+  private(k)
 #endif
         for (k=0;k<p;++k) 
             kf_work( Fout +k*m, f+ fstride*in_stride*k,fstride*p,in_stride,factors,st);
