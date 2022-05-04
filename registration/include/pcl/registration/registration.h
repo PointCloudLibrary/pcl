1218a4e314601cc87c36d8daad46313c206e6a14
keineahnung2345 <mimifasosofamire1123@gmail.com>
2022-05-04 10:54:12 +0800

    [fix] in RegistrationVisualizerMine::runDisplay, cloud_source_, cloud_target_, cloud_intermediate_ are empty

--- registration/include/pcl/registration/registration.h
+++ registration/include/pcl/registration/registration.h
@@ -446,6 +446,8 @@ public:
   {
     if (visualizerCallback) {
       update_visualizer_ = visualizerCallback;
+      pcl::Indices indices;
+      update_visualizer_(*input_, indices, *target_, indices );
       return (true);
     }
     return (false);
