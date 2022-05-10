73094ebe1b621690c9b7d9c79b6b31613591b3ed
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2022-05-10 20:48:24 +0200

    Fix problem with spin() and spinOnce() for X Window System Due to a problem in VTK (versions 9.0.2, 9.0.3, and 9.1.0), the Start() function of the vtkXRenderWindowInteractor crashes when Terminate() is called. This leads to spin() and spinOnce() crashing. This commit adds a fixed version of the vtkXRenderWindowInteractor that is used for the previously mentioned VTK versions. Additionally, it is necessary to change spinOnce() from Start()+timer+Terminate() to ProcessEvents()+sleep since Terminate() closes the window in future VTK versions (and the fixed vtkXRenderWindowInteractor). The changes are limited to the affected versions and the X Window System to avoid regressions for e.g. Windows and MacOS

--- visualization/src/vtk/vtkRenderWindowInteractorFix.cpp
+++ visualization/src/vtk/vtkRenderWindowInteractorFix.cpp
@@ -34,12 +34,27 @@
   *  POSSIBILITY OF SUCH DAMAGE.
   *
   */
+#include <pcl/console/print.h> // for PCL_DEBUG
 #include <pcl/visualization/vtk/vtkRenderWindowInteractorFix.h>
+#include <pcl/visualization/vtk/vtkFixedXRenderWindowInteractor.h>
+
+#include <vtkVersion.h>
 
 #ifndef __APPLE__
 vtkRenderWindowInteractor* vtkRenderWindowInteractorFixNew ()
 {
+#if VTK_MAJOR_VERSION == 9 && ((VTK_MINOR_VERSION == 0 && (VTK_BUILD_VERSION == 2 || VTK_BUILD_VERSION == 3)) || (VTK_MINOR_VERSION == 1 && VTK_BUILD_VERSION == 0))
+// VTK versions 9.0.2, 9.0.3, 9.1.0
+  vtkRenderWindowInteractor* interactor = vtkRenderWindowInteractor::New ();
+  if(interactor->IsA("vtkXRenderWindowInteractor")) {
+    PCL_DEBUG("Using a fixed version of the vtkXRenderWindowInteractor!\n");
+    interactor->Delete ();
+    interactor = pcl::vtkXRenderWindowInteractor::New ();
+  }
+  return (interactor);
+#else
   return (vtkRenderWindowInteractor::New ());
+#endif
 }
 #endif
 
