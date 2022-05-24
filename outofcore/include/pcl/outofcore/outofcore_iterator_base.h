ff08c5412c62e9a049cd53ed63f299a1d0437ad5
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2022-05-24 20:48:18 +0200

    Remove std::iterator, deprecated in C++17

--- outofcore/include/pcl/outofcore/outofcore_iterator_base.h
+++ outofcore/include/pcl/outofcore/outofcore_iterator_base.h
@@ -56,13 +56,15 @@ namespace pcl
      *  \author Stephen Fox (foxstephend@gmail.com)
      */
     template<typename PointT, typename ContainerT>
-    class OutofcoreIteratorBase : public std::iterator<std::forward_iterator_tag,     /*iterator type*/
-                                                      const OutofcoreOctreeBaseNode<ContainerT, PointT>,
-                                                       void,  /*no defined distance between iterator*/
-                                                       const OutofcoreOctreeBaseNode<ContainerT, PointT>*,/*Pointer type*/
-                                                       const OutofcoreOctreeBaseNode<ContainerT, PointT>&>/*Reference type*/
+    class OutofcoreIteratorBase
     {
       public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = const OutofcoreOctreeBaseNode<ContainerT, PointT>;
+        using difference_type = void;
+        using pointer = const OutofcoreOctreeBaseNode<ContainerT, PointT>*;
+        using reference = const OutofcoreOctreeBaseNode<ContainerT, PointT>&;
+
         using OctreeDisk = pcl::outofcore::OutofcoreOctreeBase<ContainerT, PointT>;
         using OctreeDiskNode = pcl::outofcore::OutofcoreOctreeBaseNode<ContainerT, PointT>;
         
