725fed976d3770de8b56d097728eb2873d4dfdd5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2022-08-05 16:05:33 +0800

    [octree] make addPointIndex, getPointIndex, getPointIndices virtual (#5360)
    * [refactor] OctreeContainerBase: make addPointIndex, getPointIndex, getPointIndices virtual
    
    * clang-format
    
    * [fix] error: 'pcl::octree::OctreePointCloudDensityContainer::addPointIndex' hides overloaded virtual function [-Werror,-Woverloaded-virtual]
    
    * add back non-virtual getPointIndex

--- octree/include/pcl/octree/octree_container.h
+++ octree/include/pcl/octree/octree_container.h
@@ -89,9 +89,7 @@ public:
   /** \brief Empty addPointIndex implementation. This leaf node does not store any point
    * indices.
    */
-  void
-  addPointIndex(const index_t&)
-  {}
+  virtual void addPointIndex(index_t) {}
 
   /** \brief Empty getPointIndex implementation as this leaf node does not store any
    * point indices.
@@ -100,10 +98,20 @@ public:
   getPointIndex(index_t&) const
   {}
 
+  /** \brief Empty getPointIndex implementation as this leaf node does not store any
+   * point indices.
+   */
+  virtual index_t
+  getPointIndex() const
+  {
+    assert("getPointIndex: undefined point index");
+    return -1;
+  }
+
   /** \brief Empty getPointIndices implementation as this leaf node does not store any
    * data. \
    */
-  void
+  virtual void
   getPointIndices(Indices&) const
   {}
 };
@@ -141,13 +149,13 @@ public:
   /** \brief Empty addPointIndex implementation. This leaf node does not store any point
    * indices.
    */
-  void addPointIndex(index_t) {}
+  void addPointIndex(index_t) override {}
 
   /** \brief Empty getPointIndex implementation as this leaf node does not store any
    * point indices.
    */
   index_t
-  getPointIndex() const
+  getPointIndex() const override
   {
     assert("getPointIndex: undefined point index");
     return -1;
@@ -157,7 +165,7 @@ public:
    * data.
    */
   void
-  getPointIndices(Indices&) const
+  getPointIndices(Indices&) const override
   {}
 };
 
@@ -195,7 +203,7 @@ public:
    * \param[in] data_arg index to be stored within leaf node.
    */
   void
-  addPointIndex(index_t data_arg)
+  addPointIndex(index_t data_arg) override
   {
     data_ = data_arg;
   }
@@ -205,7 +213,7 @@ public:
    * \return index stored within container.
    */
   index_t
-  getPointIndex() const
+  getPointIndex() const override
   {
     return data_;
   }
@@ -216,7 +224,7 @@ public:
    * data vector
    */
   void
-  getPointIndices(Indices& data_vector_arg) const
+  getPointIndices(Indices& data_vector_arg) const override
   {
     if (data_ != static_cast<index_t>(-1))
       data_vector_arg.push_back(data_);
@@ -274,7 +282,7 @@ public:
    * \param[in] data_arg index to be stored within leaf node.
    */
   void
-  addPointIndex(index_t data_arg)
+  addPointIndex(index_t data_arg) override
   {
     leafDataTVector_.push_back(data_arg);
   }
@@ -284,7 +292,7 @@ public:
    * \return index stored within container.
    */
   index_t
-  getPointIndex() const
+  getPointIndex() const override
   {
     return leafDataTVector_.back();
   }
@@ -295,7 +303,7 @@ public:
    * within data vector
    */
   void
-  getPointIndices(Indices& data_vector_arg) const
+  getPointIndices(Indices& data_vector_arg) const override
   {
     data_vector_arg.insert(
         data_vector_arg.end(), leafDataTVector_.begin(), leafDataTVector_.end());
