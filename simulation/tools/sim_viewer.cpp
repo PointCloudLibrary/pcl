a46eb24e3b4548fc9ace3da44ca5da902db57063
Norm Evangelista <norm.evangelista@gmail.com>
2022-08-23 09:47:04 -0700

    Fixed clang-tidy and clang-format escapes

--- simulation/tools/sim_viewer.cpp
+++ simulation/tools/sim_viewer.cpp
@@ -96,7 +96,7 @@ using GeometryHandlerConstPtr = GeometryHandler::ConstPtr;
 #define NORMALS_SCALE 0.01
 #define PC_SCALE 0.001
 
-std::uint16_t t_gamma[2048];
+std::uint16_t t_gamma[2048]{};
 Scene::Ptr scene_;
 Camera::Ptr camera_;
 RangeLikelihood::Ptr range_likelihood_;
@@ -494,8 +494,8 @@ main(int argc, char** argv)
 
     int y_s = 0;
     if (!p_file_indices.empty()) {
-      y_s =
-          static_cast<int>(std::floor(std::sqrt(static_cast<float>(p_file_indices.size()))));
+      y_s = static_cast<int>(
+          std::floor(std::sqrt(static_cast<float>(p_file_indices.size()))));
       x_s = y_s + static_cast<int>(std::ceil(
                       (p_file_indices.size() / static_cast<double>(y_s)) - y_s));
       print_highlight("Preparing to load ");
