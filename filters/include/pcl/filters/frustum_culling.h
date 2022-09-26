4dad5b3cd4aced85792df398d410dd78861e12b0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2022-09-26 20:06:48 +0800

    [filters] FrustumCulling: asymmetrical Field of View (#5438)
    * [feat] asymmetrical Field of View
    Ref: https://github.com/PointCloudLibrary/pcl/issues/5422
    
    * add back fov getter and add warning for them
    
    * [doc] add doc for setHorizontalFOV and setVerticalFOV
    
    * fix typo
    
    * [doc] clarify the description of extracted objects

--- filters/include/pcl/filters/frustum_culling.h
+++ filters/include/pcl/filters/frustum_culling.h
@@ -90,8 +90,10 @@ namespace pcl
       FrustumCulling (bool extract_removed_indices = false) 
         : FilterIndices<PointT> (extract_removed_indices)
         , camera_pose_ (Eigen::Matrix4f::Identity ())
-        , hfov_ (60.0f)
-        , vfov_ (60.0f)
+        , fov_left_bound_ (-30.0f)
+        , fov_right_bound_ (30.0f)
+        , fov_lower_bound_ (-30.0f)
+        , fov_upper_bound_ (30.0f)
         , np_dist_ (0.1f)
         , fp_dist_ (5.0f)
         , roi_x_ (0.5f)
@@ -135,6 +137,7 @@ namespace pcl
 
       /** \brief Set the horizontal field of view for the camera in degrees
         * \param[in] hfov the field of view
+        * Note: setHorizontalFOV(60.0) is equivalent to setHorizontalFOV(-30.0, 30.0).
         */
       void 
       setHorizontalFOV (float hfov)
@@ -144,18 +147,57 @@ namespace pcl
           throw PCLException ("Horizontal field of view should be between 0 and 180(excluded).",
             "frustum_culling.h", "setHorizontalFOV");
         }
-        hfov_ = hfov;
+        fov_left_bound_ = -hfov / 2;
+        fov_right_bound_ = hfov / 2;
+      }
+
+      /** \brief Set the horizontal field of view for the camera in degrees
+        * \param[in] fov_left_bound the left bound of horizontal field of view
+        * \param[in] fov_right_bound the right bound of horizontal field of view
+        * Note: Bounds can be either positive or negative values.
+        * Negative value means the camera would look to its left,
+        * and positive value means the camera would look to its right.
+        * In general cases, fov_left_bound should be set to a negative value,
+        * if it is set to a positive value, the camera would only look to its right.
+        * Also note that setHorizontalFOV(-30.0, 30.0) is equivalent to setHorizontalFOV(60.0).
+        */
+      void
+      setHorizontalFOV (float fov_left_bound, float fov_right_bound)
+      {
+        if (fov_left_bound <= -90 || fov_right_bound >= 90 || fov_left_bound >= fov_right_bound)
+        {
+          throw PCLException ("Horizontal field of view bounds should be between -90 and 90(excluded). "
+              "And left bound should be smaller than right bound.",
+            "frustum_culling.h", "setHorizontalFOV");
+        }
+        fov_left_bound_ = fov_left_bound;
+        fov_right_bound_ = fov_right_bound;
       }
 
       /** \brief Get the horizontal field of view for the camera in degrees */
-      float 
-      getHorizontalFOV () const
+      float
+      getHorizontalFOV() const
       {
-        return (hfov_);
+        if (std::fabs(fov_right_bound_) != std::fabs(fov_left_bound_)) {
+          PCL_WARN("Your horizontal field of view is asymmetrical: "
+            "left bound's absolute value(%f) != right bound's absolute value(%f)! "
+            "Please use getHorizontalFOV (float& fov_left_bound, float& fov_right_bound) instead.\n",
+            std::fabs(fov_left_bound_), std::fabs(fov_right_bound_));
+        }
+        return (fov_right_bound_ - fov_left_bound_);
+      }
+
+      /** \brief Get the horizontal field of view for the camera in degrees */
+      void
+      getHorizontalFOV (float& fov_left_bound, float& fov_right_bound) const
+      {
+        fov_left_bound = fov_left_bound_;
+        fov_right_bound = fov_right_bound_;
       }
 
       /** \brief Set the vertical field of view for the camera in degrees
         * \param[in] vfov the field of view
+        * Note: setVerticalFOV(60.0) is equivalent to setVerticalFOV(-30.0, 30.0).
         */
       void 
       setVerticalFOV (float vfov)
@@ -165,14 +207,52 @@ namespace pcl
           throw PCLException ("Vertical field of view should be between 0 and 180(excluded).",
             "frustum_culling.h", "setVerticalFOV");
         }
-        vfov_ = vfov;
+        fov_lower_bound_ = -vfov / 2;
+        fov_upper_bound_ = vfov / 2;
+      }
+
+      /** \brief Set the vertical field of view for the camera in degrees
+        * \param[in] fov_lower_bound the lower bound of vertical field of view
+        * \param[in] fov_upper_bound the upper bound of vertical field of view
+        * Note: Bounds can be either positive or negative values.
+        * Negative value means the camera would look down,
+        * and positive value means the camera would look up.
+        * In general cases, fov_lower_bound should be set to a negative value,
+        * if it is set to a positive value, the camera would only look up.
+        * Also note that setVerticalFOV(-30.0, 30.0) is equivalent to setVerticalFOV(60.0).
+        */
+      void
+      setVerticalFOV (float fov_lower_bound, float fov_upper_bound)
+      {
+        if (fov_lower_bound <= -90 || fov_upper_bound >= 90 || fov_lower_bound >= fov_upper_bound)
+        {
+          throw PCLException ("Vertical field of view bounds should be between -90 and 90(excluded). "
+              "And lower bound should be smaller than upper bound.",
+            "frustum_culling.h", "setVerticalFOV");
+        }
+        fov_lower_bound_ = fov_lower_bound;
+        fov_upper_bound_ = fov_upper_bound;
       }
 
       /** \brief Get the vertical field of view for the camera in degrees */
-      float 
-      getVerticalFOV () const
+      float
+      getVerticalFOV() const
       {
-        return (vfov_);
+        if (std::fabs(fov_upper_bound_) != std::fabs(fov_lower_bound_)) {
+          PCL_WARN("Your vertical field of view is asymmetrical: "
+            "lower bound's absolute value(%f) != upper bound's absolute value(%f)! "
+            "Please use getVerticalFOV (float& fov_lower_bound, float& fov_upper_bound) instead.\n",
+            std::fabs(fov_lower_bound_), std::fabs(fov_upper_bound_));
+        }
+        return (fov_upper_bound_ - fov_lower_bound_);
+      }
+
+      /** \brief Get the vertical field of view for the camera in degrees */
+      void
+      getVerticalFOV (float& fov_lower_bound, float& fov_upper_bound) const
+      {
+        fov_lower_bound = fov_lower_bound_;
+        fov_upper_bound = fov_upper_bound_;
       }
 
       /** \brief Set the near plane distance
@@ -282,10 +362,14 @@ namespace pcl
 
       /** \brief The camera pose */
       Eigen::Matrix4f camera_pose_;
-      /** \brief Horizontal field of view */
-      float hfov_;
-      /** \brief Vertical field of view */
-      float vfov_;
+      /** \brief The left bound of horizontal field of view */
+      float fov_left_bound_;
+      /** \brief The right bound of horizontal field of view */
+      float fov_right_bound_;
+      /** \brief The lower bound of vertical field of view */
+      float fov_lower_bound_;
+      /** \brief The upper bound of vertical field of view */
+      float fov_upper_bound_;
       /** \brief Near plane distance */
       float np_dist_;
       /** \brief Far plane distance */
