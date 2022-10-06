a3e804b1b7bb43681aa3799219547f3ad50e97ca
Markus Vieth <39675748+mvieth@users.noreply.github.com>
2022-10-06 18:20:35 +0200

    Fixes (doc and other stuff)

--- filters/include/pcl/filters/farthest_point_sampling.h
+++ filters/include/pcl/filters/farthest_point_sampling.h
@@ -1,36 +1,10 @@
 /*
- * Software License Agreement (BSD License)
+ * SPDX-License-Identifier: BSD-3-Clause
  *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
  *  Copyright (c) 2020-, Open Perception, Inc.
  *
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the copyright holder(s) nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
+ *  All rights reserved
  */
 
 #pragma once
@@ -72,7 +46,7 @@ namespace pcl
         }
 
         /** \brief Set number of points to be sampled.
-          * \param sample
+          * \param sample_size the number of points to sample
           */
         inline void
         setSample (std::size_t sample_size)
@@ -89,7 +63,7 @@ namespace pcl
         }
 
         /** \brief Set seed of random function.
-          * \param seed
+          * \param seed for the random number generator, to choose the first sample point
           */
         inline void
         setSeed (unsigned int  seed)
@@ -116,7 +90,7 @@ namespace pcl
           * \param indices indices of the filtered point cloud
           */
         void
-        applyFilter (std::vector<int> &indices) override;
+        applyFilter (pcl::Indices &indices) override;
 
     };
  }
