b2a3214c2d7f422c5d3944b7838cf37da6bf42fb
Markus Vieth <39675748+mvieth@users.noreply.github.com>
2022-10-17 09:38:32 +0200

    New VTK version check

--- apps/src/manual_registration/manual_registration.cpp
+++ apps/src/manual_registration/manual_registration.cpp
@@ -45,7 +45,8 @@
 #include <QEvent>
 #include <QMutexLocker>
 #include <QObject>
-#if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(8, 2, 0)
+#include <vtkVersion.h>
+#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION == 8 && VTK_MINOR_VERSION >= 2)
 #define HAS_QVTKOPENGLWINDOW_H
 #include <QVTKOpenGLWindow.h>
 #endif
