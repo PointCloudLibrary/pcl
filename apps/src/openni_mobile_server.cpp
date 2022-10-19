818a3aae8f4202723d1e0de80057267446d511a7
filbert14 <filbertfebrianto14@gmail.com>
2022-10-20 00:33:41 +0200

    #5475 Fixed 8 more files in apps/src.

--- apps/src/openni_mobile_server.cpp
+++ apps/src/openni_mobile_server.cpp
@@ -221,7 +221,7 @@ public:
 void
 usage(char** argv)
 {
-  std::cout << "usage: " << argv[0] << " <options>\n"
+  std::cout << "usage: " << argv[0] << " <device_id> <options>\n"
             << "where options are:\n"
             << "  -port p :: set the server port (default: 11111)\n"
             << "  -leaf x, y, z  :: set the voxel grid leaf size (default: 0.01)\n";
@@ -230,19 +230,23 @@ usage(char** argv)
 int
 main(int argc, char** argv)
 {
-  if (pcl::console::find_argument(argc, argv, "-h") != -1) {
+  std::string arg;
+  if (argc > 1)
+    arg = std::string(argv[1]);
+
+  if (arg == "--help" || arg == "-h") {
     usage(argv);
-    return 0;
+    return 1;
   }
 
   int port = 11111;
   float leaf_x = 0.01f, leaf_y = 0.01f, leaf_z = 0.01f;
-  std::string device_id;
+  std::string device_id = arg;
 
   pcl::console::parse_argument(argc, argv, "-port", port);
   pcl::console::parse_3x_arguments(argc, argv, "-leaf", leaf_x, leaf_y, leaf_z, false);
 
-  pcl::OpenNIGrabber grabber("");
+  pcl::OpenNIGrabber grabber(device_id);
   if (!grabber.providesCallback<pcl::OpenNIGrabber::sig_cb_openni_point_cloud_rgba>()) {
     std::cout << "OpenNI grabber does not provide the rgba cloud format." << std::endl;
     return 1;
