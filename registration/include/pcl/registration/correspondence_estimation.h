5eba45555e8ed1e9860a8ce4a2db44710506196f
Markus Vieth <39675748+mvieth@users.noreply.github.com>
2024-01-06 19:50:25 +0100

    Fix typo

--- registration/include/pcl/registration/correspondence_estimation.h
+++ registration/include/pcl/registration/correspondence_estimation.h
@@ -147,7 +147,7 @@ public:
 #ifdef _OPENMP
     num_threads_ = nr_threads != 0 ? nr_threads : omp_get_num_procs();
 #else
-    if (num_threads != 1) {
+    if (nr_threads != 1) {
       PCL_WARN("OpenMP is not available. Keeping number of threads unchanged at 1\n");
     }
     num_threads_ = 1;
