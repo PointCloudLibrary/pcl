01e875f348b15c54be3fdcdf238d5892fc1f4550
Lars Glud <larshg@gmail.com>
2024-01-14 07:27:33 +0100

    Add library as INTERFACE type if no sources.

--- cmake/pcl_targets.cmake
+++ cmake/pcl_targets.cmake
@@ -192,7 +192,12 @@ function(PCL_ADD_LIBRARY _name)
     message(FATAL_ERROR "PCL_ADD_LIBRARY requires parameter COMPONENT.")
   endif()
 
-  add_library(${_name} ${PCL_LIB_TYPE} ${ARGS_SOURCES})
+  if(NOT ARGS_SOURCES)
+    add_library(${_name} INTERFACE)
+  else()
+    add_library(${_name} ${PCL_LIB_TYPE} ${ARGS_SOURCES})
+  endif()
+  
   PCL_ADD_VERSION_INFO(${_name})
   target_compile_features(${_name} PUBLIC ${PCL_CXX_COMPILE_FEATURES})
 
