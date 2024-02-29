38c74a92622af144059b1009a018ef2e716b411a
Norm Evangelista <norm.evangelista@gmail.com>
2024-02-28 17:26:57 -0800

    Fixed another clang-tidy complaint

--- tools/openni2_viewer.cpp
+++ tools/openni2_viewer.cpp
@@ -58,7 +58,7 @@
   ++count; \
   if (now - last >= 1.0) \
 { \
-  std::cout << "Average framerate ("<< _WHAT_ << "): " << double (count)/double (now - last) << " Hz" <<  std::endl; \
+  std::cout << "Average framerate ("<< (_WHAT_) << "): " << double (count)/double (now - last) << " Hz" <<  std::endl; \
   count = 0; \
   last = now; \
 } \
