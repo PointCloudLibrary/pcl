cb6d4c6849e1d6e6811c22380086224ab3d8ac88
Lars Glud <larshg@gmail.com>
2024-10-01 16:10:00 +0200

    Remove checks for cmake less than 3.16 as that is minimum required.

--- cmake/Modules/FindSphinx.cmake
+++ cmake/Modules/FindSphinx.cmake
@@ -8,16 +8,9 @@
 find_package(PkgConfig QUIET)
 pkg_check_modules(PC_SPHINX sphinx-build)
 
-if(CMAKE_VERSION VERSION_LESS 3.12.0)
-  find_package(PythonInterp)
-  if(PYTHONINTERP_FOUND)
-    get_filename_component(PYTHON_DIR "${PYTHON_EXECUTABLE}" PATH)
-  endif()
-else()
-  find_package(Python)
-  if(Python_Interpreter_FOUND)
-    get_filename_component(PYTHON_DIR "${Python_EXECUTABLE}" PATH)
-  endif()
+find_package(Python)
+if(Python_Interpreter_FOUND)
+  get_filename_component(PYTHON_DIR "${Python_EXECUTABLE}" PATH)
 endif()
 
 find_program(SPHINX_EXECUTABLE NAMES sphinx-build
