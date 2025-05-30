f484baa8c18ac40f1096860f96bd6fbfff4b7e96
jmackay2 <jmackay2@gmail.com>
2025-05-29 23:39:36 -0400

    clang format

--- registration/include/pcl/registration/impl/ndt.hpp
+++ registration/include/pcl/registration/impl/ndt.hpp
@@ -121,17 +121,15 @@ NormalDistributionsTransform<PointSource, PointTarget, Scalar>::computeTransform
     // 2009]
     Eigen::JacobiSVD<Eigen::Matrix<double, 6, 6>> sv(
         hessian, Eigen::ComputeFullU | Eigen::ComputeFullV);
-    #if EIGEN_VERSION_AT_LEAST(3, 4, 0)
+#if EIGEN_VERSION_AT_LEAST(3, 4, 0)
     if (sv.info() != Eigen::ComputationInfo::Success) {
-	trans_likelihood_ = score / static_cast<double>(input_->size());
-	converged_ = 0;
-	return;
+      trans_likelihood_ = score / static_cast<double>(input_->size());
+      converged_ = 0;
+      return;
     }
-    #endif
+#endif
     // Negative for maximization as opposed to minimization
     Eigen::Matrix<double, 6, 1> delta = sv.solve(-score_gradient);
-
-
 
     // Calculate step length with guaranteed sufficient decrease [More, Thuente 1994]
     double delta_norm = delta.norm();
