9e02fc23492baa6879f8c0720e1e710c999b5f0d
Lars Glud <larshg@gmail.com>
2025-06-19 08:03:54 +0200

    Rename setThreads to setNumberOfThreads.

--- surface/include/pcl/surface/poisson.h
+++ surface/include/pcl/surface/poisson.h
@@ -219,8 +219,26 @@ namespace pcl
       /** \brief Set the number of threads to use.
        * \param[in] threads the number of threads
        */
+      PCL_DEPRECATED(1,18, "Use setNumberOfThreads() instead.")
       void
       setThreads(unsigned int num_threads = 0);
+
+      /** \brief Initialize the scheduler and set the number of threads to use.
+      * \param num_threads the number of hardware threads to use (0 sets the value back
+      * to automatic)
+      */
+      inline void
+      setNumberOfThreads(unsigned int num_threads = 0)
+      {
+#ifdef _OPENMP
+        num_threads_ = num_threads != 0 ? num_threads : omp_get_num_procs();
+#else
+        if (num_threads_ != 1) {
+          PCL_WARN(
+              "OpenMP is not available. Keeping number of threads unchanged at 1\n");
+        }
+#endif
+      }
       
 
       /** \brief Get the number of threads*/
