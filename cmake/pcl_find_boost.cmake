PointCloudLibrary/pcl

commit 0404f7c382099e50196da6640c0ebba6d829392f

Link Boost::filesystem to pcl_outofcore (#6325)
* Link Boost::filesystem to pcl_outofcore
outofcore uses Boost::filesystem and needs to link to it. If visualization is not built, building outofcore fails due to missing symbols, likely as a consequence of https://github.com/PointCloudLibrary/pcl/pull/6254 (before, outofcore was not built if visualization was not built either)

* Use Boost::system target

outofcore/CMakeLists.txt
@@ -76,10 +76,10 @@ set(LIB_NAME "pcl_${SUBSYS_NAME}")
 
 if(BUILD_visualization)
   PCL_ADD_LIBRARY(${LIB_NAME} COMPONENT ${SUBSYS_NAME} SOURCES ${srcs} ${incs} ${impl_incs} ${visualization_incs})
-  target_link_libraries("${LIB_NAME}" pcl_common pcl_visualization ${Boost_SYSTEM_LIBRARY})
+  target_link_libraries("${LIB_NAME}" pcl_common pcl_visualization Boost::system Boost::filesystem)
 else()
   PCL_ADD_LIBRARY(${LIB_NAME} COMPONENT ${SUBSYS_NAME} SOURCES ${srcs} ${incs} ${impl_incs})
-  target_link_libraries("${LIB_NAME}" pcl_common ${Boost_SYSTEM_LIBRARY})
+  target_link_libraries("${LIB_NAME}" pcl_common Boost::system Boost::filesystem)
 endif()
 if(HAVE_CJSON)
   target_link_libraries("${LIB_NAME}" ${CJSON_LIBRARIES})

commit 8dfb0e10ebdf4a5086328b38f854294d2d6b1627

Fix build with Boost 1.89.0
This fixes a regression in 0404f7c382099e50196da6640c0ebba6d829392f
/ 1.15.1 as `Boost::system` is not valid since Boost 1.89.0.

PCL requires a minimum of Boost 1.71.0 so Boost.System is guaranteed to
be header-only (since 1.69[^1]) and the component can be removed.

[^1]: https://www.boost.org/doc/libs/1_69_0/libs/system/doc/html/system.html#changes_in_boost_1_69

cmake/pcl_find_boost.cmake
@@ -15,12 +15,12 @@ endif()
 
 if(CMAKE_CXX_STANDARD MATCHES "14")
   # Optional boost modules
-  set(BOOST_OPTIONAL_MODULES serialization mpi system)
+  set(BOOST_OPTIONAL_MODULES serialization mpi)
   # Required boost modules
   set(BOOST_REQUIRED_MODULES filesystem iostreams)
 else()
   # Optional boost modules
-  set(BOOST_OPTIONAL_MODULES filesystem serialization mpi system)
+  set(BOOST_OPTIONAL_MODULES filesystem serialization mpi)
   # Required boost modules
   set(BOOST_REQUIRED_MODULES iostreams)
 endif()

outofcore/CMakeLists.txt
@@ -76,10 +76,10 @@ set(LIB_NAME "pcl_${SUBSYS_NAME}")
 
 if(BUILD_visualization)
   PCL_ADD_LIBRARY(${LIB_NAME} COMPONENT ${SUBSYS_NAME} SOURCES ${srcs} ${incs} ${impl_incs} ${visualization_incs})
-  target_link_libraries("${LIB_NAME}" pcl_common pcl_visualization Boost::system Boost::filesystem)
+  target_link_libraries("${LIB_NAME}" pcl_common pcl_visualization Boost::filesystem)
 else()
   PCL_ADD_LIBRARY(${LIB_NAME} COMPONENT ${SUBSYS_NAME} SOURCES ${srcs} ${incs} ${impl_incs})
-  target_link_libraries("${LIB_NAME}" pcl_common Boost::system Boost::filesystem)
+  target_link_libraries("${LIB_NAME}" pcl_common Boost::filesystem)
 endif()
 if(HAVE_CJSON)
   target_link_libraries("${LIB_NAME}" ${CJSON_LIBRARIES})