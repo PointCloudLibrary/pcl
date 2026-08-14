4dd5652c59d8c5fb522156a85382fdf76639592c
Yuxin Yao <47818173+yaoyx689@users.noreply.github.com>
2026-08-14 16:43:23 +0800

    Add a part of the new rigid registration method [FRICP]  (#6199)
    * add a part of the FRICP (robust transformation estimation)
    
    * update
    
    * Correct format
    
    * add Anderson acceleration fricp.hpp etc.UPdate cmakelist
    
    * fix bug
    
    * fix bug
    
    * fix bug in test
    
    * fix the bug of parameter
    
    * Update test
    
    * repush
    
    * fix bug in clang-tidy,merge the new update in pcl
    
    * fix bug in clang-tidy,merge the new update in pcl
    
    * fix bug in clang-tidy again
    
    * fix bug in clang-tidy again
    
    * fix bug in clang-tidy again
    
    * fix bug in clang-tidy again
    
    * Address all  review comments in PR 6199
    
    * Fix FRICP KdTree/Search mismatch in test_registration_api
    
    * Update registration/include/pcl/registration/fricp.h
    
    Co-authored-by: Markus Vieth <39675748+mvieth@users.noreply.github.com>
    
    * Apply PR6199 review fixes without build artifacts
    
    * Update registration/include/pcl/registration/fricp.h
    
    Co-authored-by: Markus Vieth <39675748+mvieth@users.noreply.github.com>
    
    * Address review comments: use #pragma once, pcl::index_t, squared residuals, Eigen::Index
    
    * Fix clang-tidy: use range-based for loop in findKNearestMedian
    
    * Use pcl::uindex_t for source indices per review feedback
    
    * Address reviewer feedback: add Ptr/ConstPtr, NaN asserts, and class documentation
    
    - Add Ptr/ConstPtr type aliases to FRICP class header
    - Add assert for finite source/target points (no NaN/Inf)
    - Document that FRICP ignores maxCorrespondenceDistance,
      correspondence estimators/rejectors, and that
      maxIterations controls inner-loop iterations only
    - Fix setDynamicWelschDecay() doc to note 0 maps to 0.5
    - Remove redundant ctor init-list values (robust_function_,
      nu_end_ratio_ already have in-class defaults)
    
    * Apply clang-format to fricp.h
    
    * Fix file encoding: convert fricp.h from UTF-16 LE to UTF-8
    
    * Fix: add in-class default for robust_function_ after removing from init-list
    
    Previously removed robust_function_ from constructor init-list along with
    nu_end_ratio_, but robust_function_ had no in-class initializer, leading
    to default-initialization of an enum class (indeterminate value). Add
    explicit in-class default to WELSCH to preserve correct behavior.
    
    * Integrate DefaultConvergenceCriteria into FRICP
    
    - Add ConvergenceTrigger enum to track why FRICP stopped
    - Add getLastConvergenceTrigger() for diagnostic queries
    - Integrate DefaultConvergenceCriteria as an additional
      convergence check alongside the existing transform delta
    - Populate pcl::Correspondences in updateCorrespondences
      so getFitnessScore() works correctly
    - Set transformation_ delta each iteration for criteria checks
    - Track convergence state on all error/early-return paths
    
    Based on larshg's proposal in PR discussion.
    
    * Use DefaultConvergenceCriteria per larshg review
    
    - Remove ConvergenceTrigger enum and getLastConvergenceTrigger()
      (larshg's test code, not needed for production)
    - Replace FRICP's own matrix delta check with DefaultConvergenceCriteria
    - Move delta_transform calculation after Anderson acceleration step
    - Use CONVERGENCE_CRITERIA_NO_CORRESPONDENCES on error paths
    - Populate correspondences_ so getFitnessScore() works
    
    ---------
    
    Co-authored-by: Guanghao Li <1909631936@qq.com>
    Co-authored-by: Markus Vieth <39675748+mvieth@users.noreply.github.com>
    Co-authored-by: yaoyx689 <yaoyx689@users.noreply.github.com>

---
+++ registration/include/pcl/registration/transformation_estimation_point_to_point_robust.h
@@ -0,0 +1,163 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2011, Willow Garage, Inc.
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *
+ *  All rights reserved.
+ */
+
+#pragma once
+
+#include <pcl/registration/transformation_estimation.h>
+#include <pcl/cloud_iterator.h>
+#include <pcl/pcl_config.h> // for PCL_NO_PRECOMPILE
+
+namespace pcl {
+namespace registration {
+/** @b TransformationEstimationPointToPointRobust implements SVD-based estimation of
+ * the transformation aligning the given correspondences for minimizing the Welsch
+ * function instead of L2-norm, For additional details, see "Fast and Robust Iterative
+ * Closest Point", Juyong Zhang, Yuxin Yao, Bailin Deng, 2021.
+ * \note The class is templated on the source and target point types as well as on the
+ * output scalar of the transformation matrix (i.e., float or double). Default: float.
+ * \author Yuxin Yao
+ * \ingroup registration
+ */
+template <typename PointSource, typename PointTarget, typename Scalar = float>
+class TransformationEstimationPointToPointRobust
+: public TransformationEstimation<PointSource, PointTarget, Scalar> {
+public:
+  using Ptr = shared_ptr<
+      TransformationEstimationPointToPointRobust<PointSource, PointTarget, Scalar>>;
+  using ConstPtr =
+      shared_ptr<const TransformationEstimationPointToPointRobust<PointSource,
+                                                                  PointTarget,
+                                                                  Scalar>>;
+
+  using Matrix4 =
+      typename TransformationEstimation<PointSource, PointTarget, Scalar>::Matrix4;
+
+  /** \brief Constructor */
+  TransformationEstimationPointToPointRobust() = default;
+
+  ~TransformationEstimationPointToPointRobust() override = default;
+
+  /** \brief Estimate a rigid rotation transformation between a source and a target
+   * point cloud using SVD. \param[in] cloud_src the source point cloud dataset
+   * \param[in] cloud_tgt the target point cloud dataset
+   * \param[out] transformation_matrix the resultant transformation matrix
+   */
+  inline void
+  estimateRigidTransformation(const pcl::PointCloud<PointSource>& cloud_src,
+                              const pcl::PointCloud<PointTarget>& cloud_tgt,
+                              Matrix4& transformation_matrix) const override;
+
+  /** \brief Estimate a rigid rotation transformation between a source and a target
+   * point cloud using SVD. \param[in] cloud_src the source point cloud dataset
+   * \param[in] indices_src the vector of indices describing the points of interest in
+   * \a cloud_src
+   * \param[in] cloud_tgt the target point cloud dataset
+   * \param[out] transformation_matrix the resultant transformation matrix
+   */
+  inline void
+  estimateRigidTransformation(const pcl::PointCloud<PointSource>& cloud_src,
+                              const pcl::Indices& indices_src,
+                              const pcl::PointCloud<PointTarget>& cloud_tgt,
+                              Matrix4& transformation_matrix) const override;
+
+  /** \brief Estimate a rigid rotation transformation between a source and a target
+   * point cloud using SVD. \param[in] cloud_src the source point cloud dataset
+   * \param[in] indices_src the vector of indices describing the points of interest in
+   * \a cloud_src
+   * \param[in] cloud_tgt the target point cloud dataset
+   * \param[in] indices_tgt the vector of indices describing the correspondences of the
+   * interest points from \a indices_src
+   * \param[out] transformation_matrix the resultant transformation matrix
+   */
+  inline void
+  estimateRigidTransformation(const pcl::PointCloud<PointSource>& cloud_src,
+                              const pcl::Indices& indices_src,
+                              const pcl::PointCloud<PointTarget>& cloud_tgt,
+                              const pcl::Indices& indices_tgt,
+                              Matrix4& transformation_matrix) const override;
+
+  /** \brief Estimate a rigid rotation transformation between a source and a target
+   * point cloud using SVD. \param[in] cloud_src the source point cloud dataset
+   * \param[in] cloud_tgt the target point cloud dataset
+   * \param[in] correspondences the vector of correspondences between source and target
+   * point cloud \param[out] transformation_matrix the resultant transformation matrix
+   */
+  void
+  estimateRigidTransformation(const pcl::PointCloud<PointSource>& cloud_src,
+                              const pcl::PointCloud<PointTarget>& cloud_tgt,
+                              const pcl::Correspondences& correspondences,
+                              Matrix4& transformation_matrix) const override;
+
+  /** \brief Set the Welsch scale parameter used for robust reweighting.
+   *
+   * If set to a positive value, this exact sigma is used. If set to a negative
+   * value (default), sigma is estimated automatically from current correspondences.
+   * Smaller positive values increase outlier suppression, larger values make the
+   * estimator closer to standard least-squares behavior.
+   */
+  void
+  setSigma(Scalar sigma)
+  {
+    sigma_ = sigma;
+  };
+
+protected:
+  /** \brief Estimate a rigid rotation transformation between a source and a target
+   * \param[in] source_it an iterator over the source point cloud dataset
+   * \param[in] target_it an iterator over the target point cloud dataset
+   * \param[out] transformation_matrix the resultant transformation matrix
+   */
+  void
+  estimateRigidTransformation(ConstCloudIterator<PointSource>& source_it,
+                              ConstCloudIterator<PointTarget>& target_it,
+                              Matrix4& transformation_matrix) const;
+
+  /** \brief Obtain a 4x4 rigid transformation matrix from a correlation matrix H = src
+   * * tgt' \param[in] cloud_src_demean the input source cloud, demeaned, in Eigen
+   * format \param[in] centroid_src the input source centroid, in Eigen format
+   * \param[in] cloud_tgt_demean the input target cloud, demeaned, in Eigen format
+   * \param[in] centroid_tgt the input target cloud, in Eigen format
+   * \param[out] transformation_matrix the resultant 4x4 rigid transformation matrix
+   */
+  virtual void
+  getTransformationFromCorrelation(
+      const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& cloud_src_demean,
+      const Eigen::Matrix<Scalar, 4, 1>& centroid_src,
+      const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& cloud_tgt_demean,
+      const Eigen::Matrix<Scalar, 4, 1>& centroid_tgt,
+      const Eigen::Matrix<Scalar, Eigen::Dynamic, 1>& weights,
+      Matrix4& transformation_matrix) const;
+
+  /** \brief Compute the 3D (X-Y-Z) centroid of a set of weighted points and return it
+   * as a 3D vector.
+   * \param[in] cloud_iterator an iterator over the input point cloud
+   * \param[in] weights the weights corresponding to points in the input point cloud
+   * \param[out] centroid the output centroid
+   * \return number of valid points used to determine the centroid. In case of dense
+   * point clouds, this is the same as the size of input cloud.
+   * \note if return value is 0, the centroid is not changed, thus not valid.
+   * The last component of the vector is set to 1, this allows to transform the centroid
+   * vector with 4x4 matrices.
+   * \ingroup common
+   */
+  template <typename PointT>
+  inline unsigned int
+  computeWeighted3DCentroid(ConstCloudIterator<PointT>& cloud_iterator,
+                            const Eigen::Matrix<Scalar, Eigen::Dynamic, 1>& weights,
+                            Eigen::Matrix<Scalar, 4, 1>& centroid) const;
+
+  /** parameter for the Welsch function */
+  Scalar sigma_ = -1;
+};
+
+} // namespace registration
+} // namespace pcl
+
+#include <pcl/registration/impl/transformation_estimation_point_to_point_robust.hpp>
